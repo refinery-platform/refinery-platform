9bf74c9311c9f1e4bf1b06829496d161dcdd47e3
Psalm Haseley <psm3426@gmail.com>
2012-01-31 13:39:44 -0500

    isa_tab views; has an index that lists all the investigations and a detail view that shows all parsed information associated with the investigation

--- refinery/isa_tab/views.py
+++ refinery/isa_tab/views.py
@@ -1,1 +1,22 @@
 # Create your views here.
+from django.views.generic import View
+from django.shortcuts import render_to_response, get_object_or_404, get_list_or_404
+from django.http import Http404
+from isa_tab.models import *
+
+#displays Investigation accession, title, and description
+def index(request):
+    investigation_list = Investigation.objects.all()
+    return render_to_response('isa_tab/index.html', 
+                              {'investigation_list': investigation_list})
+
+#displays all parsed ISA-Tab information
+def detail(request, accession):
+    i = get_object_or_404(Investigation, pk=accession)
+    a = get_list_or_404(Assay, investigation=i)
+
+    return render_to_response('isa_tab/detail.html', 
+                              {
+                               'investigation': i,
+                               'assay': a
+                               })
