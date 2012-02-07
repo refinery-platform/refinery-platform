ff13e9b86bb25cf213b6b26e5ef46bd1391dfa3f
Psalm Haseley <psm3426@gmail.com>
2012-02-07 17:24:21 -0500

    When debugging removed the generic views I was planning to use...but as long as it works, it's ok =)

--- refinery/isa_tab/urls.py
+++ refinery/isa_tab/urls.py
@@ -1,7 +1,9 @@
-#isa_tab pages
+#isatab pages
 from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('isa_tab.views',
     url(r'^$', 'index'),
-    url(r'^(?P<accession>E-[A-Z]+-\d+)/$', 'detail')
+    url(r'^(?P<accession>E-[A-Z]+-\d+)/$', 'detail'),
+    url(r'^(?P<accession>E-[A-Z]+-\d+)/results/$', 'results'),
+    url(r'^(?P<accession>E-[A-Z]+-\d+)/download/$', 'download')
 )
