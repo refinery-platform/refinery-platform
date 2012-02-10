8ad43745ffa9e539f1a0cdcacad0b4d772042afe
Psalm Haseley <psm3426@gmail.com>
2012-02-10 16:50:47 -0500

    edited download_http_file because I forgot to read from the downloading stream

--- refinery/isa_tab/tasks.py
+++ refinery/isa_tab/tasks.py
@@ -61,4 +61,22 @@ def download_http_file(url, out_dir):
     if(not os.path.exists(file_path)):
         u = urllib2.urlopen(url)
         f = open(file_path, 'wb')
-    f.close()
+        meta = u.info()
+        file_size = int(meta.getheaders("Content-Length")[0])
+        print "Downloading: %s Bytes: %s" % (file_name, file_size)
+        file_size_dl = 0
+        block_sz = 8192
+        while True:
+            buffer = u.read(block_sz)
+#        buffer = u.read()
+            if not buffer:
+                break
+
+            file_size_dl += len(buffer)
+            f.write(buffer)
+            status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
+            status = status + chr(8)*(len(status)+1)
+            print status,
+
+        f.close()
+    
