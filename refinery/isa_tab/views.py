87eea81fd8c15280a8315b56b584220b75863c48
Psalm Haseley <psm3426@gmail.com>
2012-02-10 16:49:23 -0500

    put in code so it paginates; currently shows 5 investigations per page

--- refinery/isa_tab/views.py
+++ refinery/isa_tab/views.py
@@ -6,16 +6,28 @@ from django.template import RequestContext
 from django.core.urlresolvers import reverse
 from isa_tab.tasks import call_download
 import simplejson
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 def index(request):
     investigation_list = Investigation.objects.all()
+    paginator = Paginator(investigation_list, 5) # Show 5 investigations per page
+
+    page = request.GET.get('page', 1)
+    try:
+        investigations = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        investigations = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        investigations = paginator.page(paginator.num_pages)
     return render_to_response('isa_tab/index.html', 
-                              {'investigation_list': investigation_list})
+                              {'investigations': investigations})
 
 def detail(request, accession):
     i = get_object_or_404(Investigation, pk=accession)
-    return render_to_response('isa_tab/detail.html', {'investigation': i},
+    return render_to_response('isa_tab/detail.html', {'investigation_list': i},
                               context_instance=RequestContext(request))
     
 def results(request, accession):
