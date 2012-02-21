f757cfb8c43d6d5e66e7c7bd50cd1abbeef749c4
Nils Gehlenborg <nils@gehlenborg.com>
2012-02-21 12:59:16 -0500

    Added application with core features for Refinery such as User, Project, DataSet, Workflow, Analysis.

---
+++ refinery/core/views.py
@@ -0,0 +1,33 @@
+from core.models import User
+from core.models import Project
+from core.models import Workflow
+from core.models import DataSet
+from core.models import ProjectUserRelationship
+from django.template import RequestContext
+from django.shortcuts import render_to_response, get_object_or_404
+
+def index(request):
+    users = User.objects.all()
+    projects = Project.objects.all()
+    workflows = Workflow.objects.all()
+    data_sets = DataSet.objects.all()
+
+    return render_to_response('core/index.html', {'users': users, 'projects': projects, 'workflows': workflows, 'data_sets': data_sets }, context_instance=RequestContext( request ) )
+
+def user(request,query):
+    
+    try:
+        user = User.objects.get( name=query )
+    except User.DoesNotExist:
+        user = get_object_or_404( User, uuid=query )
+            
+    project_rels = ProjectUserRelationship.objects.filter( user=user )
+    print project_rels
+    
+    return render_to_response('core/user.html', {'user': user, 'project_rels': project_rels }, context_instance=RequestContext( request ) )
+
+def project(request,uuid):
+    project = get_object_or_404( Project, uuid=uuid )
+    user_rels = ProjectUserRelationship.objects.filter( resource=project )
+    
+    return render_to_response('core/project.html', { 'project': project, 'project_rels': user_rels }, context_instance=RequestContext( request ) )
