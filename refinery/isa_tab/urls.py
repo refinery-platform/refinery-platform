f3e14ee37fb603ed93f82deb633a55c97c3e5281
rpark <richardpark@rpark-pro2.partners.org>
2012-02-29 14:23:45 -0500

    Removing isa_tab old application names

--- refinery/isa_tab/urls.py
+++
@@ -1,9 +0,0 @@
-#isatab pages
-from django.conf.urls.defaults import patterns, include, url
-
-urlpatterns = patterns('isa_tab.views',
-    url(r'^$', 'index'),
-    url(r'^(?P<accession>E-[A-Z]+-\d+)/$', 'detail'),
-    url(r'^(?P<accession>E-[A-Z]+-\d+)/results/$', 'results'),
-    url(r'^(?P<accession>E-[A-Z]+-\d+)/download/$', 'download')
-)
