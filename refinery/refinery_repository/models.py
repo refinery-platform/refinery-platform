d5d5b87f6c10e428f4aeded660e537b71e75c4c8
Psalm Haseley <psm3426@gmail.com>
2012-03-01 16:52:49 -0500

    changed the CharFields to TextFields and added in a new field into Assay

--- refinery/refinery_repository/models.py
+++ refinery/refinery_repository/models.py
@@ -6,10 +6,10 @@ class Ontology(models.Model):
     def __unicode__(self):
         return self.term_source_name
     
-    term_source_name = models.CharField(max_length=50)
-    term_source_version = models.CharField(max_length=10, blank=True, null=True)
-    term_source_file = models.CharField(max_length=1024, primary_key=True)
-    term_source_description = models.CharField(max_length=2048, blank=True, null=True)
+    term_source_name = models.TextField()
+    term_source_version = models.TextField(blank=True, null=True)
+    term_source_file = models.TextField(primary_key=True)
+    term_source_description = models.TextField(blank=True, null=True)
     
     class Meta:
         #even though pk is an auto-incremented number, ensures every row has a
@@ -20,18 +20,18 @@ class StudyDesignDescriptor(models.Model):
     def __unicode__(self):
         return self.study_design_type
     
-    study_design_type = models.CharField(max_length=255, primary_key=True)
-    study_design_type_term_accession_number = models.CharField(max_length=255, blank=True, null=True)
-    study_design_type_term_source_ref = models.CharField(max_length=1024, blank=True, null=True)
+    study_design_type = models.TextField(primary_key=True)
+    study_design_type_term_accession_number = models.TextField(blank=True, null=True)
+    study_design_type_term_source_ref = models.TextField(blank=True, null=True)
 
 class StudyFactor(models.Model):
     def __unicode__(self):
         return self.study_factor_name
     
-    study_factor_name = models.CharField(max_length=50)
-    study_factor_type = models.CharField(max_length=50)
-    study_factor_type_term_accession_number = models.CharField(max_length=255, blank=True, null=True)
-    study_factor_type_term_source_ref = models.CharField(max_length=1024, blank=True, null=True)
+    study_factor_name = models.TextField()
+    study_factor_type = models.TextField()
+    study_factor_type_term_accession_number = models.TextField(blank=True, null=True)
+    study_factor_type_term_source_ref = models.TextField(blank=True, null=True)
     
     class Meta:
         unique_together = ('study_factor_name', 'study_factor_type')
@@ -40,40 +40,40 @@ class Protocol(models.Model):
     def __unicode__(self):
         return self.study_protocol_name
     
-    study_protocol_name = models.CharField(max_length=100, primary_key=True)
-    study_protocol_type = models.CharField(max_length=100, blank=True, null=True)
-    study_protocol_type_term_accession_number = models.CharField(max_length=255, blank=True, null=True)
-    study_protocol_type_term_source_ref = models.CharField(max_length=1024, blank=True, null=True)
-    study_protocol_description = models.CharField(max_length=2048, blank=True, null=True)
-    study_protocol_uri = models.CharField(max_length=1024, blank=True, null=True)
-    study_protocol_version = models.CharField(max_length=50, blank=True, null=True)
-    study_protocol_parameters_name = models.CharField(max_length=50, blank=True, null=True)
-    study_protocol_parameters_name_term_accession_number = models.CharField(max_length=255, blank=True, null=True)
-    study_protocol_parameters_name_term_source_ref = models.CharField(max_length=1024, blank=True, null=True)
-    study_protocol_components_name = models.CharField(max_length=50, blank=True, null=True)
-    study_protocol_components_type = models.CharField(max_length=50, blank=True, null=True)
-    study_protocol_components_type_term_accession_number = models.CharField(max_length=255, blank=True, null=True)
-    study_protocol_components_type_term_source_ref = models.CharField(max_length=1024, blank=True, null=True)
+    study_protocol_name = models.TextField(primary_key=True)
+    study_protocol_type = models.TextField(blank=True, null=True)
+    study_protocol_type_term_accession_number = models.TextField(blank=True, null=True)
+    study_protocol_type_term_source_ref = models.TextField(blank=True, null=True)
+    study_protocol_description = models.TextField(2048, blank=True, null=True)
+    study_protocol_uri = models.TextField(blank=True, null=True)
+    study_protocol_version = models.TextField(blank=True, null=True)
+    study_protocol_parameters_name = models.TextField(blank=True, null=True)
+    study_protocol_parameters_name_term_accession_number = models.TextField(blank=True, null=True)
+    study_protocol_parameters_name_term_source_ref = models.TextField(blank=True, null=True)
+    study_protocol_components_name = models.TextField(blank=True, null=True)
+    study_protocol_components_type = models.TextField(blank=True, null=True)
+    study_protocol_components_type_term_accession_number = models.TextField(blank=True, null=True)
+    study_protocol_components_type_term_source_ref = models.TextField(blank=True, null=True)
 
 class Investigation(models.Model):
     def __unicode__(self):
         return self.study_identifier
 
-    study_identifier = models.CharField(max_length=20, primary_key=True)
-    study_title = models.CharField(max_length=255)
-    study_description = models.CharField(max_length=2048)
+    study_identifier = models.TextField(primary_key=True)
+    study_title = models.TextField()
+    study_description = models.TextField()
     study_public_release_date = models.DateField(blank=True, null=True)
     #study_submission_date = models.DateField(blank=True, null=True)
-    study_file_name = models.CharField(max_length=50)
+    study_file_name = models.TextField()
     #assay attributes
-    study_assay_measurement_type = models.CharField(max_length=255, blank=True, null=True)
-    study_assay_measurement_type_term_accession_number = models.CharField(max_length=255, blank=True, null=True)
-    study_assay_measurement_type_term_source_ref = models.CharField(max_length=1024, blank=True, null=True)
-    study_assay_technology_type = models.CharField(max_length=255, blank=True, null=True)
-    study_assay_technology_type_term_accession_number = models.CharField(max_length=255, blank=True, null=True)
-    study_assay_technology_type_term_source_ref = models.CharField(max_length=255, blank=True, null=True)
-    study_assay_technology_platform = models.CharField(max_length=255, blank=True, null=True)
-    study_assay_file_name = models.CharField(max_length=50)
+    study_assay_measurement_type = models.TextField(blank=True, null=True)
+    study_assay_measurement_type_term_accession_number = models.TextField(blank=True, null=True)
+    study_assay_measurement_type_term_source_ref = models.TextField(blank=True, null=True)
+    study_assay_technology_type = models.TextField(blank=True, null=True)
+    study_assay_technology_type_term_accession_number = models.TextField(blank=True, null=True)
+    study_assay_technology_type_term_source_ref = models.TextField(blank=True, null=True)
+    study_assay_technology_platform = models.TextField(blank=True, null=True)
+    study_assay_file_name = models.TextField()
     
     #0, 1, or more ontologies can be used for many different investigations
     ontologies = models.ManyToManyField(Ontology, blank=True, null=True)
@@ -85,15 +85,15 @@ class Investigation(models.Model):
     
 class Publication(models.Model):
     def __unicode__(self):
-        return self.study_pubmed_id
+        return str(self.study_pubmed_id)
     
     study_pubmed_id = models.IntegerField(primary_key=True)
-    study_publication_doi = models.CharField(max_length=50, blank=True, null=True)
-    study_publication_author_list = models.CharField(max_length=1024, blank=True, null=True)
-    study_publication_title = models.CharField(max_length=255, blank=True, null=True)
-    study_publication_status = models.CharField(max_length=50, blank=True, null=True)
-    study_publication_status_term_accession_number = models.CharField(max_length=255, blank=True, null=True)
-    study_publication_status_term_source_ref = models.CharField(max_length=1024, blank=True, null=True)
+    study_publication_doi = models.TextField(blank=True, null=True)
+    study_publication_author_list = models.TextField(blank=True, null=True)
+    study_publication_title = models.TextField(blank=True, null=True)
+    study_publication_status = models.TextField(blank=True, null=True)
+    study_publication_status_term_accession_number = models.TextField(blank=True, null=True)
+    study_publication_status_term_source_ref = models.TextField(blank=True, null=True)
 
     investigation = models.ForeignKey(Investigation)
 
@@ -104,17 +104,17 @@ class Investigator(models.Model):
                               self.study_person_mid_initials)
         return name
 
-    study_person_email = models.EmailField(max_length=255, primary_key=True)
-    study_person_last_name = models.CharField(max_length=100)
-    study_person_first_name = models.CharField(max_length=100)
-    study_person_mid_initials = models.CharField(max_length=100, blank=True, null=True)
-    study_person_phone = models.CharField(max_length=50)
-    study_person_fax = models.CharField(max_length=50, blank=True, null=True)
-    study_person_address = models.CharField(max_length=1024)
-    study_person_affiliation = models.CharField(max_length=255)
-    study_person_roles = models.CharField(max_length=255)
-    study_person_roles_term_accession_number = models.CharField(max_length=255, blank=True, null=True)
-    study_person_roles_term_source_ref = models.CharField(max_length=1024, blank=True, null=True)
+    study_person_email = models.EmailField(max_length=1024, primary_key=True)
+    study_person_last_name = models.TextField()
+    study_person_first_name = models.TextField()
+    study_person_mid_initials = models.TextField(blank=True, null=True)
+    study_person_phone = models.TextField(blank=True, null=True)
+    study_person_fax = models.TextField(blank=True, null=True)
+    study_person_address = models.TextField()
+    study_person_affiliation = models.TextField()
+    study_person_roles = models.TextField()
+    study_person_roles_term_accession_number = models.TextField(blank=True, null=True)
+    study_person_roles_term_source_ref = models.TextField(blank=True, null=True)
     
     investigations = models.ManyToManyField(Investigation)
     
@@ -124,12 +124,12 @@ class SubType(models.Model):
     def __unicode__(self):
         return self.type
 
-    type = models.CharField(max_length=255, primary_key=True)
+    type = models.TextField(primary_key=True)
 
 class Comment(models.Model):
     def __unicode__(self):
         return "%s: %s" % (self.type.type, self.value)
-    value = models.CharField(max_length=1024)
+    value = models.TextField()
     type = models.ForeignKey(SubType)
     
     #comments can belong to both studies and assays, but only one at a time
@@ -142,10 +142,10 @@ class Comment(models.Model):
 class Study(models.Model):
     def __unicode__(self):
         return self.source_name
-    source_name = models.CharField(max_length=255, primary_key=True)
-    sample_name = models.CharField(max_length=255)
-    material_type = models.CharField(max_length=255, blank=True, null=True)
-    provider = models.CharField(max_length=255, blank=True, null=True) 
+    source_name = models.TextField(primary_key=True)
+    sample_name = models.TextField()
+    material_type = models.TextField(blank=True, null=True)
+    provider = models.TextField(blank=True, null=True) 
     
     #usually more than one protocol per study
     protocols = models.ManyToManyField(Protocol)
@@ -155,9 +155,9 @@ class Characteristic(models.Model):
     def __unicode__(self):
         return "%s: %s" % (self.type.type, self.value)
 
-    value = models.CharField(max_length=1024)
-    term_source_ref = models.CharField(max_length=20, blank=True, null=True)
-    term_accession_number = models.CharField(max_length=1024, blank=True, null=True)
+    value = models.TextField()
+    term_source_ref = models.TextField(blank=True, null=True)
+    term_accession_number = models.TextField(blank=True, null=True)
     
     type = models.ForeignKey(SubType)
     study = models.ForeignKey(Study)
@@ -168,32 +168,33 @@ class RawData(models.Model):
     def __unicode__(self):
         return self.raw_data_file
 
-    raw_data_file = models.CharField(max_length=2048)
-    data_transformation_name = models.CharField(max_length=1024)
+    raw_data_file = models.TextField()
+    data_transformation_name = models.TextField()
 
 class ProcessedData(models.Model):
     def __unicode__(self):
         return self.derived_arrayexpress_ftp_file
 
-    derived_arrayexpress_ftp_file = models.CharField(max_length=2048)
-    derived_data_file = models.CharField(max_length=1024)
+    derived_arrayexpress_ftp_file = models.TextField()
+    derived_data_file = models.TextField()
     
 class Assay(models.Model):
     def __unicode__(self):
         return self.sample_name
 
-    sample_name = models.CharField(max_length=255)
-    material_type = models.CharField(max_length=255, blank=True, null=True)
-    assay_name = models.CharField(max_length=255, blank=True, null=True)
-    extract_name = models.CharField(max_length=255, blank=True, null=True)
-    performer = models.CharField(max_length=255, blank=True, null=True)
-    technology_type = models.CharField(max_length=255, blank=True, null=True)
-    scan_name = models.CharField(max_length=255, blank=True, null=True)
-    labeled_extract_name = models.CharField(max_length=255, blank=True, null=True)
-    label = models.CharField(max_length=255, blank=True, null=True)
-    hybridization_assay_name = models.CharField(max_length=255, blank=True, null=True)
-    array_design_ref = models.CharField(max_length=255, blank=True, null=True)
-    protocol_ref = models.CharField(max_length=255, blank=True, null=True)
+    sample_name = models.TextField()
+    material_type = models.TextField(blank=True, null=True)
+    assay_name = models.TextField(blank=True, null=True)
+    extract_name = models.TextField(blank=True, null=True)
+    performer = models.TextField(blank=True, null=True)
+    technology_type = models.TextField(blank=True, null=True)
+    scan_name = models.TextField(blank=True, null=True)
+    labeled_extract_name = models.TextField(blank=True, null=True)
+    label = models.TextField(blank=True, null=True)
+    hybridization_assay_name = models.TextField(blank=True, null=True)
+    array_design_ref = models.TextField(blank=True, null=True)
+    protocol_ref = models.TextField(blank=True, null=True)
+    normalization_name = models.TextField(blank=True, null=True)
     
     #one raw/processed data file may be associated with multiple assays
     raw_data = models.ManyToManyField(RawData, null=True, blank=True)
@@ -208,9 +209,9 @@ class FactorValue(models.Model):
     def __unicode__(self):
         return "%s: %s" % (self.type.type, self.value)
 
-    value = models.CharField(max_length=255)
-    term_source_ref = models.CharField(max_length=50, blank=True, null=True)
-    term_accession_number = models.CharField(max_length=1024, blank=True, null=True)
+    value = models.TextField()
+    term_source_ref = models.TextField(blank=True, null=True)
+    term_accession_number = models.TextField(blank=True, null=True)
     
     type = models.ForeignKey(SubType)
     assay = models.ForeignKey(Assay)
@@ -223,7 +224,7 @@ class HaveSubtype(models.Model):
     def __unicode__(self):
         return "%s: %s" % (self.type, self.value)
     
-    value = models.CharField(max_length=255)
+    value = models.TextField()
     type = models.ForeignKey(SubType)
-    super_type = models.CharField(max_length=255)
+    super_type = models.TextField()
     assay = models.ForeignKey(Assay)
