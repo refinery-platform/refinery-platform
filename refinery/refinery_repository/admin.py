883161acf890c7f579b0f3a773de733f6c82d99e
Psalm Haseley <psm3426@gmail.com>
2012-03-15 10:19:27 -0400

    Got rid of the SubType Model, so needed to change admin.py to reflect that

--- refinery/refinery_repository/admin.py
+++ refinery/refinery_repository/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from refinery_repository.models import Ontology, StudyDesignDescriptor, StudyFactor, Protocol, Investigation, Publication, Investigator, SubType, Study, StudyBracketedField, RawData, ProcessedData, Assay, AssayBracketedField
+from refinery_repository.models import Ontology, StudyDesignDescriptor, StudyFactor, Protocol, Investigation, Publication, Investigator, Study, StudyBracketedField, RawData, ProcessedData, Assay, AssayBracketedField
 
+
+# The following classes define the admin interface for your models.
+# See http://docs.djangoproject.com/en/dev/ref/contrib/admin/ for
+# a full list of the options you can use in these classes.
 
 class OntologyAdmin(admin.ModelAdmin):
     pass
@@ -30,10 +34,6 @@ class InvestigatorAdmin(admin.ModelAdmin):
     pass
 
 
-class SubTypeAdmin(admin.ModelAdmin):
-    pass
-
-
 class StudyAdmin(admin.ModelAdmin):
     pass
 
@@ -58,7 +58,9 @@ class AssayBracketedFieldAdmin(admin.ModelAdmin):
     pass
 
 
-
+# Each of these lines registers the admin interface for one model. If
+# you don't want the admin interface for a particular model, remove
+# the line which registers it.
 admin.site.register(Ontology, OntologyAdmin)
 admin.site.register(StudyDesignDescriptor, StudyDesignDescriptorAdmin)
 admin.site.register(StudyFactor, StudyFactorAdmin)
@@ -66,7 +68,6 @@ admin.site.register(Protocol, ProtocolAdmin)
 admin.site.register(Investigation, InvestigationAdmin)
 admin.site.register(Publication, PublicationAdmin)
 admin.site.register(Investigator, InvestigatorAdmin)
-admin.site.register(SubType, SubTypeAdmin)
 admin.site.register(Study, StudyAdmin)
 admin.site.register(StudyBracketedField, StudyBracketedFieldAdmin)
 admin.site.register(RawData, RawDataAdmin)
