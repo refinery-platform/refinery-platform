782e5fe064243ef772dce1adc005cc767947fac2
Psalm Haseley <psm3426@gmail.com>
2012-03-17 00:51:54 -0400

    moved around imports to decrease the amount of loading time required; added tasks to automatically check ArrayExpress for new/updated ChIP-seq studies, convert them to ISA-Tab, and parse the new studies into Django.  I haven't been able to get the email notifications to work yet.

--- refinery/refinery_repository/tasks.py
+++ refinery/refinery_repository/tasks.py
@@ -1,11 +1,17 @@
-from celery.task import task, Task
-from celery import current_app, events
-import sys, os, string, errno, subprocess
-from StringIO import StringIO
+from celery.task import task, Task, periodic_task
+from celery.schedules import crontab
+#from celery import current_app, events
+import os, errno
 from django.conf import settings
-from refinery_repository.models import Investigation
-        
 
+        
+"""
+Name: create_dir
+Description:
+    creates a directory if it needs to be created
+Parameters:
+    file_path: directory to create if necessary
+"""
 def create_dir(file_path):
     try:
         os.makedirs(file_path)
@@ -15,15 +21,15 @@ def create_dir(file_path):
 
 @task()
 def download_ftp_file(ftp, out_dir, accession):
-    import ftplib, socket
-    
+    import ftplib, socket, os.path, string, sys
+
     file_name = ftp.split('/')[-1] #get the file name
-    out_dir = "%s/%s" % (out_dir, accession) #directory where file downloads
+    out_dir = os.path.join(out_dir, accession) #directory where file downloads
     
     #make super-directory (out_dir/accession) if it doesn't exist
     create_dir(out_dir)
 
-    file_path = "%s/%s" % (out_dir, file_name) # path where file downloads
+    file_path = os.path.join(out_dir, file_name) # path where file downloads
     if(not os.path.exists(file_path)): #if file exists already don't download
         ftp = ftp[6:] #remove the ftp:// part from the front
     
@@ -85,19 +91,15 @@ def download_ftp_file(ftp, out_dir, accession):
     
 @task()
 def download_http_file(url, out_dir, accession):
-    import urllib2
+    import urllib2, os.path
     
     file_name = url.split('/')[-1] #get the file name
-    out_dir = "%s/%s" % (out_dir, accession) #directory where file downloads
-    print 'out_dir:',
-    print out_dir
+    out_dir = os.path.join(out_dir, accession) #directory where file downloads
     
     #make super-directory (out_dir/accession) if it doesn't exist
     create_dir(out_dir)
     
-    file_path = "%s/%s" % (out_dir, file_name) # path where file downloads
-    print 'file_path:',
-    print file_path
+    file_path = os.path.join(out_dir, file_name) # path where file downloads
 
     if(not os.path.exists(file_path)):
         u = urllib2.urlopen(url)
@@ -131,6 +133,8 @@ def download_http_file(url, out_dir, accession):
                 
 @task()
 def call_download(file_url):
+    from refinery_repository.models import Investigation
+    
     """args = (file_url)
     orig_stdout = sys.stdout
     sys.stdout = content = StringIO()
@@ -171,30 +175,142 @@ def call_download(file_url):
 
 @task()
 def convert_to_isatab(accession):
-    command = "%s %s" % (settings.CONVERSION, accession)
-    subprocess.call(command) 
+    import subprocess, shutil, tempfile, os.path
     
-@task()
+    retval = 1 #successful conversion
+    command = "./convert.sh %s" % accession
+    
+    #send stdout and stderr to a unique temp directory to avoid console
+    temp_dir = tempfile.mkdtemp()
+    stderr_n = tempfile.NamedTemporaryFile(dir=temp_dir, prefix='ae_stderr').name
+    stdout_n = tempfile.NamedTemporaryFile(dir=temp_dir, prefix='ae_stdout').name
+
+    #create the subprocess
+    process = subprocess.Popen(args=command, shell=True, 
+                               cwd=settings.CONVERSION_DIR, 
+                               stderr=open(stderr_n, 'wb'),
+                               stdout=open(stdout_n, 'wb'))
+    #run the subprocess and grab the exit code
+    exit_code = process.wait()
+    #process stderr
+    stderr = open(stderr_n).read().strip()
+    if stderr:
+        #unsuccessful conversion, so remove the investigation file and dir
+        shutil.rmtree(os.path.join(settings.ISA_TAB_DIR, accession))
+        
+        if exit_code != 0: #something bad happened
+            shutil.rmtree(temp_dir)
+            raise Exception, "Error Converting to ISA-Tab: %s" % stderr
+        else:
+            #print stderr
+            retval = 0 #unsuccessful conversion, but clean exit
+
+    #clean up the temporary directory and other files
+    os.unlink(stderr_n)
+    os.unlink(stdout_n)
+    os.rmdir(temp_dir)
+
+    return retval
+   
+@periodic_task(run_every=crontab(minute="*/15", day_of_week="thursday"))
 def get_arrayexpress_studies():
-    wget_cmd = "wget -O arrayexpress_studies -P %s %s" % (settings.WGET_DIR,
-                                                          settings.WGET_URL)
-    subprocess.call(wget_cmd)
+    import urllib2, time, re, os.path, string, sys
+    from celery.task.sets import TaskSet
+    from django.core.management import call_command
+    from StringIO import StringIO
+    from datetime import date, datetime, timedelta
+
+    create_dir(settings.WGET_DIR) #make the directory if it's not there
+
+    print "getting %s" % settings.WGET_URL
+    u = urllib2.urlopen(settings.WGET_URL)
+    ae_file = os.path.join(settings.WGET_DIR, 'arrayexpress_studies')
+    
+    print "writing to file %s" % ae_file
+    f = open(ae_file, 'w')
+    #download in pieces to make sure you're never biting off too much
+    block_sz = 8192
+    while True:
+        buffer = u.read(block_sz) #read block_sz bytes from url
+        if not buffer:
+            break
+
+        f.write(buffer) #write what you read from url to file
+
+    f.close()
+    
+    last_date_run = datetime.strptime(settings.LAST_AE_UPDATE, '%Y-%m-%d').date()
     
     ae_accessions = list()
-    f = open("%s/arrayexpress_studies" % settings.WGET_DIR, 'rb')
+    f = open(ae_file, 'r')
     for line in f:
-        #get the date that the study was updated between "lastupdatedate" tags
-        updated = string.split(line, 'lastupdatedate>').pop(1)
-        updated = updated[:-2] #take off the </ connected to the date
-        accessions = string.split(line, 'accession>')
-        for a in accessions: #many accessions, so search for right one
-            if re.search(r'^E-', a):
-                isatab_dir = os.path.join(settings.ISA_TAB_DIR, ae_accession)
-                #will only convert new studies
-                if not os.path.isdir(isatab_dir):
-                    ae_accessions.append(a)
-                else: #if updated recently, then convert also
-                    updated
+        try:
+            #get date that study was updated; between "lastupdatedate" tags
+            updated = string.split(line, 'lastupdatedate>').pop(1)
+            updated = updated[:-2] #take off the </ connected to the date
+            accessions = string.split(line, 'accession>')
+            for a in accessions: #many accessions, so search for right one
+                if re.search(r'^E-', a):
+                    a = a[:-2] #take off the </ connected to the accession
+                    isatab_dir = os.path.join(settings.ISA_TAB_DIR, a)
+                    #will only convert new studies
+                    if not os.path.isdir(isatab_dir): #hasn't been done before
+                        ae_accessions.append(a)
+                    else: #if updated recently, then convert also
+                        #convert string to datetime.date object for comparison
+                        update = datetime.strptime(updated, '%Y-%m-%d').date()
+                        #if the study has been updated since the last time we
+                        #did this, update the ISA-Tab
+                        if (update - last_date_run) > timedelta(days=-1):
+                            ae_accessions.append(a)
+        except IndexError: #looking at line without interesting information
+            pass                    
+    f.close()
 
-    for ae_accession in ae_accessions:        
-            convert_to_isatab.delay(ae_accession)
+    s_tasks = list()
+    for ae_accession in ae_accessions:
+        #print ae_accession        
+        #task = convert_to_isatab.delay(ae_accession)
+        s_task = convert_to_isatab.subtask(args=(ae_accession,))
+        s_tasks.append(s_task)
+        
+    job = TaskSet(tasks=s_tasks)
+    result = job.apply_async()
+    
+    #go to sleep for 3 seconds at a time until all tasks are finished
+    while result.waiting():
+        print 'sleeping'
+        time.sleep(3)
+        
+    results = result.join() #list of the results in dispatch order
+    
+    success_list = list()
+    for a, r in zip(ae_accessions, results):
+        if(r):
+            success_list.append(a)
+        
+    #print success_list
+
+    #grab the stderr instead of having it go to console
+    orig_stderr = sys.stderr
+    sys.stderr = content = StringIO()
+    call_command('parser', *success_list, stderr=content)
+    sys.stderr = orig_stderr
+    content.seek(0)
+    
+    #if there was a problem, have it email you
+    stderr = content.read().strip()
+    if stderr:
+        print stderr
+        """
+        msg = MIMEText(stderr)
+        msg['Subject'] = 'Refinery Parser Error'
+        msg['From'] = settings.FROM_EMAIL
+        msg['To'] = settings.TO_EMAIL
+            
+        s = smtplib.SMTP('localhost')
+        s.sendmail(settings.FROM_EMAIL, [settings.TO_EMAIL], msg.as_string())
+        s.quit()
+        """
+            
+    #settings.LAST_AE_UPDATE = str(date.today())
