b3d84807d67680ae1650c433efbff7a20c89b19e
Nils Gehlenborg <nils@gehlenborg.com>
2012-04-06 16:48:29 -0400

    Added WorkflowEngine resource.

--- refinery/core/admin.py
+++ refinery/core/admin.py
@@ -9,6 +9,7 @@ from core.models import UserProfile
 from core.models import Project
 from core.models import DataSet
 from core.models import Workflow
+from core.models import WorkflowEngine
 from core.models import WorkflowDataInput
 from core.models import WorkflowDataInputMap
 from core.models import Analysis
@@ -22,6 +23,9 @@ class ProjectAdmin(GuardedModelAdmin):
 class WorkflowAdmin(GuardedModelAdmin):
     pass
 
+class WorkflowEngineAdmin(GuardedModelAdmin):
+    pass
+
 class DataSetAdmin(GuardedModelAdmin):
     pass
 
@@ -29,6 +33,7 @@ admin.site.register(UserProfile)
 admin.site.register(Project,ProjectAdmin)
 admin.site.register(DataSet,DataSetAdmin)
 admin.site.register(Workflow,WorkflowAdmin)
+admin.site.register(WorkflowEngine,WorkflowEngineAdmin)
 admin.site.register(WorkflowDataInput)
 admin.site.register(WorkflowDataInputMap)
 admin.site.register(Analysis)
