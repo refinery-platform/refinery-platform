b7c9553ae0d210cd8d4b68bdc38926e0bea78ee4
Nils Gehlenborg <nils@gehlenborg.com>
2012-04-06 19:16:01 -0400

    Added support for WorkflowEngines. Workflows are now imported through the WorkflowManager and are associated with a WorkflowEngine.

--- refinery/core/views.py
+++ refinery/core/views.py
@@ -2,7 +2,6 @@ from core.models import *
 from django.contrib.auth.models import User 
 from django.template import RequestContext
 from django.shortcuts import render_to_response, get_object_or_404
-from core.tasks import grab_workflows
 from django.http import HttpResponse, HttpResponseForbidden
 from django.contrib.auth.decorators import login_required
 from guardian.shortcuts import get_objects_for_user, get_perms, get_users_with_perms
@@ -17,10 +16,10 @@ def index(request):
         users = []
 
     projects = get_objects_for_user( request.user, "core.read_project" )
-    workflows = get_objects_for_user( request.user, "core.read_workflow" )
+    workflow_engines = get_objects_for_user( request.user, "core.read_workflow_engine" )
     data_sets = get_objects_for_user( request.user, "core.read_data_set" )
             
-    return render_to_response('core/index.html', {'users': users, 'projects': projects, 'workflows': workflows, 'data_sets': data_sets }, context_instance=RequestContext( request ) )
+    return render_to_response('core/index.html', {'users': users, 'projects': projects, 'workflow_engines': workflow_engines, 'data_sets': data_sets }, context_instance=RequestContext( request ) )
 
 @login_required()
 def user(request,query):
@@ -71,11 +70,13 @@ def workflow(request,uuid):
     return render_to_response('core/workflow.html', { 'workflow': workflow, "permissions": permissions }, context_instance=RequestContext( request ) )
 
 
-@login_required()
-def import_workflows(request):
+def workflow_engine(request,uuid):
     
-    current_workflow_count = Workflow.objects.count()    
-    grab_workflows();
-    new_workflow_count = Workflow.objects.count()
+    workflow_engine = get_object_or_404( WorkflowEngine, uuid=uuid )
     
-    return HttpResponse( str( ( new_workflow_count - current_workflow_count ) ) + ' workflows imported.' ) 
+    if not request.user.has_perm('core.read_workflow_engine', workflow_engine ):
+        return HttpResponseForbidden("<h1>User " + request.user.username + " is not allowed to view this workflow engine.</h1>" )
+        
+    permissions = get_users_with_perms( workflow_engine, attach_perms=True )
+    
+    return render_to_response('core/workflow_engine.html', { 'workflow_engine': workflow_engine, "permissions": permissions }, context_instance=RequestContext( request ) )
--- refinery/workflow_manager/views.py
+++ refinery/workflow_manager/views.py
@@ -1,1 +1,46 @@
-# Create your views here.
+'''
+Created on April 6, 2012
+
+@author: nils
+'''
+
+from django.http import HttpResponse
+from galaxy_connector.connection import Connection
+from core.models import Workflow
+from core.models import WorkflowEngine
+from workflow_manager.tasks import get_workflows
+from django.contrib.auth.decorators import login_required
+import simplejson
+
+
+@login_required()
+def import_workflows(request):
+    
+    workflow_engines = WorkflowEngine.objects.all()
+    
+    for engine in workflow_engines:
+        current_workflow_count = engine.workflow_set.all().count()    
+        get_workflows( engine );
+        new_workflow_count = engine.workflow_set.all().count()
+        print "Engine: " + engine.name + " - " + str( ( current_workflow_count ) ) + ' workflows before.'
+        print "Engine: " + engine.name + " - " + str( ( new_workflow_count ) ) + ' workflows after.'
+    
+    return HttpResponse( str( ( new_workflow_count - current_workflow_count ) ) + ' workflows imported.' ) 
+
+
+def download_workflow ( request, workflow_uuid ):
+    """
+    Returns a specified workflow_id as a dictionary object.
+    Requires simplejson
+    """
+    
+    workflow = Workflow.objects.filter( uuid=workflow_uuid ).get()
+    
+    connection = Connection( workflow.workflow_engine.instance.base_url,
+                             workflow.workflow_engine.instance.data_url,
+                             workflow.workflow_engine.instance.api_url,
+                             workflow.workflow_engine.instance.api_key )
+
+    result = connection.get_workflow_dict( workflow.internal_id );
+
+    return HttpResponse( simplejson.dumps(result) ) 
