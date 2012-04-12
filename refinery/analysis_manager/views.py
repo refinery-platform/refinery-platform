3ea263f4b69e603fdc23131dca20832b4a6d4c13
Nils Gehlenborg <nils@gehlenborg.com>
2012-04-12 15:09:42 -0400

    Added basic view to inspect AnalysisStatus objects.

--- refinery/analysis_manager/views.py
+++ refinery/analysis_manager/views.py
@@ -1,1 +1,12 @@
 # Create your views here.
+
+from analysis_manager.models import AnalysisStatus
+from django.template import RequestContext
+from django.shortcuts import render_to_response, get_object_or_404
+from django.http import HttpResponse, HttpResponseForbidden
+
+
+def index(request):
+    statuses = AnalysisStatus.objects.all()
+    
+    return render_to_response( 'analysis_manager/index.html', { 'statuses': statuses }, context_instance=RequestContext( request ) )
