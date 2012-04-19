88275b3168c41df93d03176b199ea85f35e8587f
rpark <richardpark@rpark-pro2.local>
2012-04-19 02:59:05 -0400

    Updated analysis_manager models

--- refinery/analysis_manager/models.py
+++ refinery/analysis_manager/models.py
@@ -20,15 +20,18 @@ xrange(10)).apply_async()
 class AnalysisStatus( models.Model ):
     analysis_uuid = UUIDField( unique=True, auto=False )
     
-    preprocessing_taskset_id = UUIDField( blank=True, null=True )
-    execution_taskset_id = UUIDField( blank=True, null=True )
-    postprocessing_taskset_id = UUIDField( blank=True, null=True )
-    execution_monitor_task_id = UUIDField( blank=True, null=True )
+    preprocessing_taskset_id = UUIDField( blank=True, null=True, auto=False )
+    execution_taskset_id = UUIDField( blank=True, null=True, auto=False )
+    postprocessing_taskset_id = UUIDField( blank=True, null=True, auto=False )
+    cleanup_taskset_id = UUIDField( blank=True, null=True, auto=False )
+    execution_monitor_task_id = UUIDField( blank=True, null=True, auto=False )
     
     def current(self):        
         preprocessing_result = False
         execution_result = False
         postprocessing_result = False
+        cleanup_result = False
+        execution_monitor_result = False
         
         if not self.preprocessing_taskset_id is None:
             preprocessing_taskset = TaskSetResult.restore( self.preprocessing_taskset_id )
@@ -48,8 +51,18 @@ class AnalysisStatus( models.Model ):
             if postprocessing_taskset is not None:
                 postprocessing_result = postprocessing_taskset.waiting()
         
+        if not self.cleanup_taskset_id is None:            
+            cleanup_taskset = TaskSetResult.restore( self.cleanup_taskset_id )
+            if cleanup_taskset is not None:
+                cleanup_result = cleanup_taskset.waiting()
+        
+        if not self.execution_monitor_task_id is None:            
+            execution_monitor_taskset = TaskSetResult.restore( self.execution_monitor_task_id )
+            if execution_monitor_taskset is not None:
+                execution_monitor_result = execution_monitor_taskset.waiting()
+           
         #return { "preprocessing": preprocessing_result, "preprocessing_tasks": preprocessing_subtasks, "execution": execution_result, "postprocessing": postprocessing_result } 
-        return { "preprocessing": preprocessing_result, "execution": execution_result, "postprocessing": postprocessing_result } 
+        return { "preprocessing": preprocessing_result, "execution": execution_result, "postprocessing": postprocessing_result, "execution_monitor":execution_monitor_result, "cleanup":cleanup_result } 
 
 
 
