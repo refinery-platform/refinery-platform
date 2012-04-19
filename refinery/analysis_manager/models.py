46650e66e45d58e7b69dfc6db88a1c64dc1eb343
rpark <richardpark@rpark-pro2.local>
2012-04-19 03:52:03 -0400

    Getting results through asyncresult instead of tasksetresult in celery for analysis monitoring

--- refinery/analysis_manager/models.py
+++ refinery/analysis_manager/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django_extensions.db.fields import UUIDField
 from celery.result import TaskSetResult 
+from celery.result import AsyncResult
 
 '''
 http://permalink.gmane.org/gmane.comp.python.amqp.celery.user/2036
@@ -34,32 +35,40 @@ class AnalysisStatus( models.Model ):
         execution_monitor_result = False
         
         if not self.preprocessing_taskset_id is None:
-            preprocessing_taskset = TaskSetResult.restore( self.preprocessing_taskset_id )
+            #preprocessing_taskset = TaskSetResult.restore( self.preprocessing_taskset_id )
+            preprocessing_taskset = AsyncResult( self.preprocessing_taskset_id )
+            
             if preprocessing_taskset is not None:
-                preprocessing_result = preprocessing_taskset.waiting()
+                preprocessing_result = preprocessing_taskset.state
                 preprocessing_subtasks = []
-                for res in preprocessing_taskset.results:
-                    preprocessing_subtasks.append(res.task_id)
+                #for res in preprocessing_taskset.results:
+                    #preprocessing_subtasks.append(res.task_id)
             
         if not self.preprocessing_taskset_id is None:
-            execution_taskset = TaskSetResult.restore( self.execution_taskset_id )
+            #execution_taskset = TaskSetResult.restore( self.execution_taskset_id )
+            execution_taskset = AsyncResult( self.execution_taskset_id )
+            
             if execution_taskset is not None:
-                execution_result = execution_taskset.waiting()
+                execution_result = execution_taskset.state
             
         if not self.preprocessing_taskset_id is None:            
-            postprocessing_taskset = TaskSetResult.restore( self.postprocessing_taskset_id )
+            #postprocessing_taskset = TaskSetResult.restore( self.postprocessing_taskset_id )
+            postprocessing_taskset = AsyncResult( self.postprocessing_taskset_id )
+            
             if postprocessing_taskset is not None:
-                postprocessing_result = postprocessing_taskset.waiting()
+                postprocessing_result = postprocessing_taskset.state
         
         if not self.cleanup_taskset_id is None:            
-            cleanup_taskset = TaskSetResult.restore( self.cleanup_taskset_id )
+            cleanup_taskset = AsyncResult( self.cleanup_taskset_id )
+            #cleanup_taskset = TaskSetResult.restore( self.cleanup_taskset_id )
             if cleanup_taskset is not None:
-                cleanup_result = cleanup_taskset.waiting()
+                cleanup_result = cleanup_taskset.state
         
         if not self.execution_monitor_task_id is None:            
-            execution_monitor_taskset = TaskSetResult.restore( self.execution_monitor_task_id )
+            execution_monitor_taskset = AsyncResult( self.execution_monitor_task_id )
+            #execution_monitor_taskset = TaskSetResult.restore( self.execution_monitor_task_id )
             if execution_monitor_taskset is not None:
-                execution_monitor_result = execution_monitor_taskset.waiting()
+                execution_monitor_result = execution_monitor_taskset.state
            
         #return { "preprocessing": preprocessing_result, "preprocessing_tasks": preprocessing_subtasks, "execution": execution_result, "postprocessing": postprocessing_result } 
         return { "preprocessing": preprocessing_result, "execution": execution_result, "postprocessing": postprocessing_result, "execution_monitor":execution_monitor_result, "cleanup":cleanup_result } 
