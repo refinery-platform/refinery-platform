96f41b4f0d4b7e95c1c6d592f5447b215c52dff3
rpark <richardpark@rpark-pro2.local>
2012-04-21 03:29:27 -0400

    Updated ways of viewing an analysis status

--- refinery/analysis_manager/models.py
+++ refinery/analysis_manager/models.py
@@ -27,6 +27,20 @@ class AnalysisStatus( models.Model ):
     cleanup_taskset_id = UUIDField( blank=True, null=True, auto=False )
     execution_monitor_task_id = UUIDField( blank=True, null=True, auto=False )
     
+    def preprocessing_status(self):
+        return getPayload(self.preprocessing_taskset_id)
+    
+    def execution_status(self):
+        return getPayload(self.execution_monitor_task_id)
+    
+    def postprocessing_status(self):
+        return getPayload(self.postprocessing_taskset_id)
+    
+    def cleanup_status(self):
+        return getPayload(self.cleanup_taskset_id)
+  
+        
+    """
     def current(self):        
         preprocessing_result = False
         execution_result = False
@@ -35,47 +49,39 @@ class AnalysisStatus( models.Model ):
         execution_monitor_result = False
         
         if not self.preprocessing_taskset_id is None:
-            #preprocessing_taskset = TaskSetResult.restore( self.preprocessing_taskset_id )
             preprocessing_taskset = AsyncResult( self.preprocessing_taskset_id )
             
-            print "preprocessing_result"
-            print preprocessing_result
-            """
-            for i in range (0,len(preprocessing_taskset.result.results)):
-                task = preprocessing_taskset.result.results[i].result
-                print "task"
-                print task
-            """
+            print "getPayload: preprocessing_taskset"
+            test = getPayload(self.preprocessing_taskset_id)
             
             if preprocessing_taskset is not None:
-                preprocessing_result = preprocessing_taskset.state
-                preprocessing_subtasks = []
-                #for res in preprocessing_taskset.results:
-                    #preprocessing_subtasks.append(res.task_id)
+                preprocessing_taskset = preprocessing_taskset.state
             
-        if not self.preprocessing_taskset_id is None:
-            #execution_taskset = TaskSetResult.restore( self.execution_taskset_id )
+        if not self.execution_taskset_id is None:
             execution_taskset = AsyncResult( self.execution_taskset_id )
+            print "getPayload: execution_taskset"
+            test = getPayload(self.execution_taskset_id)
             
             if execution_taskset is not None:
                 execution_result = execution_taskset.state
             
-        if not self.preprocessing_taskset_id is None:            
-            #postprocessing_taskset = TaskSetResult.restore( self.postprocessing_taskset_id )
+        if not self.execution_taskset_id is None:            
             postprocessing_taskset = AsyncResult( self.postprocessing_taskset_id )
+            print "getPayload: postprocessing_taskset"
+            test = getPayload(self.postprocessing_taskset_id)
             
             if postprocessing_taskset is not None:
                 postprocessing_result = postprocessing_taskset.state
         
         if not self.cleanup_taskset_id is None:            
-            cleanup_taskset = AsyncResult( self.cleanup_taskset_id )
-            #cleanup_taskset = TaskSetResult.restore( self.cleanup_taskset_id )
-            if cleanup_taskset is not None:
-                cleanup_result = cleanup_taskset.state
-        
+            #cleanup_taskset = AsyncResult( self.cleanup_taskset_id )
+            print "getPayload: cleanup_taskset"
+            test = getPayload(self.cleanup_taskset_id)
+            
         if not self.execution_monitor_task_id is None:            
             execution_monitor_taskset = AsyncResult( self.execution_monitor_task_id )
-            #execution_monitor_taskset = TaskSetResult.restore( self.execution_monitor_task_id )
+            print "getPayload: execution_monitor_taskset"
+            test = getPayload(self.execution_monitor_task_id)
             if execution_monitor_taskset is not None:
                 execution_monitor_result = execution_monitor_taskset.state
         
@@ -83,12 +89,78 @@ class AnalysisStatus( models.Model ):
         # TO DEBUG PYTHON WEBAPPS ##
         #import pdb; pdb.set_trace()
        # print 
-        import pdb; pdb.set_trace()
+        #import pdb; pdb.set_trace()
             
         
         #return { "preprocessing": preprocessing_result, "preprocessing_tasks": preprocessing_subtasks, "execution": execution_result, "postprocessing": postprocessing_result } 
         return { "preprocessing": preprocessing_result, "execution": execution_result, "postprocessing": postprocessing_result, "execution_monitor":execution_monitor_result, "cleanup":cleanup_result } 
+"""
 
+def getPayload(ts_id):
+    
+    payload = []
+    ts = AsyncResult( ts_id )
+    if ts:
+        #print "*******"
+        #print ts
+        if ts.result:
+           # print ">>>>>>"
+            #print ts.result
+            #print "&&&&&&&&"
+            if type(ts.result) ==type(dict()):
+                temp_ret = ts.result['message']
+                temp_ret['state'] = ts.state
+                payload.append(temp_ret)
+            elif (ts.result.__class__.__name__ == 'TaskSetResult'):
+                #print "        $$$$$$$$"
+                #print ts.result.successful()
+                #print ts.result.ready()
+                #print ts.result.results
+                n_tasks = len(ts.result.results)
+                if n_tasks > 0:
+                    #print "Greater than 0 tas"
+                    #print n_tasks
+                    #import pdb; pdb.set_trace()   
 
-
+                    for j in range(0,n_tasks):
+                        #print "\t \t jjjjjjj:" + str(j)
+                        temp_ret = {};
+                        if ts.result.results[j].result:
+                            temp_ret = ts.result.results[j].result
+                            temp_ret['state'] = ts.result.results[j].state
+                            payload.append(temp_ret)
+                            #payload.append(ts.result.results[j].result)
+                            #print ts.result.results[j].state
+                        else:
+                            #print "elseelseelseelseelseelse "
+                            #import pdb; pdb.set_trace()
+                            temp_ret['state'] = ts.result.results[j].state
+                            payload.append(temp_ret)
+                #else:
+                #    print "00000 tasks"
+                #    print ts.result
+            else:
+                #print " ))))))))))) \t \ tDIFFERENT TYPE"
+                temp_ret = {'state':ts.state, 'info':ts.result}
+                payload.append(temp_ret)
+                #print ts.result
+        else:
+            #print "<<<<<<<<<<<<<<<<<< "
+            #temp_ret
+            temp_ret = {'state':ts.state}
+            payload.append(temp_ret)
+    else:
+        print "!!!!nottstststststst"
+        print ts
+        temp_ret = {'state':"### WAITING ###"}
+        payload.append(temp_ret)
+    
+    #print "payload called"
+    print "################################"
+    print payload 
+    #print len(payload)
+    print "################################"
+    
+            
+    return payload
 
