72823679c8e9f5b306dcd3563fdb85a5316da985
rpark <richardpark@rpark-pro2.local>
2012-04-21 06:10:42 -0400

    Analysis status w/ progress bars. Need to fix end state

--- refinery/analysis_manager/views.py
+++ refinery/analysis_manager/views.py
@@ -5,6 +5,9 @@ from core.models import Analysis
 from django.template import RequestContext
 from django.shortcuts import render_to_response, get_object_or_404
 from django.http import HttpResponse, HttpResponseForbidden
+from django.utils import simplejson
+#from django.core import serializers
+
 
 
 def index(request):
@@ -15,9 +18,20 @@ def index(request):
 def analysis(request, uuid):
     print "called analysis_status"
     #import pdb; pdb.set_trace()
-
+    
     analysis = Analysis.objects.get(uuid=uuid)
     statuses = AnalysisStatus.objects.get(analysis_uuid=uuid)
     
-    #eturn render_to_response('index.html', {'chan_prog_list': chan_prog_list})
-    return render_to_response( 'analysis_manager/analysis_status.html', { 'uuid':uuid, 'statuses': statuses }, context_instance=RequestContext( request ) )
+    if request.is_ajax():
+        print "is ajax"
+        #answers.values_list('id', flat=True)
+        ret_json = {}
+        ret_json['preprocessing'] = statuses.preprocessing_status()
+        ret_json['execution'] = statuses.execution_status()
+        ret_json['postprocessing'] = statuses.postprocessing_status()
+        
+        #json_serializer = serializers.get_serializer("json")()
+        return HttpResponse(simplejson.dumps(ret_json), mimetype='application/javascript')
+
+    else:
+        return render_to_response( 'analysis_manager/analysis_status.html', { 'uuid':uuid, 'statuses': statuses }, context_instance=RequestContext( request ) )
