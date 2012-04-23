a4988416e83a7319be0271b29ecf3650881ed544
Nils Gehlenborg <nils@gehlenborg.com>
2012-04-23 19:16:50 -0400

    Added view to query TDF file.

--- refinery/file_server/urls.py
+++ refinery/file_server/urls.py
@@ -8,5 +8,6 @@ from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('file_server.views',
     url(r'^$', 'index'),
-    url(r'^(?P<uuid>[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})/$', 'file'),
+    #url(r'^(?P<uuid>[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})/$', 'file'),
+    url(r'^(?P<sequence_name>[a-zA-Z0-9]+)/(?P<zoom_level>z[0-9])/(?P<start_location>[0-9]+)/(?P<end_location>[0-9]+)/$', 'file' ),
 )
--- refinery/file_server/views.py
+++ refinery/file_server/views.py
@@ -7,15 +7,32 @@ from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.template.context import RequestContext
 from file_server.tdf_file import TDFFile
+import simplejson
 
 # Create your views here.
 def index( request ):    
     
-    tdf_file = TDFFile( "/Users/nils/Sites/tdf/TCGA-AG-4007-01A-01D-1115-02_101122_SN177_0123_B20APUABXX_s_5.rg.sorted.chr21.bam.tdf" )
+    tdf_file = TDFFile( "/Users/nils/Sites/tdf/TCGA-AG-4007-01A-01D-1115-02_101122_SN177_0123_B20APUABXX_s_5.rg.sorted.chr21.bam.tdf" )    
     
     tdf_file.cache()
     
-    data_set = tdf_file.get_data_set( "chr21", "z4", "mean" )    
+    
+    ''' 
+    for track_name in tdf_file.get_track_names():
+        print( track_name )
+
+
+    for data_set_name in tdf_file.get_data_set_names():
+        print(data_set_name)
+        window_function = ""
+        if len( data_set_name.split("/") ) == 4:
+            window_function = data_set_name.split("/")[3] 
+        data_set = tdf_file.get_data_set( data_set_name.split("/")[1], data_set_name.split("/")[2], window_function )
+        data_set.read()
+        print("Tile Count: " + str(len(data_set.tile_index)))
+        print("Tile Width: " + str(data_set.tile_width))
+            
+    
     print( data_set )
     
     
@@ -42,18 +59,35 @@ def index( request ):
 
     tile = data_set.get_tile( 7 )
     print(tile)
+    '''
+        
+    profile = tdf_file.get_profile("chr21", "z0", ["mean"], start_location=13591070, end_location=14845362 )
+    
+    print( profile )
 
 
-    data_set = tdf_file.get_data_set( "chr21", "raw" )    
-    print( data_set )
+    #data_set = tdf_file.get_data_set( "chr21", "raw" )    
+    #print( data_set )
 
-    tile = data_set.get_tile( 156 )
-    print(tile)
-
+    #tile = data_set.get_tile( 156 )
+    #print(tile)
     
     
-    return HttpResponse( tdf_file )     
+    return HttpResponse( simplejson.dumps( profile ), mimetype='application/json' )     
     #return render_to_response( 'file_server/index.html', {}, context_instance=RequestContext( request ) )
 
-def file( request ):
-    return render_to_response( 'file_server/file.html', context_instance=RequestContext( request ) )
+def file( request, sequence_name, zoom_level, start_location, end_location ):
+    
+    print ( sequence_name )
+    print ( zoom_level )
+    print ( start_location )
+    print ( end_location )
+    
+    
+    tdf_file = TDFFile( "/Users/nils/Sites/tdf/TCGA-AG-4007-01A-01D-1115-02_101122_SN177_0123_B20APUABXX_s_5.rg.sorted.chr21.bam.tdf" )        
+    tdf_file.cache()    
+    profile = tdf_file.get_profile( sequence_name, zoom_level, ["mean"], int( start_location ), int( end_location ) )    
+
+    return HttpResponse( simplejson.dumps( profile ), mimetype='application/json' )     
+    
+    #return render_to_response( 'file_server/file.html', context_instance=RequestContext( request ) )
