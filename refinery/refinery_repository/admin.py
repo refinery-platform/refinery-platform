ef703827d58ff269d46b60c6ff7569694336aebe
Psalm Haseley <psm3426@gmail.com>
2012-04-25 16:26:15 -0400

    new admin file that includes a DataTransformation model

--- refinery/refinery_repository/admin.py
+++ refinery/refinery_repository/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from refinery_repository.models import Ontology, StudyDesignDescriptor, StudyFactor, Protocol, Investigation, Publication, Investigator, Study, StudyBracketedField, RawData, ProcessedData, Assay, AssayBracketedField
+from refinery_repository.models import Ontology, StudyDesignDescriptor, StudyFactor, Protocol, Publication, Investigation, StudyAssay, Investigator, Study, StudyBracketedField, RawData, ProcessedData, Assay, DataTransformation, AssayBracketedField
 
 
 # The following classes define the admin interface for your models.
@@ -22,11 +22,15 @@ class ProtocolAdmin(admin.ModelAdmin):
     pass
 
 
+class PublicationAdmin(admin.ModelAdmin):
+    pass
+
+
 class InvestigationAdmin(admin.ModelAdmin):
     pass
 
 
-class PublicationAdmin(admin.ModelAdmin):
+class StudyAssayAdmin(admin.ModelAdmin):
     pass
 
 
@@ -54,6 +58,10 @@ class AssayAdmin(admin.ModelAdmin):
     pass
 
 
+class DataTransformationAdmin(admin.ModelAdmin):
+    pass
+
+
 class AssayBracketedFieldAdmin(admin.ModelAdmin):
     pass
 
@@ -65,13 +73,15 @@ admin.site.register(Ontology, OntologyAdmin)
 admin.site.register(StudyDesignDescriptor, StudyDesignDescriptorAdmin)
 admin.site.register(StudyFactor, StudyFactorAdmin)
 admin.site.register(Protocol, ProtocolAdmin)
-admin.site.register(Investigation, InvestigationAdmin)
 admin.site.register(Publication, PublicationAdmin)
+admin.site.register(Investigation, InvestigationAdmin)
+admin.site.register(StudyAssay, StudyAssayAdmin)
 admin.site.register(Investigator, InvestigatorAdmin)
 admin.site.register(Study, StudyAdmin)
 admin.site.register(StudyBracketedField, StudyBracketedFieldAdmin)
 admin.site.register(RawData, RawDataAdmin)
 admin.site.register(ProcessedData, ProcessedDataAdmin)
 admin.site.register(Assay, AssayAdmin)
+admin.site.register(DataTransformation, DataTransformationAdmin)
 admin.site.register(AssayBracketedField, AssayBracketedFieldAdmin)
 
