9e7c0b6b4f28a125457083137c150a9718b9d4de
Nils Gehlenborg <nils@gehlenborg.com>
2012-05-01 14:38:30 -0400

    Added basic profile viewer and file server for TDF files.

--- refinery/file_server/urls.py
+++ refinery/file_server/urls.py
@@ -7,7 +7,9 @@ Created on Apr 21, 2012
 from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('file_server.views',
-    url(r'^$', 'index'),
-    #url(r'^(?P<uuid>[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})/$', 'file'),
-    url(r'^(?P<sequence_name>[a-zA-Z0-9]+)/(?P<zoom_level>z[0-9])/(?P<start_location>[0-9]+)/(?P<end_location>[0-9]+)/$', 'file' ),
+    url(r'^$', 'index', name="file_server_base" ),
+    #url(r'^tdf/(?P<uuid>[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})/$', 'tdf_data_sets' ),
+    url(r'^tdf/(?P<uuid>[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})/cache$', 'cache_tdf' ),
+    url(r'^tdf/(?P<uuid>[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})/(?P<sequence_name>[a-zA-Z0-9]+)/(?P<zoom_level>z[0-9])/(?P<start_location>[0-9]+)/(?P<end_location>[0-9]+)/$', 'get_tdf_profile' ),
+    url(r'^profile_viewer/(?P<uuid>[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})/(?P<sequence_name>[a-zA-Z0-9]+)/(?P<zoom_level>z[0-9])/(?P<start_location>[0-9]+)/(?P<end_location>[0-9]+)/$', 'profile_viewer' ),
 )
