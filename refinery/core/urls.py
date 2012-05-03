457336ef1ae4c17230fb304074c0c16e28cb0176
Nils Gehlenborg <nils@gehlenborg.com>
2012-05-03 18:37:39 -0400

    Added About, Contact, and Statistics page skeletons.

--- refinery/core/urls.py
+++ refinery/core/urls.py
@@ -8,6 +8,9 @@ from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('core.views',
     url(r'^$', 'index'),
+    url(r'^about/$', 'about', name="about" ),
+    url(r'^contact/$', 'contact', name="contact" ),
+    url(r'^statistics/$', 'statistics', name="statistics" ),
     url(r'^users/(?P<query>[\@\.\-\+a-z0-9]+)/$', 'user'),
         # "name" is required for use with the url tag in templates
     url(r'^users/(?P<query>[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})/$', 'user', name="user"),
