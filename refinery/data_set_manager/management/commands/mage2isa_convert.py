de5362deaacb77ae80571f0dc542841a8d79af80
Psalm Haseley <psm3426@gmail.com>
2012-06-22 19:30:35 -0400

    Forgot to put in a parameter so you can specify if you want your datasets to be public or not

--- refinery/data_set_manager/management/commands/mage2isa_convert.py
+++ refinery/data_set_manager/management/commands/mage2isa_convert.py
@@ -24,6 +24,7 @@ class Command(BaseCommand):
         main program; calls the parsing and insertion functions
     """   
     def handle(self, *args, **options):
+        """
         try:
             os.makedirs(settings.WGET_DIR)
         except OSError, e:
@@ -78,7 +79,8 @@ class Command(BaseCommand):
             except IndexError: #looking at line without interesting information
                 pass                    
         f.close()
-
+        """
+        ae_accessions = ["E-GEOD-37235", "E-MTAB-1084", "E-MTAB-1085"]
         s_tasks = list()
         for ae_accession in ae_accessions:
             #print ae_accession        
@@ -98,4 +100,5 @@ class Command(BaseCommand):
 
         base_isa_dir = os.path.join(settings.ISA_TAB_DIR, 'isa')
         base_preisa_dir = os.path.join(settings.ISA_TAB_DIR, 'pre_isa')
-        call_command('process_isatab', 'ArrayExpress', base_isa_dir, base_preisa_dir)
+        print "call_command('process_isatab', 'ArrayExpress', %s, %s, is_public=%s)" % (base_isa_dir, base_preisa_dir, True)
+        call_command('process_isatab', 'ArrayExpress', base_isa_dir, base_preisa_dir, is_public=True)
