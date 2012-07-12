d0688394d38fbd103176a8d888f3f28573483de2
Ilya Sytchev <isytchev@hsph.harvard.edu>
2012-07-12 18:13:21 -0400

    Added named URL for ISA-Tab import.

--- refinery/data_set_manager/urls.py
+++ refinery/data_set_manager/urls.py
@@ -18,6 +18,6 @@ urlpatterns = patterns('data_set_manager.views',
 
     url(r'^nodes/(?P<study_uuid>[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})/(?P<assay_uuid>[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})/(?P<type>[\w ]+)/annotate$', "node_annotate", name="data_set_manager_update_annotated_nodes" ),
     
-    url(r'^import/isa-tab/$', 'import_isa_tab'),
+    url(r'^import/isa-tab/$', 'import_isa_tab', name='import_isa_tab'),
 
 )
