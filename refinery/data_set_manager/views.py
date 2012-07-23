70d559f72e8cc55b5289c894be87789a33bb9f39
Nils Gehlenborg <nils@gehlenborg.com>
2012-07-23 14:47:13 -0400

    Fixed merge issue with syntax.

--- refinery/data_set_manager/views.py
+++ refinery/data_set_manager/views.py
@@ -122,8 +122,4 @@ def import_isa_tab(request):
         form = ImportISATabFileForm()
         context = RequestContext(request, {'form': form})
 
-<<<<<<< HEAD
     return render_to_response('data_set_manager/import.html', context_instance=context)
-=======
-    return render_to_response('data_set_manager/import.html', context_instance=context)
->>>>>>> 032de07d51d014b55dbbbc2a49c5015994a38fe9
