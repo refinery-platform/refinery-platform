3b4884d64cd7191d882396f7cdac3eb44ed6b382
Nils Gehlenborg <nils@gehlenborg.com>
2012-07-24 22:23:35 -0400

    Added support to remove/add field to the table. Also introduces placeholder data structure for table headers for sorting.
    Currently fields with spaces are not handled correctly due to Solr/Lucence limitations. Escaping does not solve this problem.

--- refinery/static/js/refinery/contents.js
+++ refinery/static/js/refinery/contents.js
@@ -18,14 +18,31 @@ var testNodeType = "\"Raw Data File\"";
 var facets = {};
 /*
  * facets = 
- * { "facet_name1": [ { value: "value_name", count: "count", selected: true }, ... ] },
- *   "facet_name2": [ { value: "value_name", count: "count", selected: false }, ... ] },
+ * { "facet_name1": [ { value: "value_name", count: "count", isSelected: true }, ... ] },
+ *   "facet_name2": [ { value: "value_name", count: "count", isSelected: false }, ... ] },
  * ... } 
  */
+
+var fields = {};
+/*
+ * fields = 
+ * { "field_name1": { isVisible: true, direction: "asc" },
+ *   "field_name2": { isVisible: false, direction: "desc" },
+ *   "field_name3": { isVisible: true, direction: "" },
+ * ... } 
+ * 
+ * Notes: 
+ * 	- multiple fields can be used for sorting
+ *  - direction will be checked for "asc" or "desc", everything else means no sorting
+ *  - invisible fields can not be used for sorting (even if direction is given correctly)  
+ */
+
 var documents = [];
 
 
-function buildSolrQuery( studyUuid, assayUuid, nodeType, start, rows, facets, files ) {
+
+
+function buildSolrQuery( studyUuid, assayUuid, nodeType, start, rows, facets, fields, documents ) {
 	var url = solrRoot
 		+ "?" + solrQuery 
 		+ "&" + solrSettings
@@ -38,53 +55,69 @@ function buildSolrQuery( studyUuid, assayUuid, nodeType, start, rows, facets, fi
 			+ " AND " + "type:" + nodeType
 	   	+ ")";
 	   	
-		for ( var facet in facets ) {
-			var facetValues = facets[facet];
-			var filter = null; 
-			var filterValues = [];
-			for ( var facetValue in facetValues ) {
-				if ( facetValues.hasOwnProperty( facetValue ) )
-				{
-					if ( facetValues[facetValue].selected ) {
-						// escape or encode special characters
-						facetValue = facetValue.replace( /\ /g, "\\ " );
-						facetValue = facetValue.replace( /\(/g, "\\(" );
-						facetValue = facetValue.replace( /\)/g, "\\)" );
-						facetValue = facetValue.replace( /\+/g, "%2B" );
-						facetValue = facetValue.replace( /\:/g, "%3A" );
-						filterValues.push( facetValue );
-					}
-				}				
+	for ( var facet in facets ) {
+		var facetValues = facets[facet];
+		var filter = null; 
+		var filterValues = [];
+		
+		for ( var facetValue in facetValues ) {
+			if ( facetValues.hasOwnProperty( facetValue ) )
+			{
+				if ( facetValues[facetValue].isSelected ) {
+					// escape or encode special characters
+					facetValue = facetValue.replace( /\ /g, "\\ " );
+					facetValue = facetValue.replace( /\(/g, "\\(" );
+					facetValue = facetValue.replace( /\)/g, "\\)" );
+					facetValue = facetValue.replace( /\+/g, "%2B" );
+					facetValue = facetValue.replace( /\:/g, "%3A" );
+					filterValues.push( facetValue );
+				}
+			}				
+		}
+
+		if ( filterValues.length > 0 ) {
+			filter = facet.replace( /\ /g, "_" );								
+			console.log( filterValues );
+			url += "&fq={!tag=" + filter + "}" + facet.replace( /\ /g, "\\ " ) + ":(" + filterValues.join( " OR " ) + ")";													
+		}
+		
+		if ( facets.hasOwnProperty( facet ) )
+		{
+			if ( filter ) {
+				url += "&facet.field={!ex=" + filter + "}" + facet;
 			}
-
-			// make or statement
-			filter = facet.replace( /\ /g, "_" );
-
-			if ( filterValues.length > 0 ) {
-				console.log( filterValues );
-				url += "&fq={!tag=" + filter + "}" + facet.replace( /\ /g, "\\ " ) + ":(" + filterValues.join( " OR " ) + ")";													
+			else {
+				url += "&facet.field=" + facet;					
 			}
 			
-			if ( facets.hasOwnProperty( facet ) )
-			{
-				if ( filter ) {
-					url += "&facet.field={!ex=" + filter + "}" + facet;
-				}
-				else {
-					url += "&facet.field=" + facet;					
-				}
-				
-			}
 		}
+	}
+
+	var fieldNames = [];
+	for ( var field in fields ) {
+		if ( fields.hasOwnProperty( field ) )
+		{			
+			if ( fields[field].isVisible ) {				
+				var fieldName = fields[field];
+				// escape or encode special characters
+				field = field.replace( /\ /g, "\\ " );
+				field = field.replace( /\(/g, "\\(" );
+				field = field.replace( /\)/g, "\\)" );
+				field = field.replace( /\+/g, "%2B" );
+				field = field.replace( /\:/g, "%3A" );
+				fieldNames.push( field );
+			}
+		}				
+	}	
+	url += "&fl=" + fieldNames.join( ",");
 	
-	console.log( url );
 	$( "#url-view" ).html( "" );
 	$( "<a/>", { href: url + "&indent=on", html: "Solr Query" } ).appendTo( "#url-view" );
 
 	return ( url );
 }
 
-function prettifyFacetName( name )
+function prettifyFieldName( name )
 {
 	var position = name.indexOf( "_Characteristics_s" );
 	if ( position != -1 ) {
@@ -94,7 +127,9 @@ function prettifyFacetName( name )
 	var position = name.indexOf( "_Factor_s" );
 	if ( position != -1 ) {
 		return name.substr( 0, position );
-	}	
+	}
+	
+	return name;	
 
 	/*
 	var position = name.indexOf( "_Comment_s" );
@@ -106,13 +141,14 @@ function prettifyFacetName( name )
 }
 
 function initializeData( studyUuid, assayUuid, nodeType ) {
-	$.ajax( { type: "GET", dataType: "jsonp", url: buildSolrQuery( studyUuid, assayUuid, nodeType, 0, 1, {}, {} ), success: function(data) {
+	$.ajax( { type: "GET", dataType: "jsonp", url: buildSolrQuery( studyUuid, assayUuid, nodeType, 0, 1, {}, {}, {} ), success: function(data) {
 		console.log( data );
 		
 		var doc = data.response.docs[0];		
 	
 		for ( var attribute in doc ) {
 			if ( doc.hasOwnProperty( attribute ) ) {
+				// facets
 				if ( attribute.indexOf( "Characteristics_s" ) != -1 ) {
 					facets[attribute] = [];
 				}
@@ -124,6 +160,9 @@ function initializeData( studyUuid, assayUuid, nodeType ) {
 					facets[attribute] = [];
 				}
 				*/
+				
+				// fields
+				fields[attribute] = { isVisible: true, direction: "" };
 			}		
 		}
 		
@@ -132,19 +171,29 @@ function initializeData( studyUuid, assayUuid, nodeType ) {
 }
 
 function getData( studyUuid, assayUuid, nodeType ) {
-	$.ajax( { type: "GET", dataType: "jsonp", url: buildSolrQuery( studyUuid, assayUuid, nodeType, 0, 25, facets, {} ), success: function(data) {
+	$.ajax( { type: "GET", dataType: "jsonp", url: buildSolrQuery( studyUuid, assayUuid, nodeType, 0, 25, facets, fields, {} ), success: function(data) {
 		processFacets( data );
+		processFields( data );
 		processDocs( data );
 	}});	
 }
 
 
+function composeFieldNameId( fieldName ) {
+	return ( "fieldname" + "___" + fieldName );
+}
+
+function decomposeFieldNameId( fieldNameId ) {
+	return ( { fieldName: fieldNameId.split( "___" )[1] } );
+}
+
+
 function composeFacetValueId( facet, facetValue ) {
-	return ( facet + "___" + facetValue );
+	return ( "facetvalue" + "___" + facet + "___" + facetValue );
 }
 
 function decomposeFacetValueId( facetValueId ) {
-	return ( { facet: facetValueId.split( "___" )[0], facetValue: facetValueId.split( "___" )[1] } );
+	return ( { facet: facetValueId.split( "___" )[1], facetValue: facetValueId.split( "___" )[2] } );
 }
 
 function processFacets( data ) {
@@ -164,13 +213,13 @@ function processFacets( data ) {
 				}
 				
 				if ( facets[facet][facetValue] ) {
-					facets[facet][facetValue] = { count: facetValueCount, selected: facets[facet][facetValue].selected };					
+					facets[facet][facetValue] = { count: facetValueCount, isSelected: facets[facet][facetValue].isSelected };					
 				}
 				else {
-					facets[facet][facetValue] = { count: facetValueCount, selected: false };					
+					facets[facet][facetValue] = { count: facetValueCount, isSelected: false };					
 				}
 				
-				if ( facets[facet][facetValue].selected ) {
+				if ( facets[facet][facetValue].isSelected ) {
 		    		selectedItems.push("<li class=\"facet-value\" id=\"" + composeFacetValueId( facet, facetValue ) + "\">" + facetValue + " (" + facetValueCount + ")"  + "&nbsp;<i class=\"icon-remove\"/>" + "</li>");					
 				}
 				else {
@@ -180,7 +229,7 @@ function processFacets( data ) {
 								
 			}
 			
-			$('<h3/>', { 'class': 'facet-title', html: prettifyFacetName( facet ) }).appendTo('#facet-view');
+			$('<h3/>', { 'class': 'facet-title', html: prettifyFieldName( facet ) }).appendTo('#facet-view');
 			$('<ul/>', { 'class': 'facet-value-list', html: selectedItems.join('') + "" + unselectedItems.join('') }).appendTo('#facet-view');
 			
 		}		
@@ -191,10 +240,39 @@ function processFacets( data ) {
    		var facet = decomposeFacetValueId( facetValueId ).facet;
    		var facetValue = decomposeFacetValueId( facetValueId ).facetValue;
    	   		
-   		facets[facet][facetValue].selected = !facets[facet][facetValue].selected;   		
+   		facets[facet][facetValue].isSelected = !facets[facet][facetValue].isSelected;   		
    		getData( testAssayUuid, testStudyUuid, testNodeType );
    	} );				
 }
+
+
+function processFields() {
+	var visibleItems = []
+	var invisibleItems = []
+	for ( field in fields ) {
+		if ( fields.hasOwnProperty( field ) ) {
+			if ( fields[field].isVisible ) {
+				visibleItems.push("<span class=\"field-name\" id=\"" + composeFieldNameId( field ) + "\">" + "<i class=\"icon-minus-sign\"/>&nbsp;" + prettifyFieldName( field ) + "</span>" );				
+			}
+			else {
+				invisibleItems.push("<span class=\"field-name\" id=\"" + composeFieldNameId( field ) + "\">" + "<i class=\"icon-plus-sign\"/>&nbsp;" + prettifyFieldName( field ) + "</span>" );								
+			}
+		}
+	}
+
+	$("#field-view").html("" );
+	$('<p/>', { 'class': 'field-name-list', html: visibleItems.join(' / ') + "<br>" + invisibleItems.join(' / ') }).appendTo('#field-view');
+	
+   	$(".field-name").click( function() {
+   		var fieldNameId = event.target.id;
+   		var fieldName = decomposeFieldNameId( fieldNameId ).fieldName;
+   	   		
+   		fields[fieldName].isVisible = !fields[fieldName].isVisible;   		
+   		getData( testAssayUuid, testStudyUuid, testNodeType );
+   	} );				
+	
+}
+
 
 function processDocs( data ) {
 	items = []
@@ -222,6 +300,7 @@ function processDocs( data ) {
     $( "#table-view" ).html("");
 	$('<table/>', { 'class': "table table-striped table-condensed table-bordered", html: "<tbody>" + items.join('\n') + "</tbody>" }).appendTo('#table-view');		
 }
+
 
 
 initializeData( testAssayUuid, testStudyUuid, testNodeType );
