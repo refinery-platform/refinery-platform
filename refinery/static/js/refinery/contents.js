142e6067afd120cdd52b7069770e1e68a3846c61
Nils Gehlenborg <nils@gehlenborg.com>
2012-07-31 14:50:04 -0400

    Added automatic generation of annotated node objects to isa-tab upload view.

--- refinery/static/js/refinery/contents.js
+++ refinery/static/js/refinery/contents.js
@@ -160,8 +160,8 @@ function buildSolrQuery( studyUuid, assayUuid, nodeType, start, rows, facets, fi
 	}	
 	
 	
-	//$( "#url-view" ).html( "" );
-	//$( "<a/>", { href: url + "&indent=on", html: "Solr Query" } ).appendTo( "#url-view" );
+	$( "#url-view" ).html( "" );
+	$( "<a/>", { href: url + "&indent=on", html: "Solr Query" } ).appendTo( "#url-view" );
 
 	return ( url );
 }
