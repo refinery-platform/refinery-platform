48cedcd0553348b52f26fd4bebb3d648586169eb
Ilya Sytchev <isytchev@hsph.harvard.edu>
2012-08-06 17:01:12 -0400

    Added stubs for file server models.

--- refinery/file_server/models.py
+++ refinery/file_server/models.py
@@ -5,21 +5,46 @@ Created on Apr 21, 2012
 '''
 
 from django.db import models
-from django_extensions.db.fields import UUIDField
 from file_store.models import FileStoreItem
+
 
 class FileServerItem(models.Model):
     '''Abstract base class representing pairs of files required for visualization.
     
     '''
-    #visualization_file = models.ForeignKey(FileStoreItem)
-    #data_file = models.ForeignKey(FileStoreItem, blank=True, null=True)
-
+    
+    viz_file = models.ForeignKey(FileStoreItem, related_name="%(class)s_vizfile_related")
+    data_file = models.ForeignKey(FileStoreItem, blank=True, null=True, related_name="%(class)s_datafile_related")
+    # related_name argument is required and must be unique; see below for more details:
+    # http://stackoverflow.com/questions/1142378/django-why-some-fields-clashes-with-other
+    # https://docs.djangoproject.com/en/dev/ref/models/fields/#foreignkey
+    # https://docs.djangoproject.com/en/dev/topics/db/models/#abstract-related-name
+    
     class Meta:
         abstract = True
+
 
 class TDFItem(FileServerItem):
     '''Represents TDF file.
     
     '''
-    pass
+    def add(self, viz_file_uuid, data_file_uuid=None, index=False):
+        '''Create a new FileServerItem instance.
+        
+        :param viz_file_uuid: UUID of the visualization file.
+        :param type: str.
+        :param data_file_uuid: UUID of the data file.
+        :param type: str.
+        
+        :returns: 
+        
+        '''
+        pass
+
+    def get(self, uuid):
+        '''
+        
+        :param uuid:
+        :returns: FileServerItem or None if not found
+        '''
+        pass
