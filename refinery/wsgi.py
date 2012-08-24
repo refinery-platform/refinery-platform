2d27bfcf6e08898fddb84fdbfff42308e4d94350
Nils Gehlenborg <nils@gehlenborg.com>
2012-08-24 18:46:56 -0400

    Added base path to sys.path() for mod_wsgi.

--- refinery/wsgi.py
+++ refinery/wsgi.py
@@ -1,4 +1,13 @@
 import os
+import sys
+
+# path is the parent directory of this script ('/var/www' in this case)
+path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# we check for path because we're told to at the tail end of
+# http://code.google.com/p/modwsgi/wiki/ConfigurationDirectives#WSGIReloadMechanism 
+if path not in sys.path:
+    sys.path.append(path)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "refinery.settings")
 
