c7dbd627b27d83b59388d4282e60d540a872e68b
Nils Gehlenborg <nils@gehlenborg.com>
2012-08-25 16:52:46 -0400

    Cleaned up wsgi.py.

--- refinery/wsgi.py
+++ refinery/wsgi.py
@@ -1,20 +1,8 @@
 import os
-import sys
-
-# path is the parent directory of this script ('/var/www' in this case)
-#path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-# we check for path because we're told to at the tail end of
-# http://code.google.com/p/modwsgi/wiki/ConfigurationDirectives#WSGIReloadMechanism 
-#if path not in sys.path:
-#    sys.path.append(path)
-
-sys.path.append('/srv/refinery/Refinery/refinery', '/srv/refinery/Refinery')
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
-# This application object is used by the development server
-# as well as any WSGI server configured to use this file.
+# This application object is used by the development server as well as any WSGI server configured to use this file.
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
 
