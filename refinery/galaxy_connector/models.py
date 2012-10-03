16f0031c566b646bd01bb02afb993f87a66e5730
Nils Gehlenborg <nils@gehlenborg.com>
2012-10-03 12:20:07 -0400

    Removed staging path from Instance, added defaults and removed DataFile model.

--- refinery/galaxy_connector/models.py
+++ refinery/galaxy_connector/models.py
@@ -1,25 +1,11 @@
 from django.db import models
 
 class Instance( models.Model ):
-    base_url = models.CharField( max_length = 2000 )
-    data_url = models.CharField( max_length = 100 )
-    api_url = models.CharField( max_length = 100 )
-    api_key = models.CharField( max_length = 50 )
-    staging_path = models.CharField( max_length = 2000 )
-    description = models.CharField( max_length = 500 )
+    base_url = models.CharField( max_length=2000 )
+    data_url = models.CharField( max_length=100, default="datasets" )
+    api_url = models.CharField( max_length=100, default="api" )
+    api_key = models.CharField( max_length=50 )
+    description = models.CharField( max_length=500 )
 
     def __unicode__(self):
         return self.description + " (" + self.api_key + ")"
-
-
-
-class DataFile( models.Model ):
-    path = models.CharField( max_length = 2000 )
-    kind = models.CharField( max_length = 20 );
-    description = models.CharField( max_length = 500 )
-
-
-    def __unicode__(self):
-        return self.description + " (" + self.path + ", " + self.kind + ")"
-
-
