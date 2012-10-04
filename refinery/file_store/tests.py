a96c3e0515c3bdda01345e210118be7ce2883231
Ilya Sytchev <isytchev@hsph.harvard.edu>
2012-10-04 18:22:51 -0400

    Rewritten unit tests for get_file_object() to make use of mocks.

--- refinery/file_store/tests.py
+++ refinery/file_store/tests.py
@@ -4,10 +4,11 @@ This file contains tests for file_store.models and file_store.tasks
 """
 
 import os
-import StringIO
+import mock
 from urlparse import urljoin
 from django.test import SimpleTestCase
 import file_store.models as models
+from django.core.files.base import File, ContentFile
 
 
 class FileStoreModuleTest(SimpleTestCase):
@@ -18,13 +19,13 @@ class FileStoreModuleTest(SimpleTestCase):
         self.filename = 'test_file.dat'
         self.sharename = 'labname'
 
+        # create FileStoreItem instances without any disk operations
         self.path_source = os.path.join('/example/path', self.filename)
-        # create a FileStoreItem instance without symlinking it
-        self.item_from_url = models.FileStoreItem.objects.create(source=self.path_source,
-                                                                 sharename=self.sharename)
-        self.url_source = urljoin('http://example.org/', self.filename)
-        self.item_from_path = models.FileStoreItem.objects.create(source=self.url_source,
+        self.item_from_path = models.FileStoreItem.objects.create(source=self.path_source,
                                                                   sharename=self.sharename)
+        self.url_source = urljoin('http://example.org/', self.filename)
+        self.item_from_url = models.FileStoreItem.objects.create(source=self.url_source,
+                                                                     sharename=self.sharename)
 
     def test_file_path(self):
         '''Check that the file store path contains share name and file name.
@@ -44,18 +45,22 @@ class FileStoreModuleTest(SimpleTestCase):
         '''
         self.assertEqual(models.get_temp_dir(), models.FILE_STORE_TEMP_DIR)
 
-    def test_get_file_object(self):
+    @mock.patch.object(models.FileStoreItem, 'get_file_object')
+    def test_get_file_object_with_existing_uuid(self, mock_get_file_object):
         '''Check that a file object is returned if a valid UUID is provided.
 
         '''
-        content = 'test'
-        models.FileStoreItem.get_file_object = lambda self: StringIO.StringIO(content)
-        # test with an existing UUID
-        fh = models.get_file_object(self.item_from_path.uuid)
-        self.assertEqual(fh.read(), content)
-        # test with an invalid UUID
-        fh = models.get_file_object('invalid-uuid')
-        self.assertIsNone(fh)
+        mock_get_file_object.return_value = mock.sentinel.file_object 
+        file_object = models.get_file_object(self.item_from_path.uuid)
+        mock_get_file_object.assert_called_once_with()
+        self.assertEqual(file_object, mock.sentinel.file_object)
+
+    def test_get_file_object_with_nonexisting_uuid(self):
+        '''Check that None is returned if a UUID that doesn't exist is provided.
+
+        '''
+        file_object = models.get_file_object('non-existing-uuid')
+        self.assertIsNone(file_object)
 
 
 class FileStoreItemTest(SimpleTestCase):
@@ -66,8 +71,8 @@ class FileStoreItemTest(SimpleTestCase):
         self.filename = 'test_file.dat'
         self.sharename = 'labname'
 
+        # create FileStoreItem instances without any disk operations
         self.path_source = os.path.join('/example/path', self.filename)
-        # create a FileStoreItem instance without symlinking it
         self.item_from_url = models.FileStoreItem.objects.create(source=self.path_source,
                                                                  sharename=self.sharename)
         self.url_source = urljoin('http://example.org/', self.filename)
