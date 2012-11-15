eaecfddcf3084395d5dbf7de0ee713fd680350eb
Nils Gehlenborg <nils@gehlenborg.com>
2012-11-15 15:21:54 -0500

    slug of BaseResource may now be null.

--- refinery/core/models.py
+++ refinery/core/models.py
@@ -78,7 +78,7 @@ class BaseResource ( models.Model ):
     creation_date = models.DateTimeField( auto_now_add=True )
     modification_date = models.DateTimeField( auto_now=True )    
 
-    slug = models.CharField( max_length=250, blank=True )
+    slug = models.CharField( max_length=250, blank=True, null=True )
 
     def __unicode__(self):
         return self.name + " (" + self.uuid + ")"
