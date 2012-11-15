c0852b8aeec695c52aaa03de922a75d733955fdd
Nils Gehlenborg <nils@gehlenborg.com>
2012-11-15 15:58:46 -0500

    create_user no longer explicitly adds user to public group, this is handled by the methods that create the user profile.

--- refinery/core/management/commands/create_user.py
+++ refinery/core/management/commands/create_user.py
@@ -39,9 +39,7 @@ class Command(BaseCommand):
         user_object.get_profile().affiliation = affiliation
         user_object.save()
         user_object.get_profile().save()
-        
-        user_object.groups.add( ExtendedGroup.objects.public_group() )
-        
+                
         print "User " + username + " created."
         logger.info( "User " + username + " created." )
          
