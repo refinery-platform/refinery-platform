70e43b18c476d2fb121ca3fd3645a6635a11262f
Nils Gehlenborg <nils@gehlenborg.com>
2012-11-20 00:21:45 -0500

    Now showing analyses associated with a data set on the data set overview page.

--- refinery/analysis_manager/urls.py
+++ refinery/analysis_manager/urls.py
@@ -8,9 +8,8 @@ from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('analysis_manager.views',
     url(r'^$', 'index'),
-    url(r'^(?P<uuid>[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})/$', 'analysis'),
+    url(r'^(?P<uuid>[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})/$', 'analysis', name="analysis"),
     url(r'^update_workflows/$', 'update_workflows'),
     url(r'^workflow_inputs/(?P<workflow_uuid>[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})/$', 'getWorkflowDataInputMap'),
     url(r'^analysis_run/$', 'analysis_run'),
-    
 )
