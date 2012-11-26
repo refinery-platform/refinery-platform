f3b18bb986a4634ab7fd936eec21c7b42ef11b82
Richard Park <park.rich1@gmail.com>
2012-11-26 15:01:34 -0500

    Changed analysis name to Workflow name + datetime.

--- refinery/analysis_manager/views.py
+++ refinery/analysis_manager/views.py
@@ -135,7 +135,7 @@ def analysis_run(request):
     
     ######### ANALYSIS MODEL ########
     # How to create a simple analysis object
-    temp_name = "Unnamed " + str( datetime.now() )
+    temp_name = curr_workflow.name + " " + str( datetime.now() )
     summary_name = "None provided."
     
     analysis = Analysis( summary=summary_name, name=temp_name, project=request.user.get_profile().catch_all_project, data_set=data_set, workflow=curr_workflow, time_start=datetime.now() )
