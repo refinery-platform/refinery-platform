93bf589b8d1884aafa2df61ea1f67cd181c93660
Nils Gehlenborg <nils@gehlenborg.com>
2012-12-01 18:18:05 -0500

    Prefixed workflow manager URLs with "workflows".

--- refinery/workflow_manager/urls.py
+++ refinery/workflow_manager/urls.py
@@ -7,7 +7,7 @@ Created on Mar 31, 2012
 from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('workflow_manager.views',
-    url(r'^(?P<workflow_uuid>[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})/download/$', "download_workflow"),
-    url(r'^import$', 'import_workflows'),        
+    url(r'^workflows/(?P<workflow_uuid>[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})/download/$', "download_workflow"),
+    url(r'^workflows/import$', 'import_workflows'),        
 )
 
