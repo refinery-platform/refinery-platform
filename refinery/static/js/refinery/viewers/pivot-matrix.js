ba7c115744fbe8fc64f99bd55e62593165e273d2
Nils Gehlenborg <nils@gehlenborg.com>
2012-12-13 14:06:01 -0500

    [Fixes #41045195] Highlighting of facet selection in matrix row/columns and vice versa.

--- refinery/static/js/refinery/viewers/pivot-matrix.js
+++ refinery/static/js/refinery/viewers/pivot-matrix.js
@@ -11,116 +11,135 @@ maxCount = function( matrix ) {
 	return max;
 } 
 
-PivotMatrix = function(elemid, options, matrix, facets, useGradient, updateCallback ) {
-  var self = this;
-  this.chart = document.getElementById(elemid);
-  this.cx = this.chart.clientWidth;
-  this.cy = this.chart.clientHeight;
-  this.options = options || {};
-  this.options.ymin = options.ymin || 0;
-  this.useGradient = useGradient;
-  this.matrix = matrix;
-  this.facets = facets;
-  
-var matrix2 = [ 
-			   [
-				{x:0, y:0, xlab:"x0",ylab:"y0", count:1},
-				{x:1, y:0, xlab:"x1",ylab:"y1", count:2}
-			   ], 
-			   [
-				{x:0, y:1, xlab:"x0",ylab:"y1", count:3},
-				{x:1, y:1, xlab:"x1",ylab:"y1", count:4}
-			   ],			   
-			   [
-				{x:0, y:2, xlab:"x0",ylab:"y2", count:5},
-				{x:1, y:2, xlab:"x1",ylab:"y2", count:0}
-			   ],			   
-			   [
-				{x:0, y:3, xlab:"x0",ylab:"y3", count:10},
-				{x:1, y:3, xlab:"x1",ylab:"y3", count:8}
-			   ]			   
-			 ];
-//var matrix = matrix2;
+PivotMatrix = function( elementId, options, matrix, facets, xPivot, yPivot, useGradient, updateCallback ) {
+	
+	var self = this;
+	this.chart = document.getElementById(elementId);
+	this.cx = this.chart.clientWidth;
+	this.cy = this.chart.clientHeight;
+	this.options = options || {};
+	this.options.ymin = options.ymin || 0;
+	this.useGradient = useGradient;
+	this.matrix = matrix;
+	this.facets = facets;
+    
+	var matrix2 = [ 
+		[
+			{x:0, y:0, xlab:"x0",ylab:"y0", count:1},
+			{x:1, y:0, xlab:"x1",ylab:"y1", count:2}
+		], 
+		[
+			{x:0, y:1, xlab:"x0",ylab:"y1", count:3},
+			{x:1, y:1, xlab:"x1",ylab:"y1", count:4}
+		],			   
+		[
+			{x:0, y:2, xlab:"x0",ylab:"y2", count:5},
+			{x:1, y:2, xlab:"x1",ylab:"y2", count:0}
+		],			   
+		[
+			{x:0, y:3, xlab:"x0",ylab:"y3", count:10},
+			{x:1, y:3, xlab:"x1",ylab:"y3", count:8}
+		]			   
+	];
 
-var margin = {top: 80, right: 20, bottom: 0, left: 80},
-    width = Math.max( matrix[0].length * 12, 800 );
-    height = Math.max( matrix.length * 12, 500 );
+	//var matrix = matrix2;
 
-var x = d3.scale.ordinal().rangeBands([0, width]);
-var y = d3.scale.ordinal().rangeBands([0, height]);
+	var margin = {top: 80, right: 20, bottom: 0, left: 80},
+    	width = Math.max( matrix[0].length * 12, 800 );
+    	height = Math.max( matrix.length * 12, 500 );
 
-//var x = d3.fisheye.scale(d3.scale.identity).domain([0, width]).focus(360);
-//var y = d3.fisheye.scale(d3.scale.identity).domain([0, height]).focus(90);
+	var x = d3.scale.ordinal().rangeBands([0, width]);
+	var y = d3.scale.ordinal().rangeBands([0, height]);
 
-var max = maxCount( matrix );
+	//var x = d3.fisheye.scale(d3.scale.identity).domain([0, width]).focus(360);
+	//var y = d3.fisheye.scale(d3.scale.identity).domain([0, height]).focus(90);
 
-var c;
-var selectedColor;
-if ( this.useGradient ) {
-	// gradient color map
-	c = d3.scale.linear().domain([0,max]).range(["lightgray", "black"]).clamp(true);	
-	selectedColor = d3.scale.linear().domain([0,max]).range(["lightgray", "red"]).clamp(true);	
-}
-else {
-	// binary color map
-	c = d3.scale.linear().domain([0,1]).range(["lightgray", "black"]).clamp(true);		
-	selectedColor = d3.scale.linear().domain([0,1]).range(["lightgray", "red"]).clamp(true);	
-}
+	var max = maxCount( matrix );
 
-//var c = d3.scale.category10().domain(d3.range(10));
+	var defaultColor;
+	var selectedColor;
+	
+	if ( this.useGradient ) {
+		// gradient color map
+		defaultColor = d3.scale.linear().domain([0,max]).range(["#EEEEEE", "#222222"]).clamp(true);	
+		selectedColor = d3.scale.linear().domain([0,max]).range(["#E0EAEF", "#136382"]).clamp(true);	
+	}
+	else {
+		// binary color map
+		defaultColor = d3.scale.linear().domain([0,1]).range(["lightgray", "black"]).clamp(true);		
+		selectedColor = d3.scale.linear().domain([0,1]).range(["yellow", "red"]).clamp(true);	
+	}
 
-var svg = d3.select(document.getElementById(elemid)).append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .attr("background-color", "transparent")
-    //.style("margin-left", margin.left + "px")
-  .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+	//var c = d3.scale.category10().domain(d3.range(10));
 
-  // precompute the orders.
-  var orders = {
-  	xDefault: d3.range(matrix[0].length).sort(),
-  	yDefault: d3.range(matrix.length).sort(),
-  	xLabels: d3.range(matrix[0].length).sort(function(a, b) { var labels = $.map( matrix[0], function ( entry ) { return ( entry.xlab ); } ); return d3.ascending(labels[a], labels[b]); }),
-  	yLabels: d3.range(matrix.length).sort(function(a, b) { var labels = $.map( matrix, function ( entry ) { return ( entry[0].ylab ); } ); return d3.ascending(labels[a], labels[b]); })
-  };
+	var svg = d3.select(document.getElementById(elementId)).append("svg")
+    	.attr("width", width + margin.left + margin.right)
+    	.attr("height", height + margin.top + margin.bottom)
+    	//.attr("background-color", "transparent")
+    	//.style("margin-left", margin.left + "px")
+  	.append("g")
+    	.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-  // The default sort order.
-  x.domain(orders.xLabels);
-  y.domain(orders.yLabels);
+  	// precompute the orders.
+  	var orders = {
+	  	xDefault: d3.range(matrix[0].length).sort(),
+	  	yDefault: d3.range(matrix.length).sort(),
+	  	xLabels: d3.range(matrix[0].length).sort(function(a, b) { var labels = $.map( matrix[0], function ( entry ) { return ( entry.xlab ); } ); return d3.ascending(labels[a], labels[b]); }),
+	  	yLabels: d3.range(matrix.length).sort(function(a, b) { var labels = $.map( matrix, function ( entry ) { return ( entry[0].ylab ); } ); return d3.ascending(labels[a], labels[b]); })
+  	};
 
-  svg.append("rect")
-      .attr("class", "frame")
-      .attr("width", width)
-      .attr("height", height)
+	// The default sort order.
+	x.domain(orders.xDefault);
+	y.domain(orders.yDefault);
+
+  	svg.append("rect")
+		.attr("class", "frame")
+		.attr("width", width)
+		.attr("height", height)
+		.style("fill","none")
+		.style("stroke","none");
  
-  var row = svg.selectAll(".row")
-      .data(matrix)
-    .enter().append("g")
-      .attr("class", "row")
-      .attr("transform", function(d, i) { return "translate(0," + y(i) + ")"; })
-      .each(makeRow);
+	var row = svg.selectAll(".row")
+		.data(matrix)
+		.enter().append("g")
+			.attr("class", "row")
+			.attr("transform", function(d, i) { return "translate(0," + y(i) + ")"; })
+			.style( "fill", "rgb(0,255,255)")
+			.each(makeRow);
+	
+	/*
+	// lines are generated by making the cells smaller
+	row.append("line")
+		.attr("x1", 0 )
+		.attr("x2", width )
+		.attr("y1", 0 )
+		.attr("y2", 0 )
+		.style( "stroke", "rgb(255,0,0)" )
+		.style( "stroke-width", 2 );
+	*/
 
-  //row.append("line")
-  //    .attr("x2", width);
-
-  row.append("text")
-      .attr("x", -5)
-      .attr("y", y.rangeBand()/2 )
-      .attr("dy", ".32em")
-		.attr("class", "matrix-row-label" )
+	row.append("text")
+		.attr("x", -5)
+		.attr("y", y.rangeBand()/2 )
+		.attr("dy", ".32em")
+		.attr("class", function(p) {
+			if ( facets[p[0].yfacet][p[0].ylab].isSelected ) {
+				return ( "matrix-label-selected" );		
+			}
+			return ( "matrix-label" );			
+		})
 		.attr("title", function(p) {
 			return p[0].ylab;
 		 })      
-	  .style("font-size", "12px")	            
-	  .style("cursor", "pointer")	            
-      .attr("text-anchor", "end")
-      .text(function(d, row ) { return matrix[row][0].ylab; })
-      .on("mouseover", function(p){ d3.selectAll(".row text").classed("active", function(d, i) { return i == p[0].y; }); } )
-      .on("mouseout", mouseout)      
-      .on("click", function(p) { 
-      		if ( d3.event.altKey ) {
-      			orderRows( orders.yLabels ); 
+		.style("font-size", "12px")	            
+		.style("cursor", "pointer")	            
+		.attr("text-anchor", "end")
+		.text(function(d, row ) { return matrix[row][0].ylab; })
+		.on("mouseover", function(p){ d3.selectAll(".row text").classed("active", function(d, i) { return i == p[0].y; }); } )
+		.on("mouseout", mouseout)      
+		.on("click", function(p) { 
+			if ( d3.event.altKey ) {
+      			//orderRows( orders.yLabels ); 
       		}
       		else {
       			console.log( p );
@@ -132,21 +151,34 @@ var svg = d3.select(document.getElementById(elemid)).append("svg")
       	}, true );
             
 
-  var column = svg.selectAll(".column")
-      .data(matrix[0])
-    .enter().append("g")
-      .attr("class", "column")
-      .attr("transform", function(d, i) { return "translate(" + x(i) + ")rotate(-90)"; });
+	var column = svg.selectAll(".column")
+		.data(matrix[0])
+		.enter().append("g")
+			.attr("class", "column")
+			.attr("transform", function(d, i) { return "translate(" + x(i) + ")rotate(-90)"; });
 
-  //column.append("line")
-  //    .attr("x1", -height);
-
+	/*
+	// lines are generated by making the cells smaller
+	column.append("svg:line")
+		.attr("x1", -height )
+		.attr("x2", 0 )
+		.attr("y1", 0 )
+		.attr("y2", 0 )
+		.style( "stroke", "rgb(0,255,0)" )
+		.style( "stroke-width", 2 );
+	*/
+	
   // labels
 	column.append("text")
 		.attr("x", 5 )
 		.attr("y", x.rangeBand() / 2)
-		.attr("dy", ".32em")
-		.attr("class", "matrix-column-label")
+		.attr("dy", ".32em")		
+		.attr("class", function(p) {
+			if ( facets[p.xfacet][p.xlab].isSelected ) {
+				return ( "matrix-label-selected" );		
+			}
+			return ( "matrix-label" );			
+		})
 		.attr("title", function(p) {
 			return p.xlab;
 		 })      
@@ -158,13 +190,13 @@ var svg = d3.select(document.getElementById(elemid)).append("svg")
 		.on("mouseover", function(p){ d3.selectAll(".column text").classed("active", function(d, i) { return i == p.x; }); } )
 		.on("mouseout", mouseout)
 		.on("click", function(p) {
-		if ( d3.event.altKey ) {
-			//orderColumns( orders.xLabels ); 
-		}
-		else {
-			facets[p.xfacet][p.xlab].isSelected = !facets[p.xfacet][p.xlab].isSelected;
-			updateCallback();      			
-			//orderRows( computeRowOrder( p.x, false ) ); 
+			if ( d3.event.altKey ) {
+				//orderColumns( orders.xLabels ); 
+			}
+			else {
+				facets[p.xfacet][p.xlab].isSelected = !facets[p.xfacet][p.xlab].isSelected;
+				updateCallback();      			
+				//orderRows( computeRowOrder( p.x, false ) ); 
 			} 
 		}, true );
 
@@ -194,21 +226,42 @@ var svg = d3.select(document.getElementById(elemid)).append("svg")
 	    		return p.count;
 			 })
         .attr("class", "cell")
-        .attr("x", function(d) { return x(d.x); })
-        .attr("height", y.rangeBand() )
-        .attr("width", x.rangeBand() )
+        .attr("x", function(d) { return x(d.x) + 1; })
+        .attr("height", y.rangeBand() - 2 )
+        .attr("width", x.rangeBand() - 2 )
         .style("fill-opacity", function(d) { return 1; })
         .style("fill", function(p) {
-        		if ( facets[p.yfacet][p.ylab].isSelected || facets[p.xfacet][p.xlab].isSelected ) {
-					return ( selectedColor(p.count) );        				
+        		if ( p.count == 0 ) {
+        			return d3.rgb( "#FFFFFF" );
+        		}
+        		
+        		if ( facets[p.yfacet][p.ylab].isSelected ) {
+        			console.log( p.yfacet + "  " + xPivot  );
+        			
+        			if ( !hasSelection( facets, xPivot ) ) {
+						return( selectedColor(p.count) );        			        				
+        			}
+        		}
+        		
+        		if ( facets[p.xfacet][p.xlab].isSelected ) {
+        			if ( !hasSelection( facets, yPivot ) ) {
+						return( selectedColor(p.count) );        			        				
+        			}
         		} 
-    			return( c(p.count) );
+
+        		if ( facets[p.xfacet][p.xlab].isSelected && facets[p.yfacet][p.ylab].isSelected ) {
+					return( selectedColor(p.count) );        			        				
+        		} 
+
+    			if ( !hasSelection( facets, xPivot ) && !hasSelection( facets, yPivot ) ) {
+					return( selectedColor(p.count) );        			        				
+    			}
+        		
+				return ( defaultColor(p.count) );        				
         	})
         .on("mouseover", mouseover)
         .on("mouseout", mouseout)
         .on("click", function(p) {
-        	//console.log( p.xlab + " -- " + p.ylab );
-        	//d3.select( this ).style("fill", "#f00" );
         	if ( facets[p.xfacet][p.xlab].isSelected && facets[p.yfacet][p.ylab].isSelected ) {
         		// cell is selected
         		facets[p.yfacet][p.ylab].isSelected = false;
@@ -274,5 +327,23 @@ var svg = d3.select(document.getElementById(elemid)).append("svg")
     t.selectAll(".column")
         //.delay(function(d, i) { return x(i) * 4; })
         .attr("transform", function(d, i) { return "translate(" + x(i) + ",0)rotate(-90)"; });
+  }
+  
+  
+  /*
+   * Returns true if at least one attribute has been selected in the pivot facet, false otherwise. 
+   */
+  function hasSelection( facets, pivot ) {
+		var facet = facets[pivot];
+		  		
+  		for ( facetValue in facet ) {
+  			if ( facet.hasOwnProperty( facetValue ) ) {
+  				if ( facet[facetValue].isSelected ) {
+	  				return ( true );  					
+  				}
+  			}
+  		}
+  		
+  		return ( false );  				
   }
 };
