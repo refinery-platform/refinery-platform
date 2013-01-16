55a1eaab1650c7293b05430abc45cdb806b3c0ed
Psalm Haseley <psm3426@gmail.com>
2013-01-16 11:22:03 -0500

    converted all arguments to options

--- refinery/data_set_manager/management/commands/process_metadata_table.py
+++ refinery/data_set_manager/management/commands/process_metadata_table.py
@@ -4,6 +4,7 @@ from data_set_manager.single_file_column_parser import SingleFileColumnParser
 from data_set_manager.tasks import create_dataset, annotate_nodes
 from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand, CommandError
+from optparse import make_option
 import os
 import re
 import time
@@ -12,32 +13,98 @@ class Command(BaseCommand):
     help = "Takes the directory of an ISA-Tab file as input, parses, and"
     help = "%s inputs it into the database" % help
 
+    option_list = BaseCommand.option_list + (
+                make_option('--username',
+                            action='store',
+                            type='string'
+                            ),
+                make_option('--title',
+                            action='store',
+                            type='string'
+                            ),
+                make_option('--file_name',
+                            action='store',
+                            type='string'
+                            ),
+                make_option('--source_column_index',
+                            action='store',
+                            type='string'
+                            ),
+                make_option('--data_file_column',
+                            action='store',
+                            type='string'
+                            ),
+                make_option('--auxiliary_file_column',
+                            action='store',
+                            type='string'
+                            ),
+                make_option('--base_path',
+                            action='store',
+                            type='string',
+                            default=""
+                            ),
+                make_option('--slug',
+                            action='store',
+                            type='string',
+                            default=None
+                            ),
+                make_option('--species_column',
+                            action='store',
+                            type='string',
+                            default=None
+                            ),
+                make_option('--annotation_column',
+                            action='store',
+                            type='string',
+                            default=None
+                            ),
+                make_option('--genome_build_column',
+                            action='store',
+                            type='string',
+                            default=None
+                            ),
+                make_option('--data_file_permanent',
+                            action='store_true',
+                            default=False
+                            ),
+                make_option('--is_public',
+                            action='store_true',
+                            default=False
+                            ),
+                )
 
     """
     Name: handle
     Description:
         main program; calls the parsing and insertion functions
     """   
-    def handle(self, username, title, file_name, source_column_index, data_file_column, auxiliary_file_column, base_path="", slug=None, species_column=None, annotation_column=None, genome_build_column=None, data_file_permanent=False, is_public=False, **options):    
+    def handle(self, *args, **options):
+        
+        required = ['username', 'title', 'file_name', 'source_column_index', 'data_file_column', 'auxiliary_file_column']
+        for arg in required:
+            if not options[arg]:
+                raise CommandError('%s was not provided.' % arg)
+        
+        
         parser = SingleFileColumnParser()
-        parser.file_permanent = data_file_permanent
-        parser.file_column_index = int( data_file_column )
-        parser.auxiliary_file_column_index = int( auxiliary_file_column )
-        parser.source_column_index = [int(x.strip()) for x in source_column_index.split(",")]
+        parser.file_permanent = options['data_file_permanent']
+        parser.file_column_index = int( options['data_file_column'] )
+        parser.auxiliary_file_column_index = int( options['auxiliary_file_column'] )
+        parser.source_column_index = [int(x.strip()) for x in options['source_column_index'].split(",")]
         parser.column_index_separator = "/"
-        parser.file_base_path = base_path
+        parser.file_base_path = options['base_path']
         
-        if species_column is not None:
-            parser.species_column_index = int( species_column )
+        if options['species_column'] is not None:
+            parser.species_column_index = int( options['species_column'] )
         
-        if genome_build_column is not None:
-            parser.genome_build_column_index = int( genome_build_column )                
+        if options['genome_build_column'] is not None:
+            parser.genome_build_column_index = int( options['genome_build_column'] )                
         
-        if annotation_column is not None:
-            parser.annotation_column_index = int( annotation_column )                
+        if options['annotation_column'] is not None:
+            parser.annotation_column_index = int( options['annotation_column'] )                
         
-        investigation = parser.run(file_name)
-        investigation.title = title
+        investigation = parser.run(options['file_name'])
+        investigation.title = options['title']
         investigation.save()
         
-        create_dataset( investigation.uuid, username, dataset_title=title, slug=slug, public=True )
+        create_dataset( investigation.uuid, options['username'], dataset_title=options['title'], slug=options['slug'], public=options['is_public'] )
