7d84bb6116f24f585f6ac6b900ae8723a4ae25c9
Nils Gehlenborg <nils@gehlenborg.com>
2013-02-12 11:45:20 -0800

    Added document table for solr.

--- refinery/static/js/refinery/solr/solr_document_table.js
+++ refinery/static/js/refinery/solr/solr_document_table.js
@@ -16,7 +16,7 @@
  */
 
 
-DataSetSolrTable = function( parentElementId, idPrefix, solrQuery, solrClient, configurator ) {
+SolrDocumentTable = function( parentElementId, idPrefix, solrQuery, solrClient, configurator ) {
   	
   	var self = this;
 	
@@ -27,44 +27,127 @@ DataSetSolrTable = function( parentElementId, idPrefix, solrQuery, solrClient, c
   	self.idPrefix = idPrefix;
   	
   	// Solr interaction 
-  	self.solrQuery = solrQuery;
-  	self.solrClient = solrClient;
+  	self.query = solrQuery;
+  	self.client = solrClient;
   	
   	// data set configuration
   	self.configurator = configurator;  	
 };	
 	
 	
-DataSetSolrTable.prototype.initialize = function( callback ) {
+SolrDocumentTable.prototype.initialize = function() {
 	var self = this;
 
-	solrClient.initializeDataSetQuery( self.solrQuery, self.configurator, function(query) {
-		console.log( query );
-		solrClient.executeDataSetQuery( query, DATA_SET_FULL_QUERY, 0, 10, function( data ) {
-			console.log( data );
-			var serializedQuery = query.serialize( DATA_SET_QUERY_NODE_SET_SERIALIZATION ); 
-			console.log( serializedQuery ); 
-			console.log( query.deserialize( serializedQuery ) ); 
-		});
-	});		
-
-	return null;	
+	return this;	
 };
 	
 	
 /*
  * Render the user interface components into element defined by self.elementId.
  */
-DataSetSolrTable.prototype.render = function () {
+SolrDocumentTable.prototype.render = function ( solrResponse ) {
 	var self = this;
 
 	// clear parent element
 	$( "#" + self.parentElementId ).html("");
 	
-	var code = "";
-
-	$( "#" + self.elementId ).html( code );		
+	var code = "Documents = " + solrResponse.getDocumentList().length;
+	
+	console.log( solrResponse.getDocumentList() );
+	
+	self._renderTable( solrResponse );
+	
+	//$( "#" + self.parentElementId ).html( code );		
 	
 	// attach event listeners
 	// ..
 };
+
+SolrDocumentTable.prototype._renderTable = function( solrResponse ) {
+	
+	var self = this;
+	
+	var tableHead = self._generateTableHead( solrResponse );
+	var tableBody = self._generateTableBody( solrResponse );
+	
+	console.log( "th" + tableHead );	
+
+	$('<table/>', {	
+		'class': 'table table-striped table-condensed',
+		'id': 'table_matrix',
+		'html': tableHead + "<tbody>" + tableBody + "</tbody>"
+	}).appendTo('#' + self.parentElementId );	
+}
+	
+	
+SolrDocumentTable.prototype._generateTableBody = function( solrResponse ) {
+	
+	var self = this;
+	
+	var documents = solrResponse.getDocumentList();
+	var fields = self.query._fields; //solrResponse.getFieldList();
+	
+	var rows = [];
+	
+	for ( var i = 0; i < documents.length; ++i ) {		
+		var document = documents[i];
+		
+		var s = "<tr>";
+		
+		var isDocumentSelected = self.query.isDocumentSelected( document.uuid ); 
+		
+		s += '<td><label><input id="document_' + document.uuid + '" data-uuid="' + document.uuid + '" class="document-selection-checkbox" type=\"checkbox\" ' + ( isDocumentSelected ? "checked" : "" ) + '></label>' + '</td>';							
+
+		for ( entry in fields )
+		{
+			if ( fields.hasOwnProperty( entry ) && fields[entry].isVisible && !fields[entry].isInternal ) {
+				if ( document.hasOwnProperty( entry ) ) //&& !( hiddenFieldNames.indexOf( entry ) >= 0 ) )
+				{
+					s += "<td>";
+					s += document[entry];
+					s += "</td>";				
+				}
+				else // this field does not exist in this result
+				{
+					s += "<td>";
+					s += ""
+					s += "</td>";								
+				}				
+			}
+		}
+		s += "</tr>";
+		rows.push( s );
+    }	
+		
+	return rows.join('\n');
+}
+
+
+SolrDocumentTable.prototype._generateTableHead = function( solrResponse ) {
+	
+	var self = this;
+	
+	var row = [];
+	
+	var fields = self.query._fields;
+	
+	console.log( fields );
+
+	row.push( '<th align="left" width="0" id="node-selection-column-header"></th>' );	
+		
+	for ( field in fields ) {
+		if ( fields.hasOwnProperty( field ) ) {
+			if ( fields[field].isVisible ) {
+				if ( fields[field].direction === "asc" ) {
+					row.push("<th align=left id=\"" + composeFieldNameId( field + "___header" ) + "\"><i class=\"icon-arrow-down\">&nbsp;" + prettifySolrFieldName( field, true ) + "</th>" );				
+				} else if ( fields[field].direction === "desc" ) {
+					row.push("<th align=left id=\"" + composeFieldNameId( field + "___header" ) + "\"><i class=\"icon-arrow-up\">&nbsp;" + prettifySolrFieldName( field, true ) + "</th>" );				
+				} else {
+					row.push("<th align=left id=\"" + composeFieldNameId( field + "___header" ) + "\">" + prettifySolrFieldName( field, true ) + "</th>" );									
+				}
+			}
+		}
+	}
+
+	return "<thead><tr>" + row.join("") + "</tr></thead>";	
+}
