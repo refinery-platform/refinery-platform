1426607257a5d0a00adf641478b57df64e75562c
Nils Gehlenborg <nils@gehlenborg.com>
2013-03-24 21:22:46 -0400

    [Fixes #46748161] "description" no longer a required field for Connection model.

--- refinery/galaxy_connector/models.py
+++ refinery/galaxy_connector/models.py
@@ -5,7 +5,7 @@ class Instance( models.Model ):
     data_url = models.CharField( max_length=100, default="datasets" )
     api_url = models.CharField( max_length=100, default="api" )
     api_key = models.CharField( max_length=50 )
-    description = models.CharField( max_length=500 )
+    description = models.CharField( max_length=500, null=True, blank=True, default="" )
     local_download = models.BooleanField( default=False )
 
     def __unicode__(self):
