76355c7e94f3d6956e3d619259a1d4473da1cca2
Nils Gehlenborg <nils@gehlenborg.com>
2013-03-30 23:02:14 -0400

    [Fixes #47173007] Added "request" template processor to settings to get login redirection already implemented in base template working correctly.

--- refinery/settings.py
+++ refinery/settings.py
@@ -125,6 +125,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.contrib.auth.context_processors.auth',
     'django.contrib.messages.context_processors.messages',
     "core.context_processors.extra_context",
+    "django.core.context_processors.request",    
 )
 
 MIDDLEWARE_CLASSES = (
