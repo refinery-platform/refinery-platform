e3957ac34404ff09cb0a94338345b2fb5c3ab773
Ilya Sytchev <isytchev@hsph.harvard.edu>
2013-08-12 19:10:37 -0400

    [#54878440] Add/change default values to data directory variables in settings.py.  Add Puppet functionality to create data directories, Django project initialization commands, bug fixes and code cleanup.

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,24 +1,25 @@
 $appuser = "vagrant"
-
-#exec { 'apt-get update':
-#  path => '/usr/bin',
-#}
-#Package { require => Exec['apt-get update'] }
+$virtualenv = "/home/${appuser}/.virtualenvs/refinery-platform"
+$venvpath = "${virtualenv}/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/opt/vagrant_ruby/bin"
+$requirements = "/vagrant/requirements.txt"
 
 #TODO: peg packages to specific versions
-package { 'build-essential': }
-package { 'libncurses5-dev': }
-package { 'g++': }
-package { 'libfreetype6': }  # required by matplotlib
-package { 'libfreetype6-dev': }  # required by matplotlib
-package { 'libpng12-dev': }  # required by matplotlib
-package { 'libldap2-dev': }
-package { 'libsasl2-dev': }
+class venvdeps {
+  package { 'build-essential': }
+  package { 'libncurses5-dev': }
+  package { 'libfreetype6': }      # required by matplotlib
+  package { 'libfreetype6-dev': }  # required by matplotlib
+  package { 'libpng12-dev': }      # required by matplotlib
+  package { 'libldap2-dev': }
+  package { 'libsasl2-dev': }
+  package { 'postgresql-server-dev-all': }
+}
+include venvdeps
+
 package { 'postgresql': }
-package { 'postgresql-server-dev-all': }
-package { 'rabbitmq-server': }
-package { 'virtualenvwrapper': }
-package { 'solr-jetty': }
+#package { 'virtualenvwrapper': }
+#package { 'rabbitmq-server': }
+#package { 'solr-jetty': }
 
 class { 'postgresql':
   charset => 'UTF8',
@@ -38,34 +39,65 @@ class { 'python':
   dev => true,
   virtualenv => true,
 }
-
-$virtualenv = "/home/${appuser}/.virtualenvs/refinery-platform"
-$requirements = "/vagrant/requirements.txt"
-
 # create virtualenv
 python::virtualenv { $virtualenv:
   ensure => present,
-#  requirements => $requirements,
+  #requirements => $requirements,  # creates a dependency cycle
   owner => $appuser,
   group => $appuser,
 }
 ->
 # a workaround for a bug in matplotlib installation
+# python::pip doesn't work because it creates a dependency cycle
 exec { "numpy":
   command => "pip install numpy==1.7.0",
-  path => "/home/${appuser}/.virtualenvs/refinery-platform/bin:/usr/bin:/bin",
+  path => $venvpath,
   user => $appuser,
   group => $appuser,
+  require => Class["venvdeps"],
 }
-# this doesn't work because it creates a dependency cycle
-#python::pip { 'numpy==1.7.0':
-#  virtualenv => $virtualenv,
-#  owner => 'vagrant',
-#}
 ~>
 # install packages from requirements.txt
 python::requirements { $requirements:
   virtualenv => $virtualenv,
   owner => 'vagrant',
   group => 'vagrant',
+}
+#exec { "requirements":
+#  command => "pip install -U -r ${requirements}",
+#  path => $venvpath,
+#  user => $appuser,
+#  group => $appuser,
+#}
+
+file { [ "/vagrant/media", "/vagrant/static", "/vagrant/isa-tab" ]:
+  ensure => directory,
+  owner => $appuser,
+  group => $appuser,
+}
+
+exec { "syncdb":
+  command => "/vagrant/refinery/manage.py syncdb --noinput --all",
+  path => $venvpath,
+  user => $appuser,
+  group => $appuser,
+  require => [
+               File["/vagrant/media"],
+               Python::Requirements[$requirements],
+               Postgresql::Db["refinery"]
+             ],
+}
+->
+exec { "migrate":
+  command => "/vagrant/refinery/manage.py migrate --fake",
+  path => $venvpath,
+  user => $appuser,
+  group => $appuser,
+}
+->
+exec { "init":
+  command => "/vagrant/refinery/manage.py init_refinery 'Refinery' 'localhost:8000'",
+  path => $venvpath,
+  user => $appuser,
+  group => $appuser,
 }
