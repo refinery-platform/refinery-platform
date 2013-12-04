f3e579144914926327a81d75cfd929d49872301b
Ilya Sytchev <isytchev@hsph.harvard.edu>
2013-12-04 18:18:15 -0500

    Initial commit of the framework for the new UI.

---
+++ refinery/ui/Gruntfile.js
@@ -0,0 +1,26 @@
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            build: {
+                expand: true,
+                cwd: 'src/',
+                src: 'js/**/*.js',
+                dest: 'release/',
+                ext: '.min.js',
+            }
+        },
+    });
+
+    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    // Default task(s).
+    grunt.registerTask('default', ['uglify']);
+
+};
