d27c109a53ad75950827714d8382546616147735
Ilya Sytchev <isytchev@hsph.harvard.edu>
2013-12-05 14:33:20 -0500

    Add JSHint to the UI build.

--- refinery/ui/Gruntfile.js
+++ refinery/ui/Gruntfile.js
@@ -3,6 +3,9 @@ module.exports = function(grunt) {
     // Project configuration.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        jshint: {
+            files: ['Gruntfile.js', 'src/**/*.js']
+        },
         uglify: {
             options: {
                 banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
@@ -15,12 +18,15 @@ module.exports = function(grunt) {
                 ext: '.min.js',
             }
         },
+        clean: ['release'],
     });
 
     // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-clean');
 
     // Default task(s).
-    grunt.registerTask('default', ['uglify']);
+    grunt.registerTask('default', ['jshint', 'uglify']);
 
 };
