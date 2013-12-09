45de0b7dc22556063a1adf90dd99f98a5e37f06a
Ilya Sytchev <isytchev@hsph.harvard.edu>
2013-12-09 18:14:31 -0500

    Add ui-select2 module and grunt-contrib-copy package.  Refactor Gruntfile.js.

--- refinery/ui/Gruntfile.js
+++ refinery/ui/Gruntfile.js
@@ -1,37 +1,85 @@
 module.exports = function(grunt) {
 
-    // Project configuration.
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        jshint: {
-            files: ['Gruntfile.js', 'src/**/*.js']
-        },
-        uglify: {
-            options: {
-                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-            },
-            build: {
-                expand: true,
-                cwd: 'src/',
-                src: 'js/**/*.js',
-                dest: 'release/',
-                ext: '.min.js',
-            }
-        },
-        clean: ['release'],
-        watch: {
-            files: ['<%= jshint.files %>'],
-            tasks: ['jshint', 'uglify']
-        },
-    });
+  // Project configuration.
+  grunt.initConfig({
 
-    // Load the plugin that provides the "uglify" task.
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-contrib-watch');
+    source_dir: 'src',
+    release_dir: 'release',
 
-    // Default task(s).
-    grunt.registerTask('default', ['jshint', 'uglify']);
+    app_files: {
+      js: ['<%= source_dir %>/**/*.js'],
+      css: ['<%= source_dir %>/**/*.css'],
+    },
+    vendor_files: {
+      js: [
+        'select2/select2.js',
+        'jquery/jquery.js',
+        'angular/angular.js',
+        'angular-ui-select2/src/select2.js',        
+      ],
+      css: [
+        'select2/select2.css',
+      ],
+    },
+
+    pkg: grunt.file.readJSON('package.json'),
+
+    jshint: {
+      files: ['Gruntfile.js', '<%= app_files.js %>'],
+    },
+
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      files: {
+        expand: true,
+        cwd: 'src/',
+        src: 'js/**/*.js',
+        dest: '<%= release_dir %>/',
+        ext: '.min.js',
+      },
+    },
+
+    copy: {
+      app_assets: {
+        files: [
+          {
+            expand: true,
+            cwd: '<%= source_dir %>/',
+            src: ['**'],
+            dest: '<%= release_dir %>/',
+          },
+        ],
+      },
+      vendor_assets: {
+        files: [
+          {
+            expand: true,
+            cwd: 'bower_components/',
+            src: ['<%= vendor_files.js %>', '<%= vendor_files.css %>'],
+            dest: '<%= release_dir %>/vendor/',
+          },
+        ],
+      },
+    },
+
+    clean: ['<%= release_dir %>'],
+
+    watch: {
+      files: ['<%= jshint.files %>'],
+      tasks: ['jshint', 'uglify'],
+    },
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+
+  // Default task(s).
+  grunt.registerTask('default', ['jshint', 'uglify', 'copy']);
 
 };
