e0084435989153917f718d463f166e4a9847ff21
Ilya Sytchev <isytchev@hsph.harvard.edu>
2013-12-11 15:22:30 -0500

    Enable the workflow dropdown to retrieve list of workflows from the API.

--- refinery/ui/src/js/controllers/workflows.js
+++ refinery/ui/src/js/controllers/workflows.js
@@ -1,14 +1,6 @@
-'use strict';
-
 //refineryControllers.controller('WorkflowListCtrl', ['$scope', '$http',
 //  function WorkflowListCtrl($scope, $http) {
+//    'use strict';
 //    $http.get('api/v1/workflow').success(function(data) {
 //      $scope.workflows = data;
 //    });
-
-refineryControllers.controller('WorkflowListCtrl', function($scope) {
-  $scope.workflows = [
-    {"id": 1, "name": "Workflow 1"},
-    {"id": 2, "name": "Workflow 2"},
-  ];
-});
--- refinery/ui/src/js/refinery.js
+++ refinery/ui/src/js/refinery.js
@@ -1,13 +1,41 @@
-'use strict';
-
 /* App Module */
 
-var refineryAppModule = angular.module('refineryApp', [
+var refineryApp = angular.module('refineryApp', [
   'ui.select2',
   'ngResource',
-  'refineryControllers',
+//  'refineryControllers',
 ]);
 
 /* Controllers */
 
-var refineryControllers = angular.module('refineryControllers', []);
+//var refineryControllers = angular.module('refineryControllers', []);
+
+refineryApp.factory('Workflows', function() {
+  'use strict';
+  return [
+      {"id": 1, "name": "Workflow 1"},
+      {"id": 2, "name": "Workflow 2"},
+  ];
+});
+
+refineryApp.controller('WorkflowListCtrl', function($scope, Workflows) {
+  'use strict';
+  $scope.workflows = Workflows;
+});
+
+//var service = angular.module("apiService", ["ngResource"]);
+
+refineryApp.factory("Workflow", function($resource) {
+  return $resource(
+    "/api/v1/workflow/:Id/",
+    {Id: "@Id", format: "json"},
+    {'query':  {method:'GET', isArray: false}}
+  );
+});
+
+refineryApp.controller('WorkflowListApiCtrl', function($scope, Workflow) {
+  'use strict';
+  var WorkflowList = Workflow.query(function() {
+    $scope.workflows = WorkflowList.objects;
+  });
+});
