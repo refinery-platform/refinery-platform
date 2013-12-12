8ceb5936a07c7e3dfbc6609ec3373f2e6d5b426d
Ilya Sytchev <isytchev@hsph.harvard.edu>
2013-12-12 15:01:05 -0500

    Cleanup refinery.js and add functionality to display input relationship data for a selected workflow.

--- refinery/ui/src/js/refinery.js
+++ refinery/ui/src/js/refinery.js
@@ -1,32 +1,12 @@
-/* App Module */
-
 angular.module('refineryApp', [
   'ui.select2',
   'ngResource',
 //  'refineryControllers',
 ])
 
-/* Controllers */
-
-//var refineryControllers = angular.module('refineryControllers', []);
-
-.factory('Workflows', function() {
-  'use strict';
-  return [
-      {"id": 1, "name": "Workflow 1"},
-      {"id": 2, "name": "Workflow 2"},
-  ];
-})
-
-.controller('WorkflowListCtrl', function($scope, Workflows) {
-  'use strict';
-  $scope.workflows = Workflows;
-})
-
-//var service = angular.module("apiService", ["ngResource"]);
-
 .factory("Workflow", function($resource) {
   'use strict';
+
   return $resource(
     "/api/v1/workflow/:Id/",
     {Id: "@Id", format: "json"}
@@ -35,7 +15,11 @@ angular.module('refineryApp', [
 
 .controller('WorkflowListApiCtrl', function($scope, Workflow) {
   'use strict';
-  var WorkflowList = Workflow.get(function() {
-    $scope.workflows = WorkflowList.objects;
+
+  var Workflows = Workflow.get(function() {
+    $scope.workflowList = Workflows.objects;
   });
 });
+
+//var refineryControllers = angular.module('refineryControllers', []);
+//var service = angular.module("apiService", ["ngResource"]);
