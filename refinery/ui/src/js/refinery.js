baebb21346e156e71010ee30c257a32e690a0bfb
Ilya Sytchev <isytchev@hsph.harvard.edu>
2013-12-12 00:06:27 -0500

    Change syntax for registering providers to elminate superfluous variable and code duplication.

--- refinery/ui/src/js/refinery.js
+++ refinery/ui/src/js/refinery.js
@@ -1,39 +1,39 @@
 /* App Module */
 
-var refineryApp = angular.module('refineryApp', [
+angular.module('refineryApp', [
   'ui.select2',
   'ngResource',
 //  'refineryControllers',
-]);
+])
 
 /* Controllers */
 
 //var refineryControllers = angular.module('refineryControllers', []);
 
-refineryApp.factory('Workflows', function() {
+.factory('Workflows', function() {
   'use strict';
   return [
       {"id": 1, "name": "Workflow 1"},
       {"id": 2, "name": "Workflow 2"},
   ];
-});
+})
 
-refineryApp.controller('WorkflowListCtrl', function($scope, Workflows) {
+.controller('WorkflowListCtrl', function($scope, Workflows) {
   'use strict';
   $scope.workflows = Workflows;
-});
+})
 
 //var service = angular.module("apiService", ["ngResource"]);
 
-refineryApp.factory("Workflow", function($resource) {
+.factory("Workflow", function($resource) {
   return $resource(
     "/api/v1/workflow/:Id/",
     {Id: "@Id", format: "json"},
-    {'query':  {method:'GET', isArray: false}}
+    {'query': {method: 'GET', isArray: false}}
   );
-});
+})
 
-refineryApp.controller('WorkflowListApiCtrl', function($scope, Workflow) {
+.controller('WorkflowListApiCtrl', function($scope, Workflow) {
   'use strict';
   var WorkflowList = Workflow.query(function() {
     $scope.workflows = WorkflowList.objects;
