af288474ee57ff2f135fe58192b1bdb7222a20c9
Ilya Sytchev <isytchev@hsph.harvard.edu>
2014-01-07 15:34:57 -0500

    Add NodeSet dropdown.  Code cleanup.

--- refinery/ui/src/js/refinery.js
+++ refinery/ui/src/js/refinery.js
@@ -20,19 +20,38 @@ angular.module('refineryApp', [
     $scope.workflowList = Workflows.objects;
   });
 
-  $scope.workflowSelected = function() {
-    return $scope.index === '0' ? true : $scope.index;
-    // return $scope.index;
+  $scope.isWorkflowSelected = function() {
+    return $scope.workflowIndex === '0' ? true : $scope.workflowIndex;
   };
 
   $scope.getWorkflowInputRelationship = function() {
-    if ($scope.workflowList && $scope.index) {
-      return $scope.workflowList[$scope.index].input_relationships[0];
+    if ($scope.workflowList && $scope.workflowIndex) {
+      return $scope.workflowList[$scope.workflowIndex].input_relationships[0];
     } else {
       return '';
     }
   };
 
+  $scope.isWorkflowSingleInput = function() {
+    return $scope.getWorkflowInputRelationship().set2 ? false : true;
+  };
+})
+
+.factory("NodeSet", function($resource) {
+  'use strict';
+
+  return $resource(
+    "/api/v1/nodeset/:Id/",
+    {Id: "@Id", format: "json"}
+  );
+})
+
+.controller('NodeSetListApiCtrl', function($scope, NodeSet) {
+  'use strict';
+
+  var NodeSets = NodeSet.get(function() {
+    $scope.nodesetList = NodeSets.objects;
+  });
 });
 
 //var refineryControllers = angular.module('refineryControllers', []);
