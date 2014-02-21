ceec4a51b473c9085ae104a858b35786487727f4
Ilya Sytchev <isytchev@hsph.harvard.edu>
2014-02-21 15:20:35 -0500

    Implement workflow AngularJS service.

--- refinery/ui/src/js/analysis.js
+++ refinery/ui/src/js/analysis.js
@@ -1,39 +1,43 @@
 angular.module('refineryAnalysis', [])
 
-.controller('AnalysisCtrl', function($scope, $rootScope, $http, $window, analysisConfig) {
+.controller('AnalysisCtrl', function($scope, $rootScope, $http, $window, $log, workflow) {
   'use strict';
 
-  // FIXME: workflowChangedEvent is not received when a workflow is selected
-  // for the very first time
-  // $scope.$onRootScope('workflowChangedEvent', function(event, currentWorkflow) {
-    // console.log("Updating workflow UUID");
-    // analysisConfig.workflowUuid = currentWorkflow.uuid;  
-  // });
-  // $scope.$onRootScope('nodeRelationshipChangedEvent', function(event, currentNodeRelationship) {
-    // $scope.currentNodeRelationship = currentNodeRelationship;
-  // });
+  $scope.analysisConfig = {
+    studyUuid: $window.externalStudyUuid,
+    workflowUuid: null,
+    nodeSetUuid: null,
+    nodeRelationshipUuid: null
+  };
+
+  $scope.$onRootScope('nodeSetChangedEvent', function(event, currentNodeSet) {
+    $scope.analysisConfig.nodeSetUuid = currentNodeSet.uuid;
+    $scope.analysisConfig.nodeRelationshipUuid = null;
+    $log.debug("new nodeset: " + $scope.analysisConfig.nodeSetUuid);
+  });
+  $scope.$onRootScope('nodeRelationshipChangedEvent', function(event, currentNodeRelationship) {
+    $scope.analysisConfig.nodeRelationshipUuid = currentNodeRelationship.uuid;   
+    $scope.analysisConfig.nodeSetUuid = null;
+    $log.debug("new noderel: " + $scope.analysisConfig.nodeRelationshipUuid);
+  });
 
   $scope.launchAnalysis = function() {
+    $scope.analysisConfig.workflowUuid = workflow.getUuid();
+    $log.debug("new workflow: " + $scope.analysisConfig.workflowUuid);
+
     $http({
       method: 'POST',
       url: '/analysis_manager/run/',
       headers: {'X-Requested-With': 'XMLHttpRequest'},
-      data: analysisConfig,
+      data: $scope.analysisConfig,
     }).success(function(response) {
+      $log.debug("Launching analysis with config:");
+      $log.debug("Workflow: " + $scope.analysisConfig.workflowUuid);
+      $log.debug("NodeSET: " + $scope.analysisConfig.nodeSetUuid);
+      $log.debug("NodeREL: " + $scope.analysisConfig.nodeRelationshipUuid);
       $window.location.assign(response);
     }).error(function(response, status) {
-      console.log("Request failed: error " + status);
+      $log.debug("Request failed: error " + status);
     });
-  };
-})
-
-.factory('analysisConfig', function() {
-  'use strict';
-
-  return {
-    studyUuid: externalStudyUuid,
-    workflowUuid: null,
-    nodeSetUuid: null,
-    nodeRelationshipUuid: null
   };
 });
