7160bce8f0752c40c1c534675932e91eb244a82f
Ilya Sytchev <isytchev@hsph.harvard.edu>
2015-03-09 17:05:43 -0400

    Postpone file import tasks until metadata parsing is finished (#466)

--- refinery/data_set_manager/single_file_column_parser.py
+++ refinery/data_set_manager/single_file_column_parser.py
@@ -180,6 +180,8 @@ class SingleFileColumnParser:
         logger.debug("Parsing with file column %s and auxiliary file column %s",
                      self.file_column_index, self.auxiliary_file_column_index)
 
+        # UUIDs of data files to postpone importing until parsing is finished
+        data_files = []
         # iterate over non-header rows in file
         for row in self.metadata_reader:
             #TODO: resolve relative indices
@@ -192,7 +194,7 @@ class SingleFileColumnParser:
                 row[self.file_column_index])
             data_file_uuid = create(
                 source=data_file_path, permanent=self.file_permanent)
-            import_file.delay(data_file_uuid)
+            data_files.append(data_file_uuid)
 
             # add auxiliary file to file store
             if self.auxiliary_file_column_index:
@@ -200,29 +202,26 @@ class SingleFileColumnParser:
                     row[self.auxiliary_file_column_index])
                 auxiliary_file_uuid = create(
                     source=auxiliary_file_path, permanent=self.file_permanent)
-                import_file.delay(auxiliary_file_uuid)
+                data_files.append(auxiliary_file_uuid)
             else:
                 auxiliary_file_uuid = None
 
             # add files to file server
             #TODO: add error handling in case of None values for UUIDs
-            file_server.models.add(data_file_uuid, auxiliary_file_uuid);
+            file_server.models.add(data_file_uuid, auxiliary_file_uuid)
 
             # create nodes if file was successfully created
-            
             # source node
             source_name = self._create_name(
                 row, internal_source_column_index, self.file_column_index)
             source_node, is_source_new = Node.objects.get_or_create(
                 study=study, name=source_name, type=Node.SOURCE)
-
             # sample node
             sample_name = self._create_name(
                 row, internal_sample_column_index, self.file_column_index)
             sample_node, is_sample_new = Node.objects.get_or_create(
                 study=study, name=sample_name, type=Node.SAMPLE)
             source_node.add_child(sample_node)
-
             # assay node
             assay_name = self._create_name(
                 row, internal_assay_column_index, self.file_column_index)
@@ -246,14 +245,17 @@ class SingleFileColumnParser:
                     self.auxiliary_file_column_index == column_index or
                     self.annotation_column_index == column_index):
                     continue
-
                 # create attribute as characteristic and attach to sample node
                 # if the sample node was newly created
                 if is_sample_new:
                     attribute = Attribute.objects.create(
                         node=sample_node, type=Attribute.CHARACTERISTICS,
                         subtype=self.headers[column_index].strip().lower(),
-                        value=row[column_index].strip() )             
+                        value=row[column_index].strip())
+
+        # kick off data file importing tasks
+        for uuid in data_files:
+            import_file.delay(uuid)
 
         return investigation
 
