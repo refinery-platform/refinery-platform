refinery-platform/refinery-platform

commit 9ea4eabc21db18a5a8e3abb1ab39bd84cdd979f1

Initial implementation of data file import monitoring (#466)

refinery/file_store/migrations/0002_auto__add_field_filestoreitem_import_task_id.py
@@ -1,0 +1,35 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'FileStoreItem.import_task_id'
+        db.add_column(u'file_store_filestoreitem', 'import_task_id',
+                      self.gf('django.db.models.fields.CharField')(default='', unique=True, max_length=36, blank=True),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Deleting field 'FileStoreItem.import_task_id'
+        db.delete_column(u'file_store_filestoreitem', 'import_task_id')
+
+
+    models = {
+        u'file_store.filestoreitem': {
+            'Meta': {'object_name': 'FileStoreItem'},
+            'datafile': ('django.db.models.fields.files.FileField', [], {'max_length': '1024', 'blank': 'True'}),
+            'filetype': ('django.db.models.fields.CharField', [], {'max_length': '15', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'import_task_id': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '36', 'blank': 'True'}),
+            'sharename': ('django.db.models.fields.CharField', [], {'max_length': '20', 'blank': 'True'}),
+            'source': ('django.db.models.fields.CharField', [], {'max_length': '1024'}),
+            'uuid': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '36', 'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['file_store']

refinery/core/admin.py
@@ -14,7 +14,6 @@ from django_extensions.admin import ForeignKeyAutocompleteAdmin
 from guardian.admin import GuardedModelAdmin
 
 
-
 class AnalysisNodeConnectionAdmin(ForeignKeyAutocompleteAdmin):
     raw_id_fields = ("node",)    
 
@@ -44,6 +43,9 @@ class DownloadAdmin(GuardedModelAdmin,ForeignKeyAutocompleteAdmin):
 
 class ExternalToolStatusAdmin(admin.ModelAdmin):
     readonly_fields = ('last_time_check', )
+
+class DataSetAdmin(admin.ModelAdmin):
+    readonly_fields = ('uuid',)
 
 admin.site.register(UserProfile)
 admin.site.register(ExtendedGroup)

refinery/data_set_manager/single_file_column_parser.py
@@ -11,7 +11,7 @@ import tempfile
 from annotation_server.models import species_to_taxon_id, Taxon
 from data_set_manager.models import Investigation, Study, Node, Attribute, Assay
 from data_set_manager.tasks import create_dataset
-from file_store.models import generate_file_source_translator
+from file_store.models import FileStoreItem, generate_file_source_translator
 from file_store.tasks import create, import_file
 
 
@@ -170,10 +170,9 @@ class SingleFileColumnParser:
                     self.metadata_file.name)
         #FIXME: this will not create a FileStoreItem if self.metadata_file does
         # not exist on disk (e.g., a file object like TemporaryFile)
-        investigation.pre_isarchive_file = create(self.metadata_file.name,
-                                                  permanent=True)
-        import_file(
-            investigation.pre_isarchive_file, refresh=True, permanent=True)
+        investigation.pre_isarchive_file = create(
+            self.metadata_file.name, permanent=True)
+        import_file(investigation.pre_isarchive_file, refresh=True)
         investigation.save()
 
         #TODO: test if there are fewer columns than required
@@ -192,6 +191,7 @@ class SingleFileColumnParser:
                 row[self.file_column_index])
             data_file_uuid = create(
                 source=data_file_path, permanent=self.file_permanent)
+            import_file.delay(data_file_uuid)
 
             # add auxiliary file to file store
             if self.auxiliary_file_column_index:
@@ -199,6 +199,7 @@ class SingleFileColumnParser:
                     row[self.auxiliary_file_column_index])
                 auxiliary_file_uuid = create(
                     source=auxiliary_file_path, permanent=self.file_permanent)
+                import_file.delay(auxiliary_file_uuid)
             else:
                 auxiliary_file_uuid = None
 

refinery/data_set_manager/urls.py
@@ -8,7 +8,7 @@ from django.conf.urls.defaults import patterns, url
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
 from data_set_manager.views import ImportISATabView, ProcessISATabView,\
-    ProcessMetadataTableView, CheckDataFilesView
+    ProcessMetadataTableView, CheckDataFilesView, CheckFileImportStatusView
 
 
 urlpatterns = patterns('data_set_manager.views',
@@ -33,4 +33,7 @@ urlpatterns = patterns('data_set_manager.views',
         name='process_metadata_table'),
     url(r'^import/check_files/$', CheckDataFilesView.as_view(),
         name='check_files'),
+    url(r'^import/check_file_status/(?P<data_set_uuid>[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})/$',
+        CheckFileImportStatusView.as_view(),
+        name='check_file_status'),
 )

refinery/data_set_manager/views.py
@@ -7,6 +7,7 @@ Created on May 11, 2012
 import os
 import simplejson as json
 from urlparse import urlparse
+from celery.result import AsyncResult
 from django import forms
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect, HttpResponse, \
@@ -235,7 +236,7 @@ class ProcessISATabView(View):
             logger.debug("Temp file name: '%s'", temp_file_path)
             dataset_uuid = parse_isatab.delay(request.user.username,
                                               False, temp_file_path).get()
-            #TODO: exception handling
+            #TODO: exception handling (OSError)
             os.unlink(temp_file_path)
             if dataset_uuid:
                 #TODO: redirect to the list of analysis samples for the given UUID
@@ -346,3 +347,37 @@ class CheckDataFilesView(View):
         # prefix output to protect from JSON vulnerability (stripped by Angular)
         return HttpResponse(")]}',\n" + json.dumps(bad_file_list),
                             content_type="application/json")
+
+class CheckFileImportStatusView(View):
+    """Check import status of files in a dataset
+
+    """
+    def get(self, request, data_set_uuid):
+        if not request.is_ajax():
+            return HttpResponseBadRequest()
+
+        try:
+            data_set = DataSet.objects.get(uuid=data_set_uuid)
+        except (DataSet.DoesNotExist, MultipleObjectsReturned):
+            logger.error("Dataset with UUID '%s' does not exist", data_set_uuid)
+            return HttpResponseBadRequest()
+
+        investigation = data_set.get_investigation()
+        # get list of file UUIDs in dataset
+        file_uuids = []
+        for study in investigation.study_set.all():
+            file_nodes = Node.objects.filter(
+                study=study.id, file_uuid__isnull=False)
+            file_uuids.extend(file_nodes.values_list('file_uuid', flat=True))
+        # make list of file UUIDs and corresponding import task IDs
+        files = list(FileStoreItem.objects.filter(
+            uuid__in=file_uuids).values('uuid', 'import_task_id'))
+        # replace task IDs with corresponding task states
+        for file_item in files:
+            result = AsyncResult(file_item['import_task_id'])
+            file_item['import_state'] = result.state
+            del file_item['import_task_id']
+        return HttpResponse(
+            # ")]}',\n" + json.dumps(dict(zip(file_uuids, file_import_states))),
+            json.dumps(files),
+            content_type="application/json")

refinery/file_store/admin.py
@@ -4,6 +4,8 @@ from file_store.models import FileStoreItem
 
 
 class FileStoreItemAdmin(admin.ModelAdmin):
+    readonly_fields = ('import_task_id',)
+
     def save_model(self, request, obj, form, change):
         '''Symlink if source is a local file
 

refinery/file_store/models.py
@@ -331,6 +331,7 @@ class FileStoreItem(models.Model):
     sharename = models.CharField(max_length=20, blank=True)
     #: type of the file
     filetype = models.CharField(max_length=15, choices=FILE_TYPES, blank=True)
+    import_task_id = UUIDField(unique=True, blank=True)
 
     objects = _FileStoreItemManager()
 

refinery/file_store/tasks.py
@@ -6,7 +6,8 @@ from urlparse import urlparse
 from tempfile import NamedTemporaryFile
 from celery.task import task
 from django.core.files import File
-from file_store.models import FileStoreItem, get_temp_dir, file_path, FILE_STORE_BASE_DIR
+from file_store.models import FileStoreItem, get_temp_dir, file_path,\
+    FILE_STORE_BASE_DIR
 
 
 logger = logging.getLogger('file_store')
@@ -30,6 +31,7 @@ def create(source, sharename='', filetype='', permanent=False, file_size=1):
     :returns: FileStoreItem UUID if success, None if failure.
 
     """
+    #TODO: move to file_store/models.py since it's never used as a task
     logger.info("Creating FileStoreItem using source '%s'", source)
 
     item = FileStoreItem.objects.create_item(
@@ -40,11 +42,7 @@ def create(source, sharename='', filetype='', permanent=False, file_size=1):
 
     logger.info("FileStoreItem created with UUID %s", item.uuid)
 
-    if permanent:
-        # copy to file store now and don't add to cache
-        #TODO: provide progress update
-        import_file.delay(
-            item.uuid, permanent=True, refresh=True, file_size=file_size)
+    #TODO: don't add to cache if permanent
 
     return item.uuid
 
@@ -68,6 +66,10 @@ def import_file(uuid, permanent=False, refresh=False, file_size=1):
     if not item:
         logger.error("FileStoreItem with UUID '%s' not found", uuid)
         return None
+
+    # save task ID for looking up file import status
+    item.import_task_id = import_file.request.id
+    item.save()
 
     # if file is ready to be used then return it,
     # otherwise delete it if update is requested
@@ -177,9 +179,7 @@ def import_file(uuid, permanent=False, refresh=False, file_size=1):
         # save the model instance
         item.save()
 
-    if not permanent:
-        #TODO: if permanent is False then add to cache
-        pass
+    #TODO: if permanent is False then add to cache
 
     return item
 

commit 4bd550a2925e0a21e02c44c679311e293b74624f

Bug fix to address IntegrityError during migration introduced in 9ea4eab (#466)

refinery/file_store/migrations/0002_auto__add_field_filestoreitem_import_task_id.py
@@ -10,7 +10,7 @@ class Migration(SchemaMigration):
     def forwards(self, orm):
         # Adding field 'FileStoreItem.import_task_id'
         db.add_column(u'file_store_filestoreitem', 'import_task_id',
-                      self.gf('django.db.models.fields.CharField')(default='', unique=True, max_length=36, blank=True),
+                      self.gf('django.db.models.fields.CharField')(default='', max_length=36, blank=True),
                       keep_default=False)
 
 
@@ -25,7 +25,7 @@ class Migration(SchemaMigration):
             'datafile': ('django.db.models.fields.files.FileField', [], {'max_length': '1024', 'blank': 'True'}),
             'filetype': ('django.db.models.fields.CharField', [], {'max_length': '15', 'blank': 'True'}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'import_task_id': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '36', 'blank': 'True'}),
+            'import_task_id': ('django.db.models.fields.CharField', [], {'max_length': '36', 'blank': 'True'}),
             'sharename': ('django.db.models.fields.CharField', [], {'max_length': '20', 'blank': 'True'}),
             'source': ('django.db.models.fields.CharField', [], {'max_length': '1024'}),
             'uuid': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '36', 'blank': 'True'})