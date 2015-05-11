cf2d1ef02efc2e2b5f04420349993016dd3998c0
Stefan Luger <lug.stefan@gmail.com>
2015-05-11 09:19:41 +0200

    'Merge' provenance visualization code from 'prov_vis_layer_integration_feature' branch.

--- refinery/ui/src/js/refinery.js
+++ refinery/ui/src/js/refinery.js
@@ -1,28 +1,28 @@
 angular.module('refineryApp', [
-  'refineryWorkflows',
-  'refineryNodeMapping',
-  'refineryAnalysis',
-  'refinerySolr',
-  'refineryExternalToolStatus',
-  'refineryNodeRelationship',
-  'refineryIgv',
-  'refineryStatistics',
-  'refineryMetadataTableImport',
-  'refinerySharing'
+    'refineryWorkflows',
+    'refineryNodeMapping',
+    'refineryAnalysis',
+    'refinerySolr',
+    'refineryExternalToolStatus',
+    'refineryNodeRelationship',
+    'refineryIgv',
+    'refineryStatistics',
+    'refineryMetadataTableImport',
+    'refineryProvvis'
 ])
 
-.config(['$provide', function($provide) {
-    // http://stackoverflow.com/questions/11252780/whats-the-correct-way-to-communicate-between-controllers-in-angularjs
-    $provide.decorator('$rootScope', ['$delegate', function($delegate){
+    .config(['$provide', function ($provide) {
+        // http://stackoverflow.com/questions/11252780/whats-the-correct-way-to-communicate-between-controllers-in-angularjs
+        $provide.decorator('$rootScope', ['$delegate', function ($delegate) {
 
-        Object.defineProperty($delegate.constructor.prototype, '$onRootScope', {
-            value: function(name, listener){
-                var unsubscribe = $delegate.$on(name, listener);
-                this.$on('$destroy', unsubscribe);
-            },
-            enumerable: false
-        });
+            Object.defineProperty($delegate.constructor.prototype, '$onRootScope', {
+                value: function (name, listener) {
+                    var unsubscribe = $delegate.$on(name, listener);
+                    this.$on('$destroy', unsubscribe);
+                },
+                enumerable: false
+            });
 
-        return $delegate;
+            return $delegate;
+        }]);
     }]);
-}]);
