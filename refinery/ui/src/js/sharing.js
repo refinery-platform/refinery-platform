dddd8be183d0ab67b74684a9a9fc7231393aa741
Anton Xue <antonxue1@gmail.com>
2015-05-22 17:35:04 -0400

    Fixed #487 and made changes to UI for shares for AnonymousUsers

--- refinery/ui/src/js/sharing.js
+++ refinery/ui/src/js/sharing.js
@@ -2,37 +2,25 @@ angular.module("refinerySharing", [])
 
 .controller("refinerySharingController", function ($scope, $http, $modal) {
     // Login check.
-    if (!user_id) {
-        // Hide project edit buttons if user is not logged in.
-        var pTable = document.getElementById('project-table');
-        var cells = pTable.getElementsByTagName('td');
-        // 3 cells per row. If user is not logged in, cannot edit sharing permissions.
-        for (var i = 0; i < cells.length; i += 3) {
-            cells[i+2].innerHTML = 'N/A';
-        }
+    if (user_id) {
+        $(document).ready(function () {
+            updateTableShareDisplay('dataset-table');
+            updateTableShareDisplay('project-table');
+        });
     }
 
-    // Ownership check. Assume icon-group presence indicates non-ownership.
-    // Data sets
-    var datasetTable = document.getElementById('dataset-table');
-    if (datasetTable) {
-        var datasetCells = datasetTable.getElementsByTagName('td');
-        for (var di = 0; di < datasetCells.length; di += 3) {
-            if (datasetCells[di].children[0].className === 'icon-group') {
-                datasetCells[di+2].innerHTML = 'N/A';
-           }
-       }
-    }
-
-    // Projects
-    var projectTable = document.getElementById('project-table');
-    if (projectTable) {
-        var projectCells = projectTable.getElementsByTagName('td');
-        for (var pi = 0; pi < projectCells.length; pi += 3) {
-            if (projectCells[pi].children[0].className === 'icon-group') {
-                projectCells[pi+2].innerHTML = 'N/A';
+    function updateTableShareDisplay(id) {
+        var table = document.getElementById(id);
+        if (table) {
+            cells = table.getElementsByTagName('td');
+            for (var i = 0; i < cells.length; i += 3) {
+                cells[i+2].children[0].style.display = 'inline';
+                // icon-group means that the user is not the owner.
+                if (cells[i].children[0].className === 'icon-group') {
+                    cells[i+2].children[0].innerText = 'N/A';
+                }
             }
-       }
+        }
     }
 
     function loadResource(api, uuid) {
@@ -40,38 +28,68 @@ angular.module("refinerySharing", [])
             var shareList = response.objects[0].shares;
             var pTable = document.getElementById('permission-table');
             for (var i = 0; i < shareList.length; i++) {
-                var readPerm = shareList[i].permissions.read;
-                var changePerm = shareList[i].permissions.change;
+                var canRead = shareList[i].permissions.read;
+                var canChange = shareList[i].permissions.change;
                 // Change implies read permission.
-                if (changePerm) {
-                    readPerm = true;
-                }
+                if (canChange) canRead = true;
                 
                 var row = pTable.insertRow(-1);
                 var group = row.insertCell(0);
                 group.innerHTML = shareList[i].name + '<div style="display: none;">' + shareList[i].id+ '</div>';
                 
-                var noPermDOM = row.insertCell(1);
-                var readOnlyDOM = row.insertCell(2);
-                var editDOM = row.insertCell(3);
+                var noPermCell = row.insertCell(1);
+                var readOnlyCell = row.insertCell(2);
+                var editCell = row.insertCell(3);
 
-                var isNoPerm = !(readPerm || changePerm)? 'checked' : '';
-                var isReadOnly = (readPerm && !changePerm)? 'checked' : '';
-                var isChangeAllowed = (changePerm)? 'checked': '';
+                var isNoPerm = !(canRead || canChange)? 'checked' : '';
+                var isReadOnly = (canRead && !canChange)? 'checked' : '';
+                var isChangeAllowed = (canChange)? 'checked': '';
                 
                 var noPermHTML = '<td><input type="radio" name="' + i + '" ' + isNoPerm + '></td>';
                 var readOnlyHTML = '<td><input type="radio" name="' + i + '" ' + isReadOnly + '></td>';
                 var changeAllowedHTML = '<td><input type="radio" name="' + i + '" ' + isChangeAllowed + '></td>';
 
-                noPermDOM.innerHTML = noPermHTML;
-                readOnlyDOM.innerHTML = readOnlyHTML;
-                editDOM.innerHTML = changeAllowedHTML;
+                noPermCell.innerHTML = noPermHTML;
+                readOnlyCell.innerHTML = readOnlyHTML;
+                editCell.innerHTML = changeAllowedHTML;
             }
         });
     }
+    
+    var permissionEditorController = function($scope, $http, $modalInstance, config) {
+        loadResource(config.api, config.uuid);
+        $scope.config = config;
+        
+        $scope.save = function () {
+            var pTable = document.getElementById('permission-table');
+            var cells = pTable.getElementsByTagName('td');
+            var data = '{"shares": [';
+            // Data is clustered into sets of 4 -- i is name, i+1 is noperm, i+2 is readonly, i+3 is edit.
+            for (var i = 0; i < cells.length; i += 4) {
+                // Add the group.
+                var name = cells[i].innerText;
+                var id = cells[i].children[0].innerText;
+                data += '{"name": "' + name + '", "id": ' + id + ', "permission": ';
+
+                var canRead = cells[i+2].children[0].checked || cells[i+3].children[0].checked;
+                var canChange = cells[i+3].children[0].checked;
+                
+                data += '{"read": ' + canRead + ', "change": ' + canChange + '}}';
+                if (i+5 < cells.length) {
+                    data += ', ';
+                }
+            }
+            data += ']}';
+            $http({method: 'PATCH', url: 'api/v1/' + config.api + '/' + config.uuid + '/', data: data});
+            $modalInstance.dismiss('saved');
+        };
+
+        $scope.cancel = function () {
+            $modalInstance.dismiss('cancel');
+        };
+    };
 
     $scope.openPermissionEditor = function (api, uuid) {
-        loadResource(api, uuid);
         $scope.newPermissionModalConfig = {
             title: 'Permission Editor'
         };
@@ -87,40 +105,6 @@ angular.module("refinerySharing", [])
                 }
             }
         });
-    };
-
-    var permissionEditorController = function($scope, $http, $modalInstance, config) {
-        $scope.config = config;
-        
-        $scope.save = function () {
-            var pTable = document.getElementById('permission-table');
-            var cells = pTable.getElementsByTagName('td');
-            var data = '{"shares": [';
-            // Data is clustered into sets of 4 -- i is name, i+1 is noperm, i+2 is readonly, i+3 is edit.
-            for (var i = 0; i < cells.length; i += 4) {
-                // Add the group.
-                var name = cells[i].innerText;
-                var id = cells[i].children[0].innerText;
-                data += '{"name": "' + name + '", "id": ' + id + ', "permission": ';
-
-                var read = cells[i+2].children[0].checked || cells[i+3].children[0].checked;
-                var change = cells[i+3].children[0].checked;
-                // var read = cellButtons[2].checked || cellButtons[3].checked;
-                // var change = cellButtons[3].checked;
-                
-                data += '{"read": ' + read + ', "change": ' + change + '}}';
-                if (i+5 < cells.length) {
-                    data += ', ';
-                }
-            }
-            data += ']}';
-            $http({method: 'PATCH', url: 'api/v1/' + config.api + '/' + config.uuid + '/', data: data});
-            $modalInstance.dismiss('saved');
-        };
-
-        $scope.cancel = function () {
-            $modalInstance.dismiss('cancel');
-        };
     };
 });
 
