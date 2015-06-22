9de2943c69b15f3e33df07b477f4e53d87e9202f
Nils Gehlenborg <nils@gehlenborg.com>
2015-06-22 15:58:09 -0400

    Mocking additional problematic packages.

--- docs/conf.py
+++ docs/conf.py
@@ -263,5 +263,5 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return Mock()
 
-MOCK_MODULES = ['python-ldap']
+MOCK_MODULES = ['python-ldap', 'pysolr']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
