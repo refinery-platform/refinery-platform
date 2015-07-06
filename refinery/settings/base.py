cddb8bc222c98c91e867bc3e12c1ffe039d3202b
Ilya Sytchev <isytchev@hsph.harvard.edu>
2015-07-06 17:49:04 -0400

    Remove unused logging formatter from Django settings

--- refinery/settings/base.py
+++ refinery/settings/base.py
@@ -183,11 +183,6 @@ ANONYMOUS_USER_ID = -1
 # (recommended way to extend Django user model)
 AUTH_PROFILE_MODULE = 'core.UserProfile'
 
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': True,
@@ -196,10 +191,6 @@ LOGGING = {
         'handlers': ['console'],
     },
     'formatters': {
-        'verbose': {
-            'format': '%(levelname)s %(asctime)s %(module)s %(process)d '
-                      '%(thread)d %(message)s'
-        },
         'default': {
             'format': '%(asctime)s %(levelname)-8s %(module)s %(funcName)s: '
                       '%(message)s',
