24779334d20fe39e562203f011c53d455e0a3be7
Ilya Sytchev <isytchev@hsph.harvard.edu>
2015-07-06 17:12:08 -0400

    Move USE_I18N and USE_L10N values from Django settings

--- refinery/settings/base.py
+++ refinery/settings/base.py
@@ -49,11 +49,11 @@ SITE_ID = 1
 
 # If you set this to False, Django will make some optimizations so as not
 # to load the internationalization machinery.
-USE_I18N = True
+USE_I18N = get_setting("USE_I18N")
 
 # If you set this to False, Django will not format dates, numbers and
 # calendars according to the current locale
-USE_L10N = True
+USE_L10N = get_setting("USE_L10N")
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
