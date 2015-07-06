554d0324071bc26fb66838253bb74facb407df02
Ilya Sytchev <isytchev@hsph.harvard.edu>
2015-07-06 17:21:40 -0400

    Move MEDIA_ROOT and MEDIA_URL values from Django settings

--- refinery/settings/base.py
+++ refinery/settings/base.py
@@ -57,12 +57,12 @@ USE_L10N = get_setting("USE_L10N")
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = '/vagrant/media'
+MEDIA_ROOT = get_setting("MEDIA_ROOT")
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = '/media/'
+MEDIA_URL = get_setting("MEDIA_URL")
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
