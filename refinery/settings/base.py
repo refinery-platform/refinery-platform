434bc33dad1761a9021cea7fc7377137a4529374
Ilya Sytchev <isytchev@hsph.harvard.edu>
2015-07-06 17:26:15 -0400

    Move STATIC_ROOT, STATIC_URL and ADMIN_MEDIA_PREFIX values from Django settings

--- refinery/settings/base.py
+++ refinery/settings/base.py
@@ -68,16 +68,16 @@ MEDIA_URL = get_setting("MEDIA_URL")
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/vagrant/static'
+STATIC_ROOT = get_setting("STATIC_ROOT")
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
+STATIC_URL = get_setting("STATIC_URL")
 
 # URL prefix for admin static files -- CSS, JavaScript and images.
 # Make sure to use a trailing slash.
 # Examples: "http://foo.com/static/admin/", "/static/admin/".
-ADMIN_MEDIA_PREFIX = '/static/admin/'
+ADMIN_MEDIA_PREFIX = get_setting("ADMIN_MEDIA_PREFIX")
 
 # List of finder classes that know how to find static files in
 # various locations.
