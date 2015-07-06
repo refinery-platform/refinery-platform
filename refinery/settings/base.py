19649dd9dc14dd9ecc478e693a80e079473e1a08
Ilya Sytchev <isytchev@hsph.harvard.edu>
2015-07-06 16:45:15 -0400

    Move TIME_ZONE value from Django settings

--- refinery/settings/base.py
+++ refinery/settings/base.py
@@ -41,14 +41,7 @@ DATABASES = get_setting("DATABASES")
 # transport://userid:password@hostname:port/virtual_host
 BROKER_URL = get_setting("BROKER_URL")
 
-# Local time zone for this installation. Choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-# although not all choices may be available on all operating systems.
-# On Unix systems, a value of None will cause Django to use the same
-# timezone as the operating system.
-# If running in a Windows environment this must be set to the same as your
-# system time zone.
-TIME_ZONE = 'America/New_York'
+TIME_ZONE = get_setting("TIME_ZONE")
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
