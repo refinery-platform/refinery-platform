b842686369ba6c4113979988be068a4ead88554a
Fritz Lekschas <service@lekschas.de>
2015-07-09 16:41:20 -0400

    Fixed DOI service and updated DOI and PubMed services to query Django middleware view in order to solve #555.

--- refinery/ui/src/js/commons/services/citation.js
+++ refinery/ui/src/js/commons/services/citation.js
@@ -2,9 +2,10 @@ angular
   .module('refineryApp')
   .factory('citationService', [
     '$q',
+    '_',
     'doiService',
     'pubmedService',
-    function ($q, doiService, pubmedService) {
+    function ($q, _, doiService, pubmedService) {
       var deferred = $q.defer();
 
       function get (reference) {
@@ -49,12 +50,61 @@ angular
             });
         }
 
-        var matches = this.value.match(/^(http:\/\/dx\.doi\.org\/)?(doi:\s?)?(10\.[0-9]+\/[a-zA-Z0-9\.\/]+)$/);
+        // Check if DOI
+        var matches = reference.match(/^(http:\/\/dx\.doi\.org\/)?(doi:\s?)?(10\.[0-9]+\/[a-zA-Z0-9\.\/]+)$/);
         if (matches !== null) {
-          // DOI
-          return doiService.get({
-            id: matches[3]
-          }).$promise;
+          // Query the DOI API
+          var doi = doiService
+            .get({
+              id: matches[3]
+            })
+            .$promise
+            .then(function (data) {
+              return prepareDoi(data);
+            })
+            .catch(function (error) {
+              return error;
+            });
+
+          // Look for a PubMed entry
+          var pubmed = pubmedService
+            .get(
+              'search',
+              {
+                term: doiService.escape(matches[3])
+              }
+            )
+            .then(function (data) {
+              return data.esearchresult.idlist[0];
+            })
+            .then(function (pubMedId) {
+              if (pubMedId) {
+                return pubmedService
+                  .get(
+                    'abstract',
+                    {
+                      id: pubMedId
+                    }
+                  )
+                  .then(function (data) {
+                    return preparePubmedAbstract(data);
+                  })
+                  .catch(function (error) {
+                    return error;
+                  });
+              }
+              return null;
+            })
+            .catch(function (error) {
+              return error;
+            });
+
+          return $q.all([doi, pubmed])
+            .then(mergeDoiPubmed)
+            .catch(function () {
+              // In case something failed we will return the DOI promise.
+              return doi;
+            });
         }
 
         return $q.reject(
@@ -73,6 +123,48 @@ angular
         } catch (e) {
           console.error(e);
         }
+      }
+
+      function mergeDoiPubmed (data) {
+        data[0].abstract = data[1];
+        return data[0];
+      }
+
+      function prepareDoi (data) {
+        return {
+          authors: prepareDoiAuthors(data),
+          abstract: null,
+          doi: data.DOI,
+          issue: data.issue,
+          journal: data['container-title'],
+          pages: data.page,
+          pubmed: null,
+          title: data.title,
+          volume: data.volume,
+          year: data.issued['date-parts'][0]
+        };
+      }
+
+      function prepareDoiAuthors (data) {
+        var authors = '';
+        for (var i = 0, len = data.author.length; i < len; i++) {
+          var name = data.author[i].family + ' ' + data.author[i].given.slice(0, -1);
+          switch (i) {
+            // Last author
+            case len - 1:
+              authors += name;
+              break;
+            // One before the last author
+            case len - 2:
+              authors += name + ' and ';
+              break;
+            // All other authors
+            default:
+              authors += name + ', ';
+              break;
+          }
+        }
+        return authors;
       }
 
       function preparePubmedAuthors (data) {
@@ -98,7 +190,19 @@ angular
 
       function preparePubmedAbstract (data) {
         try {
-          return data.PubmedArticleSet.PubmedArticle.MedlineCitation.Article.Abstract.AbstractText.toString();
+          var abstract = data
+            .PubmedArticleSet
+              .PubmedArticle
+                .MedlineCitation
+                  .Article
+                    .Abstract
+                      .AbstractText;
+
+          if (_.isString(abstract)) {
+            return abstract;
+          }
+
+          return _.isString(abstract['#text']) ? abstract['#text'] : '';
         } catch (e) {
           console.error(e);
           return null;
--- refinery/ui/src/js/commons/services/pubmed.js
+++ refinery/ui/src/js/commons/services/pubmed.js
@@ -4,259 +4,46 @@ angular
     function ($q, $http, $resource, x2js, settings) {
 
       /*
-       *
-       * Example:
-       *
+       * Search
        */
       var search = $resource(
-        'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&retmode=json&retmax=1&term=',
+        settings.appRoot + '/pubmed/search/:term',
         {
-          db: 'pubmed',
-          retmode: 'json',
-          retmax: 1
-        },
-        {
-          query: {
-            method: 'GET',
-            isArray: false,
-            headers: {
-              'Accept': 'application/json'
-            }
-          }
+          term: '@term'
         }
       );
 
       /*
        * Fecth full article
-       *
-       * Example:
-       * http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pubmed&retmode=XML&rettype=abstract&id=25344497
        */
       var abstract = $resource(
-        'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi',
+        settings.appRoot + '/pubmed/abstract/:id',
         {
-          db: 'pubmed',
-          retmode: 'xml',
-          rettype: 'abstract'
-        },
-        {
-          query: {
-            method: 'GET',
-            isArray: false,
-            headers: {
-              'Accept': 'text/xml'
-            }
-          }
+          id: '@id'
         }
       );
 
-      var abstractNew = {
-        get: function (id) {
-          var deferred = $q.defer();
-
-          $http({
-              headers: {
-                'Accept': 'text/xml'
-              },
-              method: 'GET',
-              params: {
-                db: 'pubmed',
-                id: id,
-                retmode: 'xml',
-                rettype: 'abstract'
-              },
-              url: 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi',
-              transformResponse:function(data) {
-                // convert the data to JSON and provide
-                // it to the success function below
-                var json = x2js.xml_str2json(data);
-                return json;
-              }
-            })
-            .then(function (response) {
-              deferred.resolve(response.data);
-            })
-            .catch(function (error) {
-              deferred.reject(error);
-            });
-
-          return deferred.promise;
-        }
-      };
-
       /*
        * Fetch basic meta data and summary
-       *
-       * Example:
-       * http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=pubmed&retmode=json&id=25344497
-       *
-       * {
-       *   "header": {
-       *     "type": "esummary",
-       *     "version": "0.3"
-       *   },
-       *   "result": {
-       *     "25344497": {
-       *       "uid": "25344497",
-       *       "pubdate": "2015 Mar 1",
-       *       "epubdate": "2014 Oct 24",
-       *       "source": "Bioinformatics",
-       *       "authors": [
-       *         {
-       *           "name": "Lekschas F",
-       *           "authtype": "Author",
-       *           "clusterid": ""
-       *         },
-       *         {
-       *           "name": "Stachelscheid H",
-       *           "authtype": "Author",
-       *           "clusterid": ""
-       *         },
-       *         {
-       *           "name": "Seltmann S",
-       *           "authtype": "Author",
-       *           "clusterid": ""
-       *         },
-       *         {
-       *           "name": "Kurtz A",
-       *           "authtype": "Author",
-       *           "clusterid": ""
-       *         }
-       *       ],
-       *       "lastauthor": "Kurtz A",
-       *       "title": "Semantic Body Browser: graphical exploration of an organism and spatially resolved expression data visualization.",
-       *       "sorttitle": "semantic body browser graphical exploration of an organism and spatially resolved expression data visualization ",
-       *       "volume": "31",
-       *       "issue": "5",
-       *       "pages": "794-6",
-       *       "lang": [
-       *         "eng"
-       *       ],
-       *       "nlmuniqueid": "9808944",
-       *       "issn": "1367-4803",
-       *       "essn": "1367-4811",
-       *       "pubtype": [
-       *         "Journal Article"
-       *       ],
-       *       "recordstatus": "PubMed - in process",
-       *       "pubstatus": "256",
-       *       "articleids": [
-       *         {
-       *           "idtype": "pii",
-       *           "idtypen": 4,
-       *           "value": "btu707"
-       *         },
-       *         {
-       *           "idtype": "doi",
-       *           "idtypen": 3,
-       *           "value": "10.1093/bioinformatics/btu707"
-       *         },
-       *         {
-       *           "idtype": "pubmed",
-       *           "idtypen": 1,
-       *           "value": "25344497"
-       *         },
-       *         {
-       *           "idtype": "eid",
-       *           "idtypen": 8,
-       *           "value": "25344497"
-       *         },
-       *         {
-       *           "idtype": "rid",
-       *           "idtypen": 8,
-       *           "value": "25344497"
-       *         }
-       *       ],
-       *       "history": [
-       *         {
-       *           "pubstatus": "aheadofprint",
-       *           "date": "2014/10/24 00:00"
-       *         },
-       *         {
-       *           "pubstatus": "entrez",
-       *           "date": "2014/10/26 06:00"
-       *         },
-       *         {
-       *           "pubstatus": "pubmed",
-       *           "date": "2014/10/26 06:00"
-       *         },
-       *         {
-       *           "pubstatus": "medline",
-       *           "date": "2014/10/26 06:00"
-       *         }
-       *       ],
-       *       "references": [],
-       *       "attributes": [
-       *         "Has Abstract"
-       *       ],
-       *       "pmcrefcount": 0,
-       *       "fulljournalname": "Bioinformatics (Oxford, England)",
-       *       "elocationid": "doi: 10.1093/bioinformatics/btu707",
-       *       "viewcount": 112,
-       *       "doctype": "citation",
-       *       "srccontriblist": [],
-       *       "booktitle": "",
-       *       "medium": "",
-       *       "edition": "",
-       *       "publisherlocation": "",
-       *       "publishername": "",
-       *       "srcdate": "",
-       *       "reportnumber": "",
-       *       "availablefromurl": "",
-       *       "locationlabel": "",
-       *       "doccontriblist": [],
-       *       "docdate": "",
-       *       "bookname": "",
-       *       "chapter": "",
-       *       "sortpubdate": "2015/03/01 00:00",
-       *       "sortfirstauthor": "Lekschas F",
-       *       "vernaculartitle": ""
-       *     },
-       *     "uids": [
-       *       "25344497"
-       *     ]
-       *   }
-       * }
        */
       var summary = $resource(
-        'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi',
+        settings.appRoot + '/pubmed/summary/:id',
         {
-          db: 'pubmed',
-          retmode: 'json'
-        },
-        {
-          query: {
-            method: 'GET',
-            isArray: false,
-            headers: {
-              'Accept': 'application/json'
-            }
-          }
+          id: '@id'
         }
       );
 
       function get (resource, queryTerms) {
         if (resource === 'abstract') {
-          var deferred = $q.defer();
-
-          abstractNew
-            .get(queryTerms)
-            .then(function (data) {
-              deferred.resolve(data);
-            })
-            .catch(function (error) {
-              deferred.reject(error);
-            });
-
-          return deferred.promise;
+          return abstract.get(queryTerms).$promise;
         }
 
         if (resource === 'search') {
-          return search.query(queryTerms).$promise;
+          return search.get(queryTerms).$promise;
         }
 
         if (resource === 'summary') {
-          return summary.query(queryTerms).$promise;
+          return summary.get(queryTerms).$promise;
         }
 
         return $q.reject('Resource "' + resource + '" not found.');
