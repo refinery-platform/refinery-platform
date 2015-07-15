9285d8a3fc6afd95c305749228943abb1dc8e669
Jennifer Marx <jennyrutledge@gmail.com>
2015-07-15 16:47:22 -0400

    Add filter to replace analyses status with an icon.

---
+++ refinery/ui/src/js/analyses/analyses-status-icon.js
@@ -0,0 +1,17 @@
+angular.module('refineryAnalyses').filter('analysesStatusIcon',analysesStatusIcon);
+
+function analysesStatusIcon(){
+  return function(param){
+    switch (param) {
+      case "SUCCESS":
+        return "icon-ok-sign";
+      case "FAILURE":
+        return "icon-warning-sign";
+      case "RUNNING":
+        return "icon-spinner icon-spin icon-large";
+      default:
+        return "icon-question-sign";
+    }
+  }
+};
+
