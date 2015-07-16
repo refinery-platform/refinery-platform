191725703fa98e4dbcfbeb2d4a827a089e164242
Jennifer Marx <jennyrutledge@gmail.com>
2015-07-15 20:25:17 -0400

    Add tooltip for analysis cancel icon.

---
+++ refinery/ui/src/js/analyses/analyses-cancel-tooltip.js
@@ -0,0 +1,17 @@
+angular.module('refineryAnalyses').directive("rpAnalysesCancelTooltip",
+rpAnalysesCancelTooltip)
+
+function rpAnalysesCancelTooltip() {
+  "use strict";
+
+  return {
+    restrict: 'A',
+    link: function(scope, element, attrs)
+      {
+        $(element)
+          .attr('title','Cancel Analysis')
+          .tooltip({placement: "right"});
+      }
+  };
+}
+
