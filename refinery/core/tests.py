7b4544cd862146df6bb48a4099706602c4592692
Fritz Lekschas <service@lekschas.de>
2015-08-05 12:41:54 -0400

    PEP8tification

--- refinery/core/tests.py
+++ refinery/core/tests.py
@@ -734,9 +734,8 @@ class AnalysisResourceTest(ResourceTestCase):
         self.assertHttpNotFound(response)
 
     def test_get_analysis_with_invalid_uuid(self):
-        '''Test retrieving an Analysis instance that doesn't exist.
-
-        '''
+        """Test retrieving an Analysis instance that doesn't exist.
+        """
         analysis = Analysis.objects.create(project=self.project,
                                            data_set=self.dataset,
                                            workflow=self.workflow)
@@ -747,39 +746,72 @@ class AnalysisResourceTest(ResourceTestCase):
         self.assertHttpNotFound(response)
 
     def test_get_analysis_list_for_given_dataset(self):
-        '''Test retrieving a list of Analysis instances for a given dataset.
-
-        '''
-        analysis1 = Analysis.objects.create(name='a1',project=self.project,
-                                            data_set=self.dataset,
-                                            workflow=self.workflow)
-        assign_perm("read_%s" % Analysis._meta.module_name, self.user, analysis1)
-        analysis2 = Analysis.objects.create(name='a2',project=self.project,
-                                            data_set=self.dataset2,
-                                            workflow=self.workflow)
-        assign_perm("read_%s" % Analysis._meta.module_name, self.user, analysis2)
+        """Test retrieving a list of Analysis instances for a given dataset.
+        """
+        analysis1 = Analysis.objects.create(
+            name='a1',
+            project=self.project,
+            data_set=self.dataset,
+            workflow=self.workflow
+        )
+        assign_perm(
+            "read_%s" % Analysis._meta.module_name,
+            self.user,
+            analysis1
+        )
+        analysis2 = Analysis.objects.create(
+            name='a2',
+            project=self.project,
+            data_set=self.dataset2,
+            workflow=self.workflow
+        )
+        assign_perm(
+            "read_%s" % Analysis._meta.module_name,
+            self.user,
+            analysis2
+        )
         analysis_uri = make_api_uri(Analysis._meta.module_name)
-        response = self.api_client.get(analysis_uri, format='json',
-                                       data={'data_set__uuid': self.dataset.uuid},
-                                       authentication=self.get_credentials())
+        response = self.api_client.get(
+            analysis_uri,
+            format='json',
+            data={'data_set__uuid': self.dataset.uuid},
+            authentication=self.get_credentials()
+        )
         self.assertValidJSONResponse(response)
         data = self.deserialize(response)['objects']
         self.assertEqual(len(data), 1)
-        self.assertKeys(data[0], ['uuid', 'name', 'creation_date', 'resource_uri'])
+        self.assertKeys(
+            data[0],
+            ['uuid', 'name', 'creation_date', 'resource_uri']
+        )
         self.assertEqual(data[0]['name'], analysis1.name)
 
     def test_get_sorted_analysis_list(self):
-        '''Get a list of Analysis instances with sorting params applied (e.g., order_by=name)
-
-        '''
-        analysis1 = Analysis.objects.create(name='a1',project=self.project,
-                                            data_set=self.dataset,
-                                            workflow=self.workflow)
-        assign_perm("read_%s" % Analysis._meta.module_name, self.user, analysis1)
-        analysis2 = Analysis.objects.create(name='a2',project=self.project,
-                                            data_set=self.dataset2,
-                                            workflow=self.workflow)
-        assign_perm("read_%s" % Analysis._meta.module_name, self.user, analysis2)
+        """Get a list of Analysis instances with sorting params applied
+        (e.g., order_by=name)
+        """
+        analysis1 = Analysis.objects.create(
+            name='a1',
+            project=self.project,
+            data_set=self.dataset,
+            workflow=self.workflow
+        )
+        assign_perm(
+            "read_%s" % Analysis._meta.module_name,
+            self.user,
+            analysis1
+        )
+        analysis2 = Analysis.objects.create(
+            name='a2',
+            project=self.project,
+            data_set=self.dataset2,
+            workflow=self.workflow
+        )
+        assign_perm(
+            "read_%s" % Analysis._meta.module_name,
+            self.user,
+            analysis2
+        )
         analysis_uri = make_api_uri(Analysis._meta.module_name)
         response = self.api_client.get(analysis_uri, format='json',
                                        authentication=self.get_credentials(),
@@ -808,7 +840,11 @@ class AnalysisResourceTest(ResourceTestCase):
                                            data_set=self.dataset,
                                            workflow=self.workflow)
         self.assertEqual(Analysis.objects.count(), 1)
-        assign_perm("delete_%s" % Analysis._meta.module_name, self.user, analysis)
+        assign_perm(
+            "delete_%s" % Analysis._meta.module_name,
+            self.user,
+            analysis
+        )
 
         analysis_uri = make_api_uri(Analysis._meta.module_name, analysis.uuid)
         response = self.api_client.delete(analysis_uri, format='json',
@@ -824,10 +860,13 @@ class AnalysisResourceTest(ResourceTestCase):
                                            data_set=self.dataset,
                                            workflow=self.workflow)
         self.assertEqual(Analysis.objects.count(), 1)
-        assign_perm("delete_%s" % Analysis._meta.module_name, self.user, analysis)
+        assign_perm(
+            "delete_%s" % Analysis._meta.module_name,
+            self.user,
+            analysis
+        )
 
         analysis_uri = make_api_uri(Analysis._meta.module_name, analysis.uuid)
         response = self.api_client.delete(analysis_uri, format='json')
         self.assertHttpMethodNotAllowed(response)
         self.assertEqual(Analysis.objects.count(), 1)
-
