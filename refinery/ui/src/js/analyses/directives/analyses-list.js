ce9284810e20a20905db0e2740085ecd308e34ba
Jennifer Marx <jennyrutledge@gmail.com>
2015-08-05 15:53:35 -0400

    Add category sorting for the analyses tab view.

--- refinery/ui/src/js/analyses/directives/analyses-list.js
+++ refinery/ui/src/js/analyses/directives/analyses-list.js
@@ -14,9 +14,23 @@ function analysesList() {
        analysesDetail: '@'
     },
     link: function(scope, element, attr){
+
       scope.$on('refinery/analyze-tab-active', function () {
         scope.AnalysesCtrl.updateAnalysesList();
       });
+
+      scope.predicate = 'name';
+      scope.reverse = true;
+      scope.analysisIcon = "icon-arrow-up";
+      scope.order = function(predicate) {
+        scope.reverse = (scope.predicate === predicate) ? !scope.reverse : false;
+        scope.predicate = predicate;
+        if(scope.reverse === true) {
+          scope.analysisIcon = "icon-arrow-up";
+        }else{
+          scope.analysisIcon = "icon-arrow-down";
+        }
+      };
     }
   };
 }
