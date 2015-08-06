18479214b8b160028d180054df1fe617c7dc602d
Ilya Sytchev <isytchev@hsph.harvard.edu>
2015-08-06 16:05:06 -0400

    PEP8 reformatting

--- refinery/annotation_server/utils.py
+++ refinery/annotation_server/utils.py
@@ -1,6 +1,9 @@
 from annotation_server.models import GenomeBuild
 
-SUPPORTED_GENOMES = [genome.name for genome in GenomeBuild.objects.filter(default_build=True)]
-EXTENDED_GENES = {'hg19': '_GenCode', 'dm3':'_FlyBase', 'ce10':'_WormBase'}
+
+SUPPORTED_GENOMES = [
+    genome.name for genome in GenomeBuild.objects.filter(default_build=True)
+    ]
+EXTENDED_GENES = {'hg19': '_GenCode', 'dm3': '_FlyBase', 'ce10': '_WormBase'}
 GAP_REGIONS = ['hg19', 'dm3']
 MAPPABILITY_THEORETICAL = ['hg19']
