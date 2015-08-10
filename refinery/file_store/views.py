9c17b9dc9d563a334f5b61388905ab51226d20b6
Ilya Sytchev <isytchev@hsph.harvard.edu>
2015-08-10 18:06:12 -0400

    Code cleanup

--- refinery/file_store/views.py
+++
@@ -1,6 +0,0 @@
-from django.conf import settings
-from django.core.urlresolvers import reverse
-from django.forms import Form
-from django.shortcuts import render
-from django.views.decorators.csrf import csrf_exempt
-
