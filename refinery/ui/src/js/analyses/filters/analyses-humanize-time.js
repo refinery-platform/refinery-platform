83b09047c9294ab986ba5623726cb66fea9672f2
Jennifer Marx <jennyrutledge@gmail.com>
2015-08-12 11:59:00 -0400

    Add error catch for undefined/null params.

--- refinery/ui/src/js/analyses/filters/analyses-humanize-time.js
+++ refinery/ui/src/js/analyses/filters/analyses-humanize-time.js
@@ -3,13 +3,15 @@ angular.module('refineryAnalyses').filter('analysesHumanizeTime',analysesHumaniz
 function analysesHumanizeTime(){
   return function(param){
     //Analyses API returns time with T in between date/time.
-    var sliceIndex = param.indexOf("T");
-    if(typeof sliceIndex !== undefined) {
-      var paramDate = param.replace(/T/i, " ");
-      var unixtime = Date.parse(paramDate)/1000;
-      return (
-        humanize.relativeTime(unixtime)
-      );
+    if(typeof param !== "undefined" && param !== null) {
+      var sliceIndex = param.indexOf("T");
+      if (typeof sliceIndex !== undefined) {
+        var paramDate = param.replace(/T/i, " ");
+        var unixtime = Date.parse(paramDate) / 1000;
+        return (
+          humanize.relativeTime(unixtime)
+        );
+      }
     }
   };
 }
