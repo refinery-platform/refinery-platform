58d48a77618c55ac43066d82ed7a430973330d3e
Ilya Sytchev <isytchev@hsph.harvard.edu>
2015-08-13 11:08:47 -0400

    Remove deprecated ADMIN_MEDIA_PREFIX Django setting

--- refinery/settings/base.py
+++ refinery/settings/base.py
@@ -80,11 +80,6 @@ if not os.path.isabs(STATIC_ROOT):
 # Example: "http://media.lawrence.com/static/"
 STATIC_URL = get_setting("STATIC_URL")
 
-# URL prefix for admin static files -- CSS, JavaScript and images.
-# Make sure to use a trailing slash.
-# Examples: "http://foo.com/static/admin/", "/static/admin/".
-ADMIN_MEDIA_PREFIX = get_setting("ADMIN_MEDIA_PREFIX")
-
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "refinery/static/production"),
     os.path.join(BASE_DIR, "refinery/ui/production")
