9ad64aab9122605ceba026033c460301b49f103b
Jennifer Marx <jennyrutledge@gmail.com>
2015-08-14 00:05:27 -0400

    This closes #595. A timezone offset was added.

--- refinery/ui/src/js/analyses/filters/analyses-humanize-time.js
+++ refinery/ui/src/js/analyses/filters/analyses-humanize-time.js
@@ -3,7 +3,12 @@ angular.module('refineryAnalyses').filter('analysesHumanizeTime',analysesHumaniz
 function analysesHumanizeTime(){
   return function(param){
     if(typeof param !== "undefined" && param !== null) {
-      var unixtime = Date.parse(param) / 1000;
+      var a = param.split(/[^0-9]/);
+      var testDate = Date.UTC(a[0], a[1] - 1, a[2], a[3], a[4], a[5]);
+      var curDate = new Date().getTimezoneOffset() * 60 * 1000;
+      var offsetDate = testDate + curDate;
+      var unixtime = offsetDate / 1000;
+
       return (
         humanize.relativeTime(unixtime)
       );
