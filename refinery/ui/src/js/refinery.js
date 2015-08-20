a4c1b5532eb2f05ab1bd15826b1d72bdcb08d707
Jennifer Marx <jennyrutledge@gmail.com>
2015-08-20 09:27:18 -0400

    Remove document watch on analyses tab triggers.

--- refinery/ui/src/js/refinery.js
+++ refinery/ui/src/js/refinery.js
@@ -70,8 +70,7 @@ angular
     //  trigger from the contents.js when the node selection list has been
     // updated. Used by node_mapping.js Trigger for analyze tab view to run
     // analyses status.
-  $(document).on('refinery/updateCurrentNodeSelection' +
-  ' refinery/analyze-tab-active refinery/analyze-tab-inactive', function(e){
+  $(document).on('refinery/updateCurrentNodeSelection', function(e){
     $rootScope.$broadcast(e.type);
     $rootScope.$digest();
   });
