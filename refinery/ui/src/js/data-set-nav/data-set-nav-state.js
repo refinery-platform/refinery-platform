029c516984ec92f911ae116fc36aadb8bb2a172b
Jennifer Marx <jennyrutledge@gmail.com>
2015-08-25 11:55:13 -0400

    Move the browse/analyze/visualize tabs routing to the data-set-nav.

--- refinery/ui/src/js/data-set-nav/data-set-nav-state.js
+++ refinery/ui/src/js/data-set-nav/data-set-nav-state.js
@@ -7,16 +7,81 @@ angular
       // Default state
       $urlRouterProvider.otherwise(function($injector) {
         var $state = $injector.get('$state');
-        $state.go('files');
+        $state.go('browse');
       });
 
       $stateProvider
         .state(
-          'files',
-          {
-            url: '/files/',
+          'files',{
+          url: '/files/',
+          templateUrl: '/static/partials/data_set_ui_mode_browse.html',
+          controller: function($scope, $rootScope, $timeout, $, $state) {
+            $rootScope.mode = "browse";
+            $rootScope.showCtrlTab = false;
+            $rootScope.mainTabSpanSize = "span12 no-margin";
+            $rootScope.ctrlTabSpanSize = "";
+
+            // calls global resizing function implemented in base.html to rescale height of scrollable elements
+            // timeout is needed to execute after DOM changes
+            $timeout(sizing, 0);
+
+            $(window).trigger('refinery/floatThead/reflow');
           }
-      )
+         })
+        .state(
+          'browse', {
+          url: '/files/',
+          templateUrl: '/static/partials/data_set_ui_mode_browse.html',
+          controller: function($scope, $rootScope, $timeout, $, $state) {
+            $('#filesTab').addClass('active');
+            $rootScope.mode = "browse";
+            $rootScope.showCtrlTab = false;
+            $rootScope.mainTabSpanSize = "span12 no-margin";
+            $rootScope.ctrlTabSpanSize = "";
+
+            // calls global resizing function implemented in base.html to rescale height of scrollable elements
+            // timeout is needed to execute after DOM changes
+            $timeout(sizing, 0);
+
+            $(window).trigger('refinery/floatThead/reflow');
+          }
+        })
+        .state('analyze', {
+          url: '/files/analyze/',
+          templateUrl: '/static/partials/data_set_ui_mode_analyze.html',
+          controller: function($scope, $rootScope, $timeout, $window, $, $state) {
+            $('#filesTab').addClass('active');
+            $rootScope.mode = "analyze";
+            $rootScope.showCtrlTab = true;
+            $rootScope.mainTabSpanSize = "span10";
+            $rootScope.ctrlTabSpanSize = "span2";
+
+
+            // calls global resizing function implemented in base.html to rescale height of scrollable elements
+            // timeout is needed to execute after DOM changes
+            $timeout(sizing, 0);
+
+            $(window).trigger('refinery/floatThead/reflow');
+          }
+        })
+        .state(
+          'visualize', {
+          templateUrl: "/static/partials/data_set_ui_mode_visualize.html",
+          url: '/files/visualize/',
+          controller: function($scope,$rootScope,$timeout, $window, $, $state) {
+            $rootScope.ctrlTabSpanSize = "span2";
+            $('#filesTab').addClass('active');
+            $rootScope.mode = "visualize";
+            $rootScope.showCtrlTab = true;
+            $rootScope.mainTabSpanSize = "span10";
+
+            // calls global resizing function implemented in base.html to rescale height of scrollable elements
+            // timeout is needed to execute after DOM changes
+            $timeout(sizing, 0);
+
+            $(window).trigger('refinery/floatThead/reflow');
+          }
+        })
         .state(
           'analyses',
           {
