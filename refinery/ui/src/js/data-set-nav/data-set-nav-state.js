5693d09d7c6b9bbf8b94e1d44e02c272298573a6
Fritz Lekschas <service@lekschas.de>
2015-08-25 19:12:54 -0400

    Added default route to address #634. Harmonized code style.

--- refinery/ui/src/js/data-set-nav/data-set-nav-state.js
+++ refinery/ui/src/js/data-set-nav/data-set-nav-state.js
@@ -1,7 +1,7 @@
 angular
   .module('refineryDataSetNav')
-  .config(['refineryStateProvider',
-    function ( refineryStateProvider) {
+  .config(['refineryStateProvider', 'refineryUrlRouterProvider',
+    function (refineryStateProvider, refineryUrlRouterProvider) {
 
       refineryStateProvider
         .state(
@@ -10,19 +10,21 @@ angular
             url: '/files/',
             templateUrl: '/static/partials/data_set_ui_mode_browse.html',
             controller: function($scope, $rootScope, $timeout, $, $state) {
-                $rootScope.mode = "browse";
-                $rootScope.showCtrlTab = false;
-                $rootScope.mainTabSpanSize = "span12 no-margin";
-                $rootScope.ctrlTabSpanSize = "";
+              $rootScope.mode = "browse";
+              $rootScope.showCtrlTab = false;
+              $rootScope.mainTabSpanSize = "span12 no-margin";
+              $rootScope.ctrlTabSpanSize = "";
 
-                // calls global resizing function implemented in base.html to rescale height of scrollable elements
-                // timeout is needed to execute after DOM changes
-                $timeout(sizing, 0);
+              // calls global resizing function implemented in base.html to rescale height of scrollable elements
+              // timeout is needed to execute after DOM changes
+              $timeout(sizing, 0);
 
-                $(window).trigger('refinery/floatThead/reflow');
-              }
-            }, '^\/data_sets\/.*\/$', true)
-
+              $(window).trigger('refinery/floatThead/reflow');
+            }
+          },
+          '^\/data_sets\/.*\/$',
+          true
+        )
         .state(
           'browse',
           {
@@ -41,29 +43,33 @@ angular
 
               $(window).trigger('refinery/floatThead/reflow');
             }
-          }, '^\/data_sets\/.*\/$', true)
-
+          },
+          '^\/data_sets\/.*\/$',
+          true
+        )
         .state(
           'analyze',
           {
-          url: '/files/analyze/',
-          templateUrl: '/static/partials/data_set_ui_mode_analyze.html',
-          controller: function($scope, $rootScope, $timeout, $window, $, $state) {
-            $('#filesTab').addClass('active');
-            $rootScope.mode = "analyze";
-            $rootScope.showCtrlTab = true;
-            $rootScope.mainTabSpanSize = "span10";
-            $rootScope.ctrlTabSpanSize = "span2";
+            url: '/files/analyze/',
+            templateUrl: '/static/partials/data_set_ui_mode_analyze.html',
+            controller: function($scope, $rootScope, $timeout, $window, $, $state) {
+              $('#filesTab').addClass('active');
+              $rootScope.mode = "analyze";
+              $rootScope.showCtrlTab = true;
+              $rootScope.mainTabSpanSize = "span10";
+              $rootScope.ctrlTabSpanSize = "span2";
 
 
-            // calls global resizing function implemented in base.html to rescale height of scrollable elements
-            // timeout is needed to execute after DOM changes
-            $timeout(sizing, 0);
+              // calls global resizing function implemented in base.html to rescale height of scrollable elements
+              // timeout is needed to execute after DOM changes
+              $timeout(sizing, 0);
 
-            $(window).trigger('refinery/floatThead/reflow');
-          }
-        }, '^\/data_sets\/.*\/$', true)
-
+              $(window).trigger('refinery/floatThead/reflow');
+            }
+          },
+          '^\/data_sets\/.*\/$',
+          true
+          )
         .state(
           'visualize',
           {
@@ -82,38 +88,58 @@ angular
 
               $(window).trigger('refinery/floatThead/reflow');
             }
-          }, '^\/data_sets\/.*\/$', true)
-
+          },
+          '^\/data_sets\/.*\/$',
+          true
+        )
         .state(
           'analyses',
           {
             url: '/analyses/',
             templateUrl: '/static/partials/analyses/partials/analyses-list.html',
-          }, '^\/data_sets\/.*\/$', true)
-
+          },
+          '^\/data_sets\/.*\/$',
+          true
+        )
         .state(
           'attributes',
           {
             url: '/attributes/',
-          }, '^\/data_sets\/.*\/$', true)
-
+          },
+          '^\/data_sets\/.*\/$',
+          true
+        )
         .state(
           'downloads',
           {
             url: '/downloads/',
-          }, '^\/data_sets\/.*\/$', true)
-
+          },
+          '^\/data_sets\/.*\/$',
+          true
+        )
         .state(
           'details',
           {
             url: '/details/',
-          }, '^\/data_sets\/.*\/$', true)
-
+          },
+          '^\/data_sets\/.*\/$',
+          true
+        )
         .state(
           'sharing',
           {
             url: '/sharing/',
-          }, '^\/data_sets\/.*\/$', true);
-      }
+          },
+          '^\/data_sets\/.*\/$',
+          true
+        );
+
+      refineryUrlRouterProvider
+        .otherwise(
+          '/files/browse',
+          '^\/data_sets\/.*\/$',
+          true
+        );
+    }
   ]
 );
