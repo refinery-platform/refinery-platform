238b3def2ff1efdc4eb29d111e0fb039308d3b68
scottx611x <scottx611x@gmail.com>
2015-09-15 18:06:08 -0400

    This Closes #678 Handled Empty Group name in Front end and Back end

--- refinery/ui/src/js/collaboration/controllers/add-group.js
+++ refinery/ui/src/js/collaboration/controllers/add-group.js
@@ -5,19 +5,31 @@ function AddGroupCtrl($modalInstance, groupService, groupDataService) {
   that.groupDataService = groupDataService;
 }
 
+function isEmptyOrSpaces(str){
+
+    return str === undefined || str === "" || str == " " || str === null || str.match(/^\s*$/) !== null || str.length === 0;
+}
+
+
 AddGroupCtrl.prototype.createGroup = function (name) {
   var that = this;
-  
-  this.groupService.create({
-    name: name
-  }).$promise.then(
-    function (data) {
+
+  this.groupService.create({name: name }).$promise.then(function (data) {
+
       that.groupDataService.update();
       that.$modalInstance.dismiss();
+
     }
   ).catch(function (error) {
-    console.error(error);
-    bootbox.alert("This name probably already exists - try a different name.");
+
+    if(isEmptyOrSpaces(name)){
+      bootbox.alert("Group Name cannot be left blank - try a different name.");
+    }
+    else{
+      console.error(error);
+      bootbox.alert("This name probably already exists - try a different name.");
+    }
+
   });
 };
 
