8ad936595a2615ff6687cfa3e0b689d74d0256a3
Jennifer Marx <jennyrutledge@gmail.com>
2015-09-17 11:04:55 -0400

    Add test for refineryrouter module.

---
+++ refinery/ui/src/js/commons/router/module.spec.js
@@ -0,0 +1,17 @@
+describe('Module: RefineryRouter', function(){
+
+  var module;
+
+  beforeEach(function(){
+    module = angular.module('refineryRouter');
+  });
+
+  it('module should exist', function(){
+    expect(module).toBeDefined();
+  });
+
+  it('module should be registered', function(){
+    expect(module).not.toEqual(null);
+  })
+
+});
