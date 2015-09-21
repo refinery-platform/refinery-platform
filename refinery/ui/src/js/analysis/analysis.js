5e13f988b67a2079f2651347054ae71a4ed9c9c7
Jennifer Marx <jennyrutledge@gmail.com>
2015-09-21 08:34:39 -0400

    Create analysis module file.

--- refinery/ui/src/js/analysis/analysis.js
+++ refinery/ui/src/js/analysis/analysis.js
@@ -1,4 +1,4 @@
-angular.module('refineryAnalysis', [])
+angular.module('refineryAnalysis')
 
 .controller('AnalysisCtrl', function(
     $scope, $rootScope, $http, $window, $log, $timeout, workflow) {
@@ -33,54 +33,87 @@ angular.module('refineryAnalysis', [])
     $scope.analysisConfig.nodeSetUuid = null;
   });
 
-  $scope.setAnalysisName = function(){
+  $scope.setAnalysisName = function() {
     var timeStamp = vm.getTimeStamp();
     var workflowUuid = workflow.getName();
     var tempName = workflowUuid + " " + timeStamp;
-    bootbox.prompt("Enter an Analysis Name", "Cancel Analysis", "Launch" +
-      " Analysis", function(name) {
-      if (name !== null) {
-        vm.launchAnalysis(name);
-      }
-    }, tempName).addClass("bootboxAnalysisWidth");
+    //bootbox.prompt("Enter an Analysis Name", "Cancel Analysis", "Launch" +
+    //  " Analysis", function(name) {
+    //  if (name !== null) {
+    //    var prom = vm.launchAnalysis(name);
+    //  }
+    //}, tempName).addClass("bootboxAnalysisWidth");
+
+    bootbox.prompt(
+      "Enter an Analysis Name",
+      "Cancel Analysis",
+      "Launch Analysis",
+      function(name) {
+        if (name !== null) {
+          vm.launchAnalysis(name).then(function (response) {
+            var msg = "<h3> Analysis Launched.</h3>" +
+          "<p>View progess in Analyses Tab.</p>";
+
+            vm.bootboxDialog(msg);
+            //  console.log(response);
+          }, function (error) {
+            var msg = "<h3> Analysis launch failed.</h3>" +
+         "<p>Please check galaxy history.</p>";
+            vm.bootboxDialog(msg);
+          });
+        }
+      },
+      tempName
+      ).addClass("bootboxAnalysisWidth");
+
   };
 
   vm.launchAnalysis = function(analysisName) {
     $scope.analysisConfig.name = analysisName;
     $scope.analysisConfig.workflowUuid = workflow.getUuid();
-    $http({
-      method: 'POST',
-      url: '/analysis_manager/run/',
-      headers: {'X-Requested-With': 'XMLHttpRequest'},
-      data: $scope.analysisConfig,
-    }).success(function (response) {
-      $log.debug("Launching analysis with config:");
-      $log.debug("Workflow: " + $scope.analysisConfig.workflowUuid);
-      $log.debug("NodeSET: " + $scope.analysisConfig.nodeSetUuid);
-      $log.debug("NodeREL: " + $scope.analysisConfig.nodeRelationshipUuid);
-    //  $window.location.assign(response);
-      var msg = "<h3> Analysis Launched.</h3>" +
-        "<p>View progess in Analyses Tab.</p>";
-        vm.bootboxDialog(msg);
-       $rootScope.$broadcast('rf/launchAnalysis');
-    }).error(function (response, status) {
-      $log.debug("Request failed: error " + status);
-      var msg = "<h3> Analysis launch failed.</h3>" +
-       "<p>Please check galaxy history.</p>";
-        vm.bootboxDialog(msg);
-    });
+    return(
+      $http({
+        method: 'POST',
+        url: '/analysis_manager/run/',
+        headers: {'X-Requested-With': 'XMLHttpRequest'},
+        data: $scope.analysisConfig,
+      }).success(function (response) {
+        $log.debug("Launching analysis with config:");
+        $log.debug("Workflow: " + $scope.analysisConfig.workflowUuid);
+        $log.debug("NodeSET: " + $scope.analysisConfig.nodeSetUuid);
+        $log.debug("NodeREL: " + $scope.analysisConfig.nodeRelationshipUuid);
+      //  $window.location.assign(response);
+      //  var msg = "<h3> Analysis Launched.</h3>" +
+      //    "<p>View progess in Analyses Tab.</p>";
+       //   vm.bootboxDialog(msg);
+         $rootScope.$broadcast('rf/launchAnalysis');
+      }).error(function (response, status) {
+        $log.debug("Request failed: error " + status);
+        //var msg = "<h3> Analysis launch failed.</h3>" +
+        // "<p>Please check galaxy history.</p>";
+       //   vm.bootboxDialog(msg);
+      })
+    );
+  };
+
+  $scope.showModal = false;
+  $scope.toggleModal = function(){
+      $scope.showModal = !$scope.showModal;
   };
 
   vm.bootboxDialog = function( msg){
+    console.log(msg);
     bootbox.dialog(msg, [{
-      "label" : "Close"
+      "label" : "Close",
     }, {
       "label" : "View Analysis",
       "class" : "btn-primary",
       "callback": function() {
         $window.location.href = '/data_sets/' + dataSetUuid + '/#/analyses';
       }
-    }]);
+    }],{
+      "animate": false
+    });
   };
 
   vm.getTimeStamp = function(){
