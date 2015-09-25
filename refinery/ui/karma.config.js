845496bc03c060975607a62cf1dc5310b22470cb
Fritz Lekschas <service@lekschas.de>
2015-09-25 13:40:03 -0400

    Added new ui-scroll file to karma.

--- refinery/ui/karma.config.js
+++ refinery/ui/karma.config.js
@@ -24,7 +24,7 @@ module.exports = function(config) {
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-ui-router/release/angular-ui-router.js',
       'bower_components/angular-ui-select2/release/select2.min.js',
-      'bower_components/angular-ui-utils/modules/scroll/scroll.js',
+      'bower_components/angular-ui-scroll/dist/ui-scroll.min.js',
       'bower_components/lodash-3/lodash.js',
       './karma.lodash.noConflict.js',
       'bower_components/c3/c3.js',
