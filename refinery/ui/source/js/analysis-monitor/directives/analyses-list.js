cd9b53ca579b29bf9a7f52dced76446cebd7f64a
Jennifer Marx <jennyrutledge@gmail.com>
2015-09-30 15:57:05 -0400

    Another typo.

--- refinery/ui/source/js/analysis-monitor/directives/analyses-list.js
+++ refinery/ui/source/js/analysis-monitor/directives/analyses-list.js
@@ -70,7 +70,7 @@ function analysisMonitorAnalysesList() {
 
       scope.pageCount = function() {
         var totalPages = Math.ceil(
-          scope.AnalysisMonitorCtrl.analysesList.length/scope.itemsPerPage
+          scope.AMCtrl.analysesList.length/scope.itemsPerPage
         );
 
         return totalPages;
