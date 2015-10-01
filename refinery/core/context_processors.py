3a8d60480a04c67780e66888f0138f2636bf8cc5
scottx611x <scottx611x@gmail.com>
2015-10-01 16:11:13 -0400

    Organized imports

--- refinery/core/context_processors.py
+++ refinery/core/context_processors.py
@@ -7,7 +7,8 @@ Created on Aug 23, 2012
 # a context processor to pass settings variables to views by default
 # from: http://stackoverflow.com/q/433162
 
-import json, logging
+import json
+import logging
 from django.conf import settings
 from django.contrib.sites.models import Site
 from django.core import serializers
