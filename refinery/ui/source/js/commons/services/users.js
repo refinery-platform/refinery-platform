ff376b5d650ae7be90e368dbc692b5a22f97bd66
Fritz Lekschas <service@lekschas.de>
2015-10-21 17:30:26 -0400

    Added human readable user name. Close #645.

---
+++ refinery/ui/source/js/commons/services/users.js
@@ -0,0 +1,69 @@
+function userFactory ($q, $resource, settings) {
+  var userService = {},
+      userResource = $resource(
+        settings.appRoot + settings.refineryApi + '/users/:uuid',
+        {
+          format: 'json'
+        },
+        {
+          query: {
+            method: 'GET',
+            isArray: false,
+          }
+        }
+      ),
+      store = {};
+
+  /**
+   * Query the API for user and user profile data.
+   *
+   * @method  getUserData
+   * @author  Fritz Lekschas
+   * @date    2015-10-21
+   *
+   * @param   {String}  uuid  User profile uuid.
+   * @return  {Object}        Angular promise resolving API data.
+   */
+  function getUserData (uuid) {
+    return userResource.get({
+      uuid: uuid
+    }).$promise;
+  }
+
+  /**
+   * Get user data.
+   *
+   * @method  get
+   * @author  Fritz Lekschas
+   * @date    2015-10-21
+   *
+   * @param   {String}  uuid  User profile uuid.
+   * @return  {Object}        Angular promise resolving unified data.
+   */
+  userService.get = function (uuid) {
+    if (store[uuid] === undefined) {
+      return getUserData(uuid).then(function (data) {
+        store[uuid] = {
+          affiliation: data.affiliation,
+          email: data.user.email,
+          firstName: data.user.first_name,
+          fullName: (data.user.first_name + ' ' + data.user.last_name).trim(),
+          lastName: data.user.last_name,
+          userId: data.user.id,
+          userName: data.user.username,
+          userProfileUuid: data.uuid
+        };
+
+        return store[uuid];
+      });
+    } else {
+      return $q.when(store[uuid]);
+    }
+  };
+
+  return userService;
+}
+
+angular
+  .module('refineryApp')
+  .factory('userService', ['$q', '$resource', 'settings', userFactory]);
