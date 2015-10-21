25eb6d0985b83ac75f89daaac330039517b21f12
scottx611x <scottx611x@gmail.com>
2015-10-21 10:14:31 -0400

    Moved "users" API into config/urls.py

--- refinery/core/urls.py
+++ refinery/core/urls.py
@@ -4,11 +4,7 @@ Created on Feb 20, 2012
 @author: nils
 '''
 
-from django.conf.urls.defaults import *
-from .api import UserProfileResource
-
-
-user_profile_resource = UserProfileResource()
+from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns(
     'core.views',
@@ -79,7 +75,4 @@ urlpatterns = patterns(
         name="pubmed_search"),
     url(r'^pubmed/summary/(?P<id>.+)/', 'pubmed_summary',
         name="pubmed_summary"),
-    url(r'^api/v1/', include(user_profile_resource.urls),
-        name="user_profiles"),
-
 )
