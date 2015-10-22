c599ca3b8c0c3d94609c181d84d5bda7b8751c7f
scottx611x <scottx611x@gmail.com>
2015-10-22 17:22:16 -0400

    Updated admin interface for FileStoreItem and FileType

--- refinery/file_store/admin.py
+++ refinery/file_store/admin.py
@@ -1,10 +1,13 @@
 import os
 from django.contrib import admin
-from file_store.models import FileStoreItem
+from file_store.models import FileStoreItem, FileType
 
 
 class FileStoreItemAdmin(admin.ModelAdmin):
     readonly_fields = ('import_task_id',)
+
+    list_display = ['__unicode__', 'id', 'datafile', 'uuid', 'source',
+                    'sharename', 'filetype', 'import_task_id']
 
     def save_model(self, request, obj, form, change):
         '''Symlink if source is a local file
@@ -13,4 +16,10 @@ class FileStoreItemAdmin(admin.ModelAdmin):
         if os.path.isabs(obj.source) and not obj.is_local():
             obj.symlink_datafile()
 
+
+class FileTypeAdmin(admin.ModelAdmin):
+
+    list_display = ['__unicode__', 'id', 'name', 'description', 'extension']
+
 admin.site.register(FileStoreItem, FileStoreItemAdmin)
+admin.site.register(FileType, FileTypeAdmin)
