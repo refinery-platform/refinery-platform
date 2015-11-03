f3239ab44853d1edd9ad0d2494cfa97880975e4b
Fritz Lekschas <service@lekschas.de>
2015-11-03 15:28:20 -0500

    Added config to capture `console.log()` from the actual code. Can be handy for developing unit tests.

--- refinery/ui/karma.config.js
+++ refinery/ui/karma.config.js
@@ -7,6 +7,10 @@ module.exports = function(config) {
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: './',
 
+    client: {
+      // Set to `true` if `console.log()` should be captured.
+      captureConsole: false
+    },
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
