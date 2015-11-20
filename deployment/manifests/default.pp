6e88211b49270f5e05dbe979586ecd327f8dd34d
Ilya Sytchev <isytchev@hsph.harvard.edu>
2015-11-20 14:52:50 -0500

    Reformat Puppet manifest according to the style guide

--- deployment/manifests/default.pp
+++ deployment/manifests/default.pp
@@ -22,39 +22,41 @@ user { $app_user: comment => $app_user }
 file { "/home/${app_user}/.ssh/config":
   ensure => file,
   source => "${deployment_root}/ssh-config",
-  owner => $app_user,
-  group => $app_group,
+  owner  => $app_user,
+  group  => $app_group,
 }
 
 class { 'postgresql::globals':
-  version => '9.3',
+  version  => '9.3',
   encoding => 'UTF8',
-  locale => 'en_US.utf8',
+  locale   => 'en_US.utf8',
 }
+
 class { 'postgresql::server':
 }
+
 class { 'postgresql::lib::devel':
 }
+
 postgresql::server::role { $app_user:
   createdb => true,
 }
 ->
 postgresql::server::db { 'refinery':
-  user => $app_user,
+  user     => $app_user,
   password => '',
-  owner => $app_user,
+  owner    => $app_user,
 }
 
 class { 'python':
-  version => 'system',
-  pip => true,
-  dev => true,
+  version    => 'system',
+  pip        => true,
+  dev        => true,
   virtualenv => true,
-  gunicorn => false,
+  gunicorn   => false,
 }
 
 class venvdeps {
-  #TODO: peg packages to specific versions
   package { 'build-essential': }
   package { 'libncurses5-dev': }
   package { 'libldap2-dev': }
@@ -66,44 +68,44 @@ include venvdeps
 file { "/home/${app_user}/.virtualenvs":
   # workaround for parent directory /home/vagrant/.virtualenvs does not exist error
   ensure => directory,
-  owner => $app_user,
-  group => $app_group,
+  owner  => $app_user,
+  group  => $app_group,
 }
 ->
 python::virtualenv { $virtualenv:
-  ensure => present,
-  owner => $app_user,
-  group => $app_group,
+  ensure  => present,
+  owner   => $app_user,
+  group   => $app_group,
   require => [ Class['venvdeps'], Class['postgresql::lib::devel'] ],
 }
 ~>
 python::requirements { $requirements:
   virtualenv => $virtualenv,
-  owner => $app_user,
-  group => $app_group,
+  owner      => $app_user,
+  group      => $app_group,
 }
 
 package { 'virtualenvwrapper': }
 ->
 file_line { "virtualenvwrapper_config":
-  path => "/home/${app_user}/.profile",
-  line => "source /etc/bash_completion.d/virtualenvwrapper",
+  path    => "/home/${app_user}/.profile",
+  line    => "source /etc/bash_completion.d/virtualenvwrapper",
   require => Python::Virtualenv[$virtualenv],
 }
 ->
 file { "virtualenvwrapper_project":
   # workaround for setvirtualenvproject command not found
-  ensure => file,
-  path => "${virtualenv}/.project",
+  ensure  => file,
+  path    => "${virtualenv}/.project",
   content => "${django_root}",
-  owner => $app_user,
-  group => $app_group,
+  owner   => $app_user,
+  group   => $app_group,
 }
 
 file { ["${project_root}/isa-tab", "${project_root}/import", "${project_root}/static"]:
   ensure => directory,
-  owner => $app_user,
-  group => $app_group,
+  owner  => $app_user,
+  group  => $app_group,
 }
 
 file_line { "django_settings_module":
@@ -112,43 +114,43 @@ file_line { "django_settings_module":
 }
 ->
 file { "${django_root}/config/config.json":
-  ensure => file,
-  source => "${django_root}/config/config.json.sample",
-  owner => $app_user,
-  group => $app_group,
+  ensure  => file,
+  source  => "${django_root}/config/config.json.sample",
+  owner   => $app_user,
+  group   => $app_group,
   replace => false,
 }
 ->
 exec { "syncdb":
-  command => "${virtualenv}/bin/python ${django_root}/manage.py syncdb --migrate --noinput",
+  command     => "${virtualenv}/bin/python ${django_root}/manage.py syncdb --migrate --noinput",
   environment => ["DJANGO_SETTINGS_MODULE=${django_settings_module}"],
-  user => $app_user,
-  group => $app_group,
-  require => [
-               Python::Requirements[$requirements],
-               Postgresql::Server::Db["refinery"]
-             ],
+  user        => $app_user,
+  group       => $app_group,
+  require     => [
+    Python::Requirements[$requirements],
+    Postgresql::Server::Db["refinery"]
+  ],
 }
 ->
 exec { "create_superuser":
-  command => "${virtualenv}/bin/python ${django_root}/manage.py loaddata superuser.json",
+  command     => "${virtualenv}/bin/python ${django_root}/manage.py loaddata superuser.json",
   environment => ["DJANGO_SETTINGS_MODULE=${django_settings_module}"],
-  user => $app_user,
-  group => $app_group,
+  user        => $app_user,
+  group       => $app_group,
 }
 ->
 exec { "init_refinery":
-  command => "${virtualenv}/bin/python ${django_root}/manage.py init_refinery 'Refinery' '192.168.50.50:8000'",
+  command     => "${virtualenv}/bin/python ${django_root}/manage.py init_refinery 'Refinery' '192.168.50.50:8000'",
   environment => ["DJANGO_SETTINGS_MODULE=${django_settings_module}"],
-  user => $app_user,
-  group => $app_group,
+  user        => $app_user,
+  group       => $app_group,
 }
 ->
 exec { "create_user":
-  command => "${virtualenv}/bin/python ${django_root}/manage.py create_user 'guest' 'guest' 'guest@example.com' 'Guest' '' ''",
+  command     => "${virtualenv}/bin/python ${django_root}/manage.py create_user 'guest' 'guest' 'guest@example.com' 'Guest' '' ''",
   environment => ["DJANGO_SETTINGS_MODULE=${django_settings_module}"],
-  user => $app_user,
-  group => $app_group,
+  user        => $app_user,
+  group       => $app_group,
 }
 
 file { "/opt":
@@ -214,14 +216,14 @@ class neo4j {
   exec { "neo4j_wget":
     command => "wget ${neo4j_url} -O /usr/src/${neo4j_archive}",
     creates => "/usr/src/${neo4j_archive}",
-    path => "/usr/bin:/bin",
+    path    => "/usr/bin:/bin",
     timeout => 600,  # downloading can take a long time
   }
   ->
   exec { "neo4j_unpack":
     command => "mkdir -p /opt && tar -xzf /usr/src/${neo4j_archive} -C /opt && chown -R ${app_user}:${app_user} /opt/${neo4j_name}",
     creates => "/opt/${neo4j_name}",
-    path => "/usr/bin:/bin",
+    path    => "/usr/bin:/bin",
   }
   ->
   file { "/opt/neo4j":
@@ -236,20 +238,20 @@ class neo4j {
   }
   ->
   file_line { "neo4j_graph_db_location":
-    path => "/opt/${neo4j_name}/conf/neo4j-server.properties",
-    line => "org.neo4j.server.database.location=/opt/neo4j-data/graph.db",
+    path  => "/opt/${neo4j_name}/conf/neo4j-server.properties",
+    line  => "org.neo4j.server.database.location=/opt/neo4j-data/graph.db",
     match => "^org.neo4j.server.database.location=",
   }
   ->
   file_line { "neo4j_no_authentication":
-    path => "/opt/${neo4j_name}/conf/neo4j-server.properties",
-    line => "dbms.security.auth_enabled=false",
+    path  => "/opt/${neo4j_name}/conf/neo4j-server.properties",
+    line  => "dbms.security.auth_enabled=false",
     match => "^dbms.security.auth_enabled=",
   }
   ->
   file_line { "neo4j_all_ips":
-    path => "/opt/${neo4j_name}/conf/neo4j-server.properties",
-    line => "org.neo4j.server.webserver.address=0.0.0.0",
+    path  => "/opt/${neo4j_name}/conf/neo4j-server.properties",
+    line  => "org.neo4j.server.webserver.address=0.0.0.0",
     match => "^#org.neo4j.server.webserver.address=",
   }
   limits::fragment {
@@ -272,7 +274,7 @@ class owl2neo4j {
   exec { "owl2neo4j_wget":
     command => "rm -f /opt/owl2neo4j.jar && wget -P /opt/ ${owl2neo4j_url}",
     creates => "/opt/owl2neo4j",
-    path => "/usr/bin:/bin",
+    path    => "/usr/bin:/bin",
     timeout => 120,  # downloading can take some time
   }
 }
@@ -284,7 +286,7 @@ class rabbit {
   class { '::rabbitmq':
     package_ensure => installed,
     service_ensure => running,
-    port => '5672',
+    port           => '5672',
   }
 }
 include rabbit
@@ -307,7 +309,7 @@ class ui {
   }
 
   package { 'nodejs':
-    name => 'nodejs',
+    name    => 'nodejs',
     ensure  => latest,
     require => Apt::Source['nodejs']
   }
@@ -318,36 +320,36 @@ class ui {
   }
   ->
   exec { "npm_local_modules":
-    command => "/usr/bin/npm prune && /usr/bin/npm install",
-    cwd => $ui_app_root,
+    command   => "/usr/bin/npm prune && /usr/bin/npm install",
+    cwd       => $ui_app_root,
     logoutput => on_failure,
-    user => $app_user,
-    group => $app_group,
+    user      => $app_user,
+    group     => $app_group,
   }
   ->
   exec { "bower_modules":
-    command => "/usr/bin/bower prune && /usr/bin/bower install --config.interactive=false",
-    cwd => $ui_app_root,
-    logoutput => on_failure,
-    user => $app_user,
-    group => $app_group,
+    command     => "/usr/bin/bower prune && /usr/bin/bower install --config.interactive=false",
+    cwd         => $ui_app_root,
+    logoutput   => on_failure,
+    user        => $app_user,
+    group       => $app_group,
     environment => ["HOME=/home/${app_user}"],
   }
   ->
   exec { "grunt":
-    command => "/usr/bin/grunt build && /usr/bin/grunt compile",
-    cwd => $ui_app_root,
+    command   => "/usr/bin/grunt build && /usr/bin/grunt compile",
+    cwd       => $ui_app_root,
     logoutput => on_failure,
-    user => $app_user,
-    group => $app_group,
+    user      => $app_user,
+    group     => $app_group,
   }
   ->
   exec { "collectstatic":
-    command => "${virtualenv}/bin/python ${django_root}/manage.py collectstatic --clear --noinput",
+    command     => "${virtualenv}/bin/python ${django_root}/manage.py collectstatic --clear --noinput",
     environment => ["DJANGO_SETTINGS_MODULE=${django_settings_module}"],
-    user => $app_user,
-    group => $app_group,
-    require => Python::Requirements[$requirements],
+    user        => $app_user,
+    group       => $app_group,
+    require     => Python::Requirements[$requirements],
   }
 }
 include ui
@@ -361,18 +363,18 @@ service { 'memcached':
 file { "${django_root}/supervisord.conf":
   ensure => file,
   source => "${django_root}/supervisord.conf.sample",
-  owner => $app_user,
-  group => $app_group,
+  owner  => $app_user,
+  group  => $app_group,
 }
 ->
 exec { "supervisord":
-  command => "${virtualenv}/bin/supervisord",
+  command     => "${virtualenv}/bin/supervisord",
   environment => ["DJANGO_SETTINGS_MODULE=${django_settings_module}"],
-  cwd => $django_root,
-  creates => "/tmp/supervisord.pid",
-  user => $app_user,
-  group => $app_group,
-  require => [
+  cwd         => $django_root,
+  creates     => "/tmp/supervisord.pid",
+  user        => $app_user,
+  group       => $app_group,
+  require     => [
     Class["ui"],
     Class["solr"],
     Class["neo4j"],
@@ -384,12 +386,12 @@ exec { "supervisord":
 package { 'libapache2-mod-wsgi': }
 package { 'apache2': }
 exec { 'apache2-wsgi':
-  command => '/usr/sbin/a2enmod wsgi',
+  command   => '/usr/sbin/a2enmod wsgi',
   subscribe => [ Package['apache2'], Package['libapache2-mod-wsgi'] ],
 }
 ->
 file { "/etc/apache2/sites-available/001-refinery.conf":
-  ensure => file,
+  ensure  => file,
   content => template("${deployment_root}/apache.conf"),
 }
 ~>
@@ -398,6 +400,6 @@ exec { 'refinery-apache2':
 }
 ~>
 service { 'apache2':
-  ensure => running,
+  ensure     => running,
   hasrestart => true,
 }
