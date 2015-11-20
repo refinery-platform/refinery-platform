5a3eb51dc402ebaf30e0fc0a07169c58bb2b13a2
Jennifer Marx <jennyrutledge@gmail.com>
2015-11-20 14:06:59 -0500

    Fix label use on analysis view filter.

--- refinery/static/source/js/refinery/solr/solr_analysis_view.js
+++ refinery/static/source/js/refinery/solr/solr_analysis_view.js
@@ -194,13 +194,13 @@ SolrAnalysisView.prototype._generateTree = function( solrResponse ) {
 					if ( self._query._facetSelection[facet][facetValue].isSelected ) {						
 						self._query._facetSelection[facet][facetValue] = { count: facetValueCount, isSelected: self._query._facetSelection[facet][facetValue].isSelected };
 						
-			    		selectedItems.push("<tr class=\"facet-value\" id=\"" + self._composeFacetValueId( facet, facetValue ) + "\"><td>" + '<label class="checkbox"><input type="checkbox" checked></label>' + "</td><td width=100%>" + analysisName + "</td><td align=right>" + facetValueCount + "</td>"  + "</tr>" );					
-		    			unselectedItems.push("<tr class=\"facet-value\" id=\"" + self._composeFacetValueId( facet, facetValue ) + "\"><td>" + '<label class="checkbox"><input type="checkbox" checked></label>' + "</td><td width=100%>" + analysisName + "</td><td align=right>" + facetValueCount + "</td>"  + "</tr>" );
+			    		selectedItems.push("<tr class=\"facet-value\" id=\"" + self._composeFacetValueId( facet, facetValue ) + "\"><td>" + '<input type="checkbox" checked>' + "</td><td width=100%>" + analysisName + "</td><td align=right>" + facetValueCount + "</td>"  + "</tr>" );
+		    			unselectedItems.push("<tr class=\"facet-value\" id=\"" + self._composeFacetValueId( facet, facetValue ) + "\"><td>" + '<input type="checkbox" checked>' + "</td><td width=100%>" + analysisName + "</td><td align=right>" + facetValueCount + "</td>"  + "</tr>" );
 					}
 					else {
 						self._query._facetSelection[facet][facetValue] = { count: facetValueCount, isSelected: self._query._facetSelection[facet][facetValue].isSelected };
 						
-						unselectedItems.push("<tr class=\"facet-value\" id=\"" + self._composeFacetValueId( facet, facetValue ) + "\"><td>" + '<label class="checkbox"><input type="checkbox"></label>' + "</td><td width=100%>" + analysisName + "</td><td align=right>" + facetValueCount + "</td><td></td>"  + "</tr>" );									
+						unselectedItems.push("<tr class=\"facet-value\" id=\"" + self._composeFacetValueId( facet, facetValue ) + "\"><td>" + '<input type="checkbox">' + "</td><td width=100%>" + analysisName + "</td><td align=right>" + facetValueCount + "</td><td></td>"  + "</tr>" );
 					}										
 				}			
 			}
