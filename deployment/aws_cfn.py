192b1610fee1a67e5c8a3f3254fd7222099370c6
David Jones <drj@pobox.com>
2015-12-04 10:53:38 +0000

    Minor edits for PEP 8 compliance

--- deployment/aws_cfn.py
+++ deployment/aws_cfn.py
@@ -4,14 +4,16 @@
 # See AWS.md for notes on how to use this
 # to deply to Amazon AWS.
 
+import os       # for os.popen
+
 cft = CloudFormationTemplate(description="refinery monolithic template.")
-import os
 branch = os.popen("""git branch | awk '$1=="*"{print $2}'""").read()
 assert branch
 
-user_data_script = (open('bootstrap.sh').read() +
-  "GIT_BRANCH={}\n".format(branch) +
-  open('aws.sh').read())
+user_data_script = (
+    open('bootstrap.sh').read() +
+    "GIT_BRANCH={}\n".format(branch) +
+    open('aws.sh').read())
 
 cft.resources.ec2_instance = Resource(
     'MonolithicInstance', 'AWS::EC2::Instance',
