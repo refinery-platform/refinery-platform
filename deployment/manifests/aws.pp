933164b9a8a954aaff7da7cd5829b335587f547b
David Jones <drj@pobox.com>
2015-12-14 14:35:11 +0000

    Bring puppetfiles in line with recent default.pp

--- deployment/manifests/aws.pp
+++ deployment/manifests/aws.pp
@@ -1,10 +1,11 @@
 $app_user = "ubuntu"
 $app_group = "ubuntu"
 $virtualenv = "/home/${app_user}/.virtualenvs/refinery-platform"
-$source_root = "/srv/refinery-platform"
-$project_root = "${source_root}/refinery"
-$requirements = "${source_root}/requirements.txt"
+$project_root = "/srv/refinery-platform"
+$deployment_root = "${project_root}/deployment"
+$django_root = "${project_root}/refinery"
+$requirements = "${project_root}/requirements.txt"
 $django_settings_module = "config.settings.dev"
-$ui_app_root = "${project_root}/ui"
+$ui_app_root = "${django_root}/ui"
 
 include refinery
