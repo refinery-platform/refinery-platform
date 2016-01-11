0de664080d5db11fef6be1f503c6f0535ab97103
David Jones <drj@pobox.com>
2016-01-11 12:41:56 +0000

    Fix typo in "intended"

--- deployment/aws.sh
+++ deployment/aws.sh
@@ -2,7 +2,7 @@
 
 set -x
 
-# This script is intened to be executed on AWS instance after
+# This script is intended to be executed on AWS instance after
 # the bootstraph.sh script. The bootstrap.sh script is common
 # to both Vagrant and AWS. Both bootstrap.sh and aws.sh (this
 # script) are supplied via cloudinit userdata.
