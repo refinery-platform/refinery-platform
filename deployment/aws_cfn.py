3e73b71227d4945692b4c7987362332da6f0003b
David Jones <drj@pobox.com>
2016-01-12 16:11:57 +0000

    Convert the RDS name into a required parameter

--- deployment/aws_cfn.py
+++ deployment/aws_cfn.py
@@ -2,17 +2,25 @@
 # for generating
 # an AWS CloudFormation json file.
 # See AWS.md for notes on how to use this
-# to deply to Amazon AWS.
+# to deploy to Amazon AWS.
+#
+# REFERENCES
+#
+# cfn-pyplates:
+#   https://cfn-pyplates.readthedocs.org/en/latest/index.html
 
 import os       # for os.popen
 
 cft = CloudFormationTemplate(description="refinery monolithic template.")
+
 branch = os.popen("""git branch | awk '$1=="*"{print $2}'""").read()
 assert branch
 
-user_data_script = (
-    open('bootstrap.sh').read() +
-    "GIT_BRANCH={}\n".format(branch) +
+user_data_script = join("",
+    "#!/bin/sh\n",
+    "RDS_NAME=", ref("RDSName"), "\n",
+    "GIT_BRANCH=", branch, "\n",
+    open('bootstrap.sh').read(),
     open('aws.sh').read())
 
 cft.resources.ec2_instance = Resource(
@@ -26,3 +34,11 @@ cft.resources.ec2_instance = Resource(
         'Tags': [{'Key': 'refinery', 'Value': 'refinery'}],
     })
 )
+
+
+cft.parameters.add(Parameter('RDSName', 'String',
+    {
+        'Description': 'Name of the RDS to connect to',
+    })
+)
+
