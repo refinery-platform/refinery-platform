65511d8e214118c9d9e7833a5b88b294bb1ddd69
David Jones <drj@pobox.com>
2016-01-25 11:32:19 +0000

    Assign a password for refinery PostgreSQL user

--- deployment/manifests/aws.pp
+++ deployment/manifests/aws.pp
@@ -1,5 +1,6 @@
 $app_user = "ubuntu"
 $app_group = "ubuntu"
+$db_password = "password"
 $virtualenv = "/home/${app_user}/.virtualenvs/refinery-platform"
 $project_root = "/srv/refinery-platform"
 $deployment_root = "${project_root}/deployment"
