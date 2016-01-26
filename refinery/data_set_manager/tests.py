60c2ea087cf63f1c8433d29b9a431e4245d855eb
Jennifer Marx <jennyrutledge@gmail.com>
2016-01-26 16:31:26 -0500

    Add test for Assay API.

--- refinery/data_set_manager/tests.py
+++ refinery/data_set_manager/tests.py
@@ -4,8 +4,20 @@ when you run "manage.py test".
 
 Replace this with more appropriate tests for your application.
 """
+import json
 
+from django.utils import unittest, simplejson
 from django.test import TestCase
+from django.core.urlresolvers import reverse
+
+from rest_framework.test import APIRequestFactory
+from rest_framework.test import APITestCase
+from rest_framework import status
+from rest_framework.test import force_authenticate
+
+from .models import AttributeOrder, Assay, Study, Investigation
+from .views import Assays
+from core.models import User
 
 
 class SimpleTest(TestCase):
@@ -14,3 +26,70 @@ class SimpleTest(TestCase):
         Tests that 1 + 1 always equals 2.
         """
         self.assertEqual(1 + 1, 2)
+
+
+class AssayAPITests(APITestCase):
+
+    def setUp(self):
+        self.factory = APIRequestFactory()
+        investigation = Investigation.objects.create()
+        study = Study.objects.create(file_name='test_filename123.txt',
+                                     title='Study Title Test',
+                                     investigation=investigation)
+        assay = Assay.objects.create(
+                study=study,
+                measurement='transcription factor binding site',
+                measurement_accession='http://www.testurl.org/testID',
+                measurement_source='OBI',
+                technology='nucleotide sequencing',
+                technology_accession='test info',
+                technology_source='test source',
+                platform='Genome Analyzer II',
+                file_name='test_assay_filename.txt',
+                )
+        self.valid_uuid = assay.uuid
+        self.view = Assays.as_view()
+        self.invalid_uuid = "0xxx000x-00xx-000x-xx00-x00x00x00x0x"
+        self.invalid_format_uuid = "xxxxxxxx"
+
+    def test_get(self):
+
+        #valid_uuid
+        uuid = self.valid_uuid
+        request = self.factory.get('/assay/%s/' % uuid)
+        response = self.view(request, uuid)
+        response.render()
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+                response.content,
+                '{"uuid":"%s",'
+                '"study":"None: Study Title Test",'
+                '"measurement":"transcription factor binding site",'
+                '"measurement_accession":"http://www.testurl.org/testID",'
+                '"measurement_source":"OBI",'
+                '"technology":"nucleotide sequencing",'
+                '"technology_accession":"test info",'
+                '"technology_source":"test source",'
+                '"platform":"Genome Analyzer II",'
+                '"file_name":"test_assay_filename.txt"}'
+                % uuid
+                )
+        # invalid_uuid
+        uuid = self.invalid_uuid
+        request = self.factory.get('/assay/%s/' % uuid)
+        response = self.view(request, uuid)
+        response.render()
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.content, '')
+
+        # invalid_format_uuid
+        uuid = self.invalid_format_uuid
+        request = self.factory.get('/assay/%s/' % uuid)
+        response = self.view(request, uuid)
+        response.render()
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.content, '')
+
+
+
+
