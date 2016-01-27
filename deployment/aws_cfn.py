ee6ef781e7a9eb2681d8c8f15fde41e4a852b7e8
David Jones <drj@pobox.com>
2016-01-27 12:13:44 +0000

    Add documentation for the cfn-pyplates file

--- deployment/aws_cfn.py
+++ deployment/aws_cfn.py
@@ -1,21 +1,35 @@
 # This is a cfn-pyplates template file
-# for generating
-# an AWS CloudFormation json file.
-# See AWS.md for notes on how to use this
-# to deploy to Amazon AWS.
+# for generating deploy.json,
+# which is an AWS CloudFormation json file.
+# See https://github.com/parklab/refinery-platform/wiki/AWS-installation
+# for notes on how to use this to deploy to Amazon AWS.
+# Instance are configured using CloudInit
+
+
 #
 # REFERENCES
 #
 # cfn-pyplates:
 #   https://cfn-pyplates.readthedocs.org/en/latest/index.html
+# AWS Cloudformation
+#   https://aws.amazon.com/cloudformation/
+# CloudInit
+#   https://help.ubuntu.com/community/CloudInit
 
 import os       # for os.popen
 
 cft = CloudFormationTemplate(description="refinery monolithic template.")
 
+# We discover the current git branch
+# so that the deployment script can use it
+# to clone the same branch.
 branch = os.popen("""git branch | awk '$1=="*"{print $2}'""").read()
 assert branch
 
+# The userdata script is executed via cloudinit.
+# It's made by concatenating a block of parameter variables,
+# with the bootstrap.sh script,
+# and the aws.sh script.
 user_data_script = join(
         "",
         "#!/bin/sh\n",
