478b3015ed9574742ba7b9894328cc3f68713ece
Jennifer Marx <jennyrutledge@gmail.com>
2016-01-27 22:07:13 -0500

    Add id to serializer and limited fields which can be updated.

--- refinery/data_set_manager/serializers.py
+++ refinery/data_set_manager/serializers.py
@@ -29,6 +29,7 @@ class AttributeOrderSerializer(serializers.Serializer):
     is_facet = serializers.BooleanField()
     is_active = serializers.BooleanField()
     is_internal = serializers.BooleanField()
+    id = serializers.IntegerField()
 
     def create(self, validated_data):
         """
@@ -42,17 +43,11 @@ class AttributeOrderSerializer(serializers.Serializer):
         Update and return an existing `AttributeOrder` instance, given the
         validated data.
         """
-        instance.study = validated_data.get('study', instance.study)
-        instance.assay = validated_data.get('assay', instance.assay)
-        instance.solr_field = validated_data.get('solr_field',
-                                                 instance.solr_field)
         instance.rank = validated_data.get('rank', instance.rank)
         instance.is_exposed = validated_data.get('is_exposed',
                                                  instance.is_exposed)
         instance.is_facet = validated_data.get('is_facet', instance.is_facet)
         instance.is_active = validated_data.get('is_active',
                                                 instance.is_active)
-        instance.is_internal = validated_data.get('is_internal',
-                                                  instance.is_internal)
         instance.save()
         return instance
