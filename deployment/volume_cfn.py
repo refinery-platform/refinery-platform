19fb32be9180365f1523f9c61533a403a07a8ee4
David Jones <drj@pobox.com>
2016-02-09 13:35:56 +0000

    Load tags for volume from aws-tags

--- deployment/volume_cfn.py
+++ deployment/volume_cfn.py
@@ -15,7 +15,30 @@
 # AWS Cloudformation
 #   https://aws.amazon.com/cloudformation/
 
+import glob
 import os       # for os.popen
+
+import yaml
+
+# Get the local user's email address
+email = os.popen("git config --get user.email").read().rstrip()
+
+def load_tags():
+    """
+    Tags come from the YAML files in the aws-tags directory,
+    additionally, the tag `owner` if it is not set by those YAML
+    files, will be set to the email address of the local git
+    user.
+    """
+
+    tags = {}
+    for filename in sorted(glob.glob("aws-tags/*")):
+        with open(filename) as f:
+            tags.update(yaml.load(f))
+    if 'owner' not in tags:
+        tags['owner'] = email
+
+    return [{'Key': k, 'Value': v} for k,v in tags.items()]
 
 cft = CloudFormationTemplate(description="refinery EBS volume.")
 
@@ -26,7 +49,7 @@ cft.resources.ebs = Resource(
         'AvailabilityZone': 'us-east-1b',
         'Encrypted': True,
         'Size': 10,
-        # 'Tags': foo,
+        'Tags': load_tags(),
         'VolumeType': 'gp2'
     })
 )
