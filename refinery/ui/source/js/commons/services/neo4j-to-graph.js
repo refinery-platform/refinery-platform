c62234893b5cbfb6c486265a638554301b21c1cc
Fritz Lekschas <service@lekschas.de>
2016-02-12 20:09:39 -0500

    Encapsulate the service to avoid future issue with other private functions of the _commons_ module.

--- refinery/ui/source/js/commons/services/neo4j-to-graph.js
+++ refinery/ui/source/js/commons/services/neo4j-to-graph.js
@@ -1,76 +1,117 @@
-function buildGraph (nodes) {
-  var j;
-  var node;
-  var parentsIds;
-  var uris = Object.keys(nodes);
+var Neo4jToGraphService = (function () {
 
-  for (var i = uris.length; i--;) {
+  /* -------------------------------- Private ------------------------------- */
 
-    node = nodes[uris[i]];
+  /**
+   * Prepared the node list returned by Neo4J to reflect a complete graph.
+   *
+   * @description
+   * This methods enhance the nodes by given information. For example, `name` is
+   * assigned depending on `label` and `ontId`. `children` are collected given
+   * `parents`. Etc.
+   *
+   * @method  prepareGraph
+   * @author  Fritz Lekschas
+   * @date    2016-02-11
+   * @private
+   * @param   {Object}  nodes  Object list of all graph nodes.
+   * @return  {Object}         Object list of fully prepared graph nodes.
+   */
+  function prepareGraph (nodes) {
+    var j;
+    var node;
+    var parentsIds;
+    var uris = Object.keys(nodes);
 
-    node.name = node.label || node.ontId;
-    node.numDataSets = 0;
+    for (var i = uris.length; i--;) {
 
-    if (!node.children) {
-      node.children = [];
-    }
+      node = nodes[uris[i]];
 
-    parentsIds = Object.keys(node.parents);
-    for (j = parentsIds.length; j--;) {
-      // Push children
-      if (!nodes[parentsIds[j]].children) {
-        nodes[parentsIds[j]].children = [node.uri];
-      } else {
-        nodes[parentsIds[j]].children.push(node.uri);
+      node.name = node.label || node.ontId;
+      node.numDataSets = 0;
+
+      if (!node.children) {
+        node.children = [];
       }
-      // Set parent reference
-      node.parents[parentsIds[j]] = nodes[parentsIds[j]];
+
+      parentsIds = Object.keys(node.parents);
+      for (j = parentsIds.length; j--;) {
+        // Push children
+        if (!nodes[parentsIds[j]].children) {
+          nodes[parentsIds[j]].children = [node.uri];
+        } else {
+          nodes[parentsIds[j]].children.push(node.uri);
+        }
+        // Set parent reference
+        node.parents[parentsIds[j]] = nodes[parentsIds[j]];
+      }
+
+      // Store assorted data sets seperately to be able to distinguish them from
+      // infered data sets.
+      node.assertedDataSets = {};
+      var dsIds = Object.keys(node.dataSets);
+      for (j = dsIds.length; j--;) {
+        node.assertedDataSets[dsIds[j]] = true;
+      }
     }
 
-    // Store assorted data sets seperately to be able to distinguish them from
-    // infered data sets.
-    node.assertedDataSets = {};
-    var dsIds = Object.keys(node.dataSets);
-    for (j = dsIds.length; j--;) {
-      node.assertedDataSets[dsIds[j]] = true;
-    }
+    return nodes;
   }
 
-  return nodes;
-}
+  /* -------------------------------- Public -------------------------------- */
 
-function Neo4jToGraph ($q, neo4j) {
-  this.$q = $q;
-  this.neo4j = neo4j;
-}
+  /**
+   * Service class.
+   *
+   * @method  Neo4jToGraph
+   * @author  Fritz Lekschas
+   * @date    2016-02-11
+   * @param   {Object}  $q     Angular's promise.
+   * @param   {Object}  neo4j  Neo4J HTTP service.
+   */
+  function Neo4jToGraph ($q, neo4j) {
+    this.$q = $q;
+    this.neo4j = neo4j;
+  }
 
-Neo4jToGraph.prototype.get = function () {
-  // Intermediate promise. We can't use the promise returned by ngResource
-  // because Neo4J doesn't report errors via HTTP codes but as part of the
-  // returned body.
-  var neo4jData = this.$q.defer();
+  /**
+   * Get annotation graph.
+   *
+   * @method  get
+   * @author  Fritz Lekschas
+   * @date    2016-02-11
+   * @return  {[type]}    [description]
+   */
+  Neo4jToGraph.prototype.get = function () {
+    // Intermediate promise. We can't use the promise returned by ngResource
+    // because Neo4J doesn't report errors via HTTP codes but as part of the
+    // returned body.
+    var neo4jData = this.$q.defer();
 
-  this.neo4j.query({
-      res: 'annotations'
-    })
-    .$promise
-    .then(function (response) {
-      neo4jData.resolve(response.nodes);
-    }.bind(this))
-    .catch(function (error) {
-      neo4jData.reject(error);
-      console.error(error);
+    this.neo4j.query({
+        res: 'annotations'
+      })
+      .$promise
+      .then(function (response) {
+        neo4jData.resolve(response.nodes);
+      })
+      .catch(function (error) {
+        neo4jData.reject(error);
+        console.error(error);
+      });
+
+    return neo4jData.promise.then(function (data) {
+      return prepareGraph(data);
     });
+  };
 
-  return neo4jData.promise.then(function (data) {
-    return buildGraph(data);
-  }.bind(this));
-};
+  return Neo4jToGraph;
+}());
 
 angular
   .module('refineryApp')
   .service('neo4jToGraph', [
     '$q',
     'neo4j',
-    Neo4jToGraph
+    Neo4jToGraphService
   ]);
