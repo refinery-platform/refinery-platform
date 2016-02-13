6e57de0e1d015020aea47f747beee4c3284f40fa
Fritz Lekschas <service@lekschas.de>
2016-02-12 21:11:37 -0500

    Fix uni test.

--- refinery/ui/source/js/globals/services/contains-dom-element.spec.js
+++ refinery/ui/source/js/globals/services/contains-dom-element.spec.js
@@ -7,13 +7,20 @@ describe('ContainsDomElement.service: unit tests', function () {
   beforeEach(function () {
     module('containsDomElement');
 
-    inject(function (_containsDomElement_, _$compile_) {
+    inject(function (_containsDomElement_) {
       containsDomElement = _containsDomElement_;
     });
 
-    html = angular.element(
-      '<body><div id="a"><div id="b"></div></div><div id="c"></div></body>'
-    );
+    // Unfortunately `angular.element` is not able to create a `body` element so
+    // we have to do that outselves...
+    html = document.implementation
+      .createDocument(
+        'http://www.w3.org/1999/xhtml',
+        'html',
+        null
+      )
+      .createElement('body');
+    html.innerHTML = '<div id="a"><div id="b"></div></div><div id="c"></div>';
   });
 
   it('should contain the containsDomElement service', function () {
@@ -21,17 +28,15 @@ describe('ContainsDomElement.service: unit tests', function () {
   });
 
   it('should identify contained element', function () {
-    var a = html.find('#a');
-    var b = html.find('#b');
-
-    console.log(html, a, b);
+    var a = html.querySelector('#a');
+    var b = html.querySelector('#b');
 
     expect(containsDomElement(a, b)).toEqual(true);
   });
 
   it('should not identify uncontained element', function () {
-    var a = html.find('#a');
-    var c = html.find('#c');
+    var a = html.querySelector('#a');
+    var c = html.querySelector('#c');
 
     expect(containsDomElement(a, c)).not.toEqual(true);
   });
