741ddf75932a88d0ccb0cffe7e48f672e8610518
Jennifer Marx <jennyrutledge@gmail.com>
2016-02-23 12:03:08 -0500

    Add factory and ctrl to generate assayFiles files and attributes.

--- refinery/ui/source/js/file-browser/ctrl.js
+++ refinery/ui/source/js/file-browser/ctrl.js
@@ -1,9 +1,27 @@
 angular.module('refineryFileBrowser')
     .controller('FileBrowserCtrl',
-    [FileBrowserCtrl]);
+    ['fileBrowserFactory', FileBrowserCtrl]);
 
 
-function FileBrowserCtrl() {
+function FileBrowserCtrl(fileBrowserFactory) {
   "use strict";
   var vm = this;
+  vm.assayFiles = [];
+
+  vm.updateAssayFiles = function () {
+    var param = {
+      format: 'json',
+      'uuid': '5eff885e-49cb-477a-ad76-f65d74d78f8a',
+    };
+
+    return fileBrowserFactory.getAssayFiles(param).then(function (response) {
+      vm.assayFiles = fileBrowserFactory.assayFiles;
+      console.log("in ctrl");
+      console.log(vm.assayFiles);
+      return response;
+    });
+  };
+
+  vm.updateAssayFiles();
+
 }
---
+++ refinery/ui/source/js/file-browser/services/factory.js
@@ -0,0 +1,30 @@
+angular.module('refineryFileBrowser')
+    .factory("fileBrowserFactory", ['$http','assayFileService', fileBrowserFactory]);
+
+function fileBrowserFactory($http, assayFileService) {
+  "use strict";
+  var assayFiles = [];
+  var assayAttributes = [];
+
+  var getAssayFiles = function (params) {
+    params = params || {};
+
+    var assayFile = assayFileService.query(params);
+    assayFile.$promise.then(function (response) {
+      console.log(response.attributes, params);
+      angular.copy(response.attributes, assayAttributes);
+      angular.copy(response.nodes, assayFiles);
+    }, function (error) {
+      console.log(error);
+    });
+
+    return assayFile.$promise;
+  };
+
+  return{
+    assayFiles: assayFiles,
+    assayAttributes: assayAttributes,
+    getAssayFiles: getAssayFiles
+  };
+
+}
