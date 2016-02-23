37b309ce283599005770bc8949c3cec8b2cacf96
Jennifer Marx <jennyrutledge@gmail.com>
2016-02-23 12:02:22 -0500

    Add common service for assay File.

---
+++ refinery/ui/source/js/commons/services/assayFile.js
@@ -0,0 +1,20 @@
+angular
+  .module('refineryApp')
+  .factory('assayFileService', ['$resource', 'settings',
+    function ($resource, settings) {
+
+      var assayFile = $resource(
+        settings.appRoot + settings.refineryApiV2 + '/assays/:uuid/files/',
+        {
+          uuid: '@uuid'
+        },
+        {
+          query: {
+            method: 'GET',
+          }
+        }
+      );
+
+      return assayFile;
+    }
+  ]);
