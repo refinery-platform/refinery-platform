246ec884d4eab858763d9939edbb535048bdc021
David Jones <drj@pobox.com>
2016-02-23 13:46:42 +0000

    Fix typo: USER -> PASSWORD

--- refinery/config/settings/aws.py
+++ refinery/config/settings/aws.py
@@ -8,6 +8,6 @@ from .dev import *
 
 EMAIL_BACKEND = 'django_smtp_ssl.SSLEmailBackend'
 EMAIL_HOST_USER = get_setting('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = get_setting('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = get_setting('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
 EMAIL_PORT = 465
