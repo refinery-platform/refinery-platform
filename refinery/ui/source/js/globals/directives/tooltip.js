72171a252e5bc8508a1772ad26f9fa74d0d6bc30
Fritz Lekschas <service@lekschas.de>
2016-02-23 19:54:01 -0500

    Refactored tooltip to make it more future proof.

--- refinery/ui/source/js/globals/directives/tooltip.js
+++ refinery/ui/source/js/globals/directives/tooltip.js
@@ -1,24 +1,40 @@
-angular
-  .module('toolTip', [])
-  .directive('toolTip', ['$', function ($) {
-    return {
-      restrict: 'A',
-      link: function(scope, element, attrs) {
-        var $element = $(element);
+var TooltipCtrl = function ($element) {
+  var _element = $element;
 
-        $element
-          .attr('data-container', attrs.toolTipContainer)
-          .one('mouseover', function () {
-            // Initialize the tool tip plugin when the element is hovered the
-            // first time by the mouse cursor. This ensures that Angular has
-            // successfully rendered the title and avoids performance issues.
-            $element.tooltip({
-              placement: attrs.toolTipPlacement
-            });
-            // After initializing the plugin we need to trigger the `mouseover`
-            // event again to immediately show the tool tip.
-            $element.trigger('mouseover');
+  // This is the actual controller, which still has access to `_element` but
+  // `_element` is hidden from the public
+  var tooltip = function () {
+    _element
+      .attr('data-container', this.container)
+      .one('mouseenter', function () {
+        // Initialize the tool tip plugin when the element is hovered the
+        // first time by the mouse cursor. This ensures that Angular has
+        // successfully rendered the title and avoids performance issues.
+        _element.tooltip({
+          placement: this.placement
         });
-      }
-    };
-  }]);
+        // After initializing the plugin we need to trigger the `mouseover`
+        // event again to immediately show the tool tip.
+        _element.trigger('mouseenter', _element);
+      }.bind(this));
+  };
+
+  return tooltip.apply(this);
+};
+
+var tooltipDirective = function () {
+  return {
+    bindToController: {
+      container: '@refineryTooltipContainer',
+      placement: '@refineryTooltipPlacement'
+    },
+    controller: 'TooltipCtrl',
+    controllerAs: 'tooltip',
+    restrict: 'A'
+  };
+};
+
+angular
+  .module('tooltip', [])
+  .controller('TooltipCtrl', ['$element', TooltipCtrl])
+  .directive('refineryTooltip', [tooltipDirective]);
