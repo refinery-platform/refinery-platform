47a3c8a1fd19304fbffc602447df02916ba16e47
J.Marx <jennifer@jkmarx.com>
2016-03-04 15:32:36 -0500

    Grab selected filter fields.

--- refinery/ui/source/js/file-browser/directives/assay-filters.js
+++ refinery/ui/source/js/file-browser/directives/assay-filters.js
@@ -17,6 +17,48 @@ function rpFileBrowserAssayFilters(fileBrowserFactory) {
     bindToController: {
        attributeFilter: '=?bind',
        analysisFilter: '=?bind'
+    },
+    link: function(scope){
+
+      scope.selectedField = {};
+
+      //scope.getAllSelected = function(internal_name) {
+      //  var value = '';
+      //  scope.selectedFieldList = {};
+      //
+      //  angular.forEach(scope.selectedField, function (key, internalNameAndField) {
+      //    field = internalNameAndField.split('=')[1];
+      //
+      //    if(key && typeof scope.selectedFieldList[internal_name] !== 'undefined') {
+      //      scope.selectedFieldList[internal_name].push(field);
+      //    }else if(key){
+      //      scope.selectedFieldList[internal_name]=[field];
+      //    }else{
+      //      var ind = scope.selectedFieldList[internal_name].indexOf(field);
+      //      if(ind > -1) {
+      //        scope.selectedFieldList[internal_name].splice(ind, 1);
+      //      }
+      //    }
+      //  });
+      //  console.log(scope.selectedFieldList);
+      //};
+
+
+      scope.selectedFieldList = {};
+      scope.attributeSelectionUpdate = function(internal_name, field){
+        if(scope.selectedField[field]){
+          if(typeof scope.selectedFieldList[internal_name] !== 'undefined'){
+            scope.selectedFieldList[internal_name].push(field);
+          }else{
+            scope.selectedFieldList[internal_name]=[field];
+          }
+        }else{
+          var ind = scope.selectedFieldList[internal_name].indexOf(field);
+          if(ind > -1){
+            scope.selectedFieldList[internal_name].splice(ind, 1);
+          }
+        }
+      };
     }
   };
 }
