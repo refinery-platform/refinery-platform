953b2b89f9724426bba8cfd9a4d42003e9c06b46
J.Marx <jennifer@jkmarx.com>
2016-03-04 16:02:31 -0500

    Refactor method.

--- refinery/ui/source/js/file-browser/directives/assay-filters.js
+++ refinery/ui/source/js/file-browser/directives/assay-filters.js
@@ -21,37 +21,16 @@ function rpFileBrowserAssayFilters(fileBrowserFactory) {
     link: function(scope){
 
       scope.selectedField = {};
-
-      //scope.getAllSelected = function(internal_name) {
-      //  var value = '';
-      //  scope.selectedFieldList = {};
-      //
-      //  angular.forEach(scope.selectedField, function (key, internalNameAndField) {
-      //    field = internalNameAndField.split('=')[1];
-      //
-      //    if(key && typeof scope.selectedFieldList[internal_name] !== 'undefined') {
-      //      scope.selectedFieldList[internal_name].push(field);
-      //    }else if(key){
-      //      scope.selectedFieldList[internal_name]=[field];
-      //    }else{
-      //      var ind = scope.selectedFieldList[internal_name].indexOf(field);
-      //      if(ind > -1) {
-      //        scope.selectedFieldList[internal_name].splice(ind, 1);
-      //      }
-      //    }
-      //  });
-      //  console.log(scope.selectedFieldList);
-      //};
-
-
       scope.selectedFieldList = {};
+
       scope.attributeSelectionUpdate = function(internal_name, field){
-        if(scope.selectedField[field]){
-          if(typeof scope.selectedFieldList[internal_name] !== 'undefined'){
-            scope.selectedFieldList[internal_name].push(field);
-          }else{
-            scope.selectedFieldList[internal_name]=[field];
-          }
+        if(scope.selectedField[field] &&
+          typeof scope.selectedFieldList[internal_name] !== 'undefined'){
+          scope.selectedFieldList[internal_name].push(field);
+
+        }else if(scope.selectedField[field]){
+          scope.selectedFieldList[internal_name]=[field];
+
         }else{
           var ind = scope.selectedFieldList[internal_name].indexOf(field);
           if(ind > -1){
