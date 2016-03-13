93255283df3a3530f77733248f2662c7a0bbdd76
Fritz Lekschas <service@lekschas.de>
2016-03-13 16:51:14 -0400

    Unsure <80 characters per line.

--- refinery/ui/source/js/node-mapping/controllers/data-set-ui-mode.js
+++ refinery/ui/source/js/node-mapping/controllers/data-set-ui-mode.js
@@ -1,13 +1,19 @@
 function DataSetUiModeCtrl ($scope, $location, $rootScope) {
   $rootScope.mode = 'browse';
 
-  $scope.$onRootScope('workflowChangedEvent', function(event, currentWorkflow) {
-    $scope.currentWorkflow = currentWorkflow;
-  });
+  $scope.$onRootScope(
+    'workflowChangedEvent',
+    function(event, currentWorkflow) {
+      $scope.currentWorkflow = currentWorkflow;
+    }
+  );
 
-  $scope.$onRootScope('nodeRelationshipChangedEvent', function( event, currentNodeRelationship ) {
-    $scope.currentNodeRelationship = currentNodeRelationship;
-  });
+  $scope.$onRootScope(
+    'nodeRelationshipChangedEvent',
+    function(event, currentNodeRelationship) {
+      $scope.currentNodeRelationship = currentNodeRelationship;
+    }
+  );
 }
 
 angular
