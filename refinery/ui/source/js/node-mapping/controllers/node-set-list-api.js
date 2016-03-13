2957994bf0d93c6f84b8b7af50d60d2d1340a036
Fritz Lekschas <service@lekschas.de>
2016-03-13 16:55:51 -0400

    Unify code styling.

--- refinery/ui/source/js/node-mapping/controllers/node-set-list-api.js
+++ refinery/ui/source/js/node-mapping/controllers/node-set-list-api.js
@@ -1,46 +1,47 @@
 function NodeSetListApiCtrl ($scope, $rootScope, $window, NodeSetList) {
   'use strict';
 
-  $scope.updateCurrentNodeSet = function() {
+  $scope.updateCurrentNodeSet = function () {
     $scope.currentNodeSet = $scope.nodesetList[$scope.nodesetIndex];
     // FIXME: temp workaround - this should be handled through the event bus
     if ($scope.currentNodeSet) {
-      $rootScope.$emit("nodeSetChangedEvent", $scope.currentNodeSet);
-      // console.log($scope.currentNodeSet);
-      // analysisConfig.nodeSetUuid = $scope.currentNodeSet.uuid;
-      // analysisConfig.nodeRelationshipUuid = null;
+      $rootScope.$emit('nodeSetChangedEvent', $scope.currentNodeSet);
     }
   };
 
-  $scope.getCurrentNodeSet = function(){
-    NodeSetList.get(
-      {
-        study__uuid: externalStudyUuid, assay__uuid: externalAssayUuid
-      }).$promise.then(function(data){
+  $scope.getCurrentNodeSet = function () {
+    NodeSetList.get({
+      study__uuid: externalStudyUuid, assay__uuid: externalAssayUuid
+    }).$promise
+      .then(function (data) {
         $scope.nodesetList = data.objects;
         $scope.updateCurrentNodeSet();
-
-      },function(error){
-        console.log("Couldn't read node set list.");
+      })
+      .catch(function (error) {
+        console.log('Couldn\'t read node set list.');
         if ($window.sessionStorage) {
-            var currentSelectionSessionKey = externalStudyUuid + "_" +
-              externalAssayUuid + "_" + "currentSelection";
-            console.log( "Reading " + currentSelectionSessionKey +
-              " from session storage" );
-            $scope.nodesetList = [angular.fromJson(
-              $window.sessionStorage.getItem(currentSelectionSessionKey))];
-            $scope.updateCurrentNodeSet();
-        }
-        else {
+          var currentSelectionSessionKey = (
+            externalStudyUuid + '_' +
+            externalAssayUuid + '_' +
+            'currentSelection'
+          );
+
+          console.log(
+            'Reading ' + currentSelectionSessionKey + ' from session storage'
+          );
+
+          $scope.nodesetList = [angular.fromJson(
+            $window.sessionStorage.getItem(currentSelectionSessionKey)
+          )];
+
+          $scope.updateCurrentNodeSet();
+        } else {
           console.log(error);
         }
-
-    });
+      });
   };
 
-  $scope.$on('refinery/updateCurrentNodeSelection', function(){
-    $scope.getCurrentNodeSet();
-  });
+  $scope.$on('refinery/updateCurrentNodeSelection', $scope.getCurrentNodeSet);
 
   $scope.getCurrentNodeSet();
 }
