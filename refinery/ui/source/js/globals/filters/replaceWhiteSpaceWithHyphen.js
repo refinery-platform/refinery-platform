d858780ac406551801ca7e3c4b7e381b5e0a6a94
J.Marx <jennifer@jkmarx.com>
2016-03-18 09:53:22 -0400

    Expand replace to be global with regex.

--- refinery/ui/source/js/globals/filters/replaceWhiteSpaceWithHyphen.js
+++ refinery/ui/source/js/globals/filters/replaceWhiteSpaceWithHyphen.js
@@ -1,11 +1,12 @@
-angular.module('refineryFileBrowser')
+angular.module('replaceWhiteSpaceWithHyphen', [])
   .filter('replaceWhiteSpaceWithHyphen',replaceWhiteSpaceWithHyphen);
 
 //swap white spaces with hypens
 function replaceWhiteSpaceWithHyphen(){
   return function(param){
     if(typeof param !== 'undefined'){
-      return (param.replace(' ', '-'));
+      //regex global search & replace
+      return (param.replace(/ /g, '-'));
     }
   };
 }
