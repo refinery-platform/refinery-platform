398a10f4ce73ea6c8958171ba65f7f78b2a6f939
J.Marx <jennifer@jkmarx.com>
2016-03-18 09:56:50 -0400

    Add test for global filter.

---
+++ refinery/ui/source/js/globals/filters/replaceWhiteSpaceWithHypen.spec.js
@@ -0,0 +1,41 @@
+describe('replaceWhiteSpaceWithHyphen.module: unit tests', function () {
+  'use strict';
+
+  var module;
+
+  beforeEach(function () {
+    module = angular.module('replaceWhiteSpaceWithHyphen');
+  });
+
+  describe('Module', function () {
+    it("should be registered", function() {
+      expect(module).not.toEqual(null);
+    });
+  });
+});
+
+describe('Filter: replaceWhiteSpaceWithHyphen', function () {
+
+  var filter;
+
+  beforeEach(function () {
+    module('refineryApp');
+    module('replaceWhiteSpaceWithHyphen');
+
+    inject(function ($filter) {
+      filter = $filter('replaceWhiteSpaceWithHyphen');
+    });
+  });
+
+  it('filter should exist', function () {
+    expect(filter).toBeDefined();
+  });
+
+  it('should return correct status depending on state', function () {
+    expect(filter('Analysis Out')).toBe('Analysis-Out');
+    expect(filter('The Analysis Output Test')).toBe('The-Analysis-Output-Test');
+    expect(filter('Title')).toBe('Title');
+    expect(filter('')).toBe('');
+    expect(filter(' ')).toBe('-');
+  });
+});
