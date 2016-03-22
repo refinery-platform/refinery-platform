ad8dbdf027945dd3906e986d1be3d4a48bc48738
J.Marx <jennifer@jkmarx.com>
2016-03-21 22:51:43 -0400

    Refactoring for readiblity.

--- refinery/ui/source/js/file-browser/directives/assay-files.js
+++ refinery/ui/source/js/file-browser/directives/assay-files.js
@@ -18,11 +18,10 @@ function rpFileBrowserAssayFiles() {
        assayFiles: '@',
        assayAttributes: '@'
     },
-    link: function(scope){
-
-      scope.FBCtrl.updateAssayFiles().then(function(){
-        scope.FBCtrl.checkUrlQueryFilters();
-        scope.FBCtrl.createColumnDefs();
+    link: function(scope, element, attr, ctrl){
+      ctrl.updateAssayFiles().then(function(){
+        ctrl.checkUrlQueryFilters();
+        ctrl.createColumnDefs();
       });
 
     }
