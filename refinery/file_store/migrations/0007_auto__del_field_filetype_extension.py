e049e70cbea0b0845e214639c6d70792e653562e
Scott Ouellette <scottx611x@gmail.com>
2016-03-23 11:42:22 -0400

    Add error handling

--- refinery/file_store/migrations/0007_auto__del_field_filetype_extension.py
+++ refinery/file_store/migrations/0007_auto__del_field_filetype_extension.py
@@ -5,43 +5,62 @@ from south.v2 import SchemaMigration
 from django.db import models
 from file_store.models import FileType
 
-class Migration(SchemaMigration):
 
+class Migration(SchemaMigration):
     def forwards(self, orm):
         # Deleting field 'FileType.extension'
         db.delete_column(u'file_store_filetype', 'extension')
-
-        db.start_transaction()
-        fastqcsanger = FileType.objects.get(description="FASTQC Sanger")
-        fastqcsanger.name = "FASTQCSANGER"
-        fastqcsanger.save()
-        db.commit_transaction()
+        try:
+            db.start_transaction()
+            fastqcsanger = orm.FileType.objects.get(description="FASTQC "
+                                                                "Sanger")
+            fastqcsanger.name = "FASTQCSANGER"
+            fastqcsanger.save()
+            db.commit_transaction()
+        except orm.FileType.DoesNotExist:
+            pass
 
     def backwards(self, orm):
         # Adding field 'FileType.extension'
         db.add_column(u'file_store_filetype', 'extension',
-                      self.gf('django.db.models.fields.CharField')(default='', max_length=50, unique=True),
+                      self.gf('django.db.models.fields.CharField')(default='',
+                                                                   max_length=50,
+                                                                   unique=True),
                       keep_default=False)
-
 
     models = {
         u'file_store.filestoreitem': {
             'Meta': {'object_name': 'FileStoreItem'},
-            'created': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'auto_now_add': 'True', 'blank': 'True'}),
-            'datafile': ('django.db.models.fields.files.FileField', [], {'max_length': '1024', 'blank': 'True'}),
-            'filetype': ('django.db.models.fields.related.ForeignKey', [], {'default': '33', 'to': u"orm['file_store.FileType']"}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'import_task_id': ('django.db.models.fields.CharField', [], {'max_length': '36', 'blank': 'True'}),
-            'sharename': ('django.db.models.fields.CharField', [], {'max_length': '20', 'blank': 'True'}),
-            'source': ('django.db.models.fields.CharField', [], {'max_length': '1024'}),
-            'updated': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'auto_now': 'True', 'blank': 'True'}),
-            'uuid': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '36', 'blank': 'True'})
+            'created': ('django.db.models.fields.DateTimeField', [],
+                        {'default': 'datetime.datetime.now',
+                         'auto_now_add': 'True', 'blank': 'True'}),
+            'datafile': ('django.db.models.fields.files.FileField', [],
+                         {'max_length': '1024', 'blank': 'True'}),
+            'filetype': ('django.db.models.fields.related.ForeignKey', [],
+                         {'default': '33',
+                          'to': u"orm['file_store.FileType']"}),
+            u'id': (
+            'django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'import_task_id': ('django.db.models.fields.CharField', [],
+                               {'max_length': '36', 'blank': 'True'}),
+            'sharename': ('django.db.models.fields.CharField', [],
+                          {'max_length': '20', 'blank': 'True'}),
+            'source': (
+            'django.db.models.fields.CharField', [], {'max_length': '1024'}),
+            'updated': ('django.db.models.fields.DateTimeField', [],
+                        {'default': 'datetime.datetime.now',
+                         'auto_now': 'True', 'blank': 'True'}),
+            'uuid': ('django.db.models.fields.CharField', [],
+                     {'unique': 'True', 'max_length': '36', 'blank': 'True'})
         },
         u'file_store.filetype': {
             'Meta': {'object_name': 'FileType'},
-            'description': ('django.db.models.fields.CharField', [], {'max_length': '250'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+            'description': (
+            'django.db.models.fields.CharField', [], {'max_length': '250'}),
+            u'id': (
+            'django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': (
+            'django.db.models.fields.CharField', [], {'max_length': '50'})
         }
     }
 
