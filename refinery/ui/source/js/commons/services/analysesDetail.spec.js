fb5e2aaa080560dc55f0a0968124f126c00165f4
Fritz Lekschas <service@lekschas.de>
2016-04-08 16:06:20 -0400

    Formatting only.

--- refinery/ui/source/js/commons/services/analysesDetail.spec.js
+++ refinery/ui/source/js/commons/services/analysesDetail.spec.js
@@ -1,24 +1,36 @@
-describe('Common.service.analysisDetail: unit tests', function () {
-  'use strict';
+'use strict';
 
-  var $httpBackend,
-      $rootScope,
-      params = '/?format=json',
-      valid_uuid = 'x508x83x-x9xx-4740-x9x7-x7x0x631280x',
-      service,
-      header = {
-        "X-Requested-With":"XMLHttpRequest",
-        "Accept":"application/json," +
-        " text/plain, */*",
-        "Content-Type":"application/json;charset=utf-8"
-      },
-      fakeResponse = {
-        "refineryImport": [{status: "PROGRESS", percent_done: 30}],
-        "galaxyImport": [{status: "", percent_done: ""}],
-        "galaxyAnalysis": [{status: "", percent_done:""}],
-        "galaxyExport": [{status: "", percent_done: ""}],
-        "overrall": "RUNNING"
-      };
+describe('Common.service.analysisDetail: unit tests', function () {
+  var $httpBackend;
+  var $rootScope;
+  var params = '/?format=json';
+  var validUuid = 'x508x83x-x9xx-4740-x9x7-x7x0x631280x';
+  var service;
+  var header = {
+    'X-Requested-With': 'XMLHttpRequest',
+    Accept: 'application/json,' +
+      ' text/plain, */*',
+    'Content-Type': 'application/json;charset=utf-8'
+  };
+  var fakeResponse = {
+    refineryImport: [{
+      status: 'PROGRESS',
+      percent_done: 30
+    }],
+    galaxyImport: [{
+      status: '',
+      percent_done: ''
+    }],
+    galaxyAnalysis: [{
+      status: '',
+      percent_done: ''
+    }],
+    galaxyExport: [{
+      status: '',
+      percent_done: ''
+    }],
+    overrall: 'RUNNING'
+  };
 
   beforeEach(function () {
     module('refineryApp');
@@ -29,19 +41,18 @@ describe('Common.service.analysisDetail: unit tests', function () {
       $rootScope = $injector.get('$rootScope');
       service = $injector.get('analysisDetailService');
 
-       $httpBackend
-         .expectPOST(
-           settings.appRoot +
-           '/analysis_manager/'+
-             valid_uuid + params,
-           {uuid: valid_uuid},
-           header
-         ).respond(200, fakeResponse);
+      $httpBackend
+        .expectPOST(
+          settings.appRoot +
+          '/analysis_manager/' +
+          validUuid + params,
+          { uuid: validUuid },
+          header
+      ).respond(200, fakeResponse);
     });
   });
 
   describe('Service', function () {
-
     it('should be defined', function () {
       expect(service).toBeDefined();
     });
@@ -52,8 +63,10 @@ describe('Common.service.analysisDetail: unit tests', function () {
 
     it('should return a resolving promise', function () {
       var results;
-      var promise = service.query({uuid: valid_uuid})
-        .$promise.then(function (response) {
+      var promise = service.query({
+        uuid: validUuid
+      })
+      .$promise.then(function (response) {
         results = response;
       });
 
