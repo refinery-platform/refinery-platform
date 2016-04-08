496f820eeb2af1644217d575697524b4c9f69d06
Fritz Lekschas <service@lekschas.de>
2016-04-08 16:04:30 -0400

    Add explanation why we need to use _constant_ instead of _service_.

--- refinery/ui/source/js/commons/router/settings-location-test.js
+++ refinery/ui/source/js/commons/router/settings-location-test.js
@@ -10,4 +10,7 @@ function locationTest (currentPath, matchPath, regex) {
 
 angular
   .module('refineryRouter')
+  // Normally we would use `service` but services can't be injected into a
+  // provider while constants can. This is weird but seems to be a limitation of
+  // Angular.
   .constant('locationTest', locationTest);
