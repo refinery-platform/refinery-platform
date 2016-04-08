0883390a7921e627790c5c34dca0d41bb8732d0c
Fritz Lekschas <service@lekschas.de>
2016-04-08 15:49:28 -0400

    Remove unnecessary controller definition + auto-formatting using esformatter.

--- refinery/ui/source/js/commons/list-graph/directive.spec.js
+++ refinery/ui/source/js/commons/list-graph/directive.spec.js
@@ -1,6 +1,6 @@
-describe('ListGraph.directive: unit tests', function () {
-  'use strict';
+'use strict';
 
+describe('ListGraph.directive: unit tests', function () {
   var directiveEl;
   var $scope;
 
@@ -11,32 +11,14 @@ describe('ListGraph.directive: unit tests', function () {
     module('refineryApp.templates');
 
     var $compile;
-    var $controller;
-    var $q;
     var $rootScope;
-    var dataSet;
-    var graph;
-    var listGraphSettings;
-    var pubSub;
 
     inject(function (
       _$compile_,
-      _$controller_,
-      _$q_,
-      _$rootScope_,
-      _dataSet_,
-      _graph_,
-      _listGraphSettings_,
-      _pubSub_
+      _$rootScope_
     ) {
       $compile = _$compile_;
-      $controller = _$controller_;
-      $q = _$q_;
       $rootScope = _$rootScope_;
-      dataSet = _dataSet_;
-      graph = _graph_;
-      listGraphSettings = _listGraphSettings_;
-      pubSub = _pubSub_;
     });
 
     $scope = $rootScope.$new();
@@ -45,28 +27,12 @@ describe('ListGraph.directive: unit tests', function () {
       angular.element('<list-graph graph="graph"></list-graph>')
     )($scope);
 
-    var ListGraphCtrl = $controller('ListGraphCtrl', {
-      $scope: $scope,
-      $element: directiveEl,
-      $rootScope: $rootScope,
-      graph: graph,
-      ListGraphVis: function () {},
-      listGraphSettings: listGraphSettings,
-      dataSet: dataSet,
-      pubSub: pubSub
-    }, {
-      graphData: $q.defer().promise,
-      valuePropertyName: undefined
-    });
-
     $scope.$digest();
   });
 
   describe('DOM', function () {
-
     it('should replace custom element with template', function () {
       expect(directiveEl.hasClass('visWrapper')).toBe(true);
     });
-
   });
 });
