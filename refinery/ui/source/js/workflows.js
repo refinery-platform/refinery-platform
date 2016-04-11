99a05f538dd347509a4a4cbfd8101d19356ba463
Fritz Lekschas <service@lekschas.de>
2016-04-11 10:12:29 -0400

    Formatting; added Angular annotations; removed unused params. Consistent return.

--- refinery/ui/source/js/workflows.js
+++ refinery/ui/source/js/workflows.js
@@ -1,41 +1,45 @@
 'use strict';
 
-angular.module('refineryWorkflows', [])
+angular
+  .module('refineryWorkflows', [])
+  .controller('WorkflowListApiCtrl', [
+    '$scope',
+    '$rootScope',
+    '$log',
+    'workflowApi',
+    'workflow',
+    function ($scope, $rootScope, $log, workflowApi, workflow) {
+      $scope.getWorkflowList = function () {
+        var Workflows = workflowApi.get(function () {
+          $scope.workflowList = Workflows.objects;
+        });
+      };
 
-  .controller('WorkflowListApiCtrl', function ($scope, $rootScope, $log, workflowApi, workflow) {
-    $scope.getWorkflowList = function () {
-      var Workflows = workflowApi.get(function () {
-        $scope.workflowList = Workflows.objects;
-      });
-    };
+      $scope.getWorkflowList();
 
-    $scope.getWorkflowList();
+      $scope.currentWorkflow = workflow;
 
-    $scope.currentWorkflow = workflow;
+      $scope.updateCurrentWorkflow = function () {
+        workflow.set($scope.workflowList[$scope.workflowIndex]);
 
-    $scope.updateCurrentWorkflow = function () {
-      workflow.set($scope.workflowList[$scope.workflowIndex]);
-
-      if (workflow.isAvailable()) {
-        $rootScope.$emit('workflowChangedEvent', workflow.get());
-      }
-    };
-
-  })
-
-  .factory('workflowApi', function ($resource) {
+        if (workflow.isAvailable()) {
+          $rootScope.$emit('workflowChangedEvent', workflow.get());
+        }
+      };
+    }
+  ])
+  .factory('workflowApi', ['$resource', function ($resource) {
     return $resource(
       '/api/v1/workflow/', {
         format: 'json'
       }
     );
-  })
-
-  .service('workflow', function ($log) {
+  }])
+  .service('workflow', [function () {
     this.instance = null;
 
     this.isAvailable = function () {
-      return this.instance ? true : false;
+      return !!this.instance;
     };
 
     this.get = function () {
@@ -47,42 +51,38 @@ angular.module('refineryWorkflows', [])
     };
 
     this.isSingleInput = function () {
-      if (this.instance) {
-        if (this.getInputSet(2)) {
-          return false;
-        } else {
-          return true;
-        }
-      } else {
-        // is this necessary?
-        return true;
+      if (this.instance && this.getInputSet(2)) {
+        return false;
       }
+      return true;
     };
 
     this.getUuid = function () {
       if (this.isAvailable()) {
         return this.instance.uuid;
       }
+      return undefined;
     };
 
     this.getSummary = function () {
       if (this.isAvailable()) {
         return this.instance.summary;
       }
+      return undefined;
     };
 
     this.getName = function () {
       if (this.isAvailable()) {
         return this.instance.name;
       }
+      return undefined;
     };
 
     this.getCategory = function () {
       if (this.isSingleInput()) {
         return 'File Set';
-      } else {
-        return this.instance.input_relationships[0].category + ' File Mapping';
       }
+      return this.instance.input_relationships[0].category + ' File Mapping';
     };
 
     this.getInputSet = function (number) {
@@ -92,13 +92,17 @@ angular.module('refineryWorkflows', [])
             return this.instance.input_relationships[0].set1;
           case 2:
             return this.instance.input_relationships[0].set2;
+          default:
+            return undefined;
         }
       }
+      return undefined;
     };
 
     this.getGalaxyInstanceId = function () {
       if (this.isAvailable()) {
         return this.instance.galaxy_instance_identifier;
       }
+      return undefined;
     };
-  });
+  }]);
