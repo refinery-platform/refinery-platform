dc3c5e8636919aff7f3678aa90daa4222d5711f0
J.Marx <jennifer@jkmarx.com>
2016-04-11 08:56:38 -0400

    Move grid reset to after modal closes.

--- refinery/ui/source/js/file-browser/directives/assay-files-util-modal.js
+++ refinery/ui/source/js/file-browser/directives/assay-files-util-modal.js
@@ -6,29 +6,43 @@ angular
       '$compile',
       '$templateCache',
       '$uibModal',
+      'resetGridService',
       rpAssayFilesUtilModal
     ]
   );
 
-function rpAssayFilesUtilModal($compile, $templateCache, $uibModal) {
+function rpAssayFilesUtilModal(
+  $compile,
+  $templateCache,
+  $uibModal,
+  resetGridService
+) {
   "use strict";
   return {
     restrict: 'AE',
     controller: 'FileBrowserCtrl',
     controllerAs: 'FBCtrl',
-    link: function(scope, element) {
+    link: function(scope, element, attr, ctrl) {
+      var modalInstance;
 
       element.bind("click", function(e) {
         var template = $templateCache.get("assayfilesutilmodal.html");
         var modalContent = $compile(template)(scope);
 
-        $uibModal.open({
+        modalInstance =  $uibModal.open({
           template:modalContent,
           controller: 'AssayFilesUtilModalCtrl',
           controllerAs: 'AFUMCtrl'
         });
 
+        modalInstance.result.then(function () {
+          resetGridService.setResetGridFlag(true);
+        }, function () {
+          resetGridService.setResetGridFlag(true);
+        });
+
       });
+
     }
   };
 }
