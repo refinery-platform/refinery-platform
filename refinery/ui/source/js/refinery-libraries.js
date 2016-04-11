a0310944e289b1389c6830ea396f0ad91ce44b78
Fritz Lekschas <service@lekschas.de>
2016-04-11 10:07:45 -0400

    Add `c3` as a constant to be able to properly inject it. Add Angular annotations for minimizing. Fix formatting.

--- refinery/ui/source/js/refinery-libraries.js
+++ refinery/ui/source/js/refinery-libraries.js
@@ -5,4 +5,5 @@ angular
   .constant('$', window.jQuery)
   .constant('_', window.lodashLatest)
   .constant('d3', window.d3)
+  .constant('c3', window.c3)
   .constant('ListGraphVis', window.ListGraph);
