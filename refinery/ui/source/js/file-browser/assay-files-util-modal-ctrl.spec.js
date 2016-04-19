b6c78083aeddb151bfb37379a4b6a1f5c708c536
J.Marx <jennifer@jkmarx.com>
2016-04-19 14:49:38 -0400

    Add test for updating attribute ranking.

--- refinery/ui/source/js/file-browser/assay-files-util-modal-ctrl.spec.js
+++ refinery/ui/source/js/file-browser/assay-files-util-modal-ctrl.spec.js
@@ -32,7 +32,7 @@ describe('Controller: Assay Files Util Modal Ctrl', function(){
     expect(ctrl.assayAttributeOrder ).toEqual([]);
   });
 
-  describe('Update AssayFilter from Factory', function(){
+  describe('Refresh and Update AssayFilter from Factory', function(){
 
 
     it("refreshAssayFilter is method", function(){
@@ -53,7 +53,7 @@ describe('Controller: Assay Files Util Modal Ctrl', function(){
       expect(mockAssayAttributes).toEqual(true);
     });
 
-    it("updateAssayAttributes is  method", function(){
+    it("updateAssayAttributes is method", function(){
       expect(angular.isFunction(ctrl.updateAssayAttributes)).toBe(true);
     });
 
@@ -69,6 +69,40 @@ describe('Controller: Assay Files Util Modal Ctrl', function(){
 
       ctrl.updateAssayAttributes();
       expect(mockPostAssayAttributes).toEqual(true);
+    });
+
+    it("updateAttributeRank method", function(){
+      expect(angular.isFunction(ctrl.updateAttributeRank)).toBe(true);
+    });
+
+    it("updateAttributeRank returns promise", function(){
+      var mockGetAssayAttributes = false;
+
+      //Mock the array after a drag and drop movement
+      ctrl.assayAttributeOrder = [
+        {'solr_field': 'Title', 'rank':1},
+        {'solr_field': 'Character', 'rank':2},
+        {'solr_field': 'Cell Type', 'rank':3},
+        {'solr_field': 'Name', 'rank':4},
+        {'solr_field': 'Character', 'rank':2}
+      ];
+      spyOn(ctrl, "updateAssayAttributes").and.callFake(function() {
+        return {
+          then: function () {
+            mockPostAssayAttributes = true;
+          }
+        };
+      });
+
+      ctrl.updateAttributeRank({'solr_field': 'Character', 'rank':2}, 1);
+      expect(mockPostAssayAttributes).toEqual(true);
+      //confirm removal of duplication in method
+      expect(ctrl.assayAttributeOrder.length).toEqual(4);
+      //check ranks are updated locally
+      expect(ctrl.assayAttributeOrder[3].rank).toEqual(4);
+      expect(ctrl.assayAttributeOrder[2].rank).toEqual(3);
+      expect(ctrl.assayAttributeOrder[1].rank).toEqual(2);
+      expect(ctrl.assayAttributeOrder[0].rank).toEqual(1);
     });
 
   });
