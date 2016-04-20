e02657a0c042290555a03ada5736af007e2531a5
J.Marx <jennifer@jkmarx.com>
2016-04-20 13:13:17 -0400

    Fix eslint in assay filters modal ctrl.

--- refinery/ui/source/js/file-browser/assay-files-util-modal-ctrl.js
+++ refinery/ui/source/js/file-browser/assay-files-util-modal-ctrl.js
@@ -1,59 +1,46 @@
-angular
-  .module('refineryFileBrowser')
-  .controller('AssayFilesUtilModalCtrl',
-    [
-      '$scope',
-      '$uibModalInstance',
-      '$window',
-      'fileBrowserFactory',
-      'resetGridService',
-      AssayFilesUtilModalCtrl
-    ]
-  );
+'use strict';
 
-
-function AssayFilesUtilModalCtrl(
+function AssayFilesUtilModalCtrl (
   $scope,
   $uibModalInstance,
   $window,
   fileBrowserFactory,
   resetGridService) {
-
   var vm = this;
   vm.assayAttributeOrder = [];
 
-  //modal close button
+  // modal close button
   $scope.close = function () {
     resetGridService.setResetGridFlag(true);
     $uibModalInstance.close('close');
   };
 
-  //Refresh attribute lists when modal opens
+  // Refresh attribute lists when modal opens
   vm.refreshAssayAttributes = function () {
-    var assay_uuid = $window.externalAssayUuid;
-    return fileBrowserFactory.getAssayAttributeOrder(assay_uuid).then(function(){
+    var assayUuid = $window.externalAssayUuid;
+    return fileBrowserFactory.getAssayAttributeOrder(assayUuid).then(function () {
       vm.assayAttributeOrder = fileBrowserFactory.assayAttributeOrder;
-    },function(error){
-      console.log(error);
+    }, function (error) {
+      console.error(error);
     });
   };
 
-  //Update ranks and attributes for owners
-  vm.updateAssayAttributes = function(attributeParam){
+  // Update ranks and attributes for owners
+  vm.updateAssayAttributes = function (attributeParam) {
     fileBrowserFactory.postAssayAttributeOrder(attributeParam).then(function () {
     });
   };
 
-  //update rank of item moved
-  vm.updateAttributeRank = function(attributeObj, index){
-    //when item is moved, it's duplication is removed
+  // update rank of item moved
+  vm.updateAttributeRank = function (attributeObj, index) {
+    // when item is moved, it's duplication is removed
     vm.assayAttributeOrder.splice(index, 1);
 
-    for(var i=0; i<vm.assayAttributeOrder.length; i++){
-      //locally update all ranks
+    for (var i = 0; i < vm.assayAttributeOrder.length; i++) {
+      // locally update all ranks
       vm.assayAttributeOrder[i].rank = i + 1;
-      if(vm.assayAttributeOrder[i].solr_field === attributeObj.solr_field){
-        //post rank update for attribute moved
+      if (vm.assayAttributeOrder[i].solr_field === attributeObj.solr_field) {
+        // post rank update for attribute moved
         vm.updateAssayAttributes(vm.assayAttributeOrder[i]);
       }
     }
@@ -61,3 +48,17 @@ function AssayFilesUtilModalCtrl(
 
   vm.refreshAssayAttributes();
 }
+
+angular
+  .module('refineryFileBrowser')
+  .controller(
+  'AssayFilesUtilModalCtrl',
+  [
+    '$scope',
+    '$uibModalInstance',
+    '$window',
+    'fileBrowserFactory',
+    'resetGridService',
+    AssayFilesUtilModalCtrl
+  ]
+  );
--- refinery/ui/source/js/file-browser/assay-files-util-modal-ctrl.spec.js
+++ refinery/ui/source/js/file-browser/assay-files-util-modal-ctrl.spec.js
@@ -1,20 +1,20 @@
 /** Unit Tests **/
+'use strict';
 
-//Global variable for both test and ctrl.
-var externalAssayUuid = 'x508x83x-x9xx-4740-x9x7-x7x0x631280x';
+// Global variable for both test and ctrl.
 
-describe('Controller: Assay Files Util Modal Ctrl', function(){
-  var ctrl,
-      scope,
-      factory;
+describe('Controller: Assay Files Util Modal Ctrl', function () {
+  var ctrl;
+  var scope;
+  var factory;
 
   beforeEach(module('refineryApp'));
   beforeEach(module('refineryFileBrowser'));
-  beforeEach(inject(function( $rootScope, _$controller_, _fileBrowserFactory_) {
+  beforeEach(inject(function ($rootScope, _$controller_, _fileBrowserFactory_) {
     scope = $rootScope.$new();
-    //Create mock version of dependency
-    var $uibModalInstance = { close: function() {} };
-    $controller = _$controller_;
+    // Create mock version of dependency
+    var $uibModalInstance = { close: function () {} };
+    var $controller = _$controller_;
     ctrl = $controller(
       'AssayFilesUtilModalCtrl',
       {
@@ -24,24 +24,22 @@ describe('Controller: Assay Files Util Modal Ctrl', function(){
     factory = _fileBrowserFactory_;
   }));
 
-  it('AssayFilesUtilModalCtrl ctrl should exist', function() {
+  it('AssayFilesUtilModalCtrl ctrl should exist', function () {
     expect(ctrl).toBeDefined();
   });
 
-  it('Data & UI displays variables should exist for views', function() {
-    expect(ctrl.assayAttributeOrder ).toEqual([]);
+  it('Data & UI displays variables should exist for views', function () {
+    expect(ctrl.assayAttributeOrder).toEqual([]);
   });
 
-  describe('Refresh and Update AssayFilter from Factory', function(){
-
-
-    it("refreshAssayFilter is method", function(){
+  describe('Refresh and Update AssayFilter from Factory', function () {
+    it('refreshAssayFilter is method', function () {
       expect(angular.isFunction(ctrl.refreshAssayAttributes)).toBe(true);
     });
 
-    it("refreshAssayFiles returns promise", function(){
-      var mockAssayFiles = false;
-      spyOn(factory, "getAssayAttributeOrder").and.callFake(function() {
+    it('refreshAssayFiles returns promise', function () {
+      var mockAssayAttributes = false;
+      spyOn(factory, 'getAssayAttributeOrder').and.callFake(function () {
         return {
           then: function () {
             mockAssayAttributes = true;
@@ -53,13 +51,13 @@ describe('Controller: Assay Files Util Modal Ctrl', function(){
       expect(mockAssayAttributes).toEqual(true);
     });
 
-    it("updateAssayAttributes is method", function(){
+    it('updateAssayAttributes is method', function () {
       expect(angular.isFunction(ctrl.updateAssayAttributes)).toBe(true);
     });
 
-    it("updateAssayAttributes returns promise", function(){
-      var mockGetAssayAttributes = false;
-      spyOn(factory, "postAssayAttributeOrder").and.callFake(function() {
+    it('updateAssayAttributes returns promise', function () {
+      var mockPostAssayAttributes = false;
+      spyOn(factory, 'postAssayAttributeOrder').and.callFake(function () {
         return {
           then: function () {
             mockPostAssayAttributes = true;
@@ -71,22 +69,22 @@ describe('Controller: Assay Files Util Modal Ctrl', function(){
       expect(mockPostAssayAttributes).toEqual(true);
     });
 
-    it("updateAttributeRank method", function(){
+    it('updateAttributeRank method', function () {
       expect(angular.isFunction(ctrl.updateAttributeRank)).toBe(true);
     });
 
-    it("updateAttributeRank returns promise", function(){
-      var mockGetAssayAttributes = false;
+    it('updateAttributeRank returns promise', function () {
+      var mockPostAssayAttributes = false;
 
-      //Mock the array after a drag and drop movement
+      // Mock the array after a drag and drop movement
       ctrl.assayAttributeOrder = [
-        {'solr_field': 'Title', 'rank':1},
-        {'solr_field': 'Character', 'rank':2},
-        {'solr_field': 'Cell Type', 'rank':3},
-        {'solr_field': 'Name', 'rank':4},
-        {'solr_field': 'Character', 'rank':2}
+        { solr_field: 'Title', rank: 1 },
+        { solr_field: 'Character', rank: 2 },
+        { solr_field: 'Cell Type', rank: 3 },
+        { solr_field: 'Name', rank: 4 },
+        { solr_field: 'Character', rank: 2 }
       ];
-      spyOn(ctrl, "updateAssayAttributes").and.callFake(function() {
+      spyOn(ctrl, 'updateAssayAttributes').and.callFake(function () {
         return {
           then: function () {
             mockPostAssayAttributes = true;
@@ -94,17 +92,15 @@ describe('Controller: Assay Files Util Modal Ctrl', function(){
         };
       });
 
-      ctrl.updateAttributeRank({'solr_field': 'Character', 'rank':2}, 1);
+      ctrl.updateAttributeRank({ solr_field: 'Character', rank: 2 }, 1);
       expect(mockPostAssayAttributes).toEqual(true);
-      //confirm removal of duplication in method
+      // confirm removal of duplication in method
       expect(ctrl.assayAttributeOrder.length).toEqual(4);
-      //check ranks are updated locally
+      // check ranks are updated locally
       expect(ctrl.assayAttributeOrder[3].rank).toEqual(4);
       expect(ctrl.assayAttributeOrder[2].rank).toEqual(3);
       expect(ctrl.assayAttributeOrder[1].rank).toEqual(2);
       expect(ctrl.assayAttributeOrder[0].rank).toEqual(1);
     });
-
   });
-
 });
