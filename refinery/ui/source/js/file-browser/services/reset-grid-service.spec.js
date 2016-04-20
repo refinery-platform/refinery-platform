6f4ab45e78547fe262a29cbb3d8cba46b889b969
J.Marx <jennifer@jkmarx.com>
2016-04-20 12:36:06 -0400

    Fix eslint errors in reset grid service.

--- refinery/ui/source/js/file-browser/services/reset-grid-service.spec.js
+++ refinery/ui/source/js/file-browser/services/reset-grid-service.spec.js
@@ -1,23 +1,20 @@
-describe("Reset-Grid-Service", function(){
- // 'use strict';
-  var factory,
-      query,
-      deferred,
-      rootScope;
+'use strict';
+
+describe('Reset-Grid-Service', function () {
+  var service;
 
   beforeEach(module('refineryApp'));
   beforeEach(module('refineryFileBrowser'));
-  beforeEach(inject( function(_resetGridService_){
+  beforeEach(inject(function (_resetGridService_) {
     service = _resetGridService_;
   }));
 
-  it('factory and tools variables should exist', function(){
+  it('factory and tools variables should exist', function () {
     expect(service).toBeDefined();
     expect(service.resetGridFlag).toEqual(false);
   });
 
-  describe("resetGridFlag", function() {
-
+  describe('resetGridFlag', function () {
     it('setResetGridFlag is a method', function () {
       expect(angular.isFunction(service.setResetGridFlag)).toBe(true);
     });
@@ -25,9 +22,8 @@ describe("Reset-Grid-Service", function(){
     it('setResetGridFlag updates flag', function () {
       service.setResetGridFlag(true);
       expect(service.resetGridFlag).toEqual(true);
-        service.setResetGridFlag(false);
+      service.setResetGridFlag(false);
       expect(service.resetGridFlag).toEqual(false);
     });
   });
-
 });
