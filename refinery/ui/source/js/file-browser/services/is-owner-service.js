664b32b83e3ddebbe3df6b98ca3f8e9f80611053
J.Marx <jennifer@jkmarx.com>
2016-04-20 11:04:06 -0400

    Update hyphen to camelcase and eslint errors.

--- refinery/ui/source/js/file-browser/services/is-owner-service.js
+++ refinery/ui/source/js/file-browser/services/is-owner-service.js
@@ -1,22 +1,23 @@
-angular.module('refineryFileBrowser')
-    .service("isOwnerService", [
-      'dataSetService',
-      '$window',
-      isOwnerService
-  ]
-);
+'use strict';
 
-function isOwnerService(dataSetService, $window) {
+function isOwnerService (dataSetService, $window) {
   var vm = this;
-  vm.is_owner = false;
+  vm.isOwner = false;
 
-  vm.refreshDataSetOwner = function (){
-    var params = {uuid: $window.dataSetUuid};
+  vm.refreshDataSetOwner = function () {
+    var params = { uuid: $window.dataSetUuid };
     var dataSet = dataSetService.query(params);
-    dataSet.$promise.then(function(response){
-      vm.is_owner = response.objects[0].is_owner;
+    dataSet.$promise.then(function (response) {
+      vm.isOwner = response.objects[0].is_owner;
     });
     return dataSet.$promise;
   };
-
 }
+
+angular.module('refineryFileBrowser')
+  .service('isOwnerService', [
+    'dataSetService',
+    '$window',
+    isOwnerService
+  ]
+);
