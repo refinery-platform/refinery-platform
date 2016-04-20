c254af6b898e4795dbc88f720227aeee2556756c
J.Marx <jennifer@jkmarx.com>
2016-04-20 12:30:43 -0400

    Fix eslint errors in reset grid service.

--- refinery/ui/source/js/file-browser/services/reset-grid-service.js
+++ refinery/ui/source/js/file-browser/services/reset-grid-service.js
@@ -1,15 +1,16 @@
-angular.module('refineryFileBrowser')
-    .service("resetGridService", [
-      resetGridService
-  ]
-);
+'use strict';
 
-function resetGridService() {
+function resetGridService () {
   var vm = this;
   vm.resetGridFlag = false;
 
   vm.setResetGridFlag = function (state) {
-     vm.resetGridFlag = state;
+    vm.resetGridFlag = state;
   };
-
 }
+
+angular.module('refineryFileBrowser')
+  .service('resetGridService', [
+    resetGridService
+  ]
+);
