7cea5b1be27e46e92060e3d49915659adc134011
David Jones <drj@pobox.com>
2016-04-21 10:25:22 +0100

    Use CloudFormation to discover region

--- deployment/stack.py
+++ deployment/stack.py
@@ -84,6 +84,7 @@ def main():
     user_data_script = functions.join(
         "",
         "#!/bin/sh\n",
+        "AWS_DEFAULT_REGION=", functions.ref("AWS::Region"), "\n",
         "RDS_NAME=", config['RDS_NAME'], "\n",
         "RDS_SUPERUSER_PASSWORD=", config['RDS_SUPERUSER_PASSWORD'], "\n",
         "RDS_ROLE=", config['RDS_ROLE'], "\n",
