39c39358db73b806bf1d721cac31d09a20ab1d7e
J.Marx <jennifer@jkmarx.com>
2016-04-28 15:15:53 -0400

    Add dependency unit test to modules.

--- refinery/ui/source/js/data-set-explorer/module.spec.js
+++ refinery/ui/source/js/data-set-explorer/module.spec.js
@@ -12,4 +12,23 @@ describe('RefineryDataSetExplorer.module: unit tests', function () {
       expect(!!module).toEqual(true);
     });
   });
+
+  describe('Dependencies:', function () {
+    var deps;
+    var hasModule = function (m) {
+      return deps.indexOf(m) >= 0;
+    };
+
+    beforeEach(function () {
+      deps = module.value('refineryDataSetExplorer').requires;
+    });
+
+    it('should have "ui-select" as a dependency', function () {
+      expect(hasModule('ui.select')).toEqual(true);
+    });
+
+    it('should have "ngSanitize" as a dependency', function () {
+      expect(hasModule('ngSanitize')).toEqual(true);
+    });
+  });
 });
