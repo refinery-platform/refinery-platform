c8d4111224c0d1f29fc96c9a547208f240d87d1f
David Jones <drj@pobox.com>
2016-05-03 15:38:46 +0100

    Create volume in CloudFormation template

--- deployment/stack.py
+++ deployment/stack.py
@@ -38,6 +38,7 @@ import datetime
 import glob
 import json     # for json.dumps
 import os       # for os.popen
+import random
 import sys      # sys.stderr, sys.exit, and so on
 
 # https://pypi.python.org/pypi/boto3
@@ -112,7 +113,7 @@ def main():
 
     rds_properties = {
         "AllocatedStorage": "5",
-        "AvailabilityZone": "us-east-1d",       # todo:?
+        "AvailabilityZone": config['AVAILABILITY_ZONE'],
         "BackupRetentionPeriod": "0",
         "DBInstanceClass": "db.t2.small",       # todo:?
         "DBInstanceIdentifier": config['RDS_NAME'],
@@ -136,6 +137,24 @@ def main():
         core.Properties(rds_properties),
         core.DeletionPolicy("Snapshot"),
         )
+
+    volume_properties = {
+        'AvailabilityZone': config['AVAILABILITY_ZONE'],
+        'Encrypted': True,
+        'Size': 100,
+        'Tags': tags.load(),
+        'VolumeType': 'gp2'
+    }
+
+    if 'DATA_SNAPSHOT' in config:
+        volume_properties['SnapshotId'] = config['DATA_SNAPSHOT']
+
+    # http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-ec2-ebs-volume.html
+    cft.resources.ebs = core.Resource(
+        'RefineryData', 'AWS::EC2::Volume',
+        core.Properties(volume_properties),
+        core.DeletionPolicy("Snapshot"),
+    )
 
     cft.resources.ec2_instance = core.Resource(
         'WebInstance', 'AWS::EC2::Instance',
@@ -239,7 +258,7 @@ def main():
         core.Properties({
             'Device': '/dev/xvdr',
             'InstanceId': functions.ref('WebInstance'),
-            'VolumeId': config['VOLUME']
+            'VolumeId': functions.ref('RefineryData'),
         })
     )
 
@@ -272,6 +291,17 @@ def load_config():
         sys.stderr.write("{:s} must have values for:\n{!r}\n".format(
             config_dir, bad))
         raise ConfigError()
+
+    # These keys were previously used, so might be set from old
+    # config files or documentation. We warn if we see them.
+    ignored = ['VOLUME']
+    bad = []
+    for key in ignored:
+        if key in config:
+            bad.append(key)
+    if bad:
+        sys.stderr.write("{:s} no longer used, ignoring\n".format(
+            bad))
 
     config.setdefault('RDS_SUPERUSER_PASSWORD', 'mypassword')
     if 'RDS_NAME' not in config:
@@ -329,7 +359,6 @@ def random_alnum(n):
     of length `n`.
     """
 
-    import random
     import string
 
     return ''.join(
@@ -342,23 +371,35 @@ def derive_config(config):
     Modify `config` so that extra, derived, configuration is
     added to it.
 
-    The only case at the moment is that the availability zone of
-    the VOLUME is added so that the instance can share the same
-    availability zone.
+    The only case at the moment is that
+    (unless already supplied)
+    an availability zone is selected
+    (at random).
     """
 
-    # Discover the Availability Zone for the volume,
-    # and add it to the config.
-    ec2 = boto3.resource('ec2')
-    volume = ec2.Volume(config['VOLUME'])
-    # http://boto3.readthedocs.org/en/latest/reference/services/ec2.html#volume
-    az = volume.availability_zone
+    if 'AVAILABILITY_ZONE' not in config:
+        az = choose_availability_zone()
+        config['AVAILABILITY_ZONE'] = az
 
-    # If AVAILABILITY_ZONE is already set, it must match.
-    if 'AVAILABILITY_ZONE' in config:
-        assert config['AVAILABILITY_ZONE'] == az
 
-    config['AVAILABILITY_ZONE'] = az
+def choose_availability_zone():
+    """
+    Choose, at random, an availability zone from amongst the
+    zones available to this AWS account
+    (the list of zones varies from account to account).
+    """
+
+    # http://boto3.readthedocs.org/en/latest/
+    import boto3
+
+    ec2 = boto3.client('ec2')
+
+    # http://boto3.readthedocs.org/en/latest/reference/services/ec2.html#EC2.Client.describe_availability_zones
+    res = ec2.describe_availability_zones()
+
+    zones = res['AvailabilityZones']
+    zoneids = [z['ZoneName'] for z in zones]
+    return random.choice(zoneids)
 
 
 if __name__ == '__main__':
