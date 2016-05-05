1cfd53b937f9992479f9cfc58108d90974085cdd
Scott Ouellette <scottx611x@gmail.com>
2016-05-05 12:18:39 -0400

    Fix Test

--- refinery/file_store/tests.py
+++ refinery/file_store/tests.py
@@ -139,7 +139,7 @@ class FileStoreItemTest(SimpleTestCase):
         # create FileStoreItem instances without any disk operations
         item_from_url = FileStoreItem.objects.create(source=self.url_source,
                                                      sharename=self.sharename)
-        self.assertEqual(item_from_url.get_datafile_url(),
+        self.assertEqual(get_full_url(item_from_url.get_datafile_url()),
                          item_from_url.source)
 
     def test_get_file_extension(self):
