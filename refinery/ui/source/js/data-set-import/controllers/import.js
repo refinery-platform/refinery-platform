729c816c7b136861237e3b337f44d8c5d60a10d0
Fritz Lekschas <service@lekschas.de>
2016-05-11 15:32:15 -0400

    Simplify template and control visibility of the two import strategies by fading out the inactive one to avoid clicking within the wrong form.

--- refinery/ui/source/js/data-set-import/controllers/import.js
+++ refinery/ui/source/js/data-set-import/controllers/import.js
@@ -2,6 +2,26 @@
 
 function RefineryImportCtrl () {}
 
+Object.defineProperty(
+  RefineryImportCtrl.prototype,
+  'isDisabledTabularImport', {
+    enumerable: true,
+    get: function () {
+      return this.option === 'isaTab';
+    }
+  }
+);
+
+Object.defineProperty(
+  RefineryImportCtrl.prototype,
+  'isDisabledIsaTabImport', {
+    enumerable: true,
+    get: function () {
+      return this.option === 'tabular';
+    }
+  }
+);
+
 angular
   .module('refineryDataSetImport')
   .controller('RefineryImportCtrl', [
