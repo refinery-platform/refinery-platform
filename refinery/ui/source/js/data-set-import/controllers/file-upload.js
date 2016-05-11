e79ac6221d7f7d27b5f90bf5b45ea337a2ef81aa
Fritz Lekschas <service@lekschas.de>
2016-05-11 10:49:05 -0400

    Updated the controller to not rely on legacy csrftoken input fields from Django templates and use an Angular service instead of jQuery AJAX call.

--- refinery/ui/source/js/data-set-import/controllers/file-upload.js
+++ refinery/ui/source/js/data-set-import/controllers/file-upload.js
@@ -6,12 +6,31 @@ function RefineryFileUploadCtrl (
   $scope,
   $timeout,
   $,
+  chunkedUploadService,
   settings,
   SparkMD5,
   dataSetImportSettings,
-  $uibModal
+  $uibModal,
+  getCookie
 ) {
-  var csrf = '';
+  var csrftoken = getCookie('csrftoken');
+
+  // The next function and jQuery call ensure that the `csrftoken` is used for
+  // every request. This is needed because the _jQuery file upload_ plugin uses
+  // jQuery's internal AJAX methods.
+  function csrfSafeMethod (method) {
+    // these HTTP methods do not require CSRF protection
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+  }
+  $.ajaxSetup({
+    beforeSend: function (xhr, settingsTwo) {
+      if (!csrfSafeMethod(settingsTwo.type) && !this.crossDomain) {
+        xhr.setRequestHeader('X-CSRFToken', csrftoken);
+      }
+    }
+  });
+
+  // var csrf = '';
   var formData = [];
   var md5 = {};
   var totalNumFilesQueued = 0;
@@ -27,16 +46,6 @@ function RefineryFileUploadCtrl (
   // progresses but the stripes are not displayed
   $scope.uploadActive = true;
   $scope.loadingFiles = false;
-
-  if ($('input[name=\'csrfmiddlewaretoken\']')[0]) {
-    csrf = $('input[name=\'csrfmiddlewaretoken\']')[0].value;
-    formData = [{
-      name: 'csrfmiddlewaretoken',
-      value: csrf
-    }];
-  } else {
-    $log.error('CSRF middleware token was not found in the upload form');
-  }
 
   var worker = false;
 
@@ -150,48 +159,47 @@ function RefineryFileUploadCtrl (
     }
   };
 
-  var uploadDone = function (e, data) {
+  var uploadDone = function (event, data) {
     var file = data.files[0];
 
-    $.ajax({
-      type: 'POST',
-      url: dataSetImportSettings.uploadCompleteUrl,
-      data: {
-        csrfmiddlewaretoken: csrf,
-        upload_id: data.result.upload_id,
-        md5: md5[file.name]
-      },
-      dataType: 'json',
-      success: function () {
-        totalNumFilesUploaded++;
+    function success () {
+      totalNumFilesUploaded++;
 
-        file.uploaded = true;
+      file.uploaded = true;
 
-        if ($element.fileupload('active') > 0) {
-          $scope.uploadActive = true;
-          $scope.uploadInProgress = true;
-        } else {
-          $scope.uploadActive = false;
-          $scope.uploadInProgress = false;
-        }
-
-        if (totalNumFilesUploaded === totalNumFilesQueued) {
-          $scope.allUploaded = true;
-          $scope.uploadActive = false;
-          $scope.uploadInProgress = false;
-        }
-
-        $timeout(function () {
-          // Fritz: I am not sure why we need to wait 100ms instead of 0ms
-          // (i.e. one digestion) but this solves the issues with the last
-          // progress bar not being changed into success mode.
-          $scope.$apply();
-        }, 100);
-      },
-      error: function (jqXHR, textStatus, errorThrown) {
-        $log.error('Error uploading file:', textStatus, '-', errorThrown);
+      if ($element.fileupload('active') > 0) {
+        $scope.uploadActive = true;
+        $scope.uploadInProgress = true;
+      } else {
+        $scope.uploadActive = false;
+        $scope.uploadInProgress = false;
       }
-    });
+
+      if (totalNumFilesUploaded === totalNumFilesQueued) {
+        $scope.allUploaded = true;
+        $scope.uploadActive = false;
+        $scope.uploadInProgress = false;
+      }
+
+      $timeout(function () {
+        // Fritz: I am not sure why we need to wait 100ms instead of 0ms
+        // (i.e. one digestion) but this solves the issues with the last
+        // progress bar not being changed into success mode.
+        $scope.$apply();
+      }, 100);
+    }
+
+    function error (errorMessage) {
+      $log.error('Error uploading file!', errorMessage);
+    }
+
+    chunkedUploadService.save({
+      upload_id: data.result.upload_id,
+      md5: md5[file.name]
+    })
+    .$promise
+    .then(success)
+    .catch(error);
   };
 
   var getFormData = function () {
@@ -300,9 +308,11 @@ angular
     '$scope',
     '$timeout',
     '$',
+    'chunkedUploadService',
     'settings',
     'SparkMD5',
     'dataSetImportSettings',
     '$uibModal',
+    'getCookie',
     RefineryFileUploadCtrl
   ]);
