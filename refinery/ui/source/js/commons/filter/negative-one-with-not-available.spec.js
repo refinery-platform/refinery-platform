4a84fbb42a76cbfc7b0de1a7f477fdd92c20e31e
Jennifer Marx <jennyrutledge@gmail.com>
2016-05-20 23:06:41 -0400

    Add unit test for common filter.

---
+++ refinery/ui/source/js/commons/filter/negative-one-with-not-available.spec.js
@@ -0,0 +1,26 @@
+'use strict';
+
+describe('Filter: Negative One With Not Available Spec', function () {
+  var filter;
+
+  beforeEach(function () {
+    module('refineryApp');
+    inject(function ($filter) {
+      filter = $filter('negativeOneWithNotAvailable');
+    });
+  });
+
+  it('filter should exist', function () {
+    expect(filter).toBeDefined();
+  });
+
+  it('should return param with non -1 values', function () {
+    expect(filter('')).toBe('');
+    expect(filter(2)).toBe(2);
+    expect(filter('None')).toBe('None');
+  });
+
+  it('should return N/A with param === -1', function () {
+    expect(filter(-1)).toBe('N/A');
+  });
+});
