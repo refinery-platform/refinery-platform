2886c5b529b03970cf0832ef58e7dc3704ef2214
David Jones <drj@pobox.com>
2016-05-23 16:40:03 +0100

    Get RDS Facter variables direct from cloud init

--- deployment/aws.sh
+++ deployment/aws.sh
@@ -42,15 +42,16 @@ export S3_CONFIG_URI
 # Tag the attached root volume
 sh /srv/refinery-platform/deployment/bin/fix-untagged-volumes
 
-# Discover IP endpoint for our PostgreSQL RDS, and place it in
-# environment variables for puppet/facter to use
-: ${RDS_NAME?RDS_NAME must be set}
-bin/aws-rds-endpoint "$RDS_NAME" > /home/ubuntu/rds
+# CloudFormation should have set the endpoint
+# (DNS address and port) for our PostgreSQL RDS.
+# Copy them so that puppet/facter can use it.
+: ${RDS_ENDPOINT_ADDRESS?RDS_ENDPOINT_ADDRESS must be set}
+: ${RDS_ENDPOINT_PORT?RDS_ENDPOINT_PORT must be set}
 
 # FACTER environment variables become facts for puppet;
 # see https://puppetlabs.com/blog/facter-part-1-facter-101
-export FACTER_RDS_HOST=$(jq -r .Address /home/ubuntu/rds)
-export FACTER_RDS_PORT=$(jq -r .Port /home/ubuntu/rds)
+export FACTER_RDS_HOST=$RDS_ENDPOINT_ADDRESS
+export FACTER_RDS_PORT=$RDS_ENDPOINT_PORT
 export FACTER_RDS_ROLE="$RDS_ROLE"
 
 # Create SMTP credentials and
