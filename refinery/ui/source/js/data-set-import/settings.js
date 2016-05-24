75b9b8910d4e2532ebde618eac85842a161c6c19
Fritz Lekschas <service@lekschas.de>
2016-05-24 13:04:02 +0200

    Add metadata table import API URL.

--- refinery/ui/source/js/data-set-import/settings.js
+++ refinery/ui/source/js/data-set-import/settings.js
@@ -5,6 +5,7 @@ angular
   .constant('dataSetImportSettings', {
     checkFiles: '/data_set_manager/import/check_files/',
     isaTabImportUrl: '/data_set_manager/import/isa-tab-form/',
+    tabularFileImportUrl: '/data_set_manager/import/metadata-table-form/',
     uploadUrl: '/data_set_manager/import/chunked-upload/',
     uploadCompleteUrl: '/data_set_manager/import/chunked-upload-complete/',
     chunkSize: 10 * 1024 * 1024  // 10 MB
