35c5e7f441f70c35b56afee776ee62c6c5e54624
Jennifer Marx <jennyrutledge@gmail.com>
2016-06-14 16:22:01 -0400

    Add serializer for node group.

--- refinery/core/serializers.py
+++ refinery/core/serializers.py
@@ -1,6 +1,26 @@
 from rest_framework import serializers
 
-from .models import Workflow
+from .models import Workflow, NodeGroup
+
+
+class NodeGroupSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = NodeGroup
+        fields = ('id', 'uuid', 'node_count', 'is_implicit', 'study',
+                  'assay', 'is_current', 'nodes_uuids')
+
+        def update(self, instance, validated_data):
+            """
+            Update and return an existing `NodeGroup` instance, given the
+            validated data.
+            """
+            instance.node_count = validated_data.get('node_count',
+                                                     instance.node_count)
+            instance.nodes_uuids = validated_data.get('nodes_uuid',
+                                                      instance.nodes_uuid)
+            instance.save()
+            return instance
 
 
 class WorkflowSerializer(serializers.HyperlinkedModelSerializer):
