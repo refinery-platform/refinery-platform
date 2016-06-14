fe59a34880fd3edff9ee1ae0bf88f268fd55f370
Jennifer Marx <jennyrutledge@gmail.com>
2016-06-14 13:30:41 -0400

    Reformat.

--- refinery/ui/source/js/data-set-about/ctrls/details-ctrl.js
+++ refinery/ui/source/js/data-set-about/ctrls/details-ctrl.js
@@ -15,44 +15,52 @@ function AboutDetailsCtrl (
   vm.fileStoreItem = {};
 
   vm.refreshDataSetStats = function () {
-    dataSetAboutFactory.getDataSet(vm.dataSetUuid).then(function () {
-      vm.dataSet = dataSetAboutFactory.dataSet;
-      // grab meta-data info
-      if (dataSetAboutFactory.dataSet.isa_archive) {
-        vm.refreshFileStoreItem(dataSetAboutFactory.dataSet.isa_archive);
-      } else if (dataSetAboutFactory.dataSet.pre_isatab_archive) {
-        vm.refreshFileStoreItem(dataSetAboutFactory.dataSet.pre_isa_archive);
-      }
-    }, function (error) {
-      $log.error(error);
-    });
+    dataSetAboutFactory
+      .getDataSet(vm.dataSetUuid)
+      .then(function () {
+        vm.dataSet = dataSetAboutFactory.dataSet;
+        // grab meta-data info
+        if (dataSetAboutFactory.dataSet.isa_archive) {
+          vm.refreshFileStoreItem(dataSetAboutFactory.dataSet.isa_archive);
+        } else if (dataSetAboutFactory.dataSet.pre_isatab_archive) {
+          vm.refreshFileStoreItem(dataSetAboutFactory.dataSet.pre_isa_archive);
+        }
+      }, function (error) {
+        $log.error(error);
+      });
   };
 
   vm.refreshStudies = function () {
-    dataSetAboutFactory.getStudies(vm.dataSetUuid).then(function () {
-      vm.studies = dataSetAboutFactory.studies;
-      for (var i = 0; i < vm.studies.length; i++) {
-        vm.refreshAssays(vm.studies[i].uuid);
-      }
-    }, function (error) {
-      $log.error(error);
-    });
+    dataSetAboutFactory
+      .getStudies(vm.dataSetUuid)
+      .then(function () {
+        vm.studies = dataSetAboutFactory.studies;
+        for (var i = 0; i < vm.studies.length; i++) {
+          vm.refreshAssays(vm.studies[i].uuid);
+        }
+      }, function (error) {
+        $log.error(error);
+      });
   };
 
   vm.refreshAssays = function (studyUuid) {
-    dataSetAboutFactory.getStudysAssays(studyUuid).then(function () {
-      vm.assays[studyUuid] = dataSetAboutFactory.assays;
-    }, function (error) {
-      $log.error(error);
-    });
+    dataSetAboutFactory
+      .getStudysAssays(studyUuid)
+      .then(function () {
+        vm.assays[studyUuid] = dataSetAboutFactory.assays;
+      }, function (error) {
+        $log.error(error);
+      });
   };
 
   vm.refreshFileStoreItem = function (isaUuid) {
-    dataSetAboutFactory.getFileStoreItem(isaUuid).then(function () {
-      vm.fileStoreItem = dataSetAboutFactory.fileStoreItem;
-    }, function (error) {
-      $log.error(error);
-    });
+    dataSetAboutFactory
+      .getFileStoreItem(isaUuid)
+      .then(function () {
+        vm.fileStoreItem = dataSetAboutFactory.fileStoreItem;
+      }, function (error) {
+        $log.error(error);
+      });
   };
 
   vm.refreshDataSetStats();
