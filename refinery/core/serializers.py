b664ba1ec16597f33ea481248c31b771e41bf112
Jennifer Marx <jennyrutledge@gmail.com>
2016-06-19 18:13:56 -0400

    Fix serializers to add nodes_ids after group creation.

--- refinery/core/serializers.py
+++ refinery/core/serializers.py
@@ -4,36 +4,41 @@ from .models import Workflow, NodeGroup
 
 
 class NodeGroupSerializer(serializers.ModelSerializer):
+    nodes_ids = serializers.PrimaryKeyRelatedField(many=True,
+                                                   read_only=True,
+                                                   required=False)
+
     class Meta:
         model = NodeGroup
         fields = ('id', 'uuid', 'node_count', 'is_implicit', 'study',
-                  'assay', 'is_current', 'nodes_uuids', 'name')
+                  'assay', 'is_current', 'nodes_ids', 'name')
 
-        def create(self, validated_data):
-            node_group = NodeGroup.objects.create(
-                study=validated_data.get('study'),
-                assay=validated_data.get('assay'),
-                name=validated_data.get('name')
-            )
-            if validated_data.get('node_count'):
-                node_group.node_count = validated_data.get('node_count')
-                node_group.save()
-            if validated_data.get('nodes_uuids'):
-                node_group.nodes_uuids = validated_data.get('nodes_uuids')
-                node_group.save()
-            return node_group
+    def create(self, validated_data):
+        node_group = NodeGroup.objects.create(
+            study=validated_data.get('study'),
+            assay=validated_data.get('assay'),
+            name=validated_data.get('name')
+        )
+        """Had to add nodes_ids after group creation. List does not show up in
+        validate_data due to read_only attribute."""
+        if self.initial_data.get('nodes_ids'):
+            node_group.nodes_ids.add(*self.initial_data.get('nodes_ids'))
+            node_group.nod_count = len(self.initial_data.get('nodes_ids'))
+            node_group.save()
 
-        def update(self, instance, validated_data):
-            """
-            Update and return an existing `NodeGroup` instance, given the
-            validated data.
-            """
-            instance.node_count = validated_data.get('node_count',
-                                                     instance.node_count)
-            instance.nodes_uuids = validated_data.get('nodes_uuids',
-                                                      instance.nodes_uuids)
-            instance.save()
-            return instance
+        return node_group
+
+    def update(self, instance, validated_data):
+        """
+        Update and return an existing `NodeGroup` instance, given the
+        validated data.
+        """
+        instance.node_count = validated_data.get('node_count',
+                                                 instance.node_count)
+        instance.nodes_uuids = validated_data.get('nodes_uuids',
+                                                  instance.nodes_uuids)
+        instance.save()
+        return instance
 
 
 class WorkflowSerializer(serializers.HyperlinkedModelSerializer):
