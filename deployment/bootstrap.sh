955da772051ce8ef947145365ec4374bd870dbac
David Jones <drj@pobox.com>
2016-07-05 15:16:38 +0100

    Bump librarian-puppet version
    This fixes the gem install bug.

--- deployment/bootstrap.sh
+++ deployment/bootstrap.sh
@@ -8,4 +8,4 @@ export DEBIAN_FRONTEND=noninteractive
 # on Vagrant (so adding them again is quick).
 /usr/bin/apt-get -q -y install git ruby-dev make puppet
 
-/usr/bin/gem install librarian-puppet -v 2.2.1 --no-rdoc --no-ri && cd /vagrant/deployment && librarian-puppet install
+/usr/bin/gem install librarian-puppet -v 2.2.3 --no-rdoc --no-ri && cd /vagrant/deployment && librarian-puppet install
