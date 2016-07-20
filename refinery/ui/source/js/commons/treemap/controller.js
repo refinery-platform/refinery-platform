d309c9a08c1367876f5c7eca2b0af07d229c3546
Fritz Lekschas <service@lekschas.de>
2016-07-20 15:02:27 +0200

    Give breadcrumb list element a unique ID.

--- refinery/ui/source/js/commons/treemap/controller.js
+++ refinery/ui/source/js/commons/treemap/controller.js
@@ -175,7 +175,7 @@ function TreemapCtrl (
   this.treemap.$element = this.$(this.treemap.element.node());
 
   this.treemap.grandParent = this.d3.select(this.$visWrapper[0])
-    .select('.root-path');
+    .select('#treemap-root-path');
   this.treemap.$grandParent = this.$(this.treemap.grandParent.node());
   this.treemap.$grandParentContainer = this.treemap.$grandParent.parent();
 
