a30cf224023eae137876bb396657561428a2f822
Jennifer Marx <jennyrutledge@gmail.com>
2016-08-03 17:13:02 -0400

    Use shift instead of slice, to adjust array.

--- refinery/ui/source/js/data-set-import/controllers/file-upload.js
+++ refinery/ui/source/js/data-set-import/controllers/file-upload.js
@@ -264,7 +264,7 @@ function RefineryFileUploadCtrl (
   };
 
   var uploadAlways = function () {
-    formData = formData.splice(1);  // clear upload_id for the next upload
+    formData.shift();  // clear upload_id for the next upload
   };
 
   // Tiggered when a new file is uploaded
