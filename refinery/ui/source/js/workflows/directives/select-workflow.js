78d51c7c2232a88cd930fcc0ff1c315292146792
Jennifer Marx <jennyrutledge@gmail.com>
2016-08-08 16:34:49 -0400

    Directive for selecting workflows.

---
+++ refinery/ui/source/js/workflows/directives/select-workflow.js
@@ -0,0 +1,24 @@
+'use strict';
+
+function rpSelectWorkflow () {
+  return {
+    restrict: 'AE',
+    templateUrl: '/static/partials/workflows/partials/select-workflow.html',
+    controller: 'WorkflowListApiCtrl',
+    controllerAs: 'WLACtrl',
+    bindToController: {
+      nodeGroups: '=?'
+    },
+    link: function (scope, element, attrs, ctrl) {
+      console.log('in the directive');
+      ctrl.getWorkflowList();
+    }
+  };
+}
+
+angular
+  .module('refineryWorkflows')
+  .directive('rpSelectWorkflow', [
+    rpSelectWorkflow
+  ]
+);
