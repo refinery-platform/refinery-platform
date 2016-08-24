bf620a23334a7854ead4b9c362860586351c8f89
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2016-08-24 18:49:47 -0400

    file upload USAGE: BASE_URL=https://test.stemcellcommons.org CREDS_YML=~/creds.yml UPLOAD=~/data/gff3.csv pytest selenium_test.py --driver Firefox

--- selenium_test.py
+++ selenium_test.py
@@ -1,6 +1,7 @@
 import os
 import yaml
 import pytest
+import time
 
 base_url = os.environ['BASE_URL']
 
@@ -19,6 +20,11 @@ def assert_body_text(selenium, *search_texts):
     all_text = selenium.find_element_by_tag_name('body').text
     for search_text in search_texts:
         assert search_text in all_text
+
+
+def upload(selenium):
+    path = os.environ['UPLOAD']
+    selenium.find_element_by_name('tabular_file').send_keys(path)
 
 
 class TestLoginNotRequired:
@@ -50,3 +56,8 @@ class TestLoginRequired:
 
     def test_login(self, selenium, login):
         assert_body_text(selenium, 'Upload', 'Logout')
+
+    def test_upload(self, selenium, login):
+        selenium.find_element_by_link_text('Upload').click()
+        time.sleep(1)  # TODO: Something better?
+        upload(selenium)
