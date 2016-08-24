3d929350103797cd4f3cc428e3d6b7187445c07d
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2016-08-24 15:23:11 -0400

    Check for text on homepage

--- selenium_test.py
+++ selenium_test.py
@@ -5,5 +5,14 @@ def base_url():
     return environ['BASE_URL']
 
 
+def assert_body_text(selenium, *search_texts):
+    all_text = selenium.find_element_by_tag_name('body').text
+    for search_text in search_texts:
+        assert search_text in all_text
+
+
 def test_homepage(selenium):
     selenium.get(base_url())
+    assert_body_text(selenium, 'Collaboration', 'Statistics', 'About',
+                     'Register', 'Login', 'Launch Pad', 'Data Sets',
+                     'Analyses', 'Workflows')
