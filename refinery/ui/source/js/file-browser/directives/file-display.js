6521280ac6e1bb0344ea0a7369c1cc07afef26fc
Jennifer Marx <jennyrutledge@gmail.com>
2016-08-30 14:42:31 -0400

    Add watchers to directive.

--- refinery/ui/source/js/file-browser/directives/file-display.js
+++ refinery/ui/source/js/file-browser/directives/file-display.js
@@ -8,7 +8,7 @@ function rpFileDisplay ($, $window) {
       $('#view-selector').select2({ minimumResultsForSearch: -1 });
       $('#view-selector').on('change', function (event) {
         $('#navigation-tabs a[href="#' + event.added.element[0].value + '"]').tab('show');
-
+        // Temp code to render pivot Matrix view
         if (event.val === 'pivot-view-tab') {
           $window.pivotMatrixView.render();
         } else if (event.val === 'provenance-view-tab') {
