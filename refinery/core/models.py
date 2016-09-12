738eba5ccb8353025567e8b9fd15c70667b8462e
Ilya Sytchev <isytchev@hsph.harvard.edu>
2016-09-12 16:12:48 -0400

    Fix a typo

--- refinery/core/models.py
+++ refinery/core/models.py
@@ -1162,11 +1162,10 @@ class Analysis(OwnableResource):
                     delete = False
 
         if delete:
-
             # Delete associated FileStoreItems
             for node in nodes:
                 if node.file_uuid:
-                    node.get_file_store_items().delete()
+                    node.get_file_store_item().delete()
 
             # Delete associated AnalysisResults
             self.get_analysis_results().delete()
