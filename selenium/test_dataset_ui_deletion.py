0d56a0ae03075071d4da634740c566be863c343e
Scott Ouellette <scottx611x@gmail.com>
2016-09-30 09:55:22 -0400

    It looks like Saucelabs may have a runtime limit, so 50 Datasets being deleted is taking too long

--- selenium/test_dataset_ui_deletion.py
+++ selenium/test_dataset_ui_deletion.py
@@ -13,7 +13,7 @@ from utils.utils import (assert_text_within_id, assert_body_text,
                          wait_until_id_clickable)
 
 # Total number of Datasets to create for the test run
-TOTAL_DATASETS = 50
+TOTAL_DATASETS = 5
 
 base_url = os.environ['BASE_URL']
 not_travis = not('TRAVIS' in os.environ and os.environ['TRAVIS'] == 'true')
