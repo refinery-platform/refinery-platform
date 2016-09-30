d73f4ae9639c8e9f7ec8c2618e53268f25016948
Jennifer Marx <jennyrutledge@gmail.com>
2016-09-30 08:46:05 -0400

    Reformat code.

--- refinery/ui/source/js/collaboration/controllers/email-invite.js
+++ refinery/ui/source/js/collaboration/controllers/email-invite.js
@@ -3,33 +3,32 @@
 function EmailInviteCtrl (
   bootbox, $uibModalInstance, groupInviteService, groupDataService
 ) {
-  this.bootbox = bootbox;
-  this.$uibModalInstance = $uibModalInstance;
-  this.groupInviteService = groupInviteService;
-  this.groupDataService = groupDataService;
-}
-
-EmailInviteCtrl.prototype.sendInvite = function (email) {
   var vm = this;
+  vm.bootbox = bootbox;
+  vm.$uibModalInstance = $uibModalInstance;
+  vm.groupInviteService = groupInviteService;
+  vm.groupDataService = groupDataService;
 
-  vm.groupInviteService.send({
-    email: email,
-    group_id: vm.groupDataService.activeGroup.id
-  })
-  .$promise
-  .then(
-    function () {
-      vm.bootbox.alert('Invitation successfully sent to ' + email);
-      vm.groupDataService.update();
-      vm.$uibModalInstance.dismiss();
-    }
-  ).catch(function () {
-    vm.bootbox.alert(
-      'Oh no, something went terribly wrong. We\'re very sorry but the ' +
-      'invitation couldn\'t be send out.'
-    );
-  });
-};
+  vm.sendInvite = function (email) {
+    vm.groupInviteService.send({
+      email: email,
+      group_id: vm.groupDataService.activeGroup.id
+    })
+    .$promise
+    .then(
+      function () {
+        vm.bootbox.alert('Invitation successfully sent to ' + email);
+        vm.groupDataService.update();
+        vm.$uibModalInstance.dismiss();
+      }
+    ).catch(function () {
+      vm.bootbox.alert(
+        'Oh no, something went terribly wrong. We\'re very sorry but the ' +
+        'invitation couldn\'t be send out.'
+      );
+    });
+  };
+}
 
 angular
   .module('refineryCollaboration')
