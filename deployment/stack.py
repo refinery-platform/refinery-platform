4c58486e99638707c60b477fcb3e6bca5ee5e7b0
David Jones <drj@pobox.com>
2016-10-04 14:44:47 +0100

    Grant power to create RDS snapshots to AWS EC2 instance

--- deployment/stack.py
+++ deployment/stack.py
@@ -242,6 +242,23 @@ def main():
                     }
                 },
                 {
+                    'PolicyName': "CreateDBSnapshot",
+                    'PolicyDocument': {
+                        "Version": "2012-10-17",
+                        "Statement": [
+                            {
+                                "Effect": "Allow",
+                                "Action": [
+                                    "rds:CreateDBSnapshot"
+                                ],
+                                "Resource": [
+                                    "*"
+                                ]
+                            }
+                        ]
+                    }
+                },
+                {
                     'PolicyName': "CreateTags",
                     'PolicyDocument': {
                         "Version": "2012-10-17",
