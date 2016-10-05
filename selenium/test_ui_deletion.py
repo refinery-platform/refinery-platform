2ded785e390f423652f1cf63aaaf97d8049190cb
Scott Ouellette <scottx611x@gmail.com>
2016-10-05 09:29:53 -0400

    Add test to make sure 404s are properly handled on the FrontEnd

--- selenium/test_ui_deletion.py
+++ selenium/test_ui_deletion.py
@@ -7,10 +7,12 @@ sys.path.append("../refinery/")
 
 from django.contrib.auth.models import User
 
+from core.models import DataSet, Analysis
 from factory_boy.django_model_factories import (
     make_datasets, make_analyses_with_single_dataset)
-from utils.utils import (assert_text_within_id, assert_body_text,
-                         wait_until_id_clickable, login, selenium)
+from utils.utils import (assert_text_within_id, wait_until_id_clickable,
+                         login, selenium)
+
 
 # Total number of objects to create for the test run
 TOTAL_DATASETS = 5
@@ -29,8 +31,6 @@ except (User.DoesNotExist, User.MultipleObjectsReturned) as e:
 
 def test_dataset_deletion(selenium, login, total_datasets=TOTAL_DATASETS):
     """Delete some datasets and make sure the ui updates properly"""
-
-    assert_body_text(selenium, 'Logout')
 
     # Create sample Data
     make_datasets(total_datasets, user)
@@ -73,8 +73,6 @@ def test_dataset_deletion(selenium, login, total_datasets=TOTAL_DATASETS):
 
 def test_analysis_deletion(selenium, login, total_analyses=TOTAL_ANALYSES):
     """Delete some analyses and make sure the ui updates properly"""
-
-    assert_body_text(selenium, 'Logout')
 
     # Create sample Data
     make_analyses_with_single_dataset(total_analyses, user)
@@ -144,8 +142,6 @@ def test_cascading_deletion_of_analyses(
     """Delete a Dataset and make sure its Analyses are removed from
     the UI as well"""
 
-    assert_body_text(selenium, 'Logout')
-
     # Create sample Data
     make_analyses_with_single_dataset(total_analyses, user)
     selenium.implicitly_wait(3)
@@ -172,6 +168,75 @@ def test_cascading_deletion_of_analyses(
 
     selenium.implicitly_wait(5)
 
+    assert_text_within_id(
+        selenium, "total-analyses", "{} analysis".format(0))
+
+    assert_text_within_id(
+        selenium, "total-datasets", "{} data sets".format(0))
+
+    if not_travis:
+        pytest.set_trace()
+
+
+def test_that_404s_are_handled(selenium, login, total_analyses=TOTAL_ANALYSES):
+    """Test use case where objects are deleted (for example by an admin)
+    while a user is about to delete said object(s) themselves
+    User should receive a "Not Found" message"""
+
+    # Create sample Data
+    make_analyses_with_single_dataset(total_analyses, user)
+    selenium.implicitly_wait(3)
+
+    selenium.refresh()
+
+    selenium.implicitly_wait(3)
+
+    assert_text_within_id(
+        selenium, "total-datasets", "{} data sets".format(1))
+
+    assert_text_within_id(
+            selenium, "total-analyses", "{} analyses".format(total_analyses)
+    )
+
+    # Simulate scenario where objects have been deleted on the backend
+    DataSet.objects.all().delete()
+    Analysis.objects.all().delete()
+
+    selenium.find_elements_by_class_name('dataset-delete')[0].click()
+
+    selenium.implicitly_wait(3)
+
+    wait_until_id_clickable(selenium, 'dataset-delete-button', 5).click()
+
+    selenium.implicitly_wait(3)
+
+    assert_text_within_id(selenium, "deletion-message-text", "not found.")
+
+    wait_until_id_clickable(
+        selenium, 'dataset-delete-close-button', 5).click()
+
+    selenium.implicitly_wait(5)
+
+    selenium.find_elements_by_class_name('analysis-delete')[0].click()
+
+    selenium.implicitly_wait(3)
+
+    wait_until_id_clickable(selenium, 'analysis-delete-button', 5).click()
+
+    selenium.implicitly_wait(3)
+
+    assert_text_within_id(selenium, "deletion-message-text", "not found.")
+
+    wait_until_id_clickable(
+        selenium, 'analysis-delete-close-button', 5).click()
+
+    selenium.implicitly_wait(5)
+
+    selenium.refresh()
+
+    selenium.implicitly_wait(3)
+
+    # Ensure that ui displays proper info after a refresh
     assert_text_within_id(
         selenium, "total-analyses", "{} analysis".format(0))
 
