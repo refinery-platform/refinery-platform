402ee3e6d266c176a65ebead056bb4d3f845d7b2
Scott Ouellette <scottx611x@users.noreply.github.com>
2016-10-18 12:39:51 -0400

    Remove all occurrences of `print` currently within our codebase (#1483)
    * Remove all occurrences of `print` currently within our codebase
    
    * Remove unused management command
    
    * Formatting changes from Code Review
    
    * Remove Unicode formatted colored strings

--- refinery/core/management/commands/solr_synonyms.py
+++ refinery/core/management/commands/solr_synonyms.py
@@ -1,7 +1,9 @@
 import logging
-import time
 import os
+import sys
+import time
 from optparse import make_option
+
 from django.conf import settings
 from django.core.management.base import BaseCommand
 
@@ -38,26 +40,18 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         if not settings.SOLR_SYNONYMS:
-            print(
-                u'\033[91m' +
-                u'Enable synonym search in settings first' +
-                u'\033[0m'
-            )
+            sys.stdout.write('Enable synonym search in settings first')
             exit()
 
         if not options['identifier']:
-            print(
-                u'\033[91m' +
-                u'Identifier file not given' +
-                u'\033[0m'
-            )
+            sys.stderr.write('Identifier file not given')
             exit()
 
         id = options['identifier'].lower()
         update = False
         removal = False if options['synonyms'] else True
 
-        print('Solr synonyms...')
+        sys.stdout.write('Solr synonyms...')
         start = time.time()
 
         # Open or create the synonyms file
@@ -111,11 +105,5 @@ class Command(BaseCommand):
         else:
             message = 'added'
 
-        print(
-            u'Solr synonyms have been ' + message + '. '
-            u'\033[32m\u2713\033[0m ' +
-            u'\033[2m({} min and {} sec)\033[22m'.format(
-                minutes,
-                seconds
-            )
-        )
+        sys.stdout.write('Solr synonyms have been {}. {} min and {} sec'
+                         .format(message, minutes, seconds))
