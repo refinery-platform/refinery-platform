0bbded8b4a996dd753709f4e2d5748a6ece5b55d
Scott Ouellette <scottx611x@gmail.com>
2016-10-20 21:19:20 -0400

    move helper functions

--- refinery/factory_boy/django_model_factories.py
+++ refinery/factory_boy/django_model_factories.py
@@ -3,11 +3,10 @@ from datetime import datetime
 
 import factory
 
-from core.models import DataSet, Analysis
-
 
 class DataSetFactory(factory.django.DjangoModelFactory):
     """Minimal representation of a DataSet for testing purposes"""
+
     class Meta:
         model = "core.DataSet"
         django_get_or_create = ('uuid',)
@@ -21,6 +20,7 @@ class DataSetFactory(factory.django.DjangoModelFactory):
 
 class InvestigationFactory(factory.django.DjangoModelFactory):
     """Minimal representation of a Investigation for testing purposes"""
+
     class Meta:
         model = "data_set_manager.Investigation"
 
@@ -29,6 +29,7 @@ class InvestigationFactory(factory.django.DjangoModelFactory):
 
 class StudyFactory(factory.django.DjangoModelFactory):
     """Minimal representation of a Study for testing purposes"""
+
     class Meta:
         model = "data_set_manager.Study"
 
@@ -37,6 +38,7 @@ class StudyFactory(factory.django.DjangoModelFactory):
 
 class InvestigationLinkFactory(factory.django.DjangoModelFactory):
     """Minimal representation of an InvestigationLink for testing purposes"""
+
     class Meta:
         model = "core.InvestigationLink"
 
@@ -46,6 +48,7 @@ class InvestigationLinkFactory(factory.django.DjangoModelFactory):
 
 class AnalysisFactory(factory.django.DjangoModelFactory):
     """Minimal representation of an Analysis for testing purposes"""
+
     class Meta:
         model = "core.Analysis"
         django_get_or_create = ('uuid',)
@@ -59,107 +62,31 @@ class AnalysisFactory(factory.django.DjangoModelFactory):
 
 class ProjectFactory(factory.django.DjangoModelFactory):
     """Minimal representation of a Project for testing purposes"""
+
     class Meta:
         model = "core.Project"
 
 
 class GalaxyInstanceFactory(factory.DjangoModelFactory):
     """Minimal representation of a GalaxyInstance for testing purposes"""
+
     class Meta:
         model = "galaxy_connector.Instance"
 
 
 class WorkflowEngineFactory(factory.DjangoModelFactory):
     """Minimal representation of a WorkflowEngine for testing purposes"""
+
     class Meta:
         model = "core.WorkflowEngine"
 
 
 class WorkflowFactory(factory.DjangoModelFactory):
     """Minimal representation of a Workflow for testing purposes"""
+
     class Meta:
         model = "core.Workflow"
+        django_get_or_create = ('uuid',)
 
     uuid = uuid_builtin.uuid4()
     name = "Test Workflow - {}".format(uuid)
-
-
-def make_datasets(number_to_create, user_instance):
-    """Create some minimal DataSets"""
-    while number_to_create >= 1:
-
-        dataset_uuid = uuid_builtin.uuid4()
-        dataset = DataSetFactory(
-                uuid=dataset_uuid,
-                title="Test DataSet - {}".format(dataset_uuid),
-                name="Test DataSet - {}".format(dataset_uuid)
-            )
-
-        investigation_uuid = uuid_builtin.uuid4()
-        investigation = InvestigationFactory(uuid=investigation_uuid)
-
-        study_uuid = uuid_builtin.uuid4()
-        study = StudyFactory(uuid=study_uuid, investigation=investigation)
-
-        InvestigationLinkFactory(
-            data_set=dataset,
-            investigation=study.investigation,
-            version=1,
-            date=datetime.now()
-        )
-
-        number_to_create -= 1
-
-    for dataset in DataSet.objects.all():
-        dataset.set_owner(user_instance)
-        dataset.save()
-
-
-def make_analyses_with_single_dataset(number_to_create, user_instance):
-    """Create some minimal Analyses"""
-    instance = GalaxyInstanceFactory()
-    workflow_engine = WorkflowEngineFactory(instance=instance)
-    workflow_uuid = uuid_builtin.uuid4()
-    workflow = WorkflowFactory(uuid=workflow_uuid,
-                               workflow_engine=workflow_engine)
-    project = ProjectFactory(is_catch_all=True)
-
-    dataset_uuid = uuid_builtin.uuid4()
-    dataset = DataSetFactory(
-            uuid=dataset_uuid,
-            title="Test DataSet - {}".format(dataset_uuid),
-            name="Test DataSet - {}".format(dataset_uuid)
-        )
-
-    investigation_uuid = uuid_builtin.uuid4()
-    investigation = InvestigationFactory(uuid=investigation_uuid)
-
-    study_uuid = uuid_builtin.uuid4()
-    study = StudyFactory(uuid=study_uuid, investigation=investigation)
-
-    InvestigationLinkFactory(
-        data_set=dataset,
-        investigation=study.investigation,
-        version=1,
-        date=datetime.now()
-    )
-
-    while number_to_create >= 1:
-        analysis_uuid = uuid_builtin.uuid4()
-        AnalysisFactory(
-                uuid=analysis_uuid,
-                name="Test Analysis - {}".format(analysis_uuid),
-                project=project,
-                data_set=dataset,
-                workflow=workflow
-            )
-
-        number_to_create -= 1
-
-    for dataset in DataSet.objects.all():
-        dataset.set_owner(user_instance)
-        dataset.save()
-
-    for analysis in Analysis.objects.all():
-        analysis.set_owner(user_instance)
-        analysis.save()
