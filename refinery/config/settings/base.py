cbd1355c342524e965574dfacc61b78a1ba227f7
Scott Ouellette <scottx611x@gmail.com>
2016-10-27 11:30:43 -0400

    Django 1.7 now supports `SessionAuthenticationMiddleware`

--- refinery/config/settings/base.py
+++ refinery/config/settings/base.py
@@ -156,6 +156,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'core.middleware.DatabaseFailureMiddleware',
 )
