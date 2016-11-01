77201c36ad52d947bc229f48a2c3d36864ac87a6
Scott Ouellette <scottx611x@gmail.com>
2016-11-01 12:26:31 -0400

    Explicitly define `DJANGO_SETTINGS_MODULE` due to Django 1.7 `django.setup()` behavior

--- refinery/config/wsgi.py
+++ refinery/config/wsgi.py
@@ -4,12 +4,10 @@ import os
 # server configured to use this file.
 from django.core.wsgi import get_wsgi_application
 
-
+os.environ["DJANGO_SETTINGS_MODULE"] = 'config.settings.prod'
 _application = get_wsgi_application()
 
 
 def application(environ, start_response):
     # pass the WSGI environment variable on through to os.environ
-    os.environ["DJANGO_SETTINGS_MODULE"] = environ.get(
-        "DJANGO_SETTINGS_MODULE", '')
     return _application(environ, start_response)
