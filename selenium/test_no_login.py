d73351bf3546333b165dab39ed9e613b9a315374
Scott Ouellette <scottx611x@gmail.com>
2016-11-02 16:41:14 -0400

    Add acceptance tests for user registration and login

---
+++ selenium/test_no_login.py
@@ -0,0 +1,33 @@
+import os
+import pytest
+from .utils import assert_body_text
+
+base_url = os.environ['BASE_URL']
+not_travis = not('TRAVIS' in os.environ and os.environ['TRAVIS'] == 'true')
+
+
+@pytest.fixture
+def selenium(selenium):
+    selenium.maximize_window()
+    return selenium
+
+
+def test_login_not_required(selenium):
+    selenium.get(base_url)
+    assert_body_text(
+        selenium, 'Collaboration', 'Statistics', 'About',
+        'Register', 'Login', 'Launch Pad', 'Data Sets',
+        'Analyses', 'Workflows')
+
+    selenium.find_element_by_link_text('Statistics').click()
+    assert_body_text(
+        selenium, 'Users', 'Groups', 'Files', 'Data Sets', 'Workflows',
+        'Projects')
+
+    selenium.find_element_by_link_text('About').click()
+    assert_body_text(selenium, 'Background', 'Contact', 'Funding', 'Team',
+                     'Most Recent Code for this Instance')
+    # TODO: All sections are empty right now
+
+    if not_travis:
+        pytest.set_trace()
