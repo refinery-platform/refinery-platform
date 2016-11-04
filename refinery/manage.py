5d1f133fadc2b0e2874e8bdbc69d9305048f99f9
Scott Ouellette <scottx611x@gmail.com>
2016-11-03 21:25:18 -0400

    move location of django.setup()

--- refinery/manage.py
+++ refinery/manage.py
@@ -4,11 +4,10 @@ import sys
 
 import django
 
-django.setup()
-
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.dev")
 
     from django.core.management import execute_from_command_line
 
+    django.setup()
     execute_from_command_line(sys.argv)
