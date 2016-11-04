1c1b8ec3104af2a642efbbee1f023ffdab3efde6
Scott Ouellette <scottx611x@gmail.com>
2016-11-03 21:33:39 -0400

    Explicitly set DJANGO_SETTINGS_MODULE

--- refinery/manage.py
+++ refinery/manage.py
@@ -6,6 +6,7 @@ import django
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.dev")
+    os.environ["DJANGO_SETTINGS_MODULE"] = 'config.settings.dev'
 
     from django.core.management import execute_from_command_line
 
