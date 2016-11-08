154e8a83f9f7dbd53cef663e3ad739d26e8d702d
Jennifer Marx <jennyrutledge@gmail.com>
2016-11-08 09:31:39 -0500

    Add comments."

--- refinery/ui/source/js/file-browser/directives/assay-files-util-modal.js
+++ refinery/ui/source/js/file-browser/directives/assay-files-util-modal.js
@@ -10,13 +10,13 @@ function rpAssayFilesUtilModal (
     restrict: 'AE',
     link: function (scope, element) {
       var modalInstance;
-
       element.bind('click', function () {
         modalInstance = $uibModal.open({
           templateUrl: '/static/partials/file-browser/partials/assay-files-util-modal-detail.html',
           controller: 'AssayFilesUtilModalCtrl',
           controllerAs: 'AFUMCtrl'
         });
+        // default calls when modal closes or dismisses
         modalInstance.result.then(function () {
           resetGridService.setRefreshGridFlag(true);
         }, function () {
