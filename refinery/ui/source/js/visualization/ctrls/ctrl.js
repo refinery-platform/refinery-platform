243c683ecbfea4e19e9922a72f6a4e9ce21cb3ba
Jennifer Marx <jennyrutledge@gmail.com>
2016-11-08 14:18:08 -0500

    Launch higlass visualization.

--- refinery/ui/source/js/visualization/ctrls/ctrl.js
+++ refinery/ui/source/js/visualization/ctrls/ctrl.js
@@ -5,24 +5,63 @@ function VisualizationCtrl (
   $uibModal,
   $compile,
   $templateCache,
-  selectedNodesService
+  fileBrowserFactory,
+  selectedNodesService,
+  nodeGroupService,
+  $window
 ) {
   var vm = this;
   // the visualizations model contains the modal partial
   vm.visualizations = [
-    { name: 'IGV', template: 'i-g-v-launch-modal.html' }
+    { name: 'IGV', template: 'i-g-v-launch-modal.html' },
+    { name: 'hiGlass', template: '' }
   ];
   vm.selectedVisualization = { select: null };
+  vm.higlassConfig = { node_selection: [] };
 
   // Main method which launches modal depending on visualization selection
   vm.launchVisualization = function () {
-    var template = $templateCache.get(vm.selectedVisualization.select.template);
-    var modalContent = $compile(template)($scope);
-    $uibModal.open({
-      template: modalContent,
-      controller: 'IGVCtrl',
-      controllerAs: 'ICtrl'
+    if (!vm.selectedVisualization.select.template) {
+      vm.launchHiglass();
+    } else {
+      var template = $templateCache.get(vm.selectedVisualization.select.template);
+      var modalContent = $compile(template)($scope);
+      $uibModal.open({
+        template: modalContent,
+        controller: 'IGVCtrl',
+        controllerAs: 'ICtrl'
+      });
+    }
+  };
+
+  // Helper method set highGlassConfig based on selected nodes
+  var setHiglassConfig = function () {
+    if (selectedNodesService.selectedAllFlag) {
+      return selectedNodesService.complementSelectedNodesUuids;
+    }
+    return selectedNodesService.selectedNodesUuids;
+  };
+
+
+  // Launches web based higlass
+  vm.launchHiglass = function () {
+    var nodeGroupParams = {
+      uuid: selectedNodesService.defaultCurrentSelectionUuid,
+      assay: $window.externalAssayUuid,
+      nodes: setHiglassConfig(),
+      use_complement_nodes: selectedNodesService.selectedAllFlag
+    };
+
+    var nodeGroupUpdate = nodeGroupService.update(nodeGroupParams);
+    nodeGroupUpdate.$promise.then(function (response) {
+      angular.copy(response.nodes, vm.higlassConfig.node_selection);
     });
+    // var params = $httpParamSerializer({
+    //  node_uuids: vm.hiGlassConfig.node_selection
+    // });
+   // $window.open('/visualize/genome?' + params);
+    console.log('launching higlass');
+    console.log(vm.higlassConfig);
   };
 
   // Helper method for UI to check if any nodes are selected
@@ -42,6 +81,9 @@ angular
     '$uibModal',
     '$compile',
     '$templateCache',
+    'fileBrowserFactory',
     'selectedNodesService',
+    'nodeGroupService',
+    '$window',
     VisualizationCtrl
   ]);
