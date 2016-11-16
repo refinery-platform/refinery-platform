6287456ba873c0a83c5d03b98749ffeebd064677
Scott Ouellette <scottx611x@gmail.com>
2016-11-16 13:42:35 -0500

    Lower `DEFAULT_WAIT` Now that we aren't relying on SauceLabs VMs

--- selenium/utils/selenium_utils.py
+++ selenium/utils/selenium_utils.py
@@ -9,7 +9,7 @@ from selenium.common.exceptions import TimeoutException
 
 from factory_boy.utils import factory_boy_cleanup
 
-DEFAULT_WAIT = 5
+DEFAULT_WAIT = 2
 base_url = os.environ['BASE_URL']
 creds = yaml.load(open(os.environ['CREDS_YML']))
 
