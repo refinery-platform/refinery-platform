2e080fb152c183a5897c5f29852e7494ef8adcd4
Scott Ouellette <scottx611x@gmail.com>
2016-11-17 14:09:21 -0500

    Remove Comments

--- selenium/acceptance_tests.sh
+++ selenium/acceptance_tests.sh
@@ -4,12 +4,10 @@
 set -e
 
 echo "Setting up env vars..."
-# Add some directories to PYTHON_PATH
 export PYTHONPATH=$PYTHONPATH:../refinery:../refinery/config
 export BASE_URL=http://localhost:8000 CREDS_YML=guest_creds.yml UPLOAD=gff3.csv
 
 echo "Installing Geckodriver..."
-# install Geckodriver dependancy
 TAR=geckodriver-v0.11.1-linux64.tar
 wget https://github.com/mozilla/geckodriver/releases/download/v0.11.1/$TAR.gz > acceptance_tests.log 2>&1
 gunzip $TAR.gz
@@ -19,15 +17,14 @@ chmod a+x geckodriver
 export PATH=$PATH:`pwd`
 
 echo "Installing Firefox and Xvfb..."
-# Install firefox xvfb
 sudo apt-get install firefox xvfb -y  > acceptance_tests.log 2>&1
 
 echo "Starting Xvfb..."
 sudo Xvfb :10 -ac &
 export DISPLAY=:10
 
-echo "Running tests...\n"
-#run tests
+echo "Running tests..."
 pytest --driver Firefox
 
 sudo killall Xvfb
+sudo rm -rf /tmp/.X10-lock
