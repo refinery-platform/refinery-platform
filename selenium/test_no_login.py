e90af1fac0bb5864d994e406c51ca09024165603
Scott Ouellette <scottx611x@gmail.com>
2016-11-17 14:27:01 -0500

    Update stats test to check for actual object counts

--- selenium/test_no_login.py
+++ selenium/test_no_login.py
@@ -1,5 +1,9 @@
 import os
 import pytest
+from django.contrib.auth.models import User, Group
+
+from core.models import DataSet, Workflow, Project
+from file_store.models import FileStoreItem
 from utils.selenium_utils import assert_body_text
 
 base_url = os.environ['BASE_URL']
@@ -20,9 +24,22 @@ def test_login_not_required(selenium):
         'Analyses', 'Workflows')
 
     selenium.find_element_by_link_text('Statistics').click()
+
     assert_body_text(
-        selenium, 'Users', 'Groups', 'Files', 'Data Sets', 'Workflows',
-        'Projects')
+        selenium,
+        str(User.objects.count()),
+        'Users',
+        str(Group.objects.count()),
+        'Groups',
+        str(FileStoreItem.objects.count()),
+        'Files',
+        str(DataSet.objects.count()),
+        'Data Sets',
+        str(Workflow.objects.count()),
+        'Workflows',
+        str(Project.objects.count()),
+        'Projects'
+    )
 
     selenium.find_element_by_link_text('About').click()
     assert_body_text(selenium, 'Background', 'Contact', 'Funding', 'Team',
