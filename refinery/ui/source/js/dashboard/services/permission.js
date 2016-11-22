4ff7e3e249dd73f0a8de7f2f1ca3bfc9755e732e
Jennifer Marx <jkmarx@users.noreply.github.com>
2016-11-22 11:33:22 -0500

    Jkmarx/sharing modal bug (#1531)
    * Refactor permissions methods and data into own service.
    
    * Add catch when user dismisses to refresh data.
    
    * Rename file name to remove redundancy.
    
    * Add unit test.
    
    * Fix typo.
    
    * Add unit test for getPermissions.

---
+++ refinery/ui/source/js/dashboard/services/permission.js
@@ -0,0 +1,65 @@
+'use strict';
+
+function permissionService (sharingService) {
+  var vm = this;
+  vm.permissions = {};
+
+  /**
+   * Load permissions for this dataset.
+   *
+   * @method  getPermissions
+   * @author  Fritz Lekschas
+   * @date    2015-08-21
+   *
+   * @param   {String}  uuid   UUID of the exact model entity.
+   * @return  {Object}         Angular promise.
+   * */
+  vm.getPermissions = function (uuid) {
+    return sharingService.get({
+      model: 'data_sets',
+      uuid: uuid
+    }).$promise
+      .then(function (data) {
+        var groups = [];
+        for (var i = 0, len = data.share_list.length; i < len; i++) {
+          groups.push({
+            id: data.share_list[i].group_id,
+            name: data.share_list[i].group_name,
+            uuid: data.share_list[i].group_uuid,
+            permission: vm.getPermissionLevel(data.share_list[i].perms)
+          });
+        }
+        vm.permissions = {
+          isOwner: data.is_owner,
+          groups: groups
+        };
+      });
+  };
+
+    /**
+   * Turns permission object into a simple string.
+   *
+   * @method  getPermissions
+   * @author  Fritz Lekschas
+   * @date    2015-08-21
+   *
+   * @param   {Object}  perms  Object of the precise permissions.
+   * @return  {String}         Permission's name.
+   */
+  vm.getPermissionLevel = function (perms) {
+    if (perms.read === false) {
+      return 'none';
+    }
+    if (perms.change === true) {
+      return 'edit';
+    }
+    return 'read';
+  };
+}
+
+angular.module('refineryDashboard')
+  .service('permissionService', [
+    'sharingService',
+    permissionService
+  ]
+);
