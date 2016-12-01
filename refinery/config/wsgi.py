c8cbb249e51c428210caf8f9e0f6c97af0c632d9
Scott Ouellette <scottx611x@gmail.com>
2016-12-01 13:55:46 -0500

    Allow for env vars set by Apache to be accessible by Django

--- refinery/config/wsgi.py
+++ refinery/config/wsgi.py
@@ -1,14 +1,23 @@
-"""
-WSGI config for a Django 1.7 project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
-"""
-
 import os
-from django.core.wsgi import get_wsgi_application
+import django
+from django.core.handlers.wsgi import WSGIHandler
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.dev")
-application = get_wsgi_application()
+
+# See http://stackoverflow.com/a/21124143/6031066
+
+class WSGIEnvironment(WSGIHandler):
+    """
+    Class to override WSGIHANDLER.__call__ so that
+    environment variables set by Apache are available to our python environment
+    """
+
+    def __call__(self, environ, start_response):
+        # Add env vars to be passed from Apache here
+        # Ex: os.environ[<env_var>] = environ[<env_var>]
+        os.environ['DJANGO_SETTINGS_MODULE'] = environ[
+            'DJANGO_SETTINGS_MODULE']
+
+        django.setup()
+        return super(WSGIEnvironment, self).__call__(environ, start_response)
+
+application = WSGIEnvironment()
