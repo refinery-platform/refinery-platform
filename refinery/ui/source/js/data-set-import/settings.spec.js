b79cef1f0ce76c1ae9cca4d12c62ad30ff4c404a
Jennifer Marx <jennyrutledge@gmail.com>
2016-12-08 16:03:37 -0500

    Add unit tests for setting.

---
+++ refinery/ui/source/js/data-set-import/settings.spec.js
@@ -0,0 +1,44 @@
+'use strict';
+
+describe('refineryDataSetImport.settings: unit tests', function () {
+  var settings;
+
+  beforeEach(module('refineryApp'));
+  beforeEach(module('refineryDataSetImport'));
+
+  beforeEach(inject(function (_dataSetImportSettings_) {
+    settings = _dataSetImportSettings_;
+  }));
+
+  describe('settings', function () {
+    it('should be registered', function () {
+      expect(settings).toBeDefined();
+    });
+
+    it('should have checkFilesUrl constant', function () {
+      expect(settings.checkFilesUrl).toEqual('/data_set_manager/import/check_files/');
+    });
+
+    it('should have isaTabImportUrl constant', function () {
+      expect(settings.isaTabImportUrl).toEqual('/data_set_manager/import/isa-tab-form/');
+    });
+
+    it('should have tabularFileImportUrl constant', function () {
+      expect(settings.tabularFileImportUrl)
+        .toEqual('/data_set_manager/import/metadata-table-form/');
+    });
+
+    it('should have uploadUrl constant', function () {
+      expect(settings.uploadUrl).toEqual('/data_set_manager/import/chunked-upload/');
+    });
+
+    it('should have uploadCompleteUrl constant', function () {
+      expect(settings.uploadCompleteUrl)
+        .toEqual('/data_set_manager/import/chunked-upload-complete/');
+    });
+
+    it('should have chunkSize constant', function () {
+      expect(settings.chunkSize).toEqual(10485760);
+    });
+  });
+});
--- refinery/ui/source/js/file-browser/settings.js
+++ refinery/ui/source/js/file-browser/settings.js
@@ -3,5 +3,5 @@
 angular
   .module('refineryFileBrowser')
   .constant('fileBrowserSettings', {
-    maxFileRequest: '100' // max number of files in API request
+    maxFileRequest: 100 // max number of files in API request
   });
