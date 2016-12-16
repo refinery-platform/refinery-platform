c9791369fe2c21e7ee2a6d9de614214ebdf5e6ad
Scott Ouellette <scottx611x@gmail.com>
2016-12-16 16:25:09 -0500

    Update selenium tests

--- refinery/selenium_testing/tests.py
+++ refinery/selenium_testing/tests.py
@@ -1,16 +1,19 @@
+import time
 from django.contrib.auth.models import User, Group
+from django.db import transaction, connection
 from django.test import LiveServerTestCase
 from pyvirtualdisplay import Display
 from selenium import webdriver
 
+from core.management.commands.create_public_group import create_public_group
+from core.management.commands.create_user import init_user
 from core.models import DataSet, Project, Analysis
 from core.models import Workflow
 from factory_boy.utils import make_analyses_with_single_dataset, make_datasets
 from file_store.models import FileStoreItem
-from selenium_testing.utils import assert_body_text, login, \
-    wait_until_id_clickable, DEFAULT_WAIT, wait_until_id_visible, \
-    assert_text_within_id
-
+from selenium_testing.utils import (
+    assert_body_text, login, wait_until_id_clickable, DEFAULT_WAIT,
+    assert_text_within_id, refresh)
 
 # Start a pyvirtualdisplay for geckodriver to interact with
 display = Display(visible=0, size=(1900, 1080))
@@ -22,11 +25,9 @@ class NoLoginTestCase(LiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.maximize_window()
-        super(NoLoginTestCase, self).setUp()
 
     def tearDown(self):
         self.browser.quit()
-        super(NoLoginTestCase, self).tearDown()
 
     def test_login_not_required(self):
         self.browser.get(self.live_server_url)
@@ -62,43 +63,37 @@ class NoLoginTestCase(LiveServerTestCase):
 class DataSetsPanelTestCase(LiveServerTestCase):
 
     def setUp(self):
+        create_public_group()
         self.browser = webdriver.Firefox()
         self.browser.maximize_window()
-        self.user = User.objects.create_user("guest", "", "guest")
+        init_user("guest", "guest", "guest@coffee.com", "Guest", "Guest",
+                  "Test User", is_active=True)
+        self.user = User.objects.get(username="guest")
         login(self.browser, self.live_server_url)
-        super(DataSetsPanelTestCase, self).setUp()
 
     def tearDown(self):
         self.user.delete()
         DataSet.objects.all().delete()
         self.browser.quit()
-        super(DataSetsPanelTestCase, self).tearDown()
 
     def test_data_set_preview(self):
         """Test DataSet Preview"""
 
         # Create sample Data & refresh page
         make_analyses_with_single_dataset(5, self.user)
-        self.browser.refresh()
-
-        wait_until_id_clickable(self.browser, "import-button",
-                                DEFAULT_WAIT)
+        refresh(self.browser)
+        time.sleep(DEFAULT_WAIT)
 
         self.browser.find_elements_by_class_name("title")[0].click()
 
-        assert_body_text(
-            self.browser,
-            "SUMMARY",
-            "Description",
-            "Technology & Sources",
-            "Number of files (total file size)",
-            "Owner",
-            "ANALYSES",
-            [a.name for a in Analysis.objects.filter(
-                name__startswith="Test Analysis")],
-            "REFERENCES",
-            "PROTOCOLS"
-        )
+        search_array = ["SUMMARY", "Description",
+                        "Number of files (total file size)", "Owner",
+                        "ANALYSES", "REFERENCES", "PROTOCOLS",
+                        ]
+        for item in Analysis.objects.filter(name__startswith="Test Analysis"):
+            search_array.append(item.name)
+
+        assert_body_text(self.browser, search_array)
 
     def test_upload_button(self):
         """Test Upload button"""
@@ -119,24 +114,27 @@ class DataSetsPanelTestCase(LiveServerTestCase):
 class UiDeletionTestCase(LiveServerTestCase):
 
     def setUp(self):
+        create_public_group()
         self.browser = webdriver.Firefox()
         self.browser.maximize_window()
-        self.user = User.objects.create_user("guest", "", "guest")
+        init_user("guest", "guest", "guest@coffee.com", "Guest", "Guest",
+                  "Test User", is_active=True)
+        self.user = User.objects.get(username="guest")
         login(self.browser, self.live_server_url)
-        super(UiDeletionTestCase, self).setUp()
 
     def tearDown(self):
         self.user.delete()
         DataSet.objects.all().delete()
         self.browser.quit()
-        super(UiDeletionTestCase, self).tearDown()
 
     def test_dataset_deletion(self, total_datasets=5):
         """Delete some datasets and make sure the ui updates properly"""
 
         # Create sample Data & refresh page
         make_datasets(total_datasets, self.user)
-        self.browser.refresh()
+        transaction.commit()
+        connection.close()
+        refresh(self.browser)
 
         wait_until_id_clickable(
             self.browser, "total-datasets", DEFAULT_WAIT)
@@ -151,27 +149,22 @@ class UiDeletionTestCase(LiveServerTestCase):
         while total_datasets:
             self.browser.find_elements_by_class_name('dataset-delete')[
                 0].click()
-
+            self.browser.save_screenshot("dataset-deletion.png")
             wait_until_id_clickable(
                 self.browser, 'dataset-delete-button', DEFAULT_WAIT).click()
 
             total_datasets -= 1
 
-            wait_until_id_visible(
-                self.browser, 'dataset-delete-close-button', DEFAULT_WAIT)
+            self.browser.implicitly_wait(DEFAULT_WAIT)
             wait_until_id_clickable(
                 self.browser, 'dataset-delete-close-button',
                 DEFAULT_WAIT).click()
 
-            wait_until_id_visible(
-                self.browser, "total-datasets", DEFAULT_WAIT)
             assert_text_within_id(
                 self.browser, "total-datasets", DEFAULT_WAIT,
                 "{} data sets".format(
                     total_datasets))
 
-        wait_until_id_visible(
-            self.browser, "total-datasets", DEFAULT_WAIT)
         assert_text_within_id(
             self.browser,
             "total-datasets",
@@ -184,7 +177,7 @@ class UiDeletionTestCase(LiveServerTestCase):
 
         # Create sample Data
         make_analyses_with_single_dataset(total_analyses, self.user)
-        self.browser.refresh()
+        refresh(self.browser)
 
         wait_until_id_clickable(self.browser, "total-datasets", DEFAULT_WAIT)
         assert_text_within_id(
@@ -199,12 +192,14 @@ class UiDeletionTestCase(LiveServerTestCase):
         while total_analyses:
             self.browser.find_elements_by_class_name('analysis-delete')[
                 0].click()
-
+            self.browser.save_screenshot("analysis-deletion.png")
             wait_until_id_clickable(
                 self.browser, 'analysis-delete-button', DEFAULT_WAIT).click()
 
             total_analyses -= 1
-
+            self.browser.save_screenshot("test_analysis_deletion{"
+                                         "}.png".format(total_analyses))
+            self.browser.implicitly_wait(DEFAULT_WAIT)
             wait_until_id_clickable(
                 self.browser, 'analysis-delete-close-button',
                 DEFAULT_WAIT).click()
@@ -215,7 +210,7 @@ class UiDeletionTestCase(LiveServerTestCase):
                 self.browser, "analyses-indicator", DEFAULT_WAIT)
             assert_text_within_id(
                 self.browser, "analyses-indicator", DEFAULT_WAIT,
-                total_analyses)
+                str(total_analyses))
 
             # Handle case where the Pluralization of `analysis` done on the
             # frontend
@@ -257,7 +252,7 @@ class UiDeletionTestCase(LiveServerTestCase):
 
         # Create sample Data
         make_analyses_with_single_dataset(total_analyses, self.user)
-        self.browser.refresh()
+        refresh(self.browser)
 
         wait_until_id_clickable(self.browser, "total-datasets", DEFAULT_WAIT)
         assert_text_within_id(
@@ -293,12 +288,12 @@ class UiDeletionTestCase(LiveServerTestCase):
     def test_that_dataset_404s_are_handled(self, total_analyses=5):
         """Test use case where DataSet objects are deleted (for example by an
         admin, or a user inbetween multiple windows) while a user is about to
-        delete said object(s) themselves. User should receive a "Not Found"
+        delete said object(s) themselves User should receive a "Not Found"
         message"""
 
         # Create sample Data
         make_analyses_with_single_dataset(total_analyses, self.user)
-        self.browser.refresh()
+        refresh(self.browser)
 
         wait_until_id_clickable(self.browser, "total-datasets", DEFAULT_WAIT)
         assert_text_within_id(
@@ -319,8 +314,6 @@ class UiDeletionTestCase(LiveServerTestCase):
 
         wait_until_id_clickable(
             self.browser, 'dataset-delete-button', DEFAULT_WAIT).click()
-        wait_until_id_clickable(
-            self.browser, 'dataset-delete-close-button', DEFAULT_WAIT).click()
 
         wait_until_id_clickable(
             self.browser, "deletion-message-text", DEFAULT_WAIT)
@@ -349,7 +342,7 @@ class UiDeletionTestCase(LiveServerTestCase):
 
         # Create sample Data
         make_analyses_with_single_dataset(total_analyses, self.user)
-        self.browser.refresh()
+        refresh(self.browser)
 
         wait_until_id_clickable(self.browser, "total-datasets", DEFAULT_WAIT)
         assert_text_within_id(
@@ -389,75 +382,3 @@ class UiDeletionTestCase(LiveServerTestCase):
             "total-datasets",
             DEFAULT_WAIT,
             "{} data sets".format(1))
-
-
-class GlobalAnalysisPopoverTestCase(LiveServerTestCase):
-
-    def setUp(self):
-        self.browser = webdriver.Firefox()
-        self.browser.maximize_window()
-        self.user = User.objects.create_user("guest", "", "guest")
-        login(self.browser, self.live_server_url)
-        super(GlobalAnalysisPopoverTestCase, self).setUp()
-
-    def tearDown(self):
-        DataSet.objects.all().delete()
-        self.browser.quit()
-        super(GlobalAnalysisPopoverTestCase, self).tearDown()
-
-    def test_global_analysis_icon(self):
-        wait_until_id_clickable(
-            self.browser, "global-analysis-status", DEFAULT_WAIT).click()
-
-        '''
-        There is currently a known bug with marionette that doesn't allow for
-        ActionChains/ mouse hovers to work properly.
-
-        See:
-        https://github.com/mozilla/geckodriver/issues/159
-        https://bugzilla.mozilla.org/show_bug.cgi?id=1292178
-        https://github.com/jgraham/webdriver-rust/tree/actions
-        https://github.com/SeleniumHQ/selenium/issues/2285
-
-        # Utilize ActionChain to hover over element
-        hover_action = ActionChains(self.browser)
-        hover_action.move_to_element(global_analysis_icon)
-        # Assert Tooltip text
-        assert_text_within_id(
-          self.browser, "global-analysis-status", "View Recent Analyses")
-        hover_action.click(global_analysis_icon)
-        hover_action.perform()
-        '''
-
-        # Assert Popover text
-        assert_body_text(
-            self.browser,
-            "Recent Analyses",
-            "No analyses performed."
-        )
-        # Click outside of popover to close
-        self.browser.find_element_by_tag_name("body").click()
-
-        # Create some Analyses
-        make_analyses_with_single_dataset(
-            5, User.objects.get(first_name="Test User"))
-
-        # Refresh page
-        self.browser.refresh()
-
-        # Assert Popover text when Analyses exist
-        wait_until_id_clickable(
-            self.browser, "global-analysis-status", DEFAULT_WAIT).click()
-
-        # Assert newly created analyses show in popover
-        assert_body_text(
-            self.browser,
-            "Recent Analyses",
-            [a.name for a in Analysis.objects.filter(
-                name__startswith="Test Analysis")]
-        )
-        # Click outside of popover to close
-        self.browser.find_element_by_tag_name("body").click()
-
-        # Remove Data Created for this test
-        DataSet.objects.filter(name__startswith="Test DataSet -").delete()
