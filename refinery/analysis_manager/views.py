1e4e2e819ea53bc294e2ce44f94674ebe5aaf3fc
Scott Ouellette <scottx611x@gmail.com>
2017-01-05 13:42:28 -0500

    Remove dead code

--- refinery/analysis_manager/views.py
+++ refinery/analysis_manager/views.py
@@ -520,37 +520,6 @@ def create_noderelationship(request):
                             content_type='application/json')
 
 
-class DictDiffer(object):
-    """A dictionary difference calculator
-    Originally posted as: http://stackoverflow.com/a/1165552
-    Calculate the difference between two dictionaries as:
-    (1) items added
-    (2) items removed
-    (3) keys same in both but changed values
-    (4) keys same in both and unchanged values
-    """
-    def __init__(self, current_dict, past_dict):
-        self.current_dict, self.past_dict = current_dict, past_dict
-        self.current_keys, self.past_keys = [
-            set(d.keys()) for d in (current_dict, past_dict)
-            ]
-        self.intersect = self.current_keys.intersection(self.past_keys)
-
-    def added(self):
-        return self.current_keys - self.intersect
-
-    def removed(self):
-        return self.past_keys - self.intersect
-
-    def changed(self):
-        return set(o for o in self.intersect
-                   if self.past_dict[o] != self.current_dict[o])
-
-    def unchanged(self):
-        return set(o for o in self.intersect
-                   if self.past_dict[o] == self.current_dict[o])
-
-
 def match_nodesets(ns1, ns2, diff_f, all_f, rel_type=None):
     """Helper function for matching 2 nodesets solr results"""
     logger.debug("analysis_manager.views match_nodesets called")
