94bb71eeeb9a8efcf9a8af6045362ffd63212b4c
Fritz Lekschas <service@lekschas.de>
2017-01-07 17:11:54 +0100

    Helper command to update the investigation of MetaboLights data sets.

---
+++ refinery/data_set_manager/management/commands/update_metabolights_inv.py
@@ -0,0 +1,29 @@
+from django.core.management.base import BaseCommand
+
+from data_set_manager.models import Investigation
+
+
+class Command(BaseCommand):
+    help = (
+        "Helper command to update the investigation of MetaboLights data "
+        "sets."
+    )
+
+    def handle(self, *args, **options):
+        for inv in Investigation.objects.all():
+            try:
+                study = inv.study_set.all()[0]
+            except Exception:
+                continue
+
+            if (
+                inv.title is None or
+                inv.title == '' or
+                inv.title == "Investigation"
+            ):
+                inv.title = study.title
+
+            if inv.description is None or inv.description == '':
+                inv.description = study.description
+
+            inv.save()
