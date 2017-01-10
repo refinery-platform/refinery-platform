c938b435c4c47063a8a0b4106a78ce8967673dcd
Fritz Lekschas <service@lekschas.de>
2017-01-10 16:14:16 -0500

    Extend and rename management command

--- refinery/data_set_manager/management/commands/update_investigation.py
+++ refinery/data_set_manager/management/commands/update_investigation.py
@@ -5,8 +5,8 @@ from data_set_manager.models import Investigation
 
 class Command(BaseCommand):
     help = (
-        "Helper command to update the investigation of MetaboLights data "
-        "sets."
+        'Helper command to update the investigation of MetaboLights data '
+        'sets.'
     )
 
     def handle(self, *args, **options):
@@ -17,9 +17,15 @@ class Command(BaseCommand):
                 continue
 
             if (
+                inv.identifier is None or
+                inv.identifier == ''
+            ):
+                inv.identifier = study.identifier
+
+            if (
                 inv.title is None or
                 inv.title == '' or
-                inv.title == "Investigation"
+                inv.title == 'Investigation'
             ):
                 inv.title = study.title
 
