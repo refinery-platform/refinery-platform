e726f65cb7b59cdd68f52770c50fef60c97ade14
Fritz Lekschas <service@lekschas.de>
2017-01-10 16:35:14 -0500

    Add management command for update the data set instances

---
+++ refinery/core/management/commands/update_datasets.py
@@ -0,0 +1,32 @@
+from django.core.management.base import BaseCommand
+
+from core.models import DataSet
+
+
+class Command(BaseCommand):
+    help = (
+        'Helper command to pull missing information of data sets from '
+        'its investigation.'
+    )
+
+    def handle(self, *args, **options):
+        for ds in DataSet.objects.all():
+            try:
+                inv = ds.investigationlink_set.first().investigation
+            except Exception:
+                continue
+
+            if ds.accession is None or ds.accession == '':
+                ds.accession = inv.identifier
+
+            if (
+                ds.title is None or
+                ds.title == '' or
+                ds.title == 'Investigation'
+            ):
+                ds.title = inv.title
+
+            if ds.description is None or ds.description == '':
+                ds.description = inv.description
+
+            ds.save()
