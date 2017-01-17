c73105e615183d85a7b9032b38033fd8097ba96d
Scott Ouellette <scottx611x@gmail.com>
2017-01-17 15:14:45 -0500

    Cleanup selenium test cases

--- refinery/selenium_testing/tests.py
+++ refinery/selenium_testing/tests.py
@@ -1,15 +1,14 @@
 import time
-from django.contrib.auth.models import User, Group
+from django.contrib.auth.models import User
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from guardian.management import create_anonymous_user
 from pyvirtualdisplay import Display
 from selenium import webdriver
 
 from core.management.commands.create_public_group import create_public_group
 from core.management.commands.create_user import init_user
-from core.models import DataSet, Project, Analysis, ExtendedGroup
-from core.models import Workflow
+from core.models import DataSet,  Analysis, ExtendedGroup
 from factory_boy.utils import make_analyses_with_single_dataset, make_datasets
-from file_store.models import FileStoreItem
 from selenium_testing.utils import (
     assert_body_text, login, wait_until_id_clickable, DEFAULT_WAIT,
     assert_text_within_id, refresh, delete_from_ui)
@@ -19,68 +18,86 @@ display = Display(visible=0, size=(1900, 1080))
 display.start()
 
 
-class NoLoginTestCase(StaticLiveServerTestCase):
-    """
-    Ensure that Refinery looks like it should when there is no currently
-    logged in user
-    """
+class SeleniumTestBase(StaticLiveServerTestCase):
+    """Abstract base class to be used for all Selenium-based tests."""
+
+    # Override `available_apps` to inhibit `post-migrate` signals run upon
+    # `fixture_teardown() See: http://bit.ly/2iJkQnU`
+    # This also allows for speedier testing
+    available_apps = ["core", "data_set_manager", "file_store",
+                      "galaxy_connector", "django.contrib.contenttypes",
+                      "django.contrib.auth", "django.contrib.humanize"]
 
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.maximize_window()
-
-    def tearDown(self):
-        self.browser.quit()
-
-    def test_login_not_required(self):
-        self.browser.get(self.live_server_url)
-        assert_body_text(
-            self.browser, 'Collaboration', 'Statistics', 'About',
-            'Register', 'Login', 'Launch Pad', 'Data Sets',
-            'Analyses', 'Workflows')
-
-        self.browser.find_element_by_link_text('Statistics').click()
-        self.browser.implicitly_wait(DEFAULT_WAIT)
-        assert_body_text(
-            self.browser,
-            str(User.objects.count()),
-            'Users',
-            str(Group.objects.count()),
-            'Groups',
-            str(FileStoreItem.objects.count()),
-            'Files',
-            str(DataSet.objects.count()),
-            'Data Sets',
-            str(Workflow.objects.count()),
-            'Workflows',
-            str(Project.objects.count()),
-            'Projects'
-        )
-
-        self.browser.find_element_by_link_text('About').click()
-        assert_body_text(self.browser, 'Background', 'Contact', 'Funding',
-                         'Team', 'Most Recent Code for this Instance')
-        # TODO: All sections are empty right now
-
-
-class DataSetsPanelTestCase(StaticLiveServerTestCase):
-    """
-    Ensure that the DataSet upload button and DataSet Preview look like
-    they're behaving normally
-    """
-    def setUp(self):
-        self.browser = webdriver.Firefox()
-        self.browser.maximize_window()
-        init_user("guest", "guest", "guest@coffee.com", "Guest", "Guest",
-                  "Test User", is_active=True)
         create_public_group()
         ExtendedGroup.objects.public_group().save()
+        init_user("guest", "guest", "guest@coffee.com", "Guest", "Guest",
+                  "Test User", is_active=True)
+        create_anonymous_user('sender')
         self.user = User.objects.get(username="guest")
         self.user.save()
         login(self.browser, self.live_server_url)
 
     def tearDown(self):
         self.browser.quit()
+
+    class Meta:
+        abstract = True
+
+
+class NoLoginTestCase(SeleniumTestBase):
+    """
+    Ensure that Refinery looks like it should when there is no currently
+    logged in user
+    """
+
+    # Overriding setUp() in this case because we don't need to login or
+    # initialize the guest user
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.maximize_window()
+        create_public_group()
+        ExtendedGroup.objects.public_group().save()
+        create_anonymous_user('sender')
+
+    def test_login_not_required(self):
+        self.browser.get(self.live_server_url)
+        assert_body_text(
+            self.browser,
+            search_array=['Collaboration', 'Statistics', 'About',
+                          'Register', 'Login', 'Launch Pad', 'Data Sets',
+                          'Analyses', 'Workflows'])
+
+        self.browser.find_element_by_link_text('Statistics').click()
+        self.browser.implicitly_wait(DEFAULT_WAIT)
+        assert_body_text(
+            self.browser,
+            search_array=[
+                'Users',
+                'Groups',
+                'Files',
+                'Data Sets',
+                'Workflows',
+                'Projects'
+            ]
+        )
+
+        self.browser.find_element_by_link_text('About').click()
+        self.browser.implicitly_wait(DEFAULT_WAIT)
+        assert_body_text(self.browser,
+                         search_array=['Background', 'Contact', 'Funding',
+                                       'Team',
+                                       'Most Recent Code for this Instance'])
+        # TODO: All sections are empty right now
+
+
+class DataSetsPanelTestCase(SeleniumTestBase):
+    """
+    Ensure that the DataSet upload button and DataSet Preview look like
+    they're behaving normally
+    """
 
     def test_data_set_preview(self):
         """Test DataSet Preview"""
@@ -108,35 +125,21 @@ class DataSetsPanelTestCase(StaticLiveServerTestCase):
                                 DEFAULT_WAIT).click()
         assert_body_text(
             self.browser,
-            "Data Set Import",
-            "Tabular Metadata",
-            "ISA-Tab Metadata",
-            "PROVIDE METADATA FILE",
-            "Download an example tabular metadata file.",
-            "The first row needs to contain column headers."
+            search_array=[
+                "Data Set Import",
+                "Tabular Metadata",
+                "ISA-Tab Metadata",
+                "PROVIDE METADATA FILE",
+                "Download an example tabular metadata file.",
+                "The first row needs to contain column headers."]
         )
 
 
-class UiDeletionTestCase(StaticLiveServerTestCase):
-    """
-    Ensure proper deletion of DataSets and Analyses from the UI
-    """
-    def setUp(self):
-        self.browser = webdriver.Firefox()
-        self.browser.maximize_window()
-        init_user("guest", "guest", "guest@coffee.com", "Guest", "Guest",
-                  "Test User", is_active=True)
-        create_public_group()
-        ExtendedGroup.objects.public_group().save()
-        self.user = User.objects.get(username="guest")
-        self.user.save()
-        login(self.browser, self.live_server_url)
-
-    def tearDown(self):
-        self.browser.quit()
+class UiDeletionTestCase(SeleniumTestBase):
+    """Ensure proper deletion of DataSets and Analyses from the UI"""
 
     def test_dataset_deletion(self, total_datasets=2):
-        """Delete some datasets and make sure the ui updates properly"""
+        """Delete some datasets and make sure the UI updates properly"""
 
         # Create sample Data & refresh page
         make_datasets(total_datasets, self.user)
@@ -153,7 +156,7 @@ class UiDeletionTestCase(StaticLiveServerTestCase):
         delete_from_ui(self.browser, "dataset", total_datasets)
 
     def test_analysis_deletion(self, total_analyses=2):
-        """Delete some analyses and make sure the ui updates properly"""
+        """Delete some analyses and make sure the UI updates properly"""
 
         # Create sample Data
         make_analyses_with_single_dataset(total_analyses, self.user)
@@ -209,7 +212,7 @@ class UiDeletionTestCase(StaticLiveServerTestCase):
     def test_that_dataset_404s_are_handled(self, total_analyses=5):
         """Test use case where DataSet objects are deleted (for example by an
         admin, or a user inbetween multiple windows) while a user is about to
-        delete said object(s) themselves User should receive a "Not Found"
+        delete said object(s) themselves, User should receive a "Not Found"
         message"""
 
         # Create sample Data
@@ -258,7 +261,7 @@ class UiDeletionTestCase(StaticLiveServerTestCase):
     def test_that_analysis_404s_are_handled(self, total_analyses=5):
         """Test use case where Analysis objects are deleted (for example by an
         admin, or a user inbetween multiple windows) while a user is about to
-        delete said object(s) themselves User should receive a "Not Found"
+        delete said object(s) themselves, User should receive a "Not Found"
         message"""
 
         # Create sample Data
