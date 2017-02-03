3192fa826af5b31cfb8d54868615cc3ec09b86f3
Jennifer Marx <jennyrutledge@gmail.com>
2017-02-03 15:37:54 -0500

    Add toollaunch module.

---
+++ refinery/ui/source/js/tool-launch/module.spec.js
@@ -0,0 +1,15 @@
+'use strict';
+
+describe('refineryToolLaunch.module: unit tests', function () {
+  var module;
+
+  beforeEach(function () {
+    module = angular.module('refineryToolLaunch');
+  });
+
+  describe('Module', function () {
+    it('should be registered', function () {
+      expect(module).not.toEqual(null);
+    });
+  });
+});
