da1f038af749e0c6636594d51464b072fe791eb7
Scott Ouellette <scottx611x@gmail.com>
2017-02-13 13:02:21 -0500

    Cleanup factory_boy factories and utils

--- refinery/factory_boy/utils.py
+++ refinery/factory_boy/utils.py
@@ -25,11 +25,11 @@ def make_datasets(number_to_create, user_instance):
         investigation = InvestigationFactory(uuid=investigation_uuid)
 
         study_uuid = uuid_builtin.uuid4()
-        study = StudyFactory(uuid=study_uuid, investigation=investigation)
+        StudyFactory(uuid=study_uuid, investigation=investigation)
 
         InvestigationLinkFactory(
             data_set=dataset,
-            investigation=study.investigation,
+            investigation=investigation,
             version=1,
             date=datetime.now()
         )
@@ -48,30 +48,25 @@ def make_analyses_with_single_dataset(number_to_create, user_instance):
 
     instance = GalaxyInstanceFactory()
     workflow_engine = WorkflowEngineFactory(instance=instance)
-    workflow_uuid = uuid_builtin.uuid4()
-    workflow = WorkflowFactory(uuid=workflow_uuid,
+    workflow = WorkflowFactory(uuid=uuid_builtin.uuid4(),
                                workflow_engine=workflow_engine)
     project = ProjectFactory(is_catch_all=True)
 
     dataset_uuid = uuid_builtin.uuid4()
     dataset = DataSetFactory(
-        uuid=dataset_uuid,
+        uuid=uuid_builtin.uuid4(),
         title="Test DataSet - {}".format(dataset_uuid),
         name="Test DataSet - {}".format(dataset_uuid)
     )
-
-    investigation_uuid = uuid_builtin.uuid4()
-    investigation = InvestigationFactory(uuid=investigation_uuid)
-
-    study_uuid = uuid_builtin.uuid4()
-    study = StudyFactory(uuid=study_uuid, investigation=investigation)
+    investigation = InvestigationFactory(uuid=uuid_builtin.uuid4())
+    StudyFactory(uuid=uuid_builtin.uuid4(), investigation=investigation)
 
     InvestigationLinkFactory(
         data_set=dataset,
-        investigation=study.investigation,
+        investigation=investigation,
         version=1,
         date=datetime.now()
-    )
+    ).save()
 
     analysis_status_counter = len(status_choices) - 1
     while number_to_create >= 1:
