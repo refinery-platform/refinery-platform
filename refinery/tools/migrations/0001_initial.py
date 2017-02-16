40f181e14101b7770f7c22dd36174708a589ba75
Scott Ouellette <scottx611x@gmail.com>
2017-02-16 15:36:45 -0500

    Add migration

---
+++ refinery/tools/migrations/0001_initial.py
@@ -0,0 +1,98 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import django_extensions.db.fields
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('file_store', '0002_filetypes_and_fileextensions'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='FileRelationship',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('uuid', django_extensions.db.fields.UUIDField(unique=True, max_length=36, editable=False, blank=True)),
+                ('name', models.TextField(max_length=100)),
+                ('value_type', models.CharField(max_length=100, choices=[(b'PAIR', b'pair'), (b'LIST', b'list')])),
+                ('nested_elements', models.ManyToManyField(to='tools.FileRelationship', null=True, blank=True)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='InputFile',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('uuid', django_extensions.db.fields.UUIDField(unique=True, max_length=36, editable=False, blank=True)),
+                ('name', models.TextField(max_length=100)),
+                ('description', models.TextField(max_length=500)),
+                ('allowed_filetypes', models.ManyToManyField(to='file_store.FileType')),
+                ('file_relationship', models.ForeignKey(to='tools.FileRelationship')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='OutputFile',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('uuid', django_extensions.db.fields.UUIDField(unique=True, max_length=36, editable=False, blank=True)),
+                ('name', models.TextField(max_length=100)),
+                ('description', models.TextField(max_length=500)),
+                ('filetype', models.ForeignKey(to='file_store.FileType')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Parameter',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('uuid', django_extensions.db.fields.UUIDField(unique=True, max_length=36, editable=False, blank=True)),
+                ('name', models.TextField(max_length=100)),
+                ('description', models.TextField(max_length=500)),
+                ('is_editable', models.BooleanField(default=False)),
+                ('value_type', models.CharField(max_length=25, choices=[(b'INTEGER', b'int'), (b'STRING', b'str'), (b'BOOLEAN', b'bool'), (b'FLOAT', b'float'), (b'GENOME_BUILD', b'genome build'), (b'ATTRIBUTE', b'attribute'), (b'FILE', b'file')])),
+                ('default_value', models.TextField(max_length=100)),
+                ('galaxy_tool_id', models.TextField(max_length=300)),
+                ('galaxy_tool_parameter', models.TextField(max_length=100)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='ToolDefinition',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('uuid', django_extensions.db.fields.UUIDField(unique=True, max_length=36, editable=False, blank=True)),
+                ('name', models.TextField(unique=True, max_length=100)),
+                ('description', models.TextField(unique=True, max_length=500)),
+                ('tool_type', models.CharField(max_length=100, choices=[(b'WORKFLOW', b'Workflow'), (b'VISUALIZATION', b'Visualization')])),
+                ('file_relationships', models.ForeignKey(to='tools.FileRelationship')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='parameter',
+            name='tool_definition',
+            field=models.ForeignKey(to='tools.ToolDefinition'),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='outputfile',
+            name='tool_definition',
+            field=models.ForeignKey(to='tools.ToolDefinition'),
+            preserve_default=True,
+        ),
+    ]
