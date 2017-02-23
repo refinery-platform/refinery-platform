8264aca4072a82a89f40edcffe10206a21ce7e76
Scott Ouellette <scottx611x@gmail.com>
2017-02-23 11:38:58 -0500

    Add more tests for ToolDef API

--- refinery/tools/tests.py
+++ refinery/tools/tests.py
@@ -1,5 +1,8 @@
+from urlparse import urljoin
+
 from django.contrib.auth.models import User
-from rest_framework.test import APITestCase, APIRequestFactory, APIClient
+from rest_framework.test import APITestCase, APIRequestFactory, APIClient, \
+    force_authenticate
 
 from core.models import ExtendedGroup
 from factory_boy.utils import make_tool_definitions
@@ -29,14 +32,56 @@ class ToolDefinitionAPITests(APITestCase):
 
         # Make reusable requests & responses
         self.get_request = self.factory.get(self.url_root)
+        force_authenticate(self.get_request, self.user)
         self.get_response = self.view(self.get_request)
+
+        self.tool_json = self.get_response.data[0]
+
+        self.delete_request = self.factory.delete(
+            urljoin(self.url_root,  self.tool_json['uuid']))
+        force_authenticate(self.delete_request, self.user)
+        self.delete_response = self.view(self.delete_request)
+        self.put_request = self.factory.put(
+            self.url_root,
+            data=self.tool_json,
+            format="json"
+        )
+        force_authenticate(self.put_request, self.user)
+        self.put_response = self.view(self.put_request)
+        self.post_request = self.factory.post(
+            self.url_root,
+            data=self.tool_json,
+            format="json"
+        )
+        force_authenticate(self.post_request, self.user)
+        self.post_response = self.view(self.post_request)
+        self.options_request = self.factory.options(
+            self.url_root,
+            data=self.tool_json,
+            format="json"
+        )
+        force_authenticate(self.options_request, self.user)
+        self.options_response = self.view(self.options_request)
 
     def test_tool_definitions_exist(self):
         self.assertEqual(ToolDefinition.objects.count(), 2)
 
-    def test_get_request(self):
+    def test_get_request_authenticated(self):
         self.assertIsNotNone(self.get_response)
+
+    def test_get_request_no_auth(self):
+        self.get_request = self.factory.get(self.url_root)
+        self.get_response = self.view(self.get_request)
+        self.assertEqual(self.get_response.status_code, 403)
+
+    def test_unallowed_http_verbs(self):
         self.assertEqual(
-            self.get_response.data[0]["description"], "Chip Seq using MACS2")
+            self.put_response.data['detail'], 'Method "PUT" not allowed.')
         self.assertEqual(
-            self.get_response.data[1]["description"], "LIST:LIST:PAIR desc")
+            self.post_response.data['detail'], 'Method "POST" not allowed.')
+        self.assertEqual(
+            self.options_response.data['detail'], 'Method "OPTIONS" not '
+                                                  'allowed.')
+        self.assertEqual(
+            self.delete_response.data['detail'], 'Method "DELETE" not '
+                                                 'allowed.')
