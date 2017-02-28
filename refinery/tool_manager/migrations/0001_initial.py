13d7544c4002f1a3a12ab2bdb0629c47ef93ecdf
Scott Ouellette <scottx611x@gmail.com>
2017-02-28 17:21:03 -0500

    Fix migration

--- refinery/tool_manager/migrations/0001_initial.py
+++ refinery/tool_manager/migrations/0001_initial.py
@@ -7,8 +7,6 @@ import django_extensions.db.fields
 
 class Migration(migrations.Migration):
 
-    replaces = [(b'tool_manager', '0001_initial'), (b'tool_manager', '0002_auto_20170228_0912'), (b'tool_manager', '0003_auto_20170228_1432'), (b'tool_manager', '0004_auto_20170228_1433'), (b'tool_manager', '0005_auto_20170228_1617')]
-
     dependencies = [
         ('file_store', '0002_filetypes_and_fileextensions'),
     ]
@@ -21,6 +19,7 @@ class Migration(migrations.Migration):
                 ('uuid', django_extensions.db.fields.UUIDField(unique=True, max_length=36, editable=False, blank=True)),
                 ('name', models.TextField(max_length=100)),
                 ('value_type', models.CharField(max_length=100, choices=[(b'PAIR', b'pair'), (b'LIST', b'list')])),
+                ('file_relationship', models.ManyToManyField(to='tool_manager.FileRelationship', null=True, blank=True)),
             ],
             options={
             },
@@ -33,7 +32,7 @@ class Migration(migrations.Migration):
                 ('uuid', django_extensions.db.fields.UUIDField(unique=True, max_length=36, editable=False, blank=True)),
                 ('name', models.TextField(max_length=100)),
                 ('description', models.TextField(max_length=500)),
-                ('allowed_filetypes', models.ManyToManyField(to=b'file_store.FileType')),
+                ('allowed_filetypes', models.ManyToManyField(to='file_store.FileType')),
             ],
             options={
             },
@@ -62,40 +61,10 @@ class Migration(migrations.Migration):
                 ('is_editable', models.BooleanField(default=False)),
                 ('value_type', models.CharField(max_length=25, choices=[(b'INTEGER', b'int'), (b'STRING', b'str'), (b'BOOLEAN', b'bool'), (b'FLOAT', b'float'), (b'GENOME_BUILD', b'genome build'), (b'ATTRIBUTE', b'attribute'), (b'FILE', b'file')])),
                 ('default_value', models.TextField(max_length=100)),
-                ('galaxy_tool_id', models.TextField(max_length=300)),
-                ('galaxy_tool_parameter', models.TextField(max_length=100)),
             ],
             options={
             },
             bases=(models.Model,),
-        ),
-        migrations.CreateModel(
-            name='ToolDefinition',
-            fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('uuid', django_extensions.db.fields.UUIDField(unique=True, max_length=36, editable=False, blank=True)),
-                ('name', models.TextField(unique=True, max_length=100)),
-                ('description', models.TextField(unique=True, max_length=500)),
-                ('tool_type', models.CharField(max_length=100, choices=[(b'WORKFLOW', b'Workflow'), (b'VISUALIZATION', b'Visualization')])),
-                ('file_relationship', models.ForeignKey(to='tool_manager.FileRelationship')),
-                ('output_files', models.ManyToManyField(to=b'tool_manager.OutputFile')),
-                ('parameters', models.ManyToManyField(to=b'tool_manager.Parameter')),
-            ],
-            options={
-            },
-            bases=(models.Model,),
-        ),
-        migrations.AddField(
-            model_name='filerelationship',
-            name='input_files',
-            field=models.ManyToManyField(to=b'tool_manager.InputFile'),
-            preserve_default=True,
-        ),
-        migrations.AddField(
-            model_name='filerelationship',
-            name='file_relationship',
-            field=models.ManyToManyField(to=b'tool_manager.FileRelationship', null=True, blank=True),
-            preserve_default=True,
         ),
         migrations.CreateModel(
             name='GalaxyParameter',
@@ -108,12 +77,26 @@ class Migration(migrations.Migration):
             },
             bases=('tool_manager.parameter',),
         ),
-        migrations.RemoveField(
-            model_name='parameter',
-            name='galaxy_tool_id',
+        migrations.CreateModel(
+            name='ToolDefinition',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('uuid', django_extensions.db.fields.UUIDField(unique=True, max_length=36, editable=False, blank=True)),
+                ('name', models.TextField(unique=True, max_length=100)),
+                ('description', models.TextField(unique=True, max_length=500)),
+                ('tool_type', models.CharField(max_length=100, choices=[(b'WORKFLOW', b'Workflow'), (b'VISUALIZATION', b'Visualization')])),
+                ('file_relationship', models.ForeignKey(to='tool_manager.FileRelationship')),
+                ('output_files', models.ManyToManyField(to='tool_manager.OutputFile')),
+                ('parameters', models.ManyToManyField(to='tool_manager.Parameter')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
         ),
-        migrations.RemoveField(
-            model_name='parameter',
-            name='galaxy_tool_parameter',
+        migrations.AddField(
+            model_name='filerelationship',
+            name='input_files',
+            field=models.ManyToManyField(to='tool_manager.InputFile'),
+            preserve_default=True,
         ),
     ]
