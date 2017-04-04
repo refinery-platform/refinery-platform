dd620f0e0f926b94e54137590a8efdcecac2bf49
Jennifer Marx <jennyrutledge@gmail.com>
2017-04-04 09:07:00 -0400

    Tweak css.

--- refinery/ui/source/js/tool-launch/ctrls/input-control-ctrl.js
+++ refinery/ui/source/js/tool-launch/ctrls/input-control-ctrl.js
@@ -29,6 +29,7 @@
           return vm.displayCtrl.selectedTool;
         },
         function () {
+          console.log(vm.displayCtrl.selectedTool);
           angular.copy(vm.displayCtrl.selectedTool, vm.tool);
         }
       );
