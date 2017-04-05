2ce79883165593bd8969d7d8b345d7a0e0cd9bb7
Scott Ouellette <scottx611x@users.noreply.github.com>
2017-04-05 14:35:55 -0400

    Scottx611x/docker puppet install (#1664)
    * 32 bit selenium on Linux doesn't exist for release `v.0.15.0`
    
    * I don't believe this code is necessary. Caused Docker trouble, and we shouldn't commit from Vagrant anyways.
    
    * Require docker before starting `supervisord`
    
    * Add `garethr-docker` dep.
    
    * Include `docker` locally and on AWS. `docker_` to avoid naming conflicts
    
    * Update `stankevich/python` to next minor version that supports fetching latest `pip`
    
    * Use `puppetlabs/docker_platform`
    
    * Rename `docker_` to `docker`
    
    * Explicitly indicate global namespace
    
    * Supervisor shouldn't care about docker
    
    * Reference global `app_user`

--- deployment/manifests/default.pp
+++ deployment/manifests/default.pp
@@ -34,6 +34,7 @@ exec { "activate_user":
 # See code in refinery-modules/refinery/...
 include refinery
 include refinery::apache2
+include refinery::docker
 include refinery::neo4j
 include refinery::pg
 include refinery::selenium
