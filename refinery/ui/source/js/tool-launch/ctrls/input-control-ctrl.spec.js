c559bda0ff029e734070170b59f5f28bc14af148
Jennifer Marx <jennyrutledge@gmail.com>
2017-04-06 14:22:28 -0400

    Add unit test.

---
+++ refinery/ui/source/js/tool-launch/ctrls/input-control-ctrl.spec.js
@@ -0,0 +1,28 @@
+(function () {
+  'use strict';
+
+  describe('Controller: Input Control Ctrl', function () {
+    var ctrl;
+    var scope;
+
+    beforeEach(module('refineryApp'));
+    beforeEach(module('refineryToolLaunch'));
+    beforeEach(inject(function (
+      $rootScope,
+      $controller
+    ) {
+      scope = $rootScope.$new();
+      ctrl = $controller('InputControlCtrl', {
+        $scope: scope
+      });
+    }));
+
+    it('Input Group ctrl should exist', function () {
+      expect(ctrl).toBeDefined();
+    });
+
+    it('Data & UI displays variables should exist for views', function () {
+      expect(ctrl.tool).toEqual({});
+    });
+  });
+})();
