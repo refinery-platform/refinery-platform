3db4b3f38eeca5ed2e629d5675c264902f401ee2
Jennifer Marx <jennyrutledge@gmail.com>
2017-04-14 09:20:33 -0400

    Fix typo in view location.

--- refinery/ui/source/js/tool-launch/directives/tool-display.js
+++ refinery/ui/source/js/tool-launch/directives/tool-display.js
@@ -3,7 +3,7 @@
   angular.module('refineryToolLaunch').component('rpToolDisplay', {
     controller: 'ToolDisplayCtrl',
     templateUrl: ['$window', function ($window) {
-      return $window.getStaticUrl('partials/tool-launch/partials/tool-display.html');
+      return $window.getStaticUrl('partials/tool-launch/views/tool-display.html');
     }]
   });
 })();
