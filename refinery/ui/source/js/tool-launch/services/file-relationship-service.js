edcd49ee82c8962d8decba1d8826b8adb8007892
Jennifer Marx <jennyrutledge@gmail.com>
2017-04-14 09:53:38 -0400

    Add comments.

--- refinery/ui/source/js/tool-launch/services/file-relationship-service.js
+++ refinery/ui/source/js/tool-launch/services/file-relationship-service.js
@@ -19,22 +19,23 @@
   ) {
     var nodeService = selectedNodesService;
     var vm = this;
-    vm.attributesObj = {};
+    vm.attributesObj = {}; // displayName: internalName, ex Name:
     vm.currentGroup = []; // index for the group coordinates
     vm.currentTypes = []; // tracks whether depths are pair or list
     vm.groupCollection = {}; // contains groups with their selected row's info
     vm.inputFileTypes = []; // maintains the required input types
-    vm.refreshFileMap = refreshFileMap; // method
-    vm.resetCurrents = resetCurrents; // method
-    vm.setGroupCollection = setGroupCollection; // method
-    vm.setNodeSelectCollection = setNodeSelectCollection; // method
+    vm.refreshFileMap = refreshFileMap;
+    vm.resetCurrents = resetCurrents;
+    vm.setGroupCollection = setGroupCollection;
+    vm.setNodeSelectCollection = setNodeSelectCollection;
     vm.nodeSelectCollection = {}; // contains rows and their group info
     /*
     *-----------------------
     * Method Definitions
     * ----------------------
     */
-    // convert attributes array to attributes obj
+    // helper method: convert attributes name array to attributes name obj,
+    // displayName: internalName
     function generateAttributeObj () {
       var attributeArr = fileBrowserFactory.assayAttributes;
       for (var i = 0; i < attributeArr.length; i ++) {
@@ -42,26 +43,36 @@
       }
     }
 
+    /**
+     * Generates obj with groups with input file types and their selected
+     * node uuid
+     * @param {string} inputTypeUuid - uuid of the input file type from tool
+     * definitions api
+     * @param {obj} selectionObj - ui-select model tracks checkboxes
+     * @param {string} deselectedFileUuid - OPTIONAL, uuid of deselected node
+     */
     function setGroupCollection (inputTypeUuid, selectionObj, deselectedFileUuid) {
       var nodeUuid = deselectedFileUuid;
+      // Node isn't deselected, grab the active row's uuid.
       if (!nodeUuid) {
         nodeUuid = nodeService.activeNodeRow.uuid;
       }
-
+      // checkbox selected
       if (selectionObj[inputTypeUuid]) {
         if (_.has(vm.groupCollection, vm.currentGroup) === false) {
+          // intialize groupCollection[groupId]
           vm.groupCollection[vm.currentGroup] = {};
         }
         if (_.has(vm.groupCollection[vm.currentGroup], inputTypeUuid) === false) {
+          // add inputTypes to groupCollectio[groupId]
           vm.groupCollection[vm.currentGroup][inputTypeUuid] = [];
         }
         vm.groupCollection[vm.currentGroup][inputTypeUuid].push(
           angular.copy(nodeService.activeNodeRow)
         );
       } else {
-        // remove property
+        // remove property when checkbox deselected
         for (var i = 0; i < vm.groupCollection[vm.currentGroup][inputTypeUuid].length; i ++) {
-          console.log(vm.groupCollection[vm.currentGroup][inputTypeUuid][i]);
           if (vm.groupCollection[vm.currentGroup][inputTypeUuid][i].uuid === nodeUuid) {
             vm.groupCollection[vm.currentGroup][inputTypeUuid].splice(i, 1);
             break;
@@ -70,24 +81,34 @@
       }
     }
 
+   /**
+     * Generates obj with node uuids and their associated input file
+     * types and groups.
+     * @param {string} inputTypeUuid - uuid of the input file type from tool
+     * definitions api
+     * @param {obj} selectionObj - ui-select model tracks checkboxes
+     * @param {string} deselectedFileUuid - OPTIONAL, uuid of deselected node
+     */
     function setNodeSelectCollection (inputTypeUuid, selectionObj, deselectedFileUuid) {
+      // Node isn't deselected, grab the active row's uuid.
       var nodeUuid = deselectedFileUuid;
       if (!nodeUuid) {
         nodeUuid = nodeService.activeNodeRow.uuid;
       }
-
+      // checkbox selected
       if (selectionObj[inputTypeUuid]) {
         if (_.has(vm.nodeSelectCollection, nodeUuid) === true) {
           vm.nodeSelectCollection[nodeUuid].inputTypeList.push(angular.copy(inputTypeUuid));
           vm.nodeSelectCollection[nodeUuid].groupList.push(angular.copy(vm.currentGroup));
         } else {
+          // intialize nodeSelectionCollection
           vm.nodeSelectCollection[nodeUuid] = {
             inputTypeList: [inputTypeUuid],
             groupList: [angular.copy(vm.currentGroup)]
           };
         }
       } else {
-        // remove property
+            // remove property when checkbox deselected
         for (var i = 0; i < vm.nodeSelectCollection[nodeUuid].inputTypeList.length; i ++) {
           if (vm.nodeSelectCollection[nodeUuid].groupList[i] === vm.currentGroup) {
             vm.nodeSelectCollection[nodeUuid].groupList.splice(i, 1);
@@ -98,10 +119,14 @@
       }
     }
 
-    // used to initialize the tool's data structure used by vm
+    /**
+     * Parses the tool definition api response into usable data structure by
+     * views. File_relationship is a self-reference nested structure.
+     * ex: file_relationship: [{file_relationship: []}]
+     */
     function refreshFileMap () {
       var scaledCopy = toolsService.selectedTool.file_relationship;
-      // this is an array
+
       while (scaledCopy.file_relationship.length > 0) {
         vm.currentGroup.push(0);
         vm.currentTypes.push(scaledCopy.value_type);
@@ -113,6 +138,9 @@
       return vm.currentGroup;
     }
 
+    /**
+     * Resets the variables needed for new tool selection
+     */
     function resetCurrents () {
       vm.currentGroup = [];
       vm.currentTypes = [];
