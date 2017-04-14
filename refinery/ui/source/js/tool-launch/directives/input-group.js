90c839470bcadd4fe596767b24b80229b6aedc2f
Jennifer Marx <jennyrutledge@gmail.com>
2017-04-14 12:50:18 -0400

    Fix formatting.

--- refinery/ui/source/js/tool-launch/directives/input-group.js
+++ refinery/ui/source/js/tool-launch/directives/input-group.js
@@ -1,12 +1,14 @@
 (function () {
   'use strict';
-  angular.module('refineryToolLaunch').component('rpInputGroup', {
-    controller: 'InputGroupCtrl',
-    require: {
-      displayCtrl: '^rpToolDisplay'
-    },
-    templateUrl: ['$window', function ($window) {
-      return $window.getStaticUrl('partials/tool-launch/partials/input-group.html');
-    }]
-  });
+
+  angular.module('refineryToolLaunch')
+    .component('rpInputGroup', {
+      controller: 'InputGroupCtrl',
+      require: {
+        displayCtrl: '^rpToolDisplay'
+      },
+      templateUrl: ['$window', function ($window) {
+        return $window.getStaticUrl('partials/tool-launch/partials/input-group.html');
+      }]
+    });
 })();
