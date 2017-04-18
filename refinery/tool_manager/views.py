fa72a3047997c81301739f4285ebe1b4c53f2a63
Scott Ouellette <scottx611x@gmail.com>
2017-04-18 10:49:33 -0400

    Cleanup view and update some placeholder comments

--- refinery/tool_manager/views.py
+++ refinery/tool_manager/views.py
@@ -22,16 +22,19 @@ class ToolDefinitionsViewSet(ModelViewSet):
     permission_classes = [IsAuthenticated]
 
 
-class ToolLaunchConfigurationViewSet(GenericViewSet):
+class ToolLaunchViewSet(GenericViewSet):
     """API endpoint that allows for Tools to be launched"""
     http_method_names = ['post']
     permission_classes = [IsAuthenticated]
 
     def launch(self, request, *args, **kwargs):
+        # TODO: Validate incoming Tool Launch Configurations against our schema
         # try:
         #     validate_tool_launch_configuration(request.data)
         # except RuntimeError as e:
         #     return HttpResponseServerError(e)
+        # else:
+        #     tool_launch_data = request.data
 
         tool_launch_data = request.data
 
@@ -45,22 +48,25 @@ class ToolLaunchConfigurationViewSet(GenericViewSet):
         ) as e:
             return HttpResponseServerError(
                 "Couldn't fetch ToolDefinition with UUID {}: {}".format(
-                    tool_launch_data["uuid"],
+                    tool_launch_data["tool_definition_uuid"],
                     e
                 )
             )
         else:
             if tool_definition.tool_type == ToolDefinition.VISUALIZATION:
+                # TODO: Talk to Node API and get urls from node UUIDs in the
+                # `file_relationships` data structure
+                # get_urls_from_node_uuids()
+
                 visualization_tool = VisualizationToolLaunch.objects.create(
                     tool_definition=tool_definition,
-                    parameters=tool_launch_data[
-                        "parameters"
-                    ],
-                    file_relationships=tool_launch_data[
-                       "file_relationships"
-                    ]
-
+                    parameters=tool_launch_data["parameters"],
+                    file_relationships=tool_launch_data["file_relationships"]
                 )
+
+                visualization_tool.set_owner(request.user)
+                visualization_tool.save()
+
                 return visualization_tool.launch()
             elif tool_definition.tool_type == ToolDefinition.WORKFLOW:
                 pass
