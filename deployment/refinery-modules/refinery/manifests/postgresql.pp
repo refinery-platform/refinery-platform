be1d2bc1821ce4444dcff20522ec7dd4fa382a46
Ilya Sytchev <isytchev@hsph.harvard.edu>
2017-04-18 18:04:38 -0400

    Avoid installing a local Postgresql server on EC2 instance

--- deployment/refinery-modules/refinery/manifests/postgresql.pp
+++ deployment/refinery-modules/refinery/manifests/postgresql.pp
@@ -1,12 +1,17 @@
 class refinery::postgresql {
 
-  if $::deployment_platform != 'aws' {
+  if $::deployment_platform == 'aws' {
+    class { '::postgresql::server':
+      package_ensure => 'absent',
+    }
+  }
+  else {
     class { '::postgresql::globals':
       version  => '9.3',
       encoding => 'UTF8',
       locale   => 'en_US.utf8',
     }
-
+    ->
     class { '::postgresql::server':
       # to make remote connections via SSH tunnel easier
       ipv4acls => ["host all $::app_user 127.0.0.1/32 trust"],
