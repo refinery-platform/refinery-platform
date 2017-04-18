334419ca606a71bf1b301d094610644697e62f92
Scott Ouellette <scottx611x@gmail.com>
2017-04-18 10:53:20 -0400

    Update urls for renamed Viewset

--- refinery/tool_manager/urls.py
+++ refinery/tool_manager/urls.py
@@ -1,13 +1,13 @@
 from django.conf.urls import url, include
 from rest_framework.routers import DefaultRouter
-from .views import ToolDefinitionsViewSet, ToolLaunchConfigurationViewSet
+from .views import ToolDefinitionsViewSet, ToolLaunchViewSet
 
 # DRF url routing
 tool_manager_router = DefaultRouter()
 tool_manager_router.register(r'tools/definitions', ToolDefinitionsViewSet)
 tool_manager_router.register(
     r'tools',
-    ToolLaunchConfigurationViewSet,
+    ToolLaunchViewSet,
     "ToolLaunch"
 )
 
