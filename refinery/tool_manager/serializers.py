9974bfe5e85398d82e8c138e90a682e1b917bad1
Scott Ouellette <scottx611x@gmail.com>
2017-04-19 16:25:10 -0400

    Fix imports

--- refinery/tool_manager/serializers.py
+++ refinery/tool_manager/serializers.py
@@ -2,8 +2,8 @@ from rest_framework import serializers
 from rest_framework_recursive.fields import RecursiveField
 
 from file_store.models import FileType
-from .models import (FileRelationship, InputFile, OutputFile, Parameter,
-                     ToolDefinition, Tool)
+from .models import (FileRelationship, InputFile, OutputFile, Parameter, Tool,
+                     ToolDefinition)
 
 
 class FileTypeSerializer(serializers.ModelSerializer):
