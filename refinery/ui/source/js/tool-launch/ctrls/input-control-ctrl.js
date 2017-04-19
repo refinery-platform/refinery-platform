03fd10cf4e910d4f410bc83d60df63e8524ab2d8
Jennifer Marx <jennyrutledge@gmail.com>
2017-04-19 15:11:44 -0400

    Consolidate components.

--- refinery/ui/source/js/tool-launch/ctrls/input-control-ctrl.js
+++ refinery/ui/source/js/tool-launch/ctrls/input-control-ctrl.js
@@ -6,16 +6,38 @@
     .controller('InputControlCtrl', InputControlCtrl);
 
   InputControlCtrl.$inject = [
-    '$scope'
+    '$scope',
+    'fileRelationshipService'
   ];
 
 
   function InputControlCtrl (
-    $scope
+    $scope,
+    fileRelationshipService
   ) {
+    var fileService = fileRelationshipService;
     var vm = this;
+    vm.attributes = fileService.attributesObj;
+    vm.currentGroup = fileService.currentGroup; // maintains nav
+    // position
+    vm.currentTypes = fileService.currentTypes;
+    vm.groups = []; // stores all the selected nodes for vm
+    vm.inputFileTypes = fileService.inputFileTypes;
+    vm.navRight = navRight;
+    vm.navLeft = navLeft;
     vm.tool = {};
+   /*
+   * ---------------------------------------------------------
+   * Methods Definitions
+   * ---------------------------------------------------------
+   */
+    function navLeft (depth) {
+      vm.currentGroup[depth]--;
+    }
 
+    function navRight (depth) {
+      vm.currentGroup[depth]++;
+    }
     /*
    * ---------------------------------------------------------
    * Watchers
@@ -29,6 +51,9 @@
         },
         function () {
           angular.copy(vm.displayCtrl.selectedTool, vm.tool);
+          vm.currentGroup = fileService.currentGroup;
+          vm.currentTypes = fileService.currentTypes;
+          vm.inputFileTypes = fileService.inputFileTypes;
         }
       );
     };
