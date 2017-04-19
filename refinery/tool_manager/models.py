dcb5b6b6431df1d813cc4bee4374ad012ce7e3d7
Scott Ouellette <scottx611x@gmail.com>
2017-04-19 15:14:06 -0400

    Remove inheritance and simplify ToolDefiniiton and Tool model's structure (by a ton, holy moly)

--- refinery/tool_manager/models.py
+++ refinery/tool_manager/models.py
@@ -1,6 +1,5 @@
 import logging
 
-from django.core.validators import RegexValidator
 from django.db import models
 from django.db.models.signals import post_delete, pre_delete
 from django.dispatch import receiver
@@ -152,121 +151,18 @@ class ToolDefinition(models.Model):
     output_files = models.ManyToManyField(OutputFile)
     parameters = models.ManyToManyField(Parameter)
 
-    def __str__(self):
-        return "{}: {} {}".format(self.tool_type, self.name, self.uuid)
-
-    def get_container_input_path(self):
-        """
-        Fetch the container_input_path of a VisualizationDefinition
-        :return: VisualizationDefinition.container_input_path if available,
-        otherwise `None`
-        """
-        try:
-            visualization_definition = self.get_visualization_definition()
-        except (VisualizationDefinition.DoesNotExist,
-                VisualizationDefinition.MultipleObjectsReturned):
-            return None
-        else:
-            return visualization_definition.container_input_path
-
-    def get_docker_image_name(self):
-        """
-        Fetch the docker_image_name of a VisualizationDefinition
-        :return: VisualizationDefinition.docker_image_name if available,
-        otherwise `None`
-        """
-        try:
-            visualization_definition = self.get_visualization_definition()
-        except (VisualizationDefinition.DoesNotExist,
-                VisualizationDefinition.MultipleObjectsReturned):
-            return None
-        else:
-            return visualization_definition.docker_image_name
-
-    def get_galaxy_workflow_id(self):
-        """
-        Fetch the galaxy_workflow_id of a WorkflowDefinition
-        :return: WorkflowDefinition.galaxy_workflow_id if available,
-        otherwise `None`
-        """
-        try:
-            workflow_definition = self.get_workflow_definition()
-        except (WorkflowDefinition.DoesNotExist,
-                WorkflowDefinition.MultipleObjectsReturned):
-            return None
-        else:
-            return workflow_definition.galaxy_workflow_id
-
-    def get_visualization_definition(self):
-        """
-        Fetch the VisualizationDefinition associated with a ToolDefinition
-        :return: <VisualizationDefinition> if available
-        """
-        try:
-            return VisualizationDefinition.objects.get(uuid=self.uuid)
-        except (VisualizationDefinition.DoesNotExist,
-                VisualizationDefinition.MultipleObjectsReturned) as e:
-            logger.info(
-                "Couldn't properly fetch VisualizationDefinition with UUID: "
-                "{} {}".format(self.uuid, e)
-            )
-            raise
-
-    def get_workflow_definition(self):
-        """
-        Fetch the WorkflowDefinition associated with a ToolDefinition
-        :return: <WorkflowDefinition> if available
-        """
-        try:
-            return WorkflowDefinition.objects.get(uuid=self.uuid)
-        except (WorkflowDefinition.DoesNotExist,
-                WorkflowDefinition.MultipleObjectsReturned) as e:
-            logger.info(
-                "Couldn't properly fetch WorkflowDefinition with UUID: "
-                "{} {}".format(self.uuid, e)
-            )
-            raise
-
-
-class VisualizationDefinition(ToolDefinition):
-    """
-    An Extension of the ToolDefinition model with fields particular to
-    Visualization tools
-    """
-    docker_image_name = models.CharField(max_length=255)
+    docker_image_name = models.CharField(max_length=255, blank=True, null=True)
     container_input_path = models.CharField(
         max_length=500,
-        blank=True,
-        null=True
+        blank=True
+    )
+    galaxy_workflow_id = models.CharField(
+        max_length=250,
+        blank=True
     )
 
-    class Meta:
-        verbose_name = "Visualization Definition"
-
     def __str__(self):
-        return "{}: {} - {} {}".format(
-            self.tool_type,
-            self.name,
-            self.docker_image_name,
-        )
-
-
-class WorkflowDefinition(ToolDefinition):
-    """
-    An Extension of the ToolDefinition model with fields particular to
-    Galaxy Workflows
-    """
-    galaxy_workflow_id = models.CharField(max_length=250)
-
-    class Meta:
-        verbose_name = "Workflow Definition"
-
-    def __str__(self):
-        return "{}: {} - {}".format(
-            self.tool_type,
-            self.name,
-            self.galaxy_workflow_id,
-        )
+        return "{}: {} {}".format(self.tool_type, self.name, self.uuid)
 
 
 @receiver(pre_delete, sender=ToolDefinition)
@@ -311,149 +207,73 @@ def delete_input_files_and_file_relationships(sender, instance, *args,
         file_relationship.delete()
 
 
-class ToolLaunch(OwnableResource):
+class Tool(OwnableResource):
     """
-    A ToolLaunch is an abstract representation of the
-    information it will take to launch a Refinery Tool
+    A Tool is a representation of the information it will take to launch a
+    Refinery-based Tool
     """
+    analysis = models.OneToOneField(Analysis, blank=True, null=True)
+    container_name = models.CharField(
+        max_length=250,
+        unique=True,
+        blank=True
+    )
     file_relationships = models.TextField()
     parameters = models.TextField()
     tool_definition = models.ForeignKey(ToolDefinition)
 
     class Meta:
-        verbose_name = "toollaunch"
+        verbose_name = "tool"
         permissions = (
             ('read_%s' % verbose_name, 'Can read %s' % verbose_name),
         )
 
     def __str__(self):
-        return "Tool Launch: {} {}".format(
+        return "Tool: {} {} {}".format(
             self.get_tool_type(),
-            self.tool_definition.name
-        )
-
-    def get_analysis(self):
-        try:
-            workflow_tool_launch = self.get_workflow_tool_launch()
-        except (WorkflowToolLaunch.DoesNotExist,
-                WorkflowToolLaunch.MultipleObjectsReturned):
-            return None
-        else:
-            return workflow_tool_launch.analysis
-
-    def get_container_name(self):
-        """
-        Fetch the container_name of a VisualizationDefinition
-        :return: VisualizationDefinition.container_name if available,
-        otherwise `None`
-        """
-        try:
-            visualization_tool_launch = self.get_visualization_tool_launch()
-        except (VisualizationDefinition.DoesNotExist,
-                VisualizationDefinition.MultipleObjectsReturned):
-            return None
-        else:
-            return visualization_tool_launch.container_name
-
-    def get_tool_type(self):
-        return self.tool_definition.tool_type
-
-    def get_workflow_tool_launch(self):
-        """
-        Fetch the WorkflowToolLaunch associated with a ToolDefinition
-        :return: <WorkflowToolLaunch> if available
-        """
-        try:
-            return WorkflowToolLaunch.objects.get(uuid=self.uuid)
-        except (WorkflowToolLaunch.DoesNotExist,
-                WorkflowToolLaunch.MultipleObjectsReturned) as e:
-            logger.info(
-                "Couldn't properly fetch WorkflowToolLaunch with UUID: "
-                "{} {}".format(self.uuid, e)
-            )
-            raise
-
-    def get_visualization_tool_launch(self):
-        """
-        Fetch the VisualizationToolLaunch associated with a ToolDefinition
-        :return: <VisualizationToolLaunch> if available
-        """
-        try:
-            return VisualizationToolLaunch.objects.get(uuid=self.uuid)
-        except (VisualizationToolLaunch.DoesNotExist,
-                VisualizationToolLaunch.MultipleObjectsReturned) as e:
-            logger.info(
-                "Couldn't properly fetch VisualizationToolLaunch with UUID: "
-                "{} {}".format(self.uuid, e)
-            )
-            raise
-
-    def parse_file_relationships_string(self):
-        pass
-
-    def populate_url_from_node_uuid(self):
-        pass
-
-
-class WorkflowToolLaunch(ToolLaunch):
-    analysis = models.OneToOneField(Analysis)
-
-    class Meta:
-        verbose_name = "workflow tool launch"
-
-    def __str__(self):
-        return "Tool Launch: {} {} - Analysis: {}".format(
-            self.get_tool_type(),
-            self.tool_definition.name,
-            self.get_analysis()
+            self.get_tool_name(),
+            self.uuid
         )
 
     def launch(self):
-        pass
-
-
-class VisualizationToolLaunch(ToolLaunch):
-    container_name = models.CharField(
-        max_length=250,
-        unique=True,
-        validators=[
-            RegexValidator(
-                regex=r'^[a-zA-Z0-9][a-zA-Z0-9_.-]$',
-                message=(
-                    '`container_name` must adhere to Docker specs. '
-                    'See here: http://bit.ly/2pPCuXM'
+        if self.get_tool_type() == ToolDefinition.VISUALIZATION:
+            container = DockerContainerSpec(
+                image_name=self.tool_definition.docker_image_name,
+                container_name=self.container_name,
+                labels={self.uuid: "visualization_uuid"},
+                container_input_path=(
+                    self.tool_definition.container_input_path
                 ),
-            ),
-        ]
-                                      )
+                input={"file_relationships": self.file_relationships}
+            )
+            try:
+                container.run()
+            except APIError as e:
+                return HttpResponseServerError(content=e)
+            else:
+                # TODO: probably need something better than a redirect
+                return redirect(
+                    self.get_relative_container_url()
+                )
 
-    class Meta:
-        verbose_name = "visualization tool launch"
+        if self.get_tool_type() == ToolDefinition.WORKFLOW:
+            raise NotImplementedError
 
     def get_relative_container_url(self):
         """
-        Construct & return the relative url of our VisualizationDefinition's
+        Construct & return the relative url of our ToolLaunches's
         container
         """
         return "/api/v2/docker/{}".format(self.container_name)
 
-    def launch(self):
-        container = DockerContainerSpec(
-            image_name=self.tool_definition.get_docker_image_name(),
-            container_name=self.container_name,
-            labels={self.uuid: "visualization_uuid"},
-            container_input_path=(
-                self.tool_definition.get_container_input_path()
-            ),
-            input={"file_relationships": self.file_relationships}
-        )
+    def get_tool_name(self):
+        return self.tool_definition.name
 
-        try:
-            container.run()
-        except APIError as e:
-            return HttpResponseServerError(content=e)
-        else:
-            # TODO: probably need something better than a redirect
-            return redirect(
-                self.get_relative_container_url()
-            )
+    def get_tool_type(self):
+        return self.tool_definition.tool_type
+
+    def parse_file_relationships_string(self):
+        raise NotImplementedError
+
+    def populate_url_from_node_uuid(self):
+        raise NotImplementedError
