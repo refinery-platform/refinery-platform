f56c6e3efd2fca0ce93aef86b17eb5b690c6df87
Scott Ouellette <scottx611x@gmail.com>
2017-04-24 19:43:38 -0400

    Test to figure out different regex behavior between local and Travis

--- refinery/tool_manager/tests.py
+++ refinery/tool_manager/tests.py
@@ -1,6 +1,7 @@
 import json
 import logging
 import mock
+import re
 import requests
 from urlparse import urljoin
 
@@ -826,7 +827,7 @@ class ToolLaunchTests(StaticLiveServerTestCase):
             )
 
         file_relationships = eval(tool_launch.file_relationships)
-        regex = r'media\/file_store\/[\d\w]+\/[\d\w]+\/test_file_[ab]_.*.txt'
+        regex = re.compile("[\d\w]+\/[\d\w]+\/test_file_[ab]_.*.txt")
         for url in file_relationships:
             self.assertRegexpMatches(url, regex)
 
