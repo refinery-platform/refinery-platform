97aa4ba2a030d8591200ff6602832ca7def23243
Scott Ouellette <scottx611x@gmail.com>
2017-04-24 17:33:12 -0400

    Remove global here to avoid circular import

--- refinery/tool_manager/urls.py
+++ refinery/tool_manager/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.conf.urls import url, include
 from rest_framework.routers import DefaultRouter
 from .views import ToolDefinitionsViewSet, ToolsViewSet
@@ -7,9 +8,8 @@ tool_manager_router = DefaultRouter()
 tool_manager_router.register(r'tools', ToolsViewSet)
 tool_manager_router.register(r'tool_definitions', ToolDefinitionsViewSet)
 
-# Django docker engine urls
-DJANGO_DOCKER_ENGINE_URL_ROOT = "visualizations"
+
 django_docker_engine_url = url(
-    r'^{}/'.format(DJANGO_DOCKER_ENGINE_URL_ROOT),
+    r'^{}/'.format(settings.DJANGO_DOCKER_ENGINE_URL_ROOT),
     include('django_docker_engine.urls')
 )
