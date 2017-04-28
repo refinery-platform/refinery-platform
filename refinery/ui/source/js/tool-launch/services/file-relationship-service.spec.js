dfcd20518835935113ac51d71c23e0fdd14ebdab
Jennifer Marx <jennyrutledge@gmail.com>
2017-04-28 13:38:56 -0400

    Refresh file map unit test.

--- refinery/ui/source/js/tool-launch/services/file-relationship-service.spec.js
+++ refinery/ui/source/js/tool-launch/services/file-relationship-service.spec.js
@@ -4,34 +4,149 @@
   describe('File Relationship Service', function () {
     var mocker;
     var service;
+    var toolsFactory;
+    var mockWorkflow;
 
     beforeEach(module('refineryApp'));
     beforeEach(module('refineryToolLaunch'));
     beforeEach(inject(function (
       fileRelationshipService,
-      mockParamsFactory
+      mockParamsFactory,
+      toolsService
     ) {
       service = fileRelationshipService;
       mocker = mockParamsFactory;
+      toolsFactory = toolsService;
+
+      mockWorkflow = {
+        file_relationship: {
+          file_relationship: [
+            {
+              file_relationship: [
+                {
+                  file_relationship: [],
+                  input_files: [
+                    {
+                      allowed_filetypes: [
+                        {
+                          name: 'BAM',
+                          description: 'Binary compressed SAM',
+                          used_for_visualization: true
+                        },
+                        {
+                          name: 'FASTQ',
+                          description: 'FASTQ file',
+                          used_for_visualization: false
+                        }
+                      ],
+                      uuid: mocker.generateUuid(),
+                      name: 'Cool Input File A',
+                      description: 'Cool Input File A Description'
+                    },
+                    {
+                      allowed_filetypes: [
+                        {
+                          name: 'BAM',
+                          description: 'Binary compressed SAM',
+                          used_for_visualization: true
+                        },
+                        {
+                          name: 'FASTQ',
+                          description: 'FASTQ file',
+                          used_for_visualization: false
+                        }
+                      ],
+                      uuid: '233986c5-d4f2-4d04-9be5-323eddcc85ba',
+                      name: 'Cool Input File B',
+                      description: 'Cool Input File B Description'
+                    }
+                  ],
+                  uuid: 'c6af0093-52fa-48b9-92fc-3b41b382a43f',
+                  name: 'Pair of input files',
+                  value_type: 'PAIR'
+                }
+              ],
+              input_files: [],
+              uuid: '283a97a6-a676-40ce-b6d9-5f5aa5cea28f',
+              name: 'List of Pairs',
+              value_type: 'LIST'
+            }
+          ],
+          input_files: [],
+          uuid: 'd8342a83-60fa-4619-a924-06e5a0711232',
+          name: 'List of Lists',
+          value_type: 'LIST'
+        },
+        output_files: [
+          {
+            filetype: {
+              name: 'BIGWIG',
+              description: 'Big WIG',
+              used_for_visualization: true
+            },
+            uuid: '566b9d6c-0b90-4dcc-8444-3013eb1ec05e',
+            name: 'output_file',
+            description: 'cool'
+          }
+        ],
+        parameters: [],
+        uuid: 'e655ecec-4833-434d-9244-d5d53445cf8e',
+        name: 'Test workflow: LIST:LIST:PAIR',
+        description: 'Test LIST:LIST:PAIR description',
+        tool_type: 'WORKFLOW',
+        docker_image_name: '',
+        container_input_path: null,
+        galaxy_workflow_id: null,
+        workflow_engine: null
+      };
     }));
 
-    it('service and variables should exist', function () {
+    it('service should exist', function () {
       expect(service).toBeDefined();
     });
 
-    it('setGroupCollection is a method', function () {
-      expect(angular.isFunction(service.setGroupCollection)).toBe(true);
+    it('view model variables should exist', function () {
+      expect(service.attributesObj).toEqual({});
+      expect(service.currentGroup).toEqual([]);
+      expect(service.currentTypes).toEqual([]);
+      expect(service.groupCollection).toEqual({});
+      expect(service.hideNodePopover).toEqual(false);
+      expect(service.inputFileTypes).toEqual([]);
+      expect(service.nodeSelectCollection).toEqual({});
     });
 
     it('setNodeSelectCollection is a method', function () {
       expect(angular.isFunction(service.setNodeSelectCollection)).toBe(true);
     });
 
-    it('refreshFileMap is a method', function () {
-      expect(angular.isFunction(service.refreshFileMap)).toBe(true);
+    describe('refreshFileMap', function () {
+      beforeEach(function () {
+        angular.copy(mockWorkflow, toolsFactory.selectedTool);
+      });
+
+      it('refreshFileMap is a method', function () {
+        expect(angular.isFunction(service.refreshFileMap)).toBe(true);
+      });
+
+      it('passes when file_relationship is empty', function () {
+        angular.copy({}, toolsFactory.selectedTool);
+        service.refreshFileMap();
+        expect(service.currentGroup).toEqual([]);
+        expect(service.currentTypes).toEqual([]);
+      });
+
+      it('generates correct currentTypes', function () {
+        service.refreshFileMap();
+        expect(service.currentTypes).toEqual(['LIST', 'LIST', 'PAIR']);
+      });
+
+      it('generates correct currentGroups', function () {
+        service.refreshFileMap();
+        expect(service.currentGroup).toEqual([0, 0, 0]);
+      });
     });
 
-    describe('File Relationship Service', function () {
+    describe('resetMethods', function () {
       it('resetCurrents is a method', function () {
         expect(angular.isFunction(service.resetCurrents)).toBe(true);
       });
@@ -51,6 +166,13 @@
         expect(service.currentGroup).toEqual([]);
         expect(service.currentTypes).toEqual([]);
         expect(service.inputFileTypes).toEqual([]);
+      });
+    });
+
+
+    describe('Set Group Collection', function () {
+      it('setGroupCollection is a method', function () {
+        expect(angular.isFunction(service.setGroupCollection)).toBe(true);
       });
     });
   });
