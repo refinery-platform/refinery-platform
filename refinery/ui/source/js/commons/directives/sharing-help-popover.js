e9048750228f17512f4d536657a3d33121f7c752
Jennifer Marx <jennyrutledge@gmail.com>
2017-05-03 13:15:18 -0400

    Add a common popover for sharing help.

---
+++ refinery/ui/source/js/commons/directives/sharing-help-popover.js
@@ -0,0 +1,8 @@
+(function () {
+  'use strict';
+  angular.module('refineryApp').component('rpSharingHelpPopover', {
+    templateUrl: ['$window', function ($window) {
+      return $window.getStaticUrl('partials/commons/partials/sharing-help-popover.html');
+    }]
+  });
+})();
