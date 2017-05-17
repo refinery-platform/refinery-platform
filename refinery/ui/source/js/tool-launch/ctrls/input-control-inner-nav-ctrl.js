28867cad2b55bf1856363755108a23ceb24062bb
Jennifer Marx <jennyrutledge@gmail.com>
2017-05-17 19:51:35 -0400

    Add validation method.

--- refinery/ui/source/js/tool-launch/ctrls/input-control-inner-nav-ctrl.js
+++ refinery/ui/source/js/tool-launch/ctrls/input-control-inner-nav-ctrl.js
@@ -7,12 +7,14 @@
 
   InputControlInnerNavCtrl.$inject = [
     '$scope',
+    '_',
     'fileRelationshipService'
   ];
 
 
   function InputControlInnerNavCtrl (
     $scope,
+    _,
     fileRelationshipService
   ) {
     var fileService = fileRelationshipService;
@@ -20,6 +22,7 @@
     vm.currentGroup = fileService.currentGroup; // maintains nav position
     vm.currentTypes = fileService.currentTypes;
     vm.inputFileTypes = fileService.inputFileTypes;
+    vm.needMoreNodes = needMoreNodes;
     vm.navRight = navRight;
     vm.navLeft = navLeft;
    /*
@@ -33,6 +36,36 @@
 
     function navRight (depth) {
       vm.currentGroup[depth]++;
+    }
+
+    function needMoreNodes () {
+      var moreFlag = false;
+      var groupType = fileService.currentTypes[fileService.currentTypes.length - 1];
+      if (!_.property(vm.currentGroup)(fileService.groupCollection)) {
+        moreFlag = true;
+      } else if (groupType === 'PAIR') {
+        var inputLength = _.keys(fileService.groupCollection[vm.currentGroup]).length;
+        if (inputLength > 1) {
+          angular.forEach(
+            fileService.groupCollection[vm.currentGroup],
+            function (inputList) {
+              if (inputList.length === 0) {
+                moreFlag = true;
+              }
+            });
+        } else {
+          moreFlag = true;
+        }
+      } else {
+        angular.forEach(
+          fileService.groupCollection[vm.currentGroup],
+          function (inputList) {
+            if (inputList.length === 0) {
+              moreFlag = true;
+            }
+          });
+      }
+      return moreFlag;
     }
     /*
    * ---------------------------------------------------------
