473ccd2aa2f5c47228ec0892877fc7505ae4ba5e
Jennifer Marx <jennyrutledge@gmail.com>
2017-05-17 21:39:56 -0400

    Add validation method.

--- refinery/ui/source/js/tool-launch/ctrls/input-control-inner-nav-ctrl.js
+++ refinery/ui/source/js/tool-launch/ctrls/input-control-inner-nav-ctrl.js
@@ -22,9 +22,9 @@
     vm.currentGroup = fileService.currentGroup; // maintains nav position
     vm.currentTypes = fileService.currentTypes;
     vm.inputFileTypes = fileService.inputFileTypes;
-    vm.needMoreNodes = needMoreNodes;
     vm.navRight = navRight;
     vm.navLeft = navLeft;
+    vm.needMoreNodes = needMoreNodes;
    /*
    * ---------------------------------------------------------
    * Methods Definitions
@@ -38,34 +38,36 @@
       vm.currentGroup[depth]++;
     }
 
+    // Main method to check if the group has minimum nodes
     function needMoreNodes () {
       var moreFlag = false;
       var groupType = fileService.currentTypes[fileService.currentTypes.length - 1];
       if (!_.property(vm.currentGroup)(fileService.groupCollection)) {
         moreFlag = true;
       } else if (groupType === 'PAIR') {
+        // pair, requires 2 inputTypes
         var inputLength = _.keys(fileService.groupCollection[vm.currentGroup]).length;
         if (inputLength > 1) {
-          angular.forEach(
-            fileService.groupCollection[vm.currentGroup],
-            function (inputList) {
-              if (inputList.length === 0) {
-                moreFlag = true;
-              }
-            });
+          moreFlag = areInputFileTypesEmpty();
         } else {
           moreFlag = true;
         }
       } else {
-        angular.forEach(
-          fileService.groupCollection[vm.currentGroup],
-          function (inputList) {
-            if (inputList.length === 0) {
-              moreFlag = true;
-            }
-          });
+        // list
+        moreFlag = areInputFileTypesEmpty();
       }
       return moreFlag;
+    }
+
+    // helper method to check if each group-inputType are empty
+    function areInputFileTypesEmpty () {
+      var isEmpty = false;
+      angular.forEach(fileService.groupCollection[vm.currentGroup], function (inputList) {
+        if (inputList.length === 0) {
+          isEmpty = true;
+        }
+      });
+      return isEmpty;
     }
     /*
    * ---------------------------------------------------------
