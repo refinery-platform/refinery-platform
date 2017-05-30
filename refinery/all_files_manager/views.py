77de2a20687ea5ca4dfb123b8572d9bd5e22ed5a
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2017-05-30 13:04:56 -0400

    Checkpoint: paste in code from Scott

--- refinery/all_files_manager/views.py
+++ refinery/all_files_manager/views.py
@@ -1,14 +1,29 @@
 import logging
+import requests
 
-from django.core import serializers
-from django.http import (HttpResponse, HttpResponseNotAllowed)
+from guardian.shortcuts import get_objects_for_user
+from django.http import (HttpResponseNotAllowed, JsonResponse)
+from data_set_manager.models import (Study, Assay)
 
 logger = logging.getLogger(__name__)
 
 
 def all_files(request):
     if request.method == 'GET':
-        data = serializers.serialize('json', {})
-        return HttpResponse(data, content_type='application/json')
+        data = []
+        datasets = get_objects_for_user(request.user, "core.read_dataset")
+        for dataset in datasets:
+            investigation_link = dataset.get_version_details()
+            study = Study.objects.get(
+                investigation=investigation_link.investigation
+            )
+            assay = Assay.objects.get(study=study)
+            response = requests.get(
+                "http://192.168.50.50:8000/api/v2/assays/{}/files/".format(
+                    assay.uuid
+                )
+            )
+            data.append(response.content)
+        return JsonResponse(data, safe=False)
     else:
         return HttpResponseNotAllowed(['GET'])
