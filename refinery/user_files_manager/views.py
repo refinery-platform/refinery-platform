39ac018c9065642c04e525c02c6f116a9360c6f8
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2017-06-01 14:26:06 -0400

    Since we are not inside django_rest here...

--- refinery/user_files_manager/views.py
+++ refinery/user_files_manager/views.py
@@ -10,7 +10,7 @@ logger = logging.getLogger(__name__)
 
 def user_files(request):
     if request.method == 'GET':
-        params = request.query_params
+        params = request.GET
 
         solr_params = generate_solr_params_for_user(params, user=request.user)
         solr_response = search_solr(solr_params, 'data_set_manager')
