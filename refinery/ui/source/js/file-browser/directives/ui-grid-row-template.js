d7739664b15a597c8fa7f1030bf3121b10c952a4
Jennifer Marx <jennyrutledge@gmail.com>
2017-06-01 09:52:04 -0400

    Fix styling.

--- refinery/ui/source/js/file-browser/directives/ui-grid-row-template.js
+++ refinery/ui/source/js/file-browser/directives/ui-grid-row-template.js
@@ -1,15 +1,19 @@
-'use strict';
-
-function rpUiGridRowTemplate ($window) {
-  return {
-    restrict: 'E',
-    templateUrl: function () {
-      return $window.getStaticUrl('partials/file-browser/partials/ui-grid-row-template.html');
-    }
-  };
-}
-
-angular
+(function () {
+  'use strict';
+  angular
   .module('refineryFileBrowser')
-  .directive('rpUiGridRowTemplate', ['$window', rpUiGridRowTemplate]
-);
+  .directive('rpUiGridRowTemplate', rpUiGridRowTemplate);
+
+  rpUiGridRowTemplate.$inject = ['$window'];
+
+  function rpUiGridRowTemplate ($window) {
+    return {
+      restrict: 'E',
+      templateUrl: function () {
+        return $window.getStaticUrl(
+          'partials/file-browser/partials/ui-grid-row-template.html'
+        );
+      }
+    };
+  }
+})();
