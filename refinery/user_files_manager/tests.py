6155afce522ad45074b3ad99a47296e7f1941122
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2017-06-02 11:17:59 -0400

    Uncomment the failing test; todo: start solr on Travis?

--- refinery/user_files_manager/tests.py
+++ refinery/user_files_manager/tests.py
@@ -11,6 +11,6 @@ class UserFilesAPITests(TestCase):
     # TODO: Fails, because solr isn't running.
     # Should I mock it, or make this an integration test?
 
-    # def test_get(self):
-    #     response = self.client.get('/api/v2/user/files/')
-    #     self.assertEqual(response.status_code, 200)
+    def test_get(self):
+        response = self.client.get('/api/v2/user/files/')
+        self.assertEqual(response.status_code, 200)
