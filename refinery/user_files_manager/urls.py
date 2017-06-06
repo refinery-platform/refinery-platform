24294bebc33b02cbc6e167e64e1db2f4ace4cc92
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2017-06-06 10:57:16 -0400

    Routing for class-based views

--- refinery/user_files_manager/urls.py
+++ refinery/user_files_manager/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 from rest_framework.routers import DefaultRouter
+from .views import UserFiles
 
 user_files_router = DefaultRouter()
 user_files_router.urls.extend([
-    url(r'^user/files/$', 'user_files_manager.views.user_files')
+    url(r'^user/files/$', UserFiles.as_view())
 ])
