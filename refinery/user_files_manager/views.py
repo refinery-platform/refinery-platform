f6591d6117e0885b9182e67c3190596caffa5476
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2017-06-06 10:17:34 -0400

    Class-based view

--- refinery/user_files_manager/views.py
+++ refinery/user_files_manager/views.py
@@ -1,21 +1,21 @@
 import logging
-from django.http import HttpResponseNotAllowed
+
+from rest_framework.views import APIView
 from rest_framework.response import Response
-from data_set_manager.utils import (
-    search_solr, format_solr_response,
-    generate_solr_params_for_user)
+
+from data_set_manager.utils import (search_solr, format_solr_response,
+                                    generate_solr_params_for_user)
+
 
 logger = logging.getLogger(__name__)
 
 
-def user_files(request):
-    if request.method == 'GET':
-        params = request.GET
+class UserFiles(APIView):
+    def get(self, request):
+        params = request.query_params
 
         solr_params = generate_solr_params_for_user(params, user=request.user)
         solr_response = search_solr(solr_params, 'data_set_manager')
         solr_response_json = format_solr_response(solr_response)
 
         return Response(solr_response_json)
-    else:
-        return HttpResponseNotAllowed(['GET'])
