396ff4f4347e2005a4d5caa4870cca1affbb4534
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2017-06-07 11:20:19 -0400

    Will the precommit run isort?

--- refinery/user_files_manager/views.py
+++ refinery/user_files_manager/views.py
@@ -2,7 +2,6 @@ import logging
 
 from data_set_manager.utils import (format_solr_response,
                                     generate_solr_params_for_user, search_solr)
-
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
