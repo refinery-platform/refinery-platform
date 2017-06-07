2bf5020fd2d0ae13e07a21846b4aab269d75532a
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2017-06-07 11:48:16 -0400

    auto-corrected

--- refinery/user_files_manager/views.py
+++ refinery/user_files_manager/views.py
@@ -4,7 +4,6 @@ from data_set_manager.utils import (format_solr_response,
                                     generate_solr_params_for_user, search_solr)
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
