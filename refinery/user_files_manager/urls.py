1619af144b4f7dc639eb3bdc38499ff6c0b24e27
mccalluc <mccalluc@users.noreply.github.com>
2017-06-08 13:52:02 -0400

    isort prehook (#1771)
    * Will the precommit run isort?
    
    * Will the precommit run isort?
    
    * Tweak pre-commit
    
    * auto-corrected
    
    * isort the other user_files_manager files
    
    * developers will need isort installed

--- refinery/user_files_manager/urls.py
+++ refinery/user_files_manager/urls.py
@@ -1,5 +1,4 @@
 from django.conf.urls import url
-
 from rest_framework.routers import DefaultRouter
 
 from .views import UserFiles
--- refinery/user_files_manager/views.py
+++ refinery/user_files_manager/views.py
@@ -2,10 +2,8 @@ import logging
 
 from data_set_manager.utils import (format_solr_response,
                                     generate_solr_params_for_user, search_solr)
-
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
