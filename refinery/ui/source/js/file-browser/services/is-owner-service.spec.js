c81f3d673758bffdc2491d540f713ee8caefbda5
Jennifer Marx <jkmarx@users.noreply.github.com>
2017-06-13 13:54:29 -0400

    Update module and setting file to style guide. (#1786)
    * Update module and setting file to style guide.
    
    * Jkmarx/file browser refactor services (#1787)
    
    * Refactor main factory file according to style guide.
    
    * Update main factory test and remove unneccessary variables.
    
    * Update smaller services according to style guide. (#1788)
    
    * Update smaller services according to style guide.
    
    * Jkmarx/generalize attribute filter (#1790)
    
    * Move uiSelectedFields to service.
    
    * Add assay filter service.
    
    * Move methods to assay-filters-ctrl
    
    * Update initialize events.
    
    * Add and update unit test for ctrl.
    
    * Fix bug related to url query.
    
    * Remove broadcast and use watcher.
    
    * Remove console.
    
    * Update unit test.
    
    * Add ctrl comments.
    
    * Fix scope and remove unneccessary variable.
    
    * fix bug due to url query generation.
    
    * Add comments and unit test.
    
    * Add unit test and comments.

--- refinery/ui/source/js/file-browser/services/is-owner-service.spec.js
+++ refinery/ui/source/js/file-browser/services/is-owner-service.spec.js
@@ -1,50 +1,52 @@
-// UNIT TESTING
-'use strict';
+(function () {
+  // UNIT TESTING
+  'use strict';
 
-describe('Is Owner Service', function () {
-  var deferred;
-  var rootScope;
-  var service;
-  var fakeUuid;
+  describe('Is Owner Service', function () {
+    var deferred;
+    var rootScope;
+    var service;
+    var fakeUuid;
 
-  beforeEach(module('refineryApp'));
-  beforeEach(module('refineryFileBrowser'));
-  beforeEach(inject(function (_isOwnerService_, _mockParamsFactory_) {
-    service = _isOwnerService_;
-    fakeUuid = _mockParamsFactory_.generateUuid();
-  }));
-
-  it('service and variables should exist', function () {
-    expect(service).toBeDefined();
-    expect(service.isOwner).toEqual(false);
-  });
-
-  describe('getAssayFiles', function () {
-    beforeEach(inject(function (dataSetService, $q, $rootScope) {
-      var responseData = { objects: [{ is_owner: true }] };
-      spyOn(dataSetService, 'query').and.callFake(function () {
-        deferred = $q.defer();
-        deferred.resolve(responseData);
-        return { $promise: deferred.promise };
-      });
-      rootScope = $rootScope;
+    beforeEach(module('refineryApp'));
+    beforeEach(module('refineryFileBrowser'));
+    beforeEach(inject(function (isOwnerService, mockParamsFactory) {
+      service = isOwnerService;
+      fakeUuid = mockParamsFactory.generateUuid();
     }));
 
-    it('refreshDataSetOwner is a method', function () {
-      expect(angular.isFunction(service.refreshDataSetOwner)).toBe(true);
+    it('service and variables should exist', function () {
+      expect(service).toBeDefined();
+      expect(service.isOwner).toEqual(false);
     });
 
-    it('refreshDataSetOwner returns a promise', function () {
-      var successData;
-      var response = service
-        .refreshDataSetOwner({ uuid: fakeUuid })
-        .then(function (responseData) {
-          successData = responseData.objects[0].is_owner;
+    describe('getAssayFiles', function () {
+      beforeEach(inject(function (dataSetService, $q, $rootScope) {
+        var responseData = { objects: [{ is_owner: true }] };
+        spyOn(dataSetService, 'query').and.callFake(function () {
+          deferred = $q.defer();
+          deferred.resolve(responseData);
+          return { $promise: deferred.promise };
         });
-      rootScope.$apply();
-      expect(typeof response.then).toEqual('function');
-      expect(successData).toEqual(true);
-      expect(service.isOwner).toEqual(true);
+        rootScope = $rootScope;
+      }));
+
+      it('refreshDataSetOwner is a method', function () {
+        expect(angular.isFunction(service.refreshDataSetOwner)).toBe(true);
+      });
+
+      it('refreshDataSetOwner returns a promise', function () {
+        var successData;
+        var response = service
+          .refreshDataSetOwner({ uuid: fakeUuid })
+          .then(function (responseData) {
+            successData = responseData.objects[0].is_owner;
+          });
+        rootScope.$apply();
+        expect(typeof response.then).toEqual('function');
+        expect(successData).toEqual(true);
+        expect(service.isOwner).toEqual(true);
+      });
     });
   });
-});
+})();
