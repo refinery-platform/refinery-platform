8d8e5c81328cbfa4f9aef595309f86b0087a07eb
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2017-06-19 14:52:21 -0400

    Move columnDefs inside promise

--- refinery/ui/source/js/user-file-browser/ctrls/user-file-browser-files-ctrl.js
+++ refinery/ui/source/js/user-file-browser/ctrls/user-file-browser-files-ctrl.js
@@ -15,36 +15,8 @@
     var promise = $q.defer();
     var getUserFiles = userFileBrowserFactory.getUserFiles;
     getUserFiles().then(function (solr) {
-      vm.gridOptions.data = [];
-      for (var i = 0; i < solr.nodes.length; i++) {
-        var node = solr.nodes[i];
-        var url = node.REFINERY_NAME_6_3_s;
-        vm.gridOptions.data.push({
-          url: url,
-          technology: 'TODO',
-          filename: url ? decodeURIComponent(url.replace(/.*\//, '')) : '',
-          organism: node.organism_Characteristics_6_3_s,
-          date: 'TODO',
-          antibody: node.antibody_Factor_Value_6_3_s,
-          cell_type: node.cell_line_Characteristics_6_3_s,
-          published: 'TODO',
-          accession: 'TODO',
-          genotype: 'TODO',
-          owner: 'TODO'
-        });
-      }
-      promise.resolve();
-    }, function () {
-      promise.reject();
-    });
-
-    vm.gridOptions = {
-      appScopeProvider: vm,
-      useExternalSorting: true,
-      selectionRowHeaderWidth: 35,
-      rowHeight: 35,
-      columnDefs: [
-          { field: 'url',
+      vm.gridOptions.columnDefs = [
+           { field: 'url',
             enableSorting: false,
             displayName: '',
             cellTemplate:
@@ -72,7 +44,36 @@
           { field: 'antibody' },
           { field: 'cell_type' },
           { field: 'genotype' }
-      ]
+      ];
+
+      vm.gridOptions.data = [];
+      for (var i = 0; i < solr.nodes.length; i++) {
+        var node = solr.nodes[i];
+        var url = node.REFINERY_NAME_6_3_s;
+        vm.gridOptions.data.push({
+          url: url,
+          technology: 'TODO',
+          filename: url ? decodeURIComponent(url.replace(/.*\//, '')) : '',
+          organism: node.organism_Characteristics_6_3_s,
+          date: 'TODO',
+          antibody: node.antibody_Factor_Value_6_3_s,
+          cell_type: node.cell_line_Characteristics_6_3_s,
+          published: 'TODO',
+          accession: 'TODO',
+          genotype: 'TODO',
+          owner: 'TODO'
+        });
+      }
+      promise.resolve();
+    }, function () {
+      promise.reject();
+    });
+
+    vm.gridOptions = {
+      appScopeProvider: vm,
+      useExternalSorting: true,
+      selectionRowHeaderWidth: 35,
+      rowHeight: 35
     };
   }
 })();
