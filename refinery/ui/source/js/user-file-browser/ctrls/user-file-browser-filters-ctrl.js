c864313cdff24cbeec6f6e149f1c42f8b2ca9dfe
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2017-06-20 11:45:57 -0400

    ... and move it to the factory

--- refinery/ui/source/js/user-file-browser/ctrls/user-file-browser-filters-ctrl.js
+++ refinery/ui/source/js/user-file-browser/ctrls/user-file-browser-filters-ctrl.js
@@ -14,50 +14,9 @@
     var vm = this;
     var promise = $q.defer();
     var getUserFiles = userFileBrowserFactory.getUserFiles;
-    getUserFiles().then(function (/* solr */) {
-      vm.attributeFilters = {
-        Technology: {
-          facetObj: [
-            { name: 'ChIP-seq', count: 4 },
-            { name: 'RNA-seq', count: 7 }
-          ]
-        },
-        Organism: {
-          facetObj: [
-            { name: 'Homo sapiens', count: 3 },
-            { name: 'Mus musculus', count: 8 }
-          ]
-        },
-        Filetype: {
-          facetObj: [
-            { name: 'GCT file', count: 42 },
-            { name: 'Affymetrix Probe Results', count: 8 }
-          ]
-        },
-        Owner: {
-          facetObj: [
-            { name: 'Chuck McCallum', count: 6 },
-            { name: 'Geoff Nelson', count: 6 }
-          ]
-        },
-        Antibody: {
-          facetObj: [
-            { name: 'HNF4A', count: 5 },
-            { name: 'FRTS4', count: 46 }
-          ]
-        },
-        Celltype: {
-          facetObj: [
-            { name: 'Caco-2', count: 42 },
-            { name: 'HeLa', count: 1 }
-          ]
-        },
-        Genotype: {
-          facetObj: [
-            { name: 'C57BL/6J', count: 12 }
-          ]
-        }
-      };
+    getUserFiles().then(function (solr) {
+      console.log(solr);
+      vm.attributeFilters = userFileBrowserFactory.createFilters(solr);
       promise.resolve();
     }, function () {
       promise.reject();
--- refinery/ui/source/js/user-file-browser/services/factory.js
+++ refinery/ui/source/js/user-file-browser/services/factory.js
@@ -21,7 +21,8 @@
     return {
       getUserFiles: getUserFiles,
       createColumnDefs: createColumnDefs,
-      createData: createData
+      createData: createData,
+      createFilters: createFilters
     };
 
     /*
@@ -104,6 +105,52 @@
         data.push(row);
       });
       return data;
+    }
+
+    function createFilters (/* solr */) {
+      return {
+        Technology: {
+          facetObj: [
+            { name: 'ChIP-seq', count: 4 },
+            { name: 'RNA-seq', count: 7 }
+          ]
+        },
+        Organism: {
+          facetObj: [
+            { name: 'Homo sapiens', count: 3 },
+            { name: 'Mus musculus', count: 8 }
+          ]
+        },
+        Filetype: {
+          facetObj: [
+            { name: 'GCT file', count: 42 },
+            { name: 'Affymetrix Probe Results', count: 8 }
+          ]
+        },
+        Owner: {
+          facetObj: [
+            { name: 'Chuck McCallum', count: 6 },
+            { name: 'Geoff Nelson', count: 6 }
+          ]
+        },
+        Antibody: {
+          facetObj: [
+            { name: 'HNF4A', count: 5 },
+            { name: 'FRTS4', count: 46 }
+          ]
+        },
+        Celltype: {
+          facetObj: [
+            { name: 'Caco-2', count: 42 },
+            { name: 'HeLa', count: 1 }
+          ]
+        },
+        Genotype: {
+          facetObj: [
+            { name: 'C57BL/6J', count: 12 }
+          ]
+        }
+      };
     }
   }
 })();
