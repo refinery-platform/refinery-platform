4a152c1b55b6d5a6a698b04b235153e1c779c34d
Ilya Sytchev <isytchev@hsph.harvard.edu>
2017-06-21 12:09:55 -0400

    Add AWS Cognito settings to Django config

--- refinery/config/settings/aws.py
+++ refinery/config/settings/aws.py
@@ -7,7 +7,7 @@
 from .prod import *  # NOQA
 
 
-# email
+# Email
 EMAIL_BACKEND = 'django_smtp_ssl.SSLEmailBackend'
 EMAIL_HOST_USER = get_setting('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = get_setting('EMAIL_HOST_PASSWORD')
@@ -15,11 +15,17 @@ EMAIL_USE_TLS = True
 EMAIL_PORT = 465
 
 
-# storage
+# Storage
 INSTALLED_APPS += (
     'storages',
 )
 STATIC_BUCKET = get_setting('S3_BUCKET_NAME_BASE') + '-static'
 STATIC_URL = 'https://{}.s3.amazonaws.com/'.format(STATIC_BUCKET)
 STATICFILES_STORAGE = 'config.utils_aws.S3StaticStorage'
+COGNITO_IDENTITY_POOL_NAME = get_setting('COGNITO_IDENTITY_POOL_NAME')
+COGNITO_DEVELOPER_PROVIDER_NAME = get_setting(
+    'COGNITO_DEVELOPER_PROVIDER_NAME'
+)
+
+# Refinery
 DEPLOYMENT_PLATFORM = 'aws'
