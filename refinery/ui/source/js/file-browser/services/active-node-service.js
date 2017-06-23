b6fe7d1cc0dd69c6099c1842dceb7cb2a6919cb4
Jennifer Marx <jennyrutledge@gmail.com>
2017-06-23 10:44:49 -0400

    Remove unused method.

--- refinery/ui/source/js/file-browser/services/active-node-service.js
+++ refinery/ui/source/js/file-browser/services/active-node-service.js
@@ -9,18 +9,5 @@
     vm.activeNodeRow = {}; // ui-grid node which is selected, shared btwn modules
     vm.selectionObj = {}; // ui-grid maintains checkboxes for popover
     // selection {groupId: {inputFileTypeUuid_1: { nodeUuid: booleanValue }}}
-
-    /**
-     * When a group is removed/clear, this will deselect all associated nodes
-     * from the ui-grid selection obj
-     * @param {str} groupId - string with group Id ex, '[0,0,0]'
-     * */
-    vm.deselectGroupFromSelectionObj = function (groupId) {
-      angular.forEach(vm.selectionObj[groupId], function (inputObj, inputUuid) {
-        angular.forEach(inputObj, function (selectionValue, nodeUuid) {
-          vm.selectionObj[groupId][inputUuid][nodeUuid] = false;
-        });
-      });
-    };
   }
 })();
