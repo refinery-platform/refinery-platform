da4742a3e0e2983b0e1161f335a5a218e18a187f
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2017-06-28 17:20:45 -0400

    Checkpoint: I have not set up my angular dependencies correctly, may need a new service?

--- refinery/ui/source/js/user-file-browser/services/user-file.js
+++ refinery/ui/source/js/user-file-browser/services/user-file.js
@@ -4,9 +4,10 @@
     .module('refineryApp')
     .factory('userFileService', userFileService);
 
-  userFileService.$inject = ['$resource', 'settings'];
+  // TODO: userFileBrowserFiltersCtrl.filters is what I need, but maybe I need a new service?
+  userFileService.$inject = ['$resource', 'settings', 'userFileBrowserFiltersCtrl'];
 
-  function userFileService ($resource, settings) {
+  function userFileService ($resource, settings, userFileBrowserFiltersCtrl) {
     var userFile = $resource(
       settings.appRoot + settings.refineryApiV2 + '/user/files/',
       {},
@@ -14,11 +15,14 @@
         query: {
           method: 'GET',
           params: {
-            fq: '(technology_Characteristics_generic_s:ChIP-seq OR '
-               + 'technology_FOOO_generic_s:ChIP-seq)'
+            fq: function () {
+              // TODO: Get the data the right way, and use it to construct the solr query
+              console.log(userFileBrowserFiltersCtrl.filters);
+              return '(technology_Characteristics_generic_s:ChIP-seq OR '
+                     + 'technology_FOOO_generic_s:ChIP-seq)';
+            }
           }
         }
-
       }
     );
     return userFile;
