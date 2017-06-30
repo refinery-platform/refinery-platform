23745e9eb9d68f096756d2eeda206e285fc67343
Ilya Sytchev <isytchev@hsph.harvard.edu>
2017-06-30 15:39:23 -0400

    Update rpFileUpload directive to conform to the style guide

--- refinery/ui/source/js/data-set-import/directives/file-upload.js
+++ refinery/ui/source/js/data-set-import/directives/file-upload.js
@@ -1,21 +1,21 @@
-'use strict';
+(function () {
+  'use strict';
 
-function rpFileUpload ($window, settings) {
-  return {
-    restrict: 'E',
-    templateUrl: function () {
-      if (settings.djangoApp.deploymentPlatform === 'aws') {
-        return $window.getStaticUrl('partials/data-set-import/partials/file-upload-s3.html');
+  angular
+    .module('refineryDataSetImport')
+    .directive('rpFileUpload', rpFileUpload);
+
+  rpFileUpload.$inject = ['$window', 'settings'];
+
+  function rpFileUpload ($window, settings) {
+    return {
+      restrict: 'E',
+      templateUrl: function () {
+        if (settings.djangoApp.deploymentPlatform === 'aws') {
+          return $window.getStaticUrl('partials/data-set-import/partials/file-upload-s3.html');
+        }
+        return $window.getStaticUrl('partials/data-set-import/partials/file-upload.html');
       }
-      return $window.getStaticUrl('partials/data-set-import/partials/file-upload.html');
-    }
-  };
-}
-
-angular
-  .module('refineryDataSetImport')
-  .directive('rpFileUpload', [
-    '$window',
-    'settings',
-    rpFileUpload
-  ]);
+    };
+  }
+})();
