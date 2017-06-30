51bdae3e24e74a9f1df117f96f9cc000e1a50b28
Ilya Sytchev <isytchev@hsph.harvard.edu>
2017-06-30 15:15:47 -0400

    Update RefineryFileUploadS3Ctrl and corresponding partial to use controllerAs pattern

--- refinery/ui/source/js/data-set-import/controllers/file-upload-s3.js
+++ refinery/ui/source/js/data-set-import/controllers/file-upload-s3.js
@@ -5,19 +5,20 @@
     .module('refineryDataSetImport')
     .controller('RefineryFileUploadS3Ctrl', RefineryFileUploadS3Ctrl);
 
-  RefineryFileUploadS3Ctrl.$inject = ['$scope', 's3UploadService'];
+  RefineryFileUploadS3Ctrl.$inject = ['s3UploadService'];
 
-  function RefineryFileUploadS3Ctrl ($scope, s3UploadService) {
-    $scope.uploadFiles = function (files) {
-      $scope.files = files;
+  function RefineryFileUploadS3Ctrl (s3UploadService) {
+    var vm = this;
+    vm.uploadFiles = function (files) {
+      vm.files = files;
       if (files && files.length > 0) {
-        angular.forEach($scope.files, function (file) {
+        angular.forEach(vm.files, function (file) {
           s3UploadService.upload(file).then(function () {
             // Mark as success
             file.success = true;
           }, function (error) {
             // Mark the error
-            $scope.error = error;
+            vm.error = error;
           }, function (progress) {
             // Write the progress as a percentage
             file.progress = (progress.loaded / progress.total) * 100;
