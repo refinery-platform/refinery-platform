b0539f21de750d9e74d36158738a04ba2a1d79e7
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2017-07-05 12:01:46 -0400

    Fix js test

--- refinery/ui/source/js/user-file-browser/services/user-file.spec.js
+++ refinery/ui/source/js/user-file-browser/services/user-file.spec.js
@@ -160,7 +160,7 @@
             .expectGET(
                 settings.appRoot +
                 settings.refineryApiV2 +
-                '/user/files/?fq='
+                '/user/files/?fq=&limit=100'
             ).respond(200, fakeResponse);
       });
     });
