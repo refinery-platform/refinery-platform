eada0f07d04ef6e65742186ba5754801a693b772
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2017-07-25 12:23:53 -0400

    test passes now

--- refinery/user_files_manager/tests.py
+++ refinery/user_files_manager/tests.py
@@ -11,8 +11,8 @@ import requests
 from rest_framework.test import (APIRequestFactory, APITestCase,
                                  force_authenticate)
 
-from core.models import DataSet, InvestigationLink
-from data_set_manager.models import Assay, AttributeOrder, Investigation, Study
+from data_set_manager.models import Assay
+from factory_boy.utils import create_dataset_with_necessary_models
 
 from .utils import generate_solr_params_for_user
 from .views import UserFiles
@@ -52,273 +52,17 @@ class UserFilesUITests(StaticLiveServerTestCase):
 
 
 class UserFilesUtilsTests(TestCase):
-    def setUp(self):
-        self.user = User.objects.create_user("user", '', 'test1234')
-
-        investigation = Investigation.objects.create()
-        data_set = DataSet.objects.create(
-            title="Test DataSet")
-        InvestigationLink.objects.create(data_set=data_set,
-                                         investigation=investigation)
-
-        self.study = Study.objects.create(file_name='test_filename123.txt',
-                                          title='Study Title Test',
-                                          investigation=investigation)
-
-        self.assay = Assay.objects.create(
-                study=self.study,
-                measurement='transcription factor binding site',
-                measurement_accession='http://www.testurl.org/testID',
-                measurement_source='OBI',
-                technology='nucleotide sequencing',
-                technology_accession='test info',
-                technology_source='test source',
-                platform='Genome Analyzer II',
-                file_name='test_assay_filename.txt',
-                )
-
-        self.attribute_order_array = [{
-            'study': self.study,
-            'assay': self.assay,
-            'solr_field': 'Character_Title',
-            'rank': 1,
-            'is_exposed': True,
-            'is_facet': False,
-            'is_active': True,
-            'is_internal': False,
-        }, {
-            'study': self.study,
-            'assay': self.assay,
-            'solr_field': 'Specimen',
-            'rank': 2,
-            'is_exposed': True,
-            'is_facet': False,
-            'is_active': True,
-            'is_internal': False
-        }, {
-            'study': self.study,
-            'assay': self.assay,
-            'solr_field': 'Cell Type',
-            'rank': 3,
-            'is_exposed': True,
-            'is_facet': True,
-            'is_active': True,
-            'is_internal': False
-        }, {
-            'study': self.study,
-            'assay': self.assay,
-            'solr_field': 'Analysis',
-            'rank': 4,
-            'is_exposed': True,
-            'is_facet': True,
-            'is_active': True,
-            'is_internal': False
-        }, {
-            'study': self.study,
-            'assay': self.assay,
-            'solr_field': 'Organism',
-            'rank': 5,
-            'is_exposed': True,
-            'is_facet': True,
-            'is_active': True,
-            'is_internal': False
-        }, {
-            'study': self.study,
-            'assay': self.assay,
-            'solr_field': 'Cell Line',
-            'rank': 6,
-            'is_exposed': True,
-            'is_facet': True,
-            'is_active': True,
-            'is_internal': False
-        }, {
-            'study': self.study,
-            'assay': self.assay,
-            'solr_field': 'Type',
-            'rank': 7,
-            'is_exposed': True,
-            'is_facet': True,
-            'is_active': True,
-            'is_internal': False
-        }, {
-            'study': self.study,
-            'assay': self.assay,
-            'solr_field': 'Group Name',
-            'rank': 8,
-            'is_exposed': True,
-            'is_facet': True,
-            'is_active': True,
-            'is_internal': False
-        }, {
-            'study': self.study,
-            'assay': self.assay,
-            'solr_field': 'Gene',
-            'rank': 9,
-            'is_exposed': False,
-            'is_facet': False,
-            'is_active': True,
-            'is_internal': False
-        }, {
-            'study': self.study,
-            'assay': self.assay,
-            'solr_field': 'Replicate Id',
-            'rank': 10,
-            'is_exposed': False,
-            'is_facet': False,
-            'is_active': True,
-            'is_internal': False
-        }, {
-            'study': self.study,
-            'assay': self.assay,
-            'solr_field': 'Organism Part',
-            'rank': 0,
-            'is_exposed': False,
-            'is_facet': False,
-            'is_active': True,
-            'is_internal': False
-        }, {
-            'study': self.study,
-            'assay': self.assay,
-            'solr_field': 'Name',
-            'rank': 0,
-            'is_exposed': False,
-            'is_facet': False,
-            'is_active': True,
-            'is_internal': False
-        }]
-
-        for attribute in self.attribute_order_array:
-            AttributeOrder.objects.create(**attribute)
-
-        self.new_assay = Assay.objects.create(
-            study=self.study,
-            measurement='transcription factor binding site',
-            measurement_accession='http://www.testurl.org/testID',
-            measurement_source='OBI',
-            technology='nucleotide sequencing',
-            technology_accession='test info',
-            technology_source='test source',
-            platform='Genome Analyzer II',
-            file_name='test_assay_filename.txt',
-        )
-
-        self.new_attribute_order_array = [{
-            'study': self.study,
-            'assay': self.new_assay,
-            'solr_field': 'Character_Title',
-            'rank': 0,
-            'is_exposed': True,
-            'is_facet': False,
-            'is_active': True,
-            'is_internal': False,
-        }, {
-            'study': self.study,
-            'assay': self.new_assay,
-            'solr_field': 'Specimen',
-            'rank': 0,
-            'is_exposed': True,
-            'is_facet': False,
-            'is_active': True,
-            'is_internal': False
-        }, {
-            'study': self.study,
-            'assay': self.new_assay,
-            'solr_field': 'Cell Type',
-            'rank': 0,
-            'is_exposed': True,
-            'is_facet': True,
-            'is_active': True,
-            'is_internal': False
-        }, {
-            'study': self.study,
-            'assay': self.new_assay,
-            'solr_field': 'Analysis',
-            'rank': 0,
-            'is_exposed': True,
-            'is_facet': True,
-            'is_active': True,
-            'is_internal': False
-        }, {
-            'study': self.study,
-            'assay': self.new_assay,
-            'solr_field': 'Organism',
-            'rank': 0,
-            'is_exposed': True,
-            'is_facet': True,
-            'is_active': True,
-            'is_internal': False
-        }, {
-            'study': self.study,
-            'assay': self.new_assay,
-            'solr_field': 'Cell Line',
-            'rank': 0,
-            'is_exposed': True,
-            'is_facet': True,
-            'is_active': True,
-            'is_internal': False
-        }, {
-            'study': self.study,
-            'assay': self.new_assay,
-            'solr_field': 'Type',
-            'rank': 0,
-            'is_exposed': True,
-            'is_facet': True,
-            'is_active': True,
-            'is_internal': True
-        }, {
-            'study': self.study,
-            'assay': self.new_assay,
-            'solr_field': 'Group Name',
-            'rank': 0,
-            'is_exposed': True,
-            'is_facet': True,
-            'is_active': True,
-            'is_internal': False
-        }, {
-            'study': self.study,
-            'assay': self.new_assay,
-            'solr_field': 'Gene',
-            'rank': 0,
-            'is_exposed': False,
-            'is_facet': False,
-            'is_active': True,
-            'is_internal': False
-        }, {
-            'study': self.study,
-            'assay': self.new_assay,
-            'solr_field': 'Replicate Id',
-            'rank': 0,
-            'is_exposed': False,
-            'is_facet': False,
-            'is_active': True,
-            'is_internal': False
-        }, {
-            'study': self.study,
-            'assay': self.new_assay,
-            'solr_field': 'uuid',
-            'rank': 0,
-            'is_exposed': False,
-            'is_facet': False,
-            'is_active': True,
-            'is_internal': True
-        }, {
-            'study': self.study,
-            'assay': self.new_assay,
-            'solr_field': 'name',
-            'rank': 0,
-            'is_exposed': False,
-            'is_facet': False,
-            'is_active': True,
-            'is_internal': True
-        }]
-
-        for attribute in self.new_attribute_order_array:
-            AttributeOrder.objects.create(**attribute)
 
     def test_generate_solr_params_for_user(self):
-        query = generate_solr_params_for_user(QueryDict({}), self.user.id)
+        user = User.objects.create_user(
+            'testuser', 'test@example.com', 'password')
+        dataset = create_dataset_with_necessary_models()
+        dataset.set_owner(user)
+        assay_uuid = Assay.objects.get(study=dataset.get_latest_study()).uuid
+
+        query = generate_solr_params_for_user(QueryDict({}), user.id)
         self.assertEqual(str(query),
-                         'fq=assay_uuid%3A%28{} OR {}%29'
+                         'fq=assay_uuid%3A%28{}%29'
                          '&facet.field=organism_Characteristics_generic_s'
                          '&facet.field=organism_Factor_Value_generic_s'
                          '&facet.field=technology_Characteristics_generic_s'
@@ -343,5 +87,4 @@ class UserFilesUtilsTests(TestCase):
                          '&q=django_ct%3Adata_set_manager.node'
                          '&wt=json'
                          '&facet=true'
-                         '&facet.limit=-1'.format(
-                             self.assay.uuid, self.new_assay.uuid))
+                         '&facet.limit=-1'.format(assay_uuid))
