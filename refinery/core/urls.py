8f6c42d3a4534b92e9bc7d0826543d1791a1c9fb
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2017-08-17 15:25:27 -0400

    Format more clear than concat

--- refinery/core/urls.py
+++ refinery/core/urls.py
@@ -24,40 +24,40 @@ urlpatterns = patterns(
         'group_invite', name='group_invite'),
     url(r'^users/(?P<query>[\@\.\-\+a-z0-9]+)/$', 'user'),
     # "name" is required for use with the url tag in templates
-    url(r'^users/(?P<query>' + UUID_RE + r')/$',
+    url(r'^users/(?P<query>{})/$'.format(UUID_RE),
         'user', name="user"),
-    url(r'^users/(?P<uuid>' + UUID_RE + r')/edit/'
-        r'$', 'user_edit', name="user_edit"),
-    url(r'^groups/(?P<query>' + UUID_RE + r')/$',
+    url(r'^users/(?P<uuid>{})/edit/$'.format(UUID_RE),
+        'user_edit', name="user_edit"),
+    url(r'^groups/(?P<query>{})/$'.format(UUID_RE),
         'group', name="group"),
-    url(r'^projects/(?P<uuid>' + UUID_RE + r')/$',
+    url(r'^projects/(?P<uuid>{})/$'.format(UUID_RE),
         'project', name="project"),
     url(r'^projects/new/$', 'project_new', name="project_new"),
     url(r'^projects/(?P<slug>[a-zA-Z0-9\_]+)/$', 'project_slug',
         name="project_slug"),
-    url(r'^projects/(?P<uuid>' + UUID_RE + r')/edit/'
-        r'$', 'project_edit', name="project_edit"),
+    url(r'^projects/(?P<uuid>{})/edit/$'.format(UUID_RE),
+        'project_edit', name="project_edit"),
     url(r'^analyses/$', 'analyses', name="analyses"),
-    url(r'^analyses/(?P<analysis_uuid>' + UUID_RE + r')/$',
+    url(r'^analyses/(?P<analysis_uuid>{})/$'.format(UUID_RE),
         'analysis', name="analysis"),
-    url(r'^data_sets_old/(?P<data_set_uuid>' + UUID_RE + r')/$',
+    url(r'^data_sets_old/(?P<data_set_uuid>{})/$'.format(UUID_RE),
         'data_set', name="data_set"),
-    url(r'^data_sets2/(?P<data_set_uuid>' + UUID_RE + r')/$',
+    url(r'^data_sets2/(?P<data_set_uuid>{})/$'.format(UUID_RE),
         'data_set2', name="data_set2"),
-    url(r'^data_sets_old/(?P<data_set_uuid>' + UUID_RE + r')/'
-        r'analysis/(?P<analysis_uuid>' + UUID_RE + r')/$',
+    url(r'^data_sets_old/(?P<data_set_uuid>{})/'
+        r'analysis/(?P<analysis_uuid>{})/$'.format(UUID_RE, UUID_RE),
         'data_set', name="data_set_analysis"),
-    url(r'^data_sets_old/(?P<uuid>' + UUID_RE + r')/edit/'
-        r'$', 'data_set_edit', name="data_set_edit"),
-    url(r'^data_sets_old/(?P<slug>[a-zA-Z0-9\_]+)/$', 'data_set_slug',
-        name="data_set_slug"),
-    url(r'^workflows/(?P<uuid>' + UUID_RE + r')/$',
+    url(r'^data_sets_old/(?P<uuid>{})/edit/$'.format(UUID_RE),
+        'data_set_edit', name="data_set_edit"),
+    url(r'^data_sets_old/(?P<slug>[a-zA-Z0-9\_]+)/$',
+        'data_set_slug', name="data_set_slug"),
+    url(r'^workflows/(?P<uuid>{})/$'.format(UUID_RE),
         'workflow', name="workflow"),
-    url(r'^workflows/(?P<uuid>' + UUID_RE + r')/edit/'
-        r'$', 'workflow_edit', name="workflow_edit"),
+    url(r'^workflows/(?P<uuid>{})/edit/$'.format(UUID_RE),
+        'workflow_edit', name="workflow_edit"),
     url(r'^workflows/(?P<slug>[a-zA-Z0-9\_]+)/$', 'workflow_slug',
         name="workflow_slug"),
-    url(r'^workflow_engines/(?P<uuid>' + UUID_RE + r')/$',
+    url(r'^workflow_engines/(?P<uuid>{})/$'.format(UUID_RE),
         'workflow_engine', name="workflow_engine"),
     url(r'^fastqc_viewer/$', 'fastqc_viewer', name='fastqc_viewer'),
     url(r'^visualize/genome/$', 'visualize_genome', name='visualize_genome'),
@@ -81,10 +81,10 @@ core_router = DefaultRouter()
 core_router.register(r'nodes', NodeViewSet)
 core_router.register(r'workflows', WorkflowViewSet)
 core_router.urls.extend([
-    url(r'^data_sets/(?P<uuid>' + UUID_RE + r')/$',
+    url(r'^data_sets/(?P<uuid>{})/$'.format(UUID_RE),
         DataSetsViewSet.as_view()),
-    url(r'^analyses/(?P<uuid>' + UUID_RE + r')/$',
+    url(r'^analyses/(?P<uuid>{})/$'.format(UUID_RE),
         AnalysesViewSet.as_view()),
-    url(r'^openid_token/$', OpenIDToken.as_view(),
-        name="openid-token")
+    url(r'^openid_token/$',
+        OpenIDToken.as_view(), name="openid-token")
 ])
