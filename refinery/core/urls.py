1118dcc22ad752ed90c381d66883ff9cbad3f000
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2017-08-17 14:47:16 -0400

    Try to make this a little easier to read by replacing RE with constant

--- refinery/core/urls.py
+++ refinery/core/urls.py
@@ -11,6 +11,8 @@ from rest_framework.routers import DefaultRouter
 from .views import (AnalysesViewSet, DataSetsViewSet, NodeViewSet, OpenIDToken,
                     WorkflowViewSet)
 
+UUID_RE = r'[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}'
+
 urlpatterns = patterns(
     'core.views',
     url(r'^$', 'home', name="home"),
@@ -18,59 +20,44 @@ urlpatterns = patterns(
     url(r'^contact/$', 'contact', name="contact"),
     url(r'^statistics/$', 'statistics', name="statistics"),
     url(r'^collaboration/$', 'collaboration', name='collaboration'),
-    url(r'^group_invite/(?P<token>'
-        r'[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})/$',
+    url(r'^group_invite/(?P<token>' + UUID_RE + r')/$',
         'group_invite', name='group_invite'),
     url(r'^users/(?P<query>[\@\.\-\+a-z0-9]+)/$', 'user'),
     # "name" is required for use with the url tag in templates
-    url(r'^users/(?P<query>'
-        r'[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})/$',
+    url(r'^users/(?P<query>' + UUID_RE + r')/$',
         'user', name="user"),
-    url(r'^users/(?P<uuid>'
-        r'[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})/edit/'
+    url(r'^users/(?P<uuid>' + UUID_RE + r')/edit/'
         r'$', 'user_edit', name="user_edit"),
-    url(r'^groups/(?P<query>'
-        r'[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})/$',
+    url(r'^groups/(?P<query>' + UUID_RE + r')/$',
         'group', name="group"),
-    url(r'^projects/(?P<uuid>'
-        r'[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})/$',
+    url(r'^projects/(?P<uuid>' + UUID_RE + r')/$',
         'project', name="project"),
     url(r'^projects/new/$', 'project_new', name="project_new"),
     url(r'^projects/(?P<slug>[a-zA-Z0-9\_]+)/$', 'project_slug',
         name="project_slug"),
-    url(r'^projects/(?P<uuid>'
-        r'[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})/edit/'
+    url(r'^projects/(?P<uuid>' + UUID_RE + r')/edit/'
         r'$', 'project_edit', name="project_edit"),
     url(r'^analyses/$', 'analyses', name="analyses"),
-    url(r'^analyses/(?P<analysis_uuid>'
-        r'[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})/$',
+    url(r'^analyses/(?P<analysis_uuid>' + UUID_RE + r')/$',
         'analysis', name="analysis"),
-    url(r'^data_sets_old/(?P<data_set_uuid>'
-        r'[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})/$',
+    url(r'^data_sets_old/(?P<data_set_uuid>' + UUID_RE + r')/$',
         'data_set', name="data_set"),
-    url(r'^data_sets2/(?P<data_set_uuid>'
-        r'[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})/$',
+    url(r'^data_sets2/(?P<data_set_uuid>' + UUID_RE + r')/$',
         'data_set2', name="data_set2"),
-    url(r'^data_sets_old/(?P<data_set_uuid>'
-        r'[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})/'
-        r'analysis/(?P<analysis_uuid>'
-        r'[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})/$',
+    url(r'^data_sets_old/(?P<data_set_uuid>' + UUID_RE + r')/'
+        r'analysis/(?P<analysis_uuid>' + UUID_RE + r')/$',
         'data_set', name="data_set_analysis"),
-    url(r'^data_sets_old/(?P<uuid>'
-        r'[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})/edit/'
+    url(r'^data_sets_old/(?P<uuid>' + UUID_RE + r')/edit/'
         r'$', 'data_set_edit', name="data_set_edit"),
     url(r'^data_sets_old/(?P<slug>[a-zA-Z0-9\_]+)/$', 'data_set_slug',
         name="data_set_slug"),
-    url(r'^workflows/(?P<uuid>'
-        r'[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})/$',
+    url(r'^workflows/(?P<uuid>' + UUID_RE + r')/$',
         'workflow', name="workflow"),
-    url(r'^workflows/(?P<uuid>'
-        r'[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})/edit/'
+    url(r'^workflows/(?P<uuid>' + UUID_RE + r')/edit/'
         r'$', 'workflow_edit', name="workflow_edit"),
     url(r'^workflows/(?P<slug>[a-zA-Z0-9\_]+)/$', 'workflow_slug',
         name="workflow_slug"),
-    url(r'^workflow_engines/(?P<uuid>'
-        r'[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})/$',
+    url(r'^workflow_engines/(?P<uuid>' + UUID_RE + r')/$',
         'workflow_engine', name="workflow_engine"),
     url(r'^fastqc_viewer/$', 'fastqc_viewer', name='fastqc_viewer'),
     url(r'^visualize/genome/$', 'visualize_genome', name='visualize_genome'),
@@ -94,13 +81,9 @@ core_router = DefaultRouter()
 core_router.register(r'nodes', NodeViewSet)
 core_router.register(r'workflows', WorkflowViewSet)
 core_router.urls.extend([
-    url(r'^data_sets/(?P<uuid>'
-        r'[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{'
-        r''r'12})/$',
+    url(r'^data_sets/(?P<uuid>' + UUID_RE + r')/$',
         DataSetsViewSet.as_view()),
-    url(r'^analyses/(?P<uuid>'
-        r'[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{'
-        r''r'12})/$',
+    url(r'^analyses/(?P<uuid>' + UUID_RE + r')/$',
         AnalysesViewSet.as_view()),
     url(r'^openid_token/$', OpenIDToken.as_view(),
         name="openid-token")
