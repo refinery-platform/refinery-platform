e82cac22ab6a31a1278a84ca0156467e209660ee
mccalluc <mccalluc@users.noreply.github.com>
2017-08-22 17:21:54 -0400

    Mccalluc/isatab test (#2035)
    * Test empty directory
    
    * Test a minimal valid isatab; had trouble with imports
    
    * Explain the error I am getting
    
    * Make some assertions
    
    * Test missing files from isatab
    
    * Failure mode changed with the merge
    
    * Multiple investigation passes, but perhaps it should not?
    
    * test multiple studies
    
    * Test missing assays
    
    * Fix imports post-merge

--- refinery/data_set_manager/tests.py
+++ refinery/data_set_manager/tests.py
@@ -1,6 +1,10 @@
 from StringIO import StringIO
+import contextlib
 import json
+import os
 import re
+import shutil
+import tempfile
 
 from django.contrib.auth.models import User
 from django.core.files.uploadedfile import (InMemoryUploadedFile,
@@ -12,6 +16,7 @@ from rest_framework.test import APIClient, APIRequestFactory, APITestCase
 
 from core.models import DataSet, ExtendedGroup, InvestigationLink
 from core.views import NodeViewSet
+from data_set_manager.isa_tab_parser import IsaTabParser, ParserException
 from data_set_manager.tasks import parse_isatab
 from file_store.models import FileStoreItem
 
@@ -343,6 +348,73 @@ class AssaysAttributesAPITests(APITestCase):
                 response.content, '"Only owner may edit attribute order."'
                 )
         self.client.logout()
+
+
+@contextlib.contextmanager
+def temporary_directory(*args, **kwargs):
+    d = tempfile.mkdtemp(*args, **kwargs)
+    try:
+        yield d
+    finally:
+        shutil.rmtree(d)
+
+
+class IsaTabParserTests(TestCase):
+
+    def parse(self, dir_name):
+        parent = os.path.dirname(os.path.abspath(__file__))
+        dir = os.path.join(parent, 'test-data', dir_name)
+        return IsaTabParser().run(dir)
+
+    def test_empty(self):
+        with temporary_directory() as tmp:
+            with self.assertRaises(ParserException):
+                IsaTabParser().run(tmp)
+
+    def test_minimal(self):
+        investigation = self.parse('minimal')
+
+        studies = investigation.study_set.all()
+        self.assertEqual(len(studies), 1)
+
+        assays = studies[0].assay_set.all()
+        self.assertEqual(len(assays), 1)
+
+    def test_mising_investigation(self):
+        with self.assertRaises(ParserException):
+            self.parse('missing-investigation')
+
+    def test_mising_study(self):
+        with self.assertRaises(IOError):
+            self.parse('missing-study')
+
+    def test_mising_assay(self):
+        with self.assertRaises(IOError):
+            self.parse('missing-assay')
+
+    def test_multiple_investigation(self):
+        # TODO: I think this should fail?
+        self.parse('multiple-investigation')
+
+    def test_multiple_study(self):
+        investigation = self.parse('multiple-study')
+
+        studies = investigation.study_set.all()
+        self.assertEqual(len(studies), 2)
+
+        assays0 = studies[0].assay_set.all()
+        self.assertEqual(len(assays0), 1)
+
+        assays1 = studies[1].assay_set.all()
+        self.assertEqual(len(assays1), 1)
+
+    def test_multiple_study_missing_assay(self):
+        with self.assertRaises(IOError):
+            self.parse('multiple-study-missing-assay')
+
+    def test_multiple_assay(self):
+        # TODO: Is it even possible to have multiple Assays per Study?
+        pass
 
 
 class UtilitiesTest(TestCase):
