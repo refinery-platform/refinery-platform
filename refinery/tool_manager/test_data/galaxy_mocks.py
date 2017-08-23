f9895d07bc47895aa9ee94482d17cb9ae0bb5d71
Scott Ouellette <scottx611x@gmail.com>
2017-08-23 13:05:18 -0400

    Thin out galaxy mock data some more

--- refinery/tool_manager/test_data/galaxy_mocks.py
+++ refinery/tool_manager/test_data/galaxy_mocks.py
@@ -24,211 +24,120 @@ history_dict = {
     u'purged': False
 }
 
-library_dict = {
-    u'id': u'a03c847bfa9a8077',
-    u'name': (u'Library for: Tool: WORKFLOW Test workflow: 5 steps without '
-              u'branching 0103d21a-d709-43a7-9448-0e06f44d9ed9')
-}
+library_dict = {u'id': u'a03c847bfa9a8077'}
 
-library_dataset_dict = [
-    {
-        u'url': u'/api/libraries/a03c847bfa9a8077/contents/f29b25b2abcdb977',
-        u'name': u'http://192.168.50.50:8000/media/file_store/d8/10/file1.txt',
-        u'id': u'f29b25b2abcdb977'
-    }
-]
+library_dataset_dict = [{u'id': u'f29b25b2abcdb977'}]
 
 history_dataset_dict = {
     u'file_name': u'/Galaxy/galaxy/database/files/000/dataset_633.dat',
     u'creating_job': u'4e889f526a9d99a4',
     u'dataset_id': u'4e889f526a9d99a4',
     u'id': u'fbb4f924481c9afd',
-    u'download_url': (
-        u'/api/histories/9b5c597dcbb59371/contents/fbb4f924481c9afd/display'
-    ),
     u'history_id': u'9b5c597dcbb59371',
     u'name': u'http://192.168.50.50:8000/media/file_store/d8/10/file1.txt',
     u'url': u'/api/histories/9b5c597dcbb59371/contents/fbb4f924481c9afd',
-    u'uuid': u'ad7e3cad-a512-4351-92bb-f5dd55d5fc4c',
     u'purged': False
 }
 
 galaxy_workflow_invocation_data = {
     u'update_time': u'2017-07-26T19:19:25.833503',
-    u'uuid': u'56824ed1-7237-11e7-a25c-0c4de9d2ab81',
     u'history_id': u'b2486a20bc56b90f',
     u'state': u'new',
     u'workflow_id': u'6fc9fbb81c497f69',
-    u'model_class': u'WorkflowInvocation',
     u'id': u'52e496b945151ee8'
 }
 
 galaxy_workflow_dict = {
     u'name': u'list test wf',
     u'steps': {
-        u'1': {
-            u'tool_id': u'refinery_test_LIST-N',
-            u'id': 1,
-            u'name': u'Refinery test tool LIST - N'
-        },
-        u'0': {
-            u'tool_id': u'refinery_test_LIST-N',
-            u'id': 0,
-            u'name': u'Refinery test tool LIST - N'
-        }
-    },
-    u'uuid': u'9e119199-c589-4e42-b358-cd6f046b7e59'
+        u'1': {u'tool_id': u'refinery_test_LIST-N', u'id': 1,
+               u'name': u'Refinery test tool LIST - N'},
+        u'0': {u'tool_id': u'refinery_test_LIST-N', u'id': 0,
+               u'name': u'Refinery test tool LIST - N'}
+    }
 }
-
 
 galaxy_datasets_list = [
     {
-        u'file_name': u'/Users/scott/PyCharmProjects/Galaxy/galaxy/database/'
-                      u'files/003/dataset_3032.dat',
         u'creating_job': u'1d1dbe75827398cd',
         u'file_size': 406,
         u'dataset_id': u'953f3a3e2982a4fa',
         u'id': u'd32aba4ae7b4124a',
-        u'download_url': u'/api/histories/67ce804af6ec796b/contents/'
-                         u'd32aba4ae7b4124a/display',
         u'state': u'ok',
         u'history_id': u'67ce804af6ec796b',
         u'name': u'Refinery test tool LIST - N on data 4',
         u'file_ext': u'txt',
         u'url': u'/api/histories/67ce804af6ec796b/contents/d32aba4ae7b4124a',
-        u'uuid': u'80c05e9b-e15d-405d-9a2d-b75d3de89273',
         u'purged': False
     },
     {
-        u'file_name': u'/Users/scott/PyCharmProjects/Galaxy/galaxy/database/'
-                      u'files/003/dataset_3032.dat',
         u'creating_job': u'1d1dbe75827398cd',
         u'file_size': 406,
         u'dataset_id': u'953f3a3e3982a4fa',
         u'id': u'd22aba4ae7b4124a',
-        u'download_url': u'/api/histories/67ce804af6ec796b/contents/'
-                         u'd32aba4ae7b4124a/display',
         u'state': u'ok',
         u'history_id': u'67ce804af6ec796b',
         u'name': u'Refinery test tool LIST - N on data 2',
         u'file_ext': u'txt',
         u'url': u'/api/histories/67ce804af6ec796b/contents/d32aba4ae7b4124a',
-        u'uuid': u'80c05e9b-e15d-405d-9a2d-b75d3de89275',
         u'purged': False
     },
     {
-         u'file_name': u'/Users/scott/PyCharmProjects/Galaxy/galaxy/database/'
-                       u'files/003/dataset_3032.dat',
          u'creating_job': u'1d1dbe75827398cd',
          u'file_size': 406,
          u'dataset_id': u'953f3a3e2982a4fa',
          u'id': u'd32aba4ae7b4124a',
-         u'download_url': u'/api/histories/67ce804af6ec796b/contents/'
-                          u'd32aba4ae7b4124a/display',
          u'state': u'ok',
          u'history_id': u'67ce804af6ec796b',
          u'name': u'Refinery test tool LIST - N on data 1',
          u'file_ext': u'txt',
          u'url': u'/api/histories/67ce804af6ec796b/contents/d32aba4ae7b4124a',
-         u'uuid': u'80c05e9b-e15d-405d-9a2d-b75d3de89275',
          u'purged': True
      }
 ]
 
 galaxy_workflow_invocation = {
-    u'inputs': {
-        u'0': {
-            u'src': u'hdca',
-            u'id': u'f0cc9868bc58f90b',
-            u'uuid': u'94f75b27-d8fd-4561-b502-54074d70e106'
-        }
-    },
+    u'inputs': {u'0': {u'src': u'hdca', u'id': u'f0cc9868bc58f90b'}},
     u'history_id': u'67ce804af6ec796b',
     u'workflow_id': u'4a56addbcc836c23',
     u'steps': [
-        {
-            u'job_id': u'1d1dbe75827398cd',
-            u'workflow_step_uuid': u'537a15ff-a8d3-4c1a-8b25-99826bdc91c2',
-            u'order_index': 1,
+        {u'job_id': u'1d1dbe75827398cd', u'order_index': 1,
             u'workflow_step_id': u'507bf06c009b95ec',
-            u'id': u'ae7005de11c97062'
-        },
-        {
-            u'job_id': u'1d1dbe75827398cd',
-            u'workflow_step_uuid': u'537a15ff-a8d3-4c1a-8b25-99826bdc91c2',
-            u'order_index': 1,
+            u'id': u'ae7005de11c97062'},
+        {u'job_id': u'1d1dbe75827398cd', u'order_index': 1,
             u'workflow_step_id': u'507bf06c009b95ec',
-            u'id': u'ae7005de11c97062'
-        }
+            u'id': u'ae7005de11c97062'}
     ],
     u'id': u'ddaca2bad6847b13'
 }
 
 galaxy_history_download_list = [
     {
-        'name': u'Refinery test tool LIST - N on data 14',
-        'url': u'/api/histories/b3e4e0c76fe49b64/contents/8ee788c99983ff96',
-        'file_name': u'galaxy/database/files/003/dataset_3047.dat',
-        'state': u'ok',
-        'file_size': 211,
-        'dataset_id': u'8ee788c99983ff96',
-        'type': u'txt'
+        u'name': u'Refinery test tool LIST - N on data 14',
+        u'state': u'ok', u'file_size': 211,
+        u'dataset_id': u'8ee788c99983ff96', u'type': u'txt'
     },
     {
-        'name': u'Refinery test tool LIST - N on data 13',
-        'url': u'/api/histories/b3e4e0c76fe49b64/contents/14bb1cdaa43f5769',
-        'file_name': u'galaxy/database/files/003/dataset_3048.dat',
-        'state': u'ok',
-        'file_size': 211,
-        'dataset_id': u'14bb1cdaa43f5769',
-        'type': u'txt'
+        u'name': u'Refinery test tool LIST - N on data 13',
+        u'state': u'ok', u'file_size': 211,
+        u'dataset_id': u'14bb1cdaa43f5769', u'type': u'txt'
     },
     {
-        'name': u'Refinery test tool LIST - N on data 12',
-        'url': u'/api/histories/b3e4e0c76fe49b64/contents/953f3a3e2982a4fa',
-        'file_name': u'galaxy/database/files/003/dataset_3049.dat',
-        'state': u'ok',
-        'file_size': 714,
-        'dataset_id': u'953f3a3e2982a4fa',
-        'type': u'txt'
+        u'name': u'Refinery test tool LIST - N on data 12',
+        u'state': u'ok', u'file_size': 714,
+        u'dataset_id': u'953f3a3e2982a4fa', u'type': u'txt'
     }
 ]
 
-galaxy_tool_data_mock = {
+galaxy_tool_data = {
     u'inputs': [
-        {
-            u'type': u'data',
-            u'name': u'input_file'
-        },
-        {
-            u'name': u'sleep_time',
-            u'value': u'0',
-            u'type': u'integer',
-        },
-        {
-            u'type': u'boolean',
-            u'value': False,
-            u'name': u'empty_outfile'
-        },
-        {
-            u'name': u'p_fail',
-            u'value': u'0.0',
-            u'type': u'float',
-        },
-        {
-            u'type': u'boolean',
-            u'value': False,
-            u'name': u'stdout'},
-        {
-            u'type': u'boolean',
-            u'value': False,
-            u'name': u'stderr'
-        },
-        {
-            u'name': u'exit_code',
-            u'value': u'0',
-            u'type': u'integer'
-        }
+        {u'type': u'data', u'name': u'input_file'},
+        {u'name': u'sleep_time', u'value': u'0', u'type': u'integer'},
+        {u'type': u'boolean', u'value': False, u'name': u'empty_outfile'},
+        {u'name': u'p_fail', u'value': u'0.0', u'type': u'float'},
+        {u'type': u'boolean', u'value': False, u'name': u'stdout'},
+        {u'type': u'boolean', u'value': False, u'name': u'stderr'},
+        {u'name': u'exit_code', u'value': u'0', u'type': u'integer'}
     ],
     u'id': u'refinery_test_LIST-N',
     u'name': u'Refinery test tool LIST - N'}
