58c1288bc43f73b3e6995fa5ffc4939398a900be
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2017-08-29 12:04:34 -0400

    Add linebreaks to url list

--- refinery/core/urls.py
+++ refinery/core/urls.py
@@ -21,14 +21,17 @@ urlpatterns = patterns(
     url(r'^collaboration/$', 'collaboration', name='collaboration'),
     url(r'^group_invite/(?P<token>' + UUID_RE + r')/$',
         'group_invite', name='group_invite'),
+
     url(r'^users/(?P<query>[\@\.\-\+a-z0-9]+)/$', 'user'),
     # "name" is required for use with the url tag in templates
     url(r'^users/(?P<query>' + UUID_RE + r')/$',
         'user', name="user"),
     url(r'^users/(?P<uuid>' + UUID_RE + r')/edit/$',
         'user_edit', name="user_edit"),
+
     url(r'^groups/(?P<query>' + UUID_RE + r')/$',
         'group', name="group"),
+
     url(r'^projects/(?P<uuid>' + UUID_RE + r')/$',
         'project', name="project"),
     url(r'^projects/new/$', 'project_new', name="project_new"),
@@ -36,40 +39,50 @@ urlpatterns = patterns(
         'project_slug', name="project_slug"),
     url(r'^projects/(?P<uuid>' + UUID_RE + r')/edit/$',
         'project_edit', name="project_edit"),
+
     url(r'^analyses/$', 'analyses', name="analyses"),
     url(r'^analyses/(?P<analysis_uuid>' + UUID_RE + r')/$',
         'analysis', name="analysis"),
+
     url(r'^provenance/(?P<data_set_uuid>' + UUID_RE + r')/$',
         'data_set', name="data_set"),
+
     url(r'^data_sets/(?P<data_set_uuid>' + UUID_RE + r')/$',
         'data_set2', name="data_set2"),
     url(r'^data_sets/(?P<slug>[a-zA-Z0-9\_]+)/$',
         'data_set2_slug', name="data_set2_slug"),
+
     url(r'^provenance/(?P<data_set_uuid>' + UUID_RE + r')/'
         r'analysis/(?P<analysis_uuid>' + UUID_RE + r')/$',
         'data_set', name="data_set_analysis"),
     url(r'^provenance/(?P<slug>[a-zA-Z0-9\_]+)/$',
         'data_set_slug', name="data_set_slug"),
+
     url(r'^workflows/(?P<uuid>' + UUID_RE + r')/$',
         'workflow', name="workflow"),
     url(r'^workflows/(?P<uuid>' + UUID_RE + r')/edit/$',
         'workflow_edit', name="workflow_edit"),
     url(r'^workflows/(?P<slug>[a-zA-Z0-9\_]+)/$',
         'workflow_slug', name="workflow_slug"),
+
     url(r'^workflow_engines/(?P<uuid>' + UUID_RE + r')/$',
         'workflow_engine', name="workflow_engine"),
     url(r'^fastqc_viewer/$', 'fastqc_viewer', name='fastqc_viewer'),
     url(r'^visualize/genome/$', 'visualize_genome', name='visualize_genome'),
+
     url(r'^solr/igv/$', 'solr_igv'),
     url(r'^solr/core/select/$', 'solr_core_search', name="solr_core_search"),
     url(r'^solr/(?P<core>.+)/select/$', 'solr_select', name="solr_select"),
+
     url(r'^doi/(?P<id>.+)/', 'doi', name="doi"),
+
     url(r'^pubmed/abstract/(?P<id>.+)/',
         'pubmed_abstract', name="pubmed_abstract"),
     url(r'^pubmed/search/(?P<term>.+)/',
         'pubmed_search', name="pubmed_search"),
     url(r'^pubmed/summary/(?P<id>.+)/',
         'pubmed_summary', name="pubmed_summary"),
+
     url(r'^neo4j/annotations/$',
         'neo4j_dataset_annotations', name="neo4j_dataset_annotations"),
     url(r'^auto_login/$', 'auto_login', name='auto_login')
