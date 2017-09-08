dadec1f4697ceebdf76f3cb8d010d59ebcaf007a
mccalluc <mccalluc@users.noreply.github.com>
2017-09-08 18:41:56 -0400

    Mccalluc/use new django docker (#2100)
    * getting ahead of myself, but this is what it will look like
    
    * Very plain please wait page
    
    * Suspect there are better ways to o this.
    
    * Bumb version to improve error catching
    
    * Hi glass comes up after 2 seconds!

--- refinery/tool_manager/urls.py
+++ refinery/tool_manager/urls.py
@@ -1,3 +1,5 @@
+from os.path import dirname, join
+
 from django.conf import settings
 from django.conf.urls import include, url
 
@@ -11,7 +13,14 @@ tool_manager_router = DefaultRouter()
 tool_manager_router.register(r'tools', ToolsViewSet)
 tool_manager_router.register(r'tool_definitions', ToolDefinitionsViewSet)
 
-url_patterns = Proxy(FileLogger(settings.PROXY_LOG)).url_patterns()
+please_wait_path = join(dirname(__file__), 'please-wait.html')
+with open(please_wait_path, 'r') as f:
+    please_wait_content = f.read()
+
+url_patterns = Proxy(
+    logger=FileLogger(settings.PROXY_LOG),
+    please_wait_content=please_wait_content
+).url_patterns()
 django_docker_engine_url = url(
     r'^{}/'.format(settings.DJANGO_DOCKER_ENGINE_BASE_URL),
     include(url_patterns)
