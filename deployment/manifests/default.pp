8a2673b0a04433e6af5584c74843ba0ba83f1465
Scott Ouellette <scottx611x@gmail.com>
2017-09-25 12:25:20 -0400

    Make the same `django_docker_engine_data_dir` on vagrant too

--- deployment/manifests/default.pp
+++ deployment/manifests/default.pp
@@ -36,9 +36,16 @@ exec { "activate_user":
 # Django-docker-engine needs a place for ephemeral data.
 # In production, this is a separate EBS mount, so we don't need to create it locally.
 file { $::data_dir:
-  ensure => 'directory',
+  ensure => directory,
   owner  => $app_user,
   group  => $app_group
+}
+->
+file { $::django_docker_engine_data_dir:
+    ensure => directory,
+    owner => "$app_user",
+    group => "$app_user",
+    mode => "0755",
 }
 
 # See code in refinery-modules/refinery/...
