ba7d8fa62ac5fa5d095ce54a76382b061a8be171
Scott Ouellette <scottx611x@gmail.com>
2017-09-25 11:42:33 -0400

    Move creation of django_docker_engine_dir into docker.pp

--- deployment/refinery-modules/refinery/manifests/docker.pp
+++ deployment/refinery-modules/refinery/manifests/docker.pp
@@ -2,4 +2,11 @@ class refinery::docker {
   class { '::docker':
     docker_users => [$::app_user]
   }
+  ->
+  file { $::django_docker_engine_data_dir:
+      ensure => directory,
+      owner => "$app_user",
+      group => "$app_user",
+      mode => "0755",
+  }
 }
