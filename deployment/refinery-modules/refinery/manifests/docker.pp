8553847020faf6877984f04f8fe52a64e3cf465f
Scott Ouellette <scottx611x@gmail.com>
2017-09-25 12:15:23 -0400

    Keep this `file` in chain after `/data` has been created

--- deployment/refinery-modules/refinery/manifests/docker.pp
+++ deployment/refinery-modules/refinery/manifests/docker.pp
@@ -2,11 +2,4 @@ class refinery::docker {
   class { '::docker':
     docker_users => [$::app_user]
   }
-  ->
-  file { $::django_docker_engine_data_dir:
-      ensure => directory,
-      owner => "$app_user",
-      group => "$app_user",
-      mode => "0755",
-  }
 }
