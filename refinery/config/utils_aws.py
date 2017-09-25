a6563925af145e231db72a1c911df219246bb802
Ilya Sytchev <isytchev@hsph.harvard.edu>
2017-09-25 17:47:03 -0400

    Add a comment describing the use of the custom_domain argument

--- refinery/config/utils_aws.py
+++ refinery/config/utils_aws.py
@@ -5,6 +5,8 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 class S3StaticStorage(S3Boto3Storage):
     def __init__(self):
+        # custom_domain is used to avoid obtaining security tokens for each of
+        # the static files (https://stackoverflow.com/a/28749849)
         super(S3StaticStorage, self).__init__(
             bucket=settings.STATIC_BUCKET, querystring_auth=False,
             custom_domain=settings.STATIC_BUCKET + '.s3.amazonaws.com'
