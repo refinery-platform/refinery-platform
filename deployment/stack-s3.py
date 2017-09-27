3fa36cb17213964d27dc701f62e2e02e92a7e852
Ilya Sytchev <isytchev@hsph.harvard.edu>
2017-09-27 12:14:26 -0400

    Hackdna/identity pool to s3 stack (#2164)
    * Move Cognito identity pool to S3 stack
    
    * Provide a cross-stack reference for Cognito identity pool ID
    
    * Fix media bucket name in storage stack

--- deployment/stack-s3.py
+++ deployment/stack-s3.py
@@ -16,7 +16,7 @@ import sys
 import boto3
 from cfn_pyplates.core import (CloudFormationTemplate, DeletionPolicy,
                                Parameter, Properties, Resource)
-from cfn_pyplates.functions import ref
+from cfn_pyplates.functions import get_att, ref
 from utils import Output, load_tags
 import yaml
 
@@ -34,23 +34,30 @@ def main():
         raise RuntimeError
 
     stack_name = config['STACK_NAME'] + 'Storage'
-    static_bucket_name = config['S3_BUCKET_NAME_BASE'] + '-static'
-    media_bucket_name = config['S3_BUCKET_NAME_BASE'] + '-media'
     template = make_storage_template()
 
     cloudformation = boto3.client('cloudformation')
     response = cloudformation.create_stack(
         StackName=stack_name,
         TemplateBody=str(template),
+        Capabilities=['CAPABILITY_IAM'],
         Tags=load_tags(),
         Parameters=[
             {
                 'ParameterKey': 'StaticBucketName',
-                'ParameterValue': static_bucket_name,
+                'ParameterValue': config['S3_BUCKET_NAME_BASE'] + '-static',
             },
             {
                 'ParameterKey': 'MediaBucketName',
-                'ParameterValue': media_bucket_name,
+                'ParameterValue': config['S3_BUCKET_NAME_BASE'] + '-media',
+            },
+            {
+                'ParameterKey': 'IdentityPoolName',
+                'ParameterValue': config['COGNITO_IDENTITY_POOL_NAME']
+            },
+            {
+                'ParameterKey': 'DeveloperProviderName',
+                'ParameterValue': config['COGNITO_DEVELOPER_PROVIDER_NAME']
             },
         ],
     )
@@ -59,72 +66,203 @@ def main():
 
 def make_storage_template():
     cft = CloudFormationTemplate(description="Refinery Platform storage")
+
     # Parameters
-    cft.parameters.add(Parameter(
-        'StaticBucketName',
-        'String',
-        {
-            'Description': 'Name of S3 bucket for Django static files',
-        }
-    ))
-    cft.parameters.add(Parameter(
-        'MediaBucketName',
-        'String',
-        {
-            'Description': 'Name of S3 bucket for Django media files',
-            # make names DNS-compliant without periods (".") for compatibility
-            # with virtual-hosted-style access and S3 Transfer Acceleration
-            'AllowedPattern': '[a-z0-9\-]+',
-            'ConstraintDescription':
-                'must only contain lower case letters, numbers, and hyphens',
-        }
-    ))
-    # Resources
-    cft.resources.add(Resource(
-        'StaticStorageBucket',
-        'AWS::S3::Bucket',
-        Properties({
-            'BucketName': ref('StaticBucketName'),
-            'AccessControl': 'PublicRead',
-            'CorsConfiguration': {
-                'CorsRules': [
-                    {
-                        'AllowedOrigins': ['*'],
-                        'AllowedMethods': ['GET'],
-                        'AllowedHeaders': ['Authorization'],
-                        'MaxAge': 3000,
-                    }
-                ]
-            },
-        }),
-        DeletionPolicy('Retain'),
-    ))
-    cft.resources.add(Resource(
-        'MediaStorageBucket',
-        'AWS::S3::Bucket',
-        Properties({
-            'BucketName': ref('MediaBucketName'),
-            'AccessControl': 'PublicRead',
-            'CorsConfiguration': {
-                'CorsRules': [
-                    {
-                        'AllowedOrigins': ['*'],
-                        'AllowedMethods': ['POST', 'PUT', 'DELETE'],
-                        'AllowedHeaders': ['*'],
-                        'ExposedHeaders': ['ETag'],
-                        'MaxAge': 3000,
-                    }
-                ]
+    cft.parameters.add(
+        Parameter(
+            'StaticBucketName',
+            'String',
+            {
+                'Description': 'Name of S3 bucket for Django static files',
             }
-        }),
-        DeletionPolicy('Retain'),
-    ))
-    cft.outputs.add(Output(
-        'MediaBucketName',
-        ref('MediaStorageBucket'),
-        {'Fn::Sub': '${AWS::StackName}Media'},
-        'Name of S3 bucket for Django media files'
-    ))
+        )
+    )
+    cft.parameters.add(
+        Parameter(
+            'MediaBucketName',
+            'String',
+            {
+                'Description': 'Name of S3 bucket for Django media files',
+                # make names DNS-compliant without periods (".") for
+                # compatibility with virtual-hosted-style access and S3
+                # Transfer Acceleration
+                'AllowedPattern': '[a-z0-9\-]+',
+                'ConstraintDescription':
+                    'must only contain lower case letters, numbers, and '
+                    'hyphens',
+            }
+        )
+    )
+    cft.parameters.add(
+        Parameter(
+            'IdentityPoolName',
+            'String',
+            {
+                'Default': 'Refinery Platform',
+                'Description': 'Name of Cognito identity pool for S3 uploads',
+            }
+        )
+    )
+    cft.parameters.add(
+        Parameter(
+            'DeveloperProviderName',
+            'String',
+            {
+                'Default': 'login.refinery',
+                'Description': '"domain" by which Cognito will refer to users',
+                'AllowedPattern': '[a-z\-\.]+',
+                'ConstraintDescription':
+                    'must only contain lower case letters, periods, '
+                    'underscores, and hyphens'
+            }
+        )
+    )
+
+    # Resources
+    cft.resources.add(
+        Resource(
+            'StaticStorageBucket',
+            'AWS::S3::Bucket',
+            Properties({
+                'BucketName': ref('StaticBucketName'),
+                'AccessControl': 'PublicRead',
+                'CorsConfiguration': {
+                    'CorsRules': [
+                        {
+                            'AllowedOrigins': ['*'],
+                            'AllowedMethods': ['GET'],
+                            'AllowedHeaders': ['Authorization'],
+                            'MaxAge': 3000,
+                        }
+                    ]
+                },
+            }),
+            DeletionPolicy('Retain'),
+        )
+    )
+    cft.resources.add(
+        Resource(
+            'MediaStorageBucket',
+            'AWS::S3::Bucket',
+            Properties({
+                'BucketName': ref('MediaBucketName'),
+                'AccessControl': 'PublicRead',
+                'CorsConfiguration': {
+                    'CorsRules': [
+                        {
+                            'AllowedOrigins': ['*'],
+                            'AllowedMethods': ['POST', 'PUT', 'DELETE'],
+                            'AllowedHeaders': ['*'],
+                            'ExposedHeaders': ['ETag'],
+                            'MaxAge': 3000,
+                        }
+                    ]
+                }
+            }),
+            DeletionPolicy('Retain'),
+        )
+    )
+    # Cognito Identity Pool for Developer Authenticated Identities Authflow
+    # http://docs.aws.amazon.com/cognito/latest/developerguide/authentication-flow.html
+    cft.resources.add(
+        Resource(
+            'IdentityPool',
+            'AWS::Cognito::IdentityPool',
+            Properties(
+                {
+                    'IdentityPoolName': ref('IdentityPoolName'),
+                    'AllowUnauthenticatedIdentities': False,
+                    'DeveloperProviderName': ref('DeveloperProviderName'),
+                }
+            )
+        )
+    )
+    cft.resources.add(
+        Resource(
+            'IdentityPoolAuthenticatedRole',
+            'AWS::Cognito::IdentityPoolRoleAttachment',
+            Properties(
+                {
+                    'IdentityPoolId': ref('IdentityPool'),
+                    'Roles': {
+                        'authenticated': get_att('CognitoS3UploadRole', 'Arn'),
+                    }
+                }
+            )
+        )
+    )
+    upload_role_trust_policy = {
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Effect": "Allow",
+                "Principal": {
+                    "Federated": "cognito-identity.amazonaws.com"
+                },
+                "Action": "sts:AssumeRoleWithWebIdentity",
+                "Condition": {
+                    "StringEquals": {
+                        "cognito-identity.amazonaws.com:aud":
+                            ref('IdentityPool')
+                    },
+                    "ForAnyValue:StringLike": {
+                        "cognito-identity.amazonaws.com:amr": "authenticated"
+                    }
+                }
+            }
+        ]
+    }
+    upload_access_policy = {
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Effect": "Allow",
+                "Action": [
+                    "cognito-identity:*"
+                ],
+                "Resource": "*"
+            },
+            {
+                "Action": [
+                    "s3:PutObject",
+                    "s3:AbortMultipartUpload"
+                ],
+                "Effect": "Allow",
+                "Resource": {
+                    "Fn::Sub":
+                        "arn:aws:s3:::${MediaStorageBucket}/uploads/"
+                        "${!cognito-identity.amazonaws.com:sub}/*"
+                }
+            }
+        ]
+    }
+    cft.resources.add(
+        Resource(
+            'CognitoS3UploadRole',
+            'AWS::IAM::Role',
+            Properties(
+                {
+                    'AssumeRolePolicyDocument': upload_role_trust_policy,
+                    'Policies': [
+                        {
+                            'PolicyName': 'AuthenticatedS3UploadPolicy',
+                            'PolicyDocument': upload_access_policy,
+                        }
+                    ]
+                }
+            )
+        )
+    )
+
+    # Outputs
+    cft.outputs.add(
+        Output(
+            'IdentityPoolId',
+            ref('IdentityPool'),
+            {'Fn::Sub': '${AWS::StackName}IdentityPoolId'},
+            'Cognito identity pool ID'
+        )
+    )
 
     return cft
 
