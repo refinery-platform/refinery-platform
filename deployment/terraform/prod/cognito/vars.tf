788dcf9ae9ff7e74c20efeda2d875c0656d140ba
Ilya Sytchev <isytchev@hsph.harvard.edu>
2017-10-06 14:28:17 -0400

    Move comments to vars.tf

--- deployment/terraform/prod/cognito/vars.tf
+++ deployment/terraform/prod/cognito/vars.tf
@@ -7,11 +7,12 @@ variable "remote_state_bucket" {
   description = "S3 bucket used for the Terraform remote state"
 }
 
+// must match COGNITO_IDENTITY_POOL_NAME in deployment/aws-config/config.yaml
 variable "identity_pool_name" {
   description = "Cognito federated identity pool name"
 }
 
-// temp workaround
+// temp workaround, must match STACK_NAME in deployment/aws-config/config.yaml
 variable "stack_name" {
   description = "Name of the CloudFormation stack for Cognito resources"
 }
