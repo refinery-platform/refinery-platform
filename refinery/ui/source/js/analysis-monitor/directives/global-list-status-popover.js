ecb5cbd816b51e47eba8c8279c1cc60e6452f2f0
Jennifer Marx <jennyrutledge@gmail.com>
2017-10-10 12:24:10 -0400

    Cancel api call when popover is closed.

--- refinery/ui/source/js/analysis-monitor/directives/global-list-status-popover.js
+++ refinery/ui/source/js/analysis-monitor/directives/global-list-status-popover.js
@@ -8,34 +8,22 @@ function rpAnalysisMonitorGlobalListStatusPopover (
     restrict: 'AE',
     controller: 'AnalysisMonitorCtrl as AMCtrl',
     link: function (scope, element, attr, ctrl) {
-      // The script is in the base.html template.
-   //   $(element).popover(options);
+      // The script is in the global-list-status.html template.
 
       // catches all clicks, so popover will hide if you click anywhere other
       // than icon & popover
-
       $('body').on('click', function (e) {
-        console.log($('#analysesCogIcon .popover').hasClass('in'));
-        console.log(e.target);
         // starts api calls if icon is clicked
         if ((e.target.id === 'global-analysis-status-run' ||
           e.target.id === 'global-analysis-status' ||
           e.target.id === 'global-analysis-status-run-div') &&
           !$('#analysesCogIcon .popover').hasClass('in')) {
-          // popover is being opened for the first time
-          console.log('in the first iffy');
           $('#global-analysis-status-run-div').tooltip('hide');
           $('#global-analysis-status').tooltip('hide');
           ctrl.updateAnalysesGlobalList();
-        } else if (e.target.id === 'global-analysis-status-run' ||
-          e.target.id === 'global-analysis-status' ||
-          e.target.id === 'global-analysis-status-run-div' &&
-          $('#analysesCogIcon .popover').hasClass('in')
-        ) { // popover is open but is being triggered closed
-          console.log('being closed');
-          ctrl.cancelTimerGlobalList();
-        } else if (!$('#analysesCogIcon .popover').hasClass('in')) {
-          console.log('closed popover so cancel');
+        } else if ($(e.target).parents('.popover.in').length === 0 &&
+          $('#analysesCogIcon .popover').hasClass('in')) {
+          // popover is being closed
           ctrl.cancelTimerGlobalList();
         }
       });
