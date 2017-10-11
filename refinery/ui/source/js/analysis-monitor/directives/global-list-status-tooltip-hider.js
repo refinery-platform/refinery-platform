4548a136b3ab1b459529507547ba88dbd10cce45
Jennifer Marx <jennyrutledge@gmail.com>
2017-10-11 11:24:36 -0400

    Update directive to style guide.

--- refinery/ui/source/js/analysis-monitor/directives/global-list-status-tooltip-hider.js
+++ refinery/ui/source/js/analysis-monitor/directives/global-list-status-tooltip-hider.js
@@ -1,34 +1,26 @@
-// (function () {
-//  'use strict';
-//  angular
-//    .module('refineryAnalysisMonitor')
-//    .component('rpAnalysisMonitorGlobalListStatusPopover', {
-//      controller: 'AnalysisMonitorCtrl'
-//    });
-// })();
-//
+(function () {
+  'use strict';
 
-'use strict';
+  angular
+    .module('refineryAnalysisMonitor')
+    .directive(
+      'rpAnalysisMonitorGlobalListStatusTooltipHider',
+      rpAnalysisMonitorGlobalListStatusTooltipHider
+    );
 
-function rpAnalysisMonitorGlobalListStatusTooltipHider () {
-  return {
-    scope: {},
-    restrict: 'A',
-    link: function (scope, element) {
-      // The script is in the global-list-status.html template.
-      element.on('click', function () {
-        if (!element.find('.popover').hasClass('in')) {
-          element.find('#global-analysis-status-run-div').tooltip('hide');
-          element.find('#global-analysis-status').tooltip('hide');
-        }
-      });
-    }
-  };
-}
-
-
-angular
-  .module('refineryAnalysisMonitor')
-  .directive('rpAnalysisMonitorGlobalListStatusTooltipHider', [
-    rpAnalysisMonitorGlobalListStatusTooltipHider
-  ]);
+  function rpAnalysisMonitorGlobalListStatusTooltipHider () {
+    return {
+      scope: {},
+      restrict: 'A',
+      link: function (scope, element) {
+        // The script is in the global-list-status.html template.
+        element.on('click', function () {
+          if (!element.find('.popover').hasClass('in')) {
+            element.find('#global-analysis-status-run-div').tooltip('hide');
+            element.find('#global-analysis-status').tooltip('hide');
+          }
+        });
+      }
+    };
+  }
+})();
