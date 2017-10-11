f07703b5de914f5f7560fdf093931f08032804e3
Jennifer Marx <jennyrutledge@gmail.com>
2017-10-11 12:52:52 -0400

    Convert directive to component.

--- refinery/ui/source/js/analysis-monitor/directives/global-list-status.js
+++ refinery/ui/source/js/analysis-monitor/directives/global-list-status.js
@@ -1,40 +1,13 @@
-'use strict';
+(function () {
+  'use strict';
 
-function rpAnalysisMonitorGlobalListStatus ($window) {
-  return {
-    templateUrl: function () {
-      return $window.getStaticUrl('partials/analysis-monitor/partials/global-list-status.html');
-    },
-    restrict: 'AE',
-    controller: 'AnalysisMonitorCtrl',
-    controllerAs: 'AMCtrl',
-    bindToController: {
-      launchAnalysisFlag: '=?',
-      analysesRunningGlobalListCount: '=?',
-      analysesRunningGlobalList: '&?'
-    },
-    link: function (scope) {
-      scope.AMCtrl.updateAnalysesRunningGlobalList();
-
-      scope.$on('rf/launchAnalysis', function () {
-        scope.AMCtrl.launchAnalysisFlag = true;
-        scope.AMCtrl.analysesRunningGlobalListCount =
-          scope.AMCtrl.analysesRunningGlobalListCount + 1;
-      });
-
-      scope.$on('rf/cancelAnalysis', function () {
-        scope.AMCtrl.cancelTimerRunningGlobalList();
-        scope.AMCtrl.analysesRunningGlobalListCount =
-          scope.AMCtrl.analysesRunningGlobalListCount - 1;
-        scope.AMCtrl.updateAnalysesRunningGlobalList();
-      });
-    }
-  };
-}
-
-angular
-  .module('refineryAnalysisMonitor')
-  .directive('rpAnalysisMonitorGlobalListStatus', [
-    '$window',
-    rpAnalysisMonitorGlobalListStatus
-  ]);
+  angular.module('refineryAnalysisMonitor')
+    .component('rpAnalysisMonitorGlobalListStatus', {
+      controller: 'AnalysisMonitorGlobalListStatusCtrl',
+      templateUrl: ['$window', function ($window) {
+        return $window.getStaticUrl(
+          'partials/analysis-monitor/partials/global-list-status.html'
+        );
+      }]
+    });
+})();
