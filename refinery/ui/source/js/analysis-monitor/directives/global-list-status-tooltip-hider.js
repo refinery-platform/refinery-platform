061bb51967ed2209fc742a5b832489c1be39ebde
Jennifer Marx <jennyrutledge@gmail.com>
2017-10-11 11:21:54 -0400

    Fix spacing and remove unneccessary controller.

--- refinery/ui/source/js/analysis-monitor/directives/global-list-status-tooltip-hider.js
+++ refinery/ui/source/js/analysis-monitor/directives/global-list-status-tooltip-hider.js
@@ -13,15 +13,13 @@
 function rpAnalysisMonitorGlobalListStatusTooltipHider () {
   return {
     scope: {},
-    restrict: 'AE',
-  //  controller: 'AnalysisMonitorCtrl',
+    restrict: 'A',
     link: function (scope, element) {
       // The script is in the global-list-status.html template.
       element.on('click', function () {
         if (!element.find('.popover').hasClass('in')) {
           element.find('#global-analysis-status-run-div').tooltip('hide');
           element.find('#global-analysis-status').tooltip('hide');
-         // ctrl.updateAnalysesGlobalList();
         }
       });
     }
