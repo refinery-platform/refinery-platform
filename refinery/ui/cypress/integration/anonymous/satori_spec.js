c63632e7bcd52bae20845263da77b9b4c4335fbf
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2017-10-17 13:05:20 -0400

    Test sorting and locking

--- refinery/ui/cypress/integration/anonymous/satori_spec.js
+++ refinery/ui/cypress/integration/anonymous/satori_spec.js
@@ -1,5 +1,5 @@
 describe('Satori', function() {
-  it('prints "No data available"', function() {
+  it('Prints "No data available" without fixtures', function() {
     cy.visit('/');
     cy.visible_btn('Explore').click();
 
@@ -14,7 +14,7 @@ describe('Satori', function() {
     cy.get('#treemap-wrapper').contains('No data available!').should('visible');
   });
 
-  it('renders mock API response', function() {
+  function fixtures_and_routes() {
     cy.fixture('api-v1-data_sets.json').as('data_sets');
     cy.fixture('neo4j-annotations.json').as('annotations');
 
@@ -29,15 +29,44 @@ describe('Satori', function() {
       url: '/neo4j/annotations/',
       response: '@annotations'
     });
+  }
+
+  it('Locks and roots nodes in list graph', function() {
+    fixtures_and_routes();
+
+    var target_title = 'Target data set.';
+    var other_title = 'Other data set.';
 
     cy.visit('/');
-    cy.visible('Mock data set.');
+    cy.visible(target_title);
+    cy.visible(other_title);
     cy.visible('Oct 16, 2017');
     cy.visible_btn('Explore').click();
 
-    cy.visible('Native cell').click();
+    cy.get('#list-graph-wrapper').visible('Native cell').click();
     cy.get('#list-graph-wrapper').visible('Lock').click();
+    cy.get('#data-set-list .locked').visible(target_title);
+    cy.get('#data-set-list :not(.locked)').visible(other_title);
 
+    cy.get('#list-graph-wrapper').visible('Root').click();
+    cy.get('#data-set-list .locked').visible(target_title);
+    cy.get('#data-set-list').invisible(other_title);
+  });
 
+  it('Sorts nodes in list graph by name', function() {
+    fixtures_and_routes();
+
+    cy.visit('/');
+    cy.visible_btn('Explore').click();
+
+    cy.get('#list-graph-wrapper').visible('Name').click();
+    cy.get('#list-graph-wrapper').visible('No annotations').click(); // Last node, alphabetically.
+    cy.get('#list-graph-wrapper').visible('Lock').click();
+    cy.get('.node.lock-directly').invoke('attr','transform').should('contain', ', 0)');
+
+    cy.get('#list-graph-wrapper').visible('Name').click();
+    cy.get('#list-graph-wrapper').visible('Biotin').click(); // First node, alphabetically.
+    cy.get('#list-graph-wrapper').visible('Lock').click();
+    cy.get('.node.lock-directly').invoke('attr','transform').should('contain', ', 0)');
   });
 });
