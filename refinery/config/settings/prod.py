0e11f2ada6aab2bab608e2472cfcc20dbda624a1
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2017-10-18 15:00:03 -0400

    Flake8 is complaining now: not sure why it was not a problem earlier

--- refinery/config/settings/prod.py
+++ refinery/config/settings/prod.py
@@ -1,10 +1,9 @@
 # custom settings for production environment
 
 from .base import *  # NOQA
-from .base import get_setting
 
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 # Required when DEBUG = False
-ALLOWED_HOSTS = get_setting("ALLOWED_HOSTS")
+ALLOWED_HOSTS = get_setting("ALLOWED_HOSTS")  # NOQA: F405
