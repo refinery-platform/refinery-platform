a91e7840ce693dfec680c0e71f1a406e04d49942
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2017-10-18 12:29:03 -0400

    preview spec with stronger assertions

--- refinery/ui/cypress/integration/logged_in/preview_spec.js
+++ refinery/ui/cypress/integration/logged_in/preview_spec.js
@@ -26,7 +26,7 @@ describe('Preview', function() {
       method: 'GET',
       url: '/api/v1/data_sets/b80f6abf-058a-4629-888f-d31375c1927e/sharing/?format=json',
       response: '@data_sets_sharing'
-    })
+    });
   }
 
   it('Works', function() {
@@ -36,11 +36,30 @@ describe('Preview', function() {
     cy.visible('Target data set.').click();
 
     cy.visible('Description');
+    // TODO
     cy.visible('Technology & Measurement');
+    // TODO
+    cy.visible('Sources');
+
     cy.visible('Number of files (total file size)');
+    // TODO
+
     cy.visible('Owner');
+    // TODO
+
     cy.visible('Analyses');
+    // TODO
+
     cy.visible('References');
+    cy.visible('Gene expression profiling of Caco-2 BBe cells');
+    cy.visible('Fleet JC, Wang L, Vitek O, Craig BA and Edenberg HJ');
+
+    cy.visible('Abstract');
+    cy.visible('We examined the pattern of gene expression');
+
+    cy.get('.protocols').scrollIntoView();q
     cy.visible('Protocols');
+    cy.visible('growth protocol');
+    cy.visible('Caco-2 cells were grown in 10% FBS');
   });
 });
