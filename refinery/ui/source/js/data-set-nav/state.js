2f412db6c378be6dfed8ff6fbffaef2321b3a0c9
Jennifer Marx <jkmarx@users.noreply.github.com>
2017-10-26 08:01:41 -0400

    Jkmarx/analysis launch ui removal (#2248)
    * Remove analysis-launch module.
    
    * Remove from unit test.
    
    * Remove node-relationship module. (#2249)
    
    * Remove node-relationship module.
    
    * Remove node-mapping module and add third party dependency to refinery… (#2250)
    
    * Remove node-mapping module and add third party dependency to refinery.js.
    
    * Jkmarx/decongest data set nav ui (#2268)
    
    * Clean up data-set-nav and remove references.
    
    * Update unit tests.
    
    * Jkmarx/remove select workflows and igv module (#2269)
    
    * Remove select workflow module.
    
    * Remove igv module.
    
    * Fix typo ,

--- refinery/ui/source/js/data-set-nav/state.js
+++ refinery/ui/source/js/data-set-nav/state.js
@@ -9,98 +9,6 @@ function refineryDataSetNavConfig (
         url: '/files/',
         templateUrl: function () {
           // unit tests redefine $window and thus make it unusable here
-          return window.getStaticUrl(
-            'partials/data-set-nav/partials/data-set-ui-mode-browse.html'
-          );
-        },
-        controller: 'refineryDataSetNavFilesCtrl'
-      },
-      '^\/provenance\/.*\/$',
-      true
-    )
-    .state(
-      'browse', {
-        url: '/files/browse',
-        templateUrl: function () {
-          // unit tests redefine $window and thus make it unusable here
-          return window.getStaticUrl(
-            'partials/data-set-nav/partials/data-set-ui-mode-browse.html'
-          );
-        },
-        controller: 'refineryDataSetNavFilesBrowseCtrl'
-      },
-      '^\/provenance\/.*\/$',
-      true
-    )
-    .state(
-      'analyze', {
-        url: '/files/analyze/',
-        templateUrl: function () {
-          // unit tests redefine $window and thus make it unusable here
-          return window.getStaticUrl(
-            'partials/data-set-nav/partials/data-set-ui-mode-analyze.html'
-          );
-        },
-        controller: 'refineryDataSetNavAnalyzeCtrl'
-      },
-      '^\/provenance\/.*\/$',
-      true
-    )
-    .state(
-      'visualize', {
-        templateUrl: function () {
-          // unit tests redefine $window and thus make it unusable here
-          return window.getStaticUrl(
-            'partials/data-set-nav/partials/data-set-ui-mode-visualize.html'
-          );
-        },
-        url: '/files/visualize/',
-        controller: 'refineryDataSetNavVisualizeCtrl'
-      },
-      '^\/provenance\/.*\/$',
-      true
-    )
-    .state(
-      'analyses', {
-        url: '/analyses/',
-        templateUrl: function () {
-          // unit tests redefine $window and thus make it unusable here
-          return window.getStaticUrl('partials/analysis-monitor/partials/analyses-list.html');
-        },
-        controller: 'refineryDataSetNavBlueprintCtrl'
-      },
-      '^\/provenance\/.*\/$',
-      true
-    )
-    .state(
-      'attributes', {
-        url: '/attributes/',
-        controller: 'refineryDataSetNavBlueprintCtrl'
-      },
-      '^\/provenance\/.*\/$',
-      true
-    )
-    .state(
-      'details', {
-        url: '/details/',
-        controller: 'refineryDataSetNavBlueprintCtrl'
-      },
-      '^\/provenance\/.*\/$',
-      true
-    )
-    .state(
-      'sharing', {
-        url: '/sharing/',
-        controller: 'refineryDataSetNavBlueprintCtrl'
-      },
-      '^\/provenance\/.*\/$',
-      true
-    )
-    .state(
-      'files', {
-        url: '/files/',
-        templateUrl: function () {
-          // unit tests redefine $window and thus make it unusable here
           return window.getStaticUrl('partials/file-browser/views/files-tab.html');
         },
         controller: 'FileBrowserCtrl',
@@ -137,11 +45,6 @@ function refineryDataSetNavConfig (
     );
 
   refineryUrlRouterProvider
-    .otherwise(
-      '/files/browse',
-      '^\/provenance\/.*\/$',
-      true
-    )
     .otherwise(
       '/files/',
       '^\/data_sets\/.*\/$',
