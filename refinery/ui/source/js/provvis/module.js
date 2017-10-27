f38b7dd27207381948789d4be429df70766e05ae
Jennifer Marx <jennyrutledge@gmail.com>
2017-10-27 10:36:26 -0400

    Seperate out provvis module initialization.

---
+++ refinery/ui/source/js/provvis/module.js
@@ -0,0 +1,12 @@
+/**
+ * Refinery Provvis
+ * @namespace refineryProvvis
+ * @desc Module for the provenance visualization
+ * @memberOf refineryApp
+ */
+(function () {
+  'use strict';
+
+  angular
+    .module('refineryProvvis', []);
+})();
