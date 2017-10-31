ff4d9b8d57403d1f4287b5eb435e0152893f21ad
Ilya Sytchev <isytchev@hsph.harvard.edu>
2017-10-31 18:03:13 -0400

    Use workspace name as S3 bucket name prefix

--- deployment/terraform/live/vars.tf
+++ deployment/terraform/live/vars.tf
@@ -2,12 +2,6 @@ variable "region" {
   description = "The AWS region to use"
 }
 
-// must match S3_BUCKET_NAME_BASE in deployment/aws-config/config.yaml
-// should match Terraform workspace name
-variable "bucket_name_base" {
-  description = "Prefix for the Refinery Platform S3 buckets"
-}
-
 // must match COGNITO_IDENTITY_POOL_NAME in deployment/aws-config/config.yaml
 variable "identity_pool_name" {
   description = "Cognito federated identity pool name"
