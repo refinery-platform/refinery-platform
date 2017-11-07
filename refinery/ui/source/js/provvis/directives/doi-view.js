47bc32a9864de80f6b630ff1026f51f94afb26fc
Jennifer Marx <jennyrutledge@gmail.com>
2017-11-07 11:35:10 -0500

    Add doi-view change to component.

---
+++ refinery/ui/source/js/provvis/directives/doi-view.js
@@ -0,0 +1,18 @@
+/**
+ * Provvis DOI View Directive
+ * @namespace rpProvvisDOIView
+ * @desc DOI view.
+ * @memberOf refineryApp.refineryProvvis
+ */
+
+(function () {
+  'use strict';
+  angular
+    .module('refineryProvvis')
+    .component('rpProvvisDoiView', {
+    //  controller: 'InputControlCtrl',
+      templateUrl: ['$window', function ($window) {
+        return $window.getStaticUrl('partials/provvis/partials/doi-view.html');
+      }]
+    });
+})();
