34e202dd15d85070504c29e792a8df91f4710efc
mccalluc <mccalluc@users.noreply.github.com>
2017-11-15 09:15:40 -0500

    Mccalluc/tools registry (#2374)
    * Loaded IGV by name
    
    * more help if missing params
    
    * vestigial [skip ci]
    
    * Explicit --visualizations [skip ci]
    
    * Lots of questionable renaming... but for vis, it really is more clear to say it is loading than to say generating
    
    * Checkpoint: ToolDefinitionGenerationTests passes locally
    
    * I misunderstood this option
    
    * More input validation, and fix another set of tests
    
    * HiGlass image is huge... Hopefully IGV suffices for test? If not, make something simple that does work.
    
    * Undo some copy and paste, and suggestion avoiding image download in future
    
    * Remove last reference to method that does not exist any more. Object count tests behavior has changed.
    
    * No reason to enforce mutual exclusivity
    
    * And reenable the counts test, though I am not sure how useful it is

--- refinery/config/settings/base.py
+++ refinery/config/settings/base.py
@@ -650,8 +650,6 @@ TEST_RUNNER = "django.test.runner.DiscoverRunner"
 TEST_NON_SERIALIZED_APPS = ['core', 'django.contrib.contenttypes',
                             'django.contrib.auth']
 
-VISUALIZATION_ANNOTATION_BASE_PATH = "tool_manager/visualization_annotations"
-
 # To avoid Port conflicts between LiveServerTestCases
 # https://docs.djangoproject.com/en/1.7/topics/testing/tools/#liveservertestcase
 os.environ["DJANGO_LIVE_TEST_SERVER_ADDRESS"] = "localhost:10000-12000"
