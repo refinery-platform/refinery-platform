b2bf4c5f575d8ca30cd1b37ddd329fada0206cbb
Jennifer Marx <jennyrutledge@gmail.com>
2017-11-26 20:55:04 -0500

    Remove unused provvis.js

--- refinery/ui/source/js/provvis/module.js
+++ refinery/ui/source/js/provvis/module.js
@@ -3,6 +3,8 @@
  * @namespace refineryProvvis
  * @desc Module for the provenance visualization
  * @memberOf refineryApp
+ * @author sluger Stefan Luger https://github.com/sluger
+ * @exports runProvVis The published function to run the visualization.
  */
 (function () {
   'use strict';
