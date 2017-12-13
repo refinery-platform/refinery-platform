731a7a84c35484e523fc1ba75d46d630d22ab393
Jennifer Marx <jennyrutledge@gmail.com>
2017-12-13 14:51:00 -0500

    Add visualization module.

---
+++ refinery/ui/source/js/data-set-visualization/module.js
@@ -0,0 +1,12 @@
+/**
+ * Refinery Data Set Visualization
+ * @namespace refineryDataSetVisualization
+ * @desc In the file browser, module handles the visualization. Right
+ * now, only use to list the launched visualizations.
+ * @memberOf refineryApp
+ */
+(function () {
+  'use strict';
+  angular
+    .module('refineryDataSetVisualization', []);
+})();
