bc3fbe21f2e5af91993100002b2f49d424caf33f
Jennifer Marx <jennyrutledge@gmail.com>
2017-12-14 22:00:33 -0500

    Update API to request tools based on data set.

--- refinery/tool_manager/views.py
+++ refinery/tool_manager/views.py
@@ -3,7 +3,6 @@ import logging
 from django.db import transaction
 from django.http import HttpResponseBadRequest
 
-from guardian.shortcuts import get_objects_for_user
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
@@ -65,25 +64,50 @@ class ToolsViewSet(ModelViewSet):
     http_method_names = ['get', 'post']
     permission_classes = [IsAuthenticated]
 
+    def list(self, request, *args, **kwargs):
+        try:
+            data_set_uuid = self.request.query_params["data_set_uuid"]
+        except (AttributeError, KeyError) as e:
+            return Response(
+                "Must specify a Dataset UUID: {}".format(e),
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        try:
+            self.data_set = DataSet.objects.get(uuid=data_set_uuid)
+        except (DataSet.DoesNotExist, DataSet.MultipleObjectsReturned) as e:
+            return Response(
+                "Couldn't fetch Dataset with UUID: {} {}".format(
+                    data_set_uuid, e
+                ),
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        return super(ToolsViewSet, self).list(request)
+
     def get_queryset(self):
         """
         This view returns a list of all the Tools that the currently
         authenticated user has read permissions on.
         """
-        user_tools = []
-        user_tools.extend(
-            get_objects_for_user(
-                self.request.user,
-                "tool_manager.read_workflowtool"
+        if self.request.user.has_perm('core.read_meta_dataset', self.data_set):
+            data_set_tools = Tool.objects.filter(dataset=self.data_set)
+        else:
+            return Response(
+                "Unauthorized for data set uuid: {}".format(
+                    self.request.query_params.get("data_set_uuid")),
+                status=status.HTTP_401_UNAUTHORIZED
             )
-        )
-        user_tools.extend(
-            get_objects_for_user(
-                self.request.user,
-                "tool_manager.read_visualizationtool"
-            )
-        )
-        return user_tools
+
+        tool_type = self.request.query_params.get("tool_type")
+
+        if not tool_type:
+            return data_set_tools
+
+        if tool_type == 'visualization':
+            return [tool for tool in data_set_tools
+                    if tool.get_tool_type() == ToolDefinition.VISUALIZATION]
+        else:
+            return [tool for tool in data_set_tools
+                    if tool.get_tool_type() == ToolDefinition.WORKFLOW]
 
     def create(self, request, *args, **kwargs):
         """
