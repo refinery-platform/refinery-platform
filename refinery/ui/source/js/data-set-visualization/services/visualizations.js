e9ce32c4a0936548d19e8052cb9d6d8be0230c92
Jennifer Marx <jennyrutledge@gmail.com>
2017-12-15 10:52:18 -0500

    Add humanize code.

--- refinery/ui/source/js/data-set-visualization/services/visualizations.js
+++ refinery/ui/source/js/data-set-visualization/services/visualizations.js
@@ -10,9 +10,11 @@
     .module('refineryDataSetVisualization')
     .factory('visualizationsService', visualizationsService);
 
-  visualizationsService.$inject = ['toolsService'];
+  visualizationsService.$inject = ['_', 'humanize', 'toolsService'];
 
   function visualizationsService (
+    _,
+    humanize,
     toolsService
   ) {
     /**
@@ -32,18 +34,40 @@
      * ----------------------
      */
     /**
-     * @name refreshToolParams
-     * @desc Used by toolSelectService, updates the selected tool's
-     * parameters used by the UI parameters panel
+     * @name getVisualizations
+     * @desc Get the visualization using the visualization service and tools
+     * api
      * @memberOf refineryToolLaunch.toolParamsService
      * @param {object} tool - select tool object, includes property parameters
     **/
     function getVisualizations (params) {
       var toolRequest = toolsService.query(params);
       toolRequest.$promise.then(function (response) {
-        angular.copy(response, visualizations);
+        angular.copy(addElapseAndHumanTime(response), visualizations);
       });
       return toolRequest.$promise;
+    }
+
+    // process responses from api
+    function addElapseAndHumanTime (toolList) {
+      for (var j = 0; j < toolList.length; j++) {
+        console.log(toolList[j]);
+        if (_.has(toolList[j], 'creation_date')) {
+          toolList[j].humanizeStartTime = humanizeTimeObj(toolList[j].creation_date);
+        }
+      }
+      return toolList;
+    }
+
+    function humanizeTimeObj (param) {
+      console.log(param);
+      var a = param.split(/[^0-9]/);
+      var testDate = Date.UTC(a[0], a[1] - 1, a[2], a[3], a[4], a[5]);
+      var curDate = new Date().getTimezoneOffset() * 60 * 1000;
+      var offsetDate = testDate + curDate;
+      var unixtime = offsetDate / 1000;
+
+      return humanize.relativeTime(unixtime);
     }
   }
 })();
