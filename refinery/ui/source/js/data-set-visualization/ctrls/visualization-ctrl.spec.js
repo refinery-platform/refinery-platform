b23e6b08d471433ca8898415b95783958de29266
Jennifer Marx <jennyrutledge@gmail.com>
2017-12-18 18:19:26 -0500

    Update unit test to avoid private method.

--- refinery/ui/source/js/data-set-visualization/ctrls/visualization-ctrl.spec.js
+++ refinery/ui/source/js/data-set-visualization/ctrls/visualization-ctrl.spec.js
@@ -3,22 +3,19 @@
 
   describe('Controller: Data Set Visualization Ctrl', function () {
     var ctrl;
-    var mockAPI;
     var scope;
 
     beforeEach(module('refineryApp'));
     beforeEach(module('refineryDataSetVisualization'));
     beforeEach(inject(function (
       $rootScope,
-      $controller,
-      visualizationService
+      $controller
 
     ) {
       scope = $rootScope.$new();
       ctrl = $controller('DataSetVisualizationCtrl', {
         $scope: scope
       });
-      mockAPI = spyOn(visualizationService, 'getVisualizations');
     }));
 
     it('Data Set Visualization Ctrl ctrl should exist', function () {
@@ -28,11 +25,6 @@
     it('Data & UI displays variables should exist for views', function () {
       expect(ctrl.visLoadingFlag).toEqual('LOADING');
       expect(ctrl.visualizations).toEqual([]);
-    });
-
-    it('Calls on the correct service', function () {
-      ctrl.refreshVisualizations();
-      expect(mockAPI).toHaveBeenCalled();
     });
   });
 })();
