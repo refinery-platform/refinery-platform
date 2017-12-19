189456a21be6e53dd0e0626f2ea97d176aa787e0
Scott Ouellette <scottx611x@gmail.com>
2017-12-19 17:42:30 -0500

    Disable csrf (this is just a temporary branch)

--- refinery/config/settings/base.py
+++ refinery/config/settings/base.py
@@ -159,7 +159,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
