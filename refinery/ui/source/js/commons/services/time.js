fb6df4ddbc84bfe7ada80a0799a0f42a453883f2
Jennifer Marx <jennyrutledge@gmail.com>
2017-12-20 09:22:54 -0500

    Remove unused method.

--- refinery/ui/source/js/commons/services/time.js
+++ refinery/ui/source/js/commons/services/time.js
@@ -15,7 +15,6 @@
 
   function timeService (humanize) {
     var vm = this;
-    vm.getTimeStamp = getTimeStamp;
     vm.humanizeTimeObj = humanizeTimeObj;
 
     /*
@@ -23,29 +22,6 @@
      * Method Definitions
      * ----------------------
      */
-    function getTimeStamp () {
-      var currentDate = new Date();
-      var month = currentDate.getMonth() + 1;
-      var day = currentDate.getDate();
-      var year = currentDate.getFullYear();
-      var hour = currentDate.getHours();
-      var mins = currentDate.getMinutes();
-      var sec = currentDate.getSeconds();
-
-      if (mins < 10) {
-        mins = '0' + mins;
-      }
-
-      if (sec < 10) {
-        sec = '0' + sec;
-      }
-
-      var dateStr = year + '-' + month + '-' + day;
-      var timeStr = '@' + hour + ':' + mins + ':' + sec;
-
-      return (dateStr + timeStr);
-    }
-
     function humanizeTimeObj (param) {
       var a = param.split(/[^0-9]/);
       var testDate = Date.UTC(a[0], a[1] - 1, a[2], a[3], a[4], a[5]);
