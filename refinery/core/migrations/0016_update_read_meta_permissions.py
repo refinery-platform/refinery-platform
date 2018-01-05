40ad2efb8ef4a989804f2a064dd45d92cacf6406
Scott Ouellette <scottx611x@gmail.com>
2018-01-05 15:09:24 -0500

    Manually trigger permission creation; If core.0015_auto_20171213_1429.py migration hasn't been run, then the read_meta_dataset permission doesn't exist yet.

--- refinery/core/migrations/0016_update_read_meta_permissions.py
+++ refinery/core/migrations/0016_update_read_meta_permissions.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.contrib.auth.management import create_permissions
 from django.contrib.auth.models import User, Group
 from django.db import models, migrations
 from guardian.core import ObjectPermissionChecker
@@ -12,6 +13,14 @@ from core.models import DataSet
 def update_read_meta_dataset_permissions(apps, schema_editor):
     """Assign the `read_meta_dataset` permission to Datasets
     that existed in the system with the `read_dataset` permission"""
+
+    # Permissions are only created after all migrations are run. Since this
+    # data migration needs access to said permissions we manually trigger
+    # their creation here.
+    apps.models_module = True
+    create_permissions(apps, verbosity=0)
+    apps.models_module = None
+
     for dataset in DataSet.objects.all():
         for queryset in [User.objects.all(), Group.objects.all()]:
             for obj in queryset:
