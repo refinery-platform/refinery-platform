a3daf1076244f1b640a5c461ab52d9e3da73b3fd
Jennifer Marx <jennyrutledge@gmail.com>
2018-01-24 12:53:19 -0500

    Update comment.

--- refinery/config/settings/dev.py
+++ refinery/config/settings/dev.py
@@ -5,10 +5,11 @@ from .base import *  # NOQA
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
-# In older versions, ALLOWED_HOSTS wasn’t checked if DEBUG=True.
-# This was also changed 1.8.16 to prevent a DNS rebinding attack.
+# ALLOWED_HOSTS required in 1.8.16 to prevent a DNS rebinding attack.
 ALLOWED_HOSTS = get_setting("ALLOWED_HOSTS")  # NOQA: F405
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
-INSTALLED_APPS += ('django_extensions')  # NOQA: F405
+INSTALLED_APPS += (  # NOQA: F405
+    'django_extensions',
+)
