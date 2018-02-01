ff912e48091f3e9320c0ac209afa99c9d3eed436
Jennifer Marx <jennyrutledge@gmail.com>
2018-01-31 21:01:15 -0500

    Fix pep8 in migration.

--- refinery/dependency_migrations/chunked_upload/0001_initial.py
+++ refinery/dependency_migrations/chunked_upload/0001_initial.py
@@ -17,15 +17,50 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='ChunkedUpload',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('upload_id', models.CharField(default=chunked_upload.models.generate_upload_id, unique=True, max_length=32, editable=False)),
-                ('file', models.FileField(max_length=255, upload_to=chunked_upload.models.generate_filename)),
+                (
+                    'id',
+                    models.AutoField(
+                        verbose_name='ID',
+                        serialize=False,
+                        auto_created=True,
+                        primary_key=True
+                    )
+                ),
+                (
+                    'upload_id',
+                    models.CharField(
+                        default=chunked_upload.models.generate_upload_id,
+                        unique=True,
+                        max_length=32,
+                        editable=False
+                    )
+                ),
+                (
+                    'file',
+                    models.FileField(
+                        max_length=255,
+                        upload_to=chunked_upload.models.generate_filename
+                    )),
                 ('filename', models.CharField(max_length=255)),
                 ('offset', models.BigIntegerField(default=0)),
                 ('created_on', models.DateTimeField(auto_now_add=True)),
-                ('status', models.PositiveSmallIntegerField(default=1, choices=[(1, 'Uploading'), (2, 'Complete'), (3, 'Failed')])),
+                (
+                    'status',
+                    models.PositiveSmallIntegerField(
+                        default=1,
+                        choices=[
+                            (1, 'Uploading'), (2, 'Complete'), (3, 'Failed')
+                        ]
+                    )
+                ),
                 ('completed_on', models.DateTimeField(null=True, blank=True)),
-                ('user', models.ForeignKey(related_name='chunked_uploads', to=settings.AUTH_USER_MODEL)),
+                (
+                    'user',
+                    models.ForeignKey(
+                        related_name='chunked_uploads',
+                        to=settings.AUTH_USER_MODEL
+                    )
+                ),
             ],
             options={
                 'abstract': False,
