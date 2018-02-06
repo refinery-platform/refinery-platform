39ba7e96fcab4ebb04fe25aaba561a823dce46f5
Jennifer Marx <jennyrutledge@gmail.com>
2018-02-06 18:01:59 -0500

    Fix unit test.

--- refinery/core/tests.py
+++ refinery/core/tests.py
@@ -651,13 +651,12 @@ class DataSetDeletionTest(TestCase):
         )
         self.analysis.set_owner(self.user)
 
-    @mock.patch('core.models.DataSet.get_investigation_links')
+    @mock.patch('core.models.DataSet.get_investigation_links', return_value=[])
     def test_verify_dataset_deletion_if_no_analysis_run_upon_it(
             self, mock_get_links
     ):
         self.assertIsNotNone(
-            DataSet.objects.get(name="dataset_without_analysis"),
-            return_value=[]
+            DataSet.objects.get(name="dataset_without_analysis")
         )
         self.dataset_without_analysis.delete()
         self.assertRaises(
