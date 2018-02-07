c1e0b379b3ed94eaad4c7f64ddeec5caffd21d77
Ilya Sytchev <isytchev@hsph.harvard.edu>
2018-02-07 15:09:42 -0500

    Clean up Puppet manifests

--- deployment/manifests/aws.pp
+++ deployment/manifests/aws.pp
@@ -1,33 +1,32 @@
-$app_user = "ubuntu"
-$app_group = "ubuntu"
+$app_user = 'ubuntu'
+$app_group = $app_user
 $virtualenv = "/home/${app_user}/.virtualenvs/refinery-platform"
-$project_root = "/srv/refinery-platform"
+$project_root = '/srv/refinery-platform'
 $deployment_root = "${project_root}/deployment"
 $django_root = "${project_root}/refinery"
 $requirements = "${project_root}/requirements.txt"
 $requirements_aws = "${project_root}/requirements-aws.txt"
-$isa_tab_dir = "/data/isa-tab"
-$media_root = "/data/media"
-$import_dir = "/data/import"
+$data_dir = '/data'
+$isa_tab_dir = "${data_dir}/isa-tab"
+$media_root = "${data_dir}/media"
+$import_dir = "${data_dir}/import"
 $solr_custom_synonyms_file = "${django_root}/solr/core/conf/custom-synonyms.txt"
 # As per https://github.com/refinery-platform/refinery-platform/issues/1153
-$solr_lib_dir = "/opt/solr/server/solr-webapp/webapp/WEB-INF/lib"
-$email_host = "email-smtp.us-east-1.amazonaws.com"
-# $email_host_user set by Facter
-# $email_host_password set by Facter
-$email_use_tls = "true"
-$conf_mode = "aws"
+$solr_lib_dir = '/opt/solr/server/solr-webapp/webapp/WEB-INF/lib'
+$email_host = 'email-smtp.us-east-1.amazonaws.com'
+$email_use_tls = 'true'
+$conf_mode = 'aws'
 $django_settings_module = "config.settings.${conf_mode}"
 $ui_app_root = "${django_root}/ui"
-# $site_name set by Facter
-# $site_url set by Facter
-
 # solr is in the /data volume on AWS:
-$solr_data_set_manager_data = "/data/solr/data_set_manager"
-$solr_core_data = "/data/solr/core"
+$solr_data_set_manager_data = "${data_dir}/solr/data_set_manager"
+$solr_core_data = "${data_dir}/solr/core"
+$django_docker_engine_data_dir = "${data_dir}/django-docker-engine-data"
 
 $deployment_platform = 'aws'
-$django_docker_engine_data_dir = "/data/django-docker-engine-data"
+
+# set by Facter:
+# $email_host_user, $email_host_password, $site_name, $site_url
 
 # See code in refinery-modules/refinery/...
 include refinery
