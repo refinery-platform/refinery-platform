b5f97eafa83400e02ba30227f207203795914e36
Jennifer Marx <jennyrutledge@gmail.com>
2018-02-21 20:19:57 -0500

    Tweak solr_response fixture.

--- refinery/ui/cypress/integration/anonymous/not_logged_in_spec.js
+++ refinery/ui/cypress/integration/anonymous/not_logged_in_spec.js
@@ -56,10 +56,10 @@ describe('Not logged in', function() {
     cy.visit('/');
     cy.visible_btn('List').click();
 
-    cy.visible('Organism');
-    cy.visible('Technology');
-    cy.visible('Antibody');
-    cy.visible('Date');
-    cy.visible('Genotype');
+     cy.visible('Organism');
+     cy.visible('Technology');
+     cy.visible('Antibody');
+     cy.visible('Date');
+     cy.visible('Genotype');
   });
 });
