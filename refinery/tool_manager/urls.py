d51b489a24c86f1fc2ad86abafff7b9a0056ebac
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2018-02-23 15:33:08 -0500

    "Please wait" for vis tools

--- refinery/tool_manager/urls.py
+++ refinery/tool_manager/urls.py
@@ -16,9 +16,32 @@ tool_manager_router.register(
 )
 
 url_patterns = Proxy(
-    settings.DJANGO_DOCKER_ENGINE_DATA_DIR
-    # please_wait_title='optional title'
-    # please_wait_body='optional html body'
+    settings.DJANGO_DOCKER_ENGINE_DATA_DIR,
+    please_wait_title='Please wait...',
+    please_wait_body='''
+        <style>
+        body {
+          font-family: "Source Sans Pro",Helvetica,Arial,sans-serif;
+          font-size: 40pt;
+          text-align: center;
+        }
+        div {
+          position: absolute;
+          top: 50%;
+          left: 50%;
+          margin-right: -50%;
+          transform: translate(-50%, -50%)
+        }
+        </style>
+
+        <main>
+        <div>
+        <img src="https://camo.githubusercontent.com/c4f6ef3406a9c83a537432b129ad99240afcee7a/68747470733a2f2f7062732e7477696d672e636f6d2f70726f66696c655f696d616765732f3531393530353635323038333734383836342f62473969744c546c5f343030783430302e706e67" width='150'>
+        <p>Please wait...</p>
+        <img width='150' src="https://user-images.githubusercontent.com/730388/36614998-b24d66ea-18ac-11e8-82f9-75542932b452.gif">
+        </div>
+        </main>
+    ''' # noqa
 ).url_patterns()
 django_docker_engine_url = url(
     r'^{}/'.format(settings.DJANGO_DOCKER_ENGINE_BASE_URL),
