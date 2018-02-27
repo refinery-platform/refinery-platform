c0b3c20defe8612a9afb6d4a651059331091d1fe
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2018-02-27 15:05:47 -0500

    Replace remove urls with references to static bucket

--- refinery/tool_manager/urls.py
+++ refinery/tool_manager/urls.py
@@ -20,28 +20,26 @@ url_patterns = Proxy(
     please_wait_title='Please wait...',
     please_wait_body_html='''
         <style>
-        body {
+        body {{
           font-family: "Source Sans Pro",Helvetica,Arial,sans-serif;
           font-size: 40pt;
           text-align: center;
-        }
-        div {
+        }}
+        div {{
           position: absolute;
           top: 50%;
           left: 50%;
           margin-right: -50%;
           transform: translate(-50%, -50%)
-        }
+        }}
         </style>
 
-        <main>
         <div>
-        <img src="https://camo.githubusercontent.com/c4f6ef3406a9c83a537432b129ad99240afcee7a/68747470733a2f2f7062732e7477696d672e636f6d2f70726f66696c655f696d616765732f3531393530353635323038333734383836342f62473969744c546c5f343030783430302e706e67" width='150'>
+        <img src="{0}/logo.svg" width='150'>
         <p>Please wait...</p>
-        <img width='150' src="https://user-images.githubusercontent.com/730388/36614998-b24d66ea-18ac-11e8-82f9-75542932b452.gif">
+        <img src="{0}/spinner.gif">
         </div>
-        </main>
-    '''  # noqa
+    '''.format(settings.STATIC_URL + 'images')  # noqa
 ).url_patterns()
 django_docker_engine_url = url(
     r'^{}/'.format(settings.DJANGO_DOCKER_ENGINE_BASE_URL),
