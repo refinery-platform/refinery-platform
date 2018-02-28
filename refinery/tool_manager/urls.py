9dad32d9d05b29086826242ec7f92e2a4b349f3b
mccalluc <mccalluc@users.noreply.github.com>
2018-02-28 10:43:33 -0500

    "Please wait" for vis tools (#2622)
    * "Please wait" for vis tools
    
    * Whitespace
    
    * Right name for kwarg
    
    * Replace remove urls with references to static bucket

--- refinery/tool_manager/urls.py
+++ refinery/tool_manager/urls.py
@@ -16,9 +16,30 @@ tool_manager_router.register(
 )
 
 url_patterns = Proxy(
-    settings.DJANGO_DOCKER_ENGINE_DATA_DIR
-    # please_wait_title='optional title'
-    # please_wait_body='optional html body'
+    settings.DJANGO_DOCKER_ENGINE_DATA_DIR,
+    please_wait_title='Please wait...',
+    please_wait_body_html='''
+        <style>
+        body {{
+          font-family: "Source Sans Pro",Helvetica,Arial,sans-serif;
+          font-size: 40pt;
+          text-align: center;
+        }}
+        div {{
+          position: absolute;
+          top: 50%;
+          left: 50%;
+          margin-right: -50%;
+          transform: translate(-50%, -50%)
+        }}
+        </style>
+
+        <div>
+        <img src="{0}/logo.svg" width='150'>
+        <p>Please wait...</p>
+        <img src="{0}/spinner.gif">
+        </div>
+    '''.format(settings.STATIC_URL + 'images')  # noqa
 ).url_patterns()
 django_docker_engine_url = url(
     r'^{}/'.format(settings.DJANGO_DOCKER_ENGINE_BASE_URL),
