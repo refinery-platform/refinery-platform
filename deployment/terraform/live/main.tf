80f8017604eba57a59a68bf6618c257369769492
Scott Ouellette <scottx611x@gmail.com>
2018-03-12 17:24:17 -0400

    Utilize new availibility zone value, align "="

--- deployment/terraform/live/main.tf
+++ deployment/terraform/live/main.tf
@@ -14,11 +14,13 @@ provider "aws" {
 
 module "object_storage" {
   source           = "../modules/s3"
+  name                = "${terraform.workspace} object_storage"
   bucket_name_base = "${terraform.workspace}"
 }
 
 module "identity_pool" {
   source                   = "../modules/cognito"
+  name                = "${terraform.workspace} identity pool"
   identity_pool_name       = "${var.identity_pool_name}"
   upload_bucket_name       = "${module.object_storage.upload_bucket_name}"
   iam_resource_name_prefix = "${terraform.workspace}"
@@ -36,13 +38,13 @@ module "docker_host" {
 
 
 module "vpc" {
-  vpc_cidr_block     = "${var.vpc_cidr_block}"
-  availability_zone  = "${var.availability_zone}"
   source               = "../modules/vpc"
   name                 = "${terraform.workspace} vpc"
+  vpc_cidr_block       = "${var.vpc_cidr_block}"
   public_cidr_block    = "${var.public_cidr_block}"
   private_cidr_block_a = "${var.private_cidr_block_a}"
   private_cidr_block_b = "${var.private_cidr_block_b}"
+  availability_zone    = "${var.availability_zone_a}"
 }
 
 module "rds" {
