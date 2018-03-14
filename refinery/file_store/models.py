7ebbea544bc69687bfea465e9f82883916711056
Ilya Sytchev <isytchev@hsph.harvard.edu>
2018-03-14 14:22:30 -0400

    Remove unused import

--- refinery/file_store/models.py
+++ refinery/file_store/models.py
@@ -17,7 +17,7 @@ import urlparse
 
 from django.conf import settings
 from django.db import models
-from django.db.models.signals import post_delete, post_save
+from django.db.models.signals import post_delete
 from django.dispatch import receiver
 
 from celery.result import AsyncResult
