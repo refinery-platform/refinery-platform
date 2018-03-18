6a305e59b5ccccab96230b503a01a939dab922c2
Scott Ouellette <scottx611x@gmail.com>
2018-03-18 17:03:21 -0400

    Remove temp assignment to `client`

--- refinery/tool_manager/tasks.py
+++ refinery/tool_manager/tasks.py
@@ -7,5 +7,6 @@ from django_docker_engine.docker_utils import DockerClientWrapper
 @task()
 def django_docker_cleanup():
     # TODO: Specify manager, if not default
-    client = DockerClientWrapper(settings.DJANGO_DOCKER_ENGINE_DATA_DIR)
-    client.purge_inactive(settings.DJANGO_DOCKER_ENGINE_SECONDS_INACTIVE)
+    DockerClientWrapper().purge_inactive(
+        settings.DJANGO_DOCKER_ENGINE_SECONDS_INACTIVE
+    )
