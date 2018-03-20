20ac5dcb493a47a9c4a116f03a6b3a7c9800ade2
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2018-03-19 20:42:52 -0400

    "name" is passed in, but was not being used

--- deployment/terraform/modules/rds/main.tf
+++ deployment/terraform/modules/rds/main.tf
@@ -1,5 +1,5 @@
 resource "aws_db_subnet_group" "db_subnet_group" {
-  name       = "rds subnet group"
+  name       = "${var.name}"
   subnet_ids = [
     "${var.private_subnet_a}",
     "${var.private_subnet_b}"
