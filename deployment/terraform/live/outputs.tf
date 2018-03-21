5666dcde166754ae2d9293608eea2f07ac0be5d1
Ilya Sytchev <isytchev@hsph.harvard.edu>
2018-03-21 14:07:47 -0400

    Remove upload_bucket_name output from root and bucket_name_base output from S3 module (#2683)

--- deployment/terraform/live/outputs.tf
+++ deployment/terraform/live/outputs.tf
@@ -1,7 +1,3 @@
-output "upload_bucket_name" {
-  value = "${module.object_storage.upload_bucket_name}"
-}
-
 output "identity_pool_id" {
   value = "${module.identity_pool.identity_pool_id}"
 }
@@ -19,5 +15,5 @@ output "rds_db_subnet_group_name" {
 }
 
 output "s3_bucket_name_base" {
-  value = "${module.object_storage.bucket_name_base}"
+  value = "${local.s3_bucket_name_base}"
 }
