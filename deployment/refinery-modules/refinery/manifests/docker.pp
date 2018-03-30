6a8ca3004175d60105fbcee1f77e1d021d873d35
Scott Ouellette <scottx611x@gmail.com>
2018-03-30 13:28:57 -0400

    Attempt to set DOCKER_HOST with puppet/Facter [skip ci]

--- deployment/refinery-modules/refinery/manifests/docker.pp
+++ deployment/refinery-modules/refinery/manifests/docker.pp
@@ -2,4 +2,11 @@ class refinery::docker {
   class { '::docker':
     docker_users => [$::app_user]
   }
+  if $::deployment_platform == 'aws' {
+    # Add DOCKER_HOST on AWS
+    file_line { "docker_host":
+      path => "/home/${app_user}/.profile",
+      line => "export DOCKER_HOST=${docker_host}",
+    }
+  }
 }
