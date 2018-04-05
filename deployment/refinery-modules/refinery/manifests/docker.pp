e8f24b022f1d7869f4438001967dcf0b4ecae701
Scott Ouellette <scottx611x@gmail.com>
2018-04-05 17:14:29 -0400

    Only export DOCKER_HOST on AWS

--- deployment/refinery-modules/refinery/manifests/docker.pp
+++ deployment/refinery-modules/refinery/manifests/docker.pp
@@ -1,5 +1,15 @@
 class refinery::docker {
-  class { '::docker':
-    docker_users => [$::app_user]
+  if $::deployment_platform == 'aws' {
+    # Add env var pointing to remote docker host
+    file_line { "docker_host":
+      path => "/home/${app_user}/.profile",
+      line => "export DOCKER_HOST=${docker_host}",
+    }
+  }
+  else {
+    # Install docker when not on AWS
+    class { '::docker':
+      docker_users => [$::app_user]
+    }
   }
 }
