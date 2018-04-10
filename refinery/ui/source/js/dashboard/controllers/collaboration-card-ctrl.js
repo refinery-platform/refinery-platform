b097212e4dbe5dcd453b2495806b4a2f7685b98a
Jennifer Marx <jennyrutledge@gmail.com>
2018-04-10 11:36:21 -0400

    Add group list.

--- refinery/ui/source/js/dashboard/controllers/collaboration-card-ctrl.js
+++ refinery/ui/source/js/dashboard/controllers/collaboration-card-ctrl.js
@@ -11,14 +11,26 @@
     .module('refineryDashboard')
     .controller('CollaborationCardCtrl', CollaborationCardCtrl);
 
-  CollaborationCardCtrl.$inject = [];
+  CollaborationCardCtrl.$inject = ['groupMemberService'];
 
   function CollaborationCardCtrl (
+    groupMemberService
   ) {
+    var vm = this;
+    vm.userGroups = [];
     activate();
 
     function activate () {
       console.log('CollaborationCardCtrl-card');
+      getGroups();
+    }
+
+    // list of groups a user is a member of
+    function getGroups () {
+      groupMemberService.query().$promise.then(function (response) {
+        vm.userGroups = response.objects;
+        console.log(vm.userGroups);
+      });
     }
 
     /*
