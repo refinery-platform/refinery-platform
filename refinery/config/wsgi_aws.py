b06d7fb2840724ceb5480891969aba42406bbcdb
Scott Ouellette <scottx611x@gmail.com>
2018-04-11 14:22:50 -0400

    Set DOCKER_HOST for wsgi processes on aws

--- refinery/config/wsgi_aws.py
+++ refinery/config/wsgi_aws.py
@@ -1,8 +1,9 @@
 import os
 
+from django.conf import settings
 from django.core.wsgi import get_wsgi_application
 
 # Set env vars to be made available to the wsgi application here
 os.environ['DJANGO_SETTINGS_MODULE'] = 'config.settings.aws'
-
+os.environ['DOCKER_HOST'] = settings.DOCKER_HOST
 application = get_wsgi_application()
