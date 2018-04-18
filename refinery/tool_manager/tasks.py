80f827d1fa15409d8967401839e36828e77c84a7
Scott Ouellette <scottx611x@gmail.com>
2018-04-18 11:00:55 -0400

    Handle expected error in start container task

--- refinery/tool_manager/tasks.py
+++ refinery/tool_manager/tasks.py
@@ -2,6 +2,7 @@ from django.conf import settings
 
 from celery.task import task
 from django_docker_engine.docker_utils import DockerClientWrapper
+from docker.errors import APIError
 
 
 @task()
@@ -12,8 +13,11 @@ def django_docker_cleanup():
     )
 
 
-@task(ignore_result=True)
+@task(ignore_result=True, throws=(APIError,))
 def start_container(visualization_tool):
+    """Start a VisualizationTool's container. docker.errors.APIError is
+    expected if a VisualizationTool is launched more than once.
+    """
     visualization_tool.django_docker_client.run(
         visualization_tool.create_container_spec()
     )
