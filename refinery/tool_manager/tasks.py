1ecf7736d622c4478417b66de88711c3900067dd
Scott Ouellette <scottx611x@gmail.com>
2018-04-18 15:35:12 -0400

    Remove volumes that aren't associated with any container

--- refinery/tool_manager/tasks.py
+++ refinery/tool_manager/tasks.py
@@ -2,6 +2,7 @@ from django.conf import settings
 
 from celery.task import task
 from django_docker_engine.docker_utils import DockerClientWrapper
+import docker
 from docker.errors import APIError
 
 
@@ -11,6 +12,10 @@ def django_docker_cleanup():
     DockerClientWrapper().purge_inactive(
         settings.DJANGO_DOCKER_ENGINE_SECONDS_INACTIVE
     )
+    # Remove volumes not associated with a container.
+    # NOTE: this functionality is provided by django-docker-engine >= 0.0.52
+    client = docker.from_env()
+    client.volumes.prune()
 
 
 @task(ignore_result=True, throws=(APIError,))
