068cc096bb516514c72c8a1fc2a6f326cdd1957a
Scott Ouellette <scottx611x@gmail.com>
2018-04-20 14:34:38 -0400

    Export DOCKER_HOST in vagrant & AWS for consistency

--- deployment/refinery-modules/refinery/manifests/docker.pp
+++ deployment/refinery-modules/refinery/manifests/docker.pp
@@ -1,15 +1,19 @@
 class refinery::docker {
+  $_docker_host = "unix:///var/run/docker.sock"
+
   if $::deployment_platform == 'aws' {
-    # Add env var pointing to remote docker host
-    file_line { "docker_host":
-      path => "/home/${app_user}/.profile",
-      line => "export DOCKER_HOST=${docker_host}",
-    }
+    # Get docker host value from FACTER_DOCKER_HOST
+    $_docker_host = "${docker_host}"
   }
   else {
     # Install docker when not on AWS
     class { '::docker':
       docker_users => [$::app_user]
     }
+  }
+  # Add env var pointing to docker host
+  file_line { "docker_host":
+    path => "/home/${::app_user}/.profile",
+    line => "export DOCKER_HOST=$_docker_host",
   }
 }
