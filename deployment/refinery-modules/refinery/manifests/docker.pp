98b41bb0b98039451b9db5e8a19bb297ae76888b
Scott Ouellette <scottx611x@gmail.com>
2018-04-23 16:55:09 -0400

    Tidy docker manifest, add tcp bind to `docker_host` on vagrant deployments

--- deployment/refinery-modules/refinery/manifests/docker.pp
+++ deployment/refinery-modules/refinery/manifests/docker.pp
@@ -1,19 +1,14 @@
 class refinery::docker {
-  $_docker_host = "unix:///var/run/docker.sock"
-
-  if $::deployment_platform == 'aws' {
-    # Get docker host value from FACTER_DOCKER_HOST
-    $_docker_host = "${docker_host}"
-  }
-  else {
-    # Install docker when not on AWS
+  if $::deployment_platform != 'aws' {
     class { '::docker':
-      docker_users => [$::app_user]
+      docker_users => [$::app_user],
+      tcp_bind     => [$::docker_host],
     }
   }
+
   # Add env var pointing to docker host
   file_line { "docker_host":
     path => "/home/${::app_user}/.profile",
-    line => "export DOCKER_HOST=$_docker_host",
+    line => "export DOCKER_HOST=${::docker_host}",
   }
 }
