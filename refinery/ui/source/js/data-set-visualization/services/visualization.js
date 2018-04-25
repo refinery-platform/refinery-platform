2902b4a9f70fb959660508bd6cacf38ed62391ea
Jennifer Marx <jennyrutledge@gmail.com>
2018-04-25 09:13:34 -0400

    Fix ui bug convert utc to local.

--- refinery/ui/source/js/data-set-visualization/services/visualization.js
+++ refinery/ui/source/js/data-set-visualization/services/visualization.js
@@ -10,10 +10,11 @@
     .module('refineryDataSetVisualization')
     .factory('visualizationService', visualizationService);
 
-  visualizationService.$inject = ['_', 'timeService', 'toolsService'];
+  visualizationService.$inject = ['_', 'humanize', 'timeService', 'toolsService'];
 
   function visualizationService (
     _,
+    humanize,
     timeService,
     toolsService
   ) {
@@ -52,12 +53,17 @@
       return toolRequest.$promise;
     }
 
+    // private method to convert utc to local time unix
+    function utcToLocalUnix (utcTime) {
+      return (new Date(utcTime)) / 1000;
+    }
+
     // process responses from api
     function addHumanTime (toolList) {
       for (var j = 0; j < toolList.length; j++) {
         if (_.has(toolList[j], 'creation_date')) {
-          toolList[j].humanizeCreateTime = timeService
-            .humanizeTimeObj(toolList[j].creation_date);
+          var localTime = utcToLocalUnix(toolList[j].creation_date);
+          toolList[j].humanizeCreateTime = humanize.relativeTime(localTime);
         }
       }
       return toolList;
