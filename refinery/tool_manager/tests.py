3e5d10c0ae0658db3dc54735a1ef4b2654a8befa
Scott Ouellette <scottx611x@gmail.com>
2018-04-26 13:20:54 -0400

    Update tests to expect the new template

--- refinery/tool_manager/tests.py
+++ refinery/tool_manager/tests.py
@@ -2878,9 +2878,11 @@ class ToolAPITests(APITestCase, ToolManagerTestBase):
             get_request,
             uuid=self.tool.uuid
         )
-        self.assertEqual(get_response.status_code, 403)
-        self.assertIn("User does not have permission",
-                      get_response.content)
+        self.assertEqual(get_response.status_code, 200)
+        self.assertTemplateUsed(
+            get_response,
+            'tool_manager/vis-tool-user-not-allowed.html'
+        )
 
     def test_relaunch_failure_tool_already_running(self):
         self.create_tool(ToolDefinition.VISUALIZATION,
@@ -3005,7 +3007,11 @@ class ToolAPITests(APITestCase, ToolManagerTestBase):
         if user_has_permission:
             self.assertTrue(launch_mock.called)
         else:
-            self.assertEqual(get_response.status_code, 403)
+            self.assertEqual(get_response.status_code, 200)
+            self.assertTemplateUsed(
+                get_response,
+                'tool_manager/vis-tool-user-not-allowed.html'
+            )
             self.assertFalse(launch_mock.called)
 
     def test_vis_tool_url_after_container_removed_relaunches(self):
