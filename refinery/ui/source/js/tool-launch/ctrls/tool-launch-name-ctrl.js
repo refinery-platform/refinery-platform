4ad90d9ae99c268e2adba09825d97ff4e88872eb
Jennifer Marx <jennyrutledge@gmail.com>
2018-06-04 08:44:09 -0400

    Update tool launch name styling.

---
+++ refinery/ui/source/js/tool-launch/ctrls/tool-launch-name-ctrl.js
@@ -0,0 +1,68 @@
+/**
+ * Tool Launch Name Ctrl
+ * @namespace ToolLaunchNameCtrl
+ * @desc Component controller for launch name input
+ * @memberOf refineryApp.refineryToolLaunch
+ */
+(function () {
+  'use strict';
+
+  angular
+    .module('refineryApp')
+    .controller('ToolLaunchNameCtrl', ToolLaunchNameCtrl);
+
+  ToolLaunchNameCtrl.$inject = [
+    '$log',
+    '$scope',
+    'settings',
+    'toolParamsService',
+    'toolSelectService'
+  ];
+
+  function ToolLaunchNameCtrl (
+    $log,
+    $scope,
+    settings,
+    toolParamsService,
+    toolSelectService
+  ) {
+    var vm = this;
+    vm.form = { name: '' };
+    vm.selectedTool = {};
+    vm.toolType = '';
+    vm.updateToolLaunchName = updateToolLaunchName;
+    vm.userName = settings.djangoApp.userName;
+
+    /*
+     * ---------------------------------------------------------
+     * Methods Definitions
+     * ---------------------------------------------------------
+     */
+    /**
+    /**
+     * @name updateToolLaunchName
+     * @desc  view method which updates tool launch name when input changes
+     * @memberOf refineryApp.refineryToolLaunch.
+    **/
+    function updateToolLaunchName () {
+      toolParamsService.toolParams.display_name = vm.form.name;
+    }
+  /*
+   * ---------------------------------------------------------
+   * Watchers
+   * ---------------------------------------------------------
+   */
+    $scope.$watchCollection(
+      function () {
+        return toolSelectService.selectedTool;
+      },
+      function () {
+        if (toolSelectService.selectedTool) {
+          angular.copy(toolSelectService.selectedTool, vm.selectedTool);
+          var lCToolType = toolSelectService.selectedTool.tool_type.toLowerCase();
+          vm.toolType = lCToolType[0].toUpperCase() + lCToolType.slice(1, lCToolType.length);
+        }
+      }
+    );
+  }
+})();
