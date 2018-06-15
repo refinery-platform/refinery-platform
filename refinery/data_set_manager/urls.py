8f1bde4fe64211715c1830728663f3debc7ffbe8
Ilya Sytchev <isytchev@hsph.harvard.edu>
2018-06-15 15:56:28 -0400

    Enable back end support for removing files from Node instances

--- refinery/data_set_manager/urls.py
+++ refinery/data_set_manager/urls.py
@@ -15,7 +15,8 @@ from .views import (AddFilesToDataSetView, Assays, AssaysAttributes,
                     AssaysFiles, CheckDataFilesView,
                     ChunkedFileUploadCompleteView, ChunkedFileUploadView,
                     DataSetImportView, ImportISATabView, ProcessISATabView,
-                    ProcessMetadataTableView, TakeOwnershipOfPublicDatasetView)
+                    ProcessMetadataTableView, RemoveFileFromNodeView,
+                    TakeOwnershipOfPublicDatasetView)
 
 urlpatterns = patterns(
     'data_set_manager.views',
@@ -46,6 +47,8 @@ urlpatterns = patterns(
         name='take_ownership_of_public_dataset'),
     url(r'^add-files/$', login_required(AddFilesToDataSetView.as_view()),
         name='add_files_to_data_set'),
+    url(r'^remove-file/$', login_required(RemoveFileFromNodeView.as_view()),
+        name='remove_file_from_node'),
 )
 
 # DRF url routing
