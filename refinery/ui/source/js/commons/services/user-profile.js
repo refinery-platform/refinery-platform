a06b457e7ccc581e72c47ac6b62bedc554b0bcc3
Jennifer Marx <jennyrutledge@gmail.com>
2018-06-20 12:12:57 -0400

    Add comments and refactor for current style guide.

--- refinery/ui/source/js/commons/services/user-profile.js
+++ refinery/ui/source/js/commons/services/user-profile.js
@@ -1,21 +1,31 @@
-'use strict';
+/**
+ * User Profile V2 Service
+ * @namespace userProfileV2Service
+ * @desc Service to update user_profile fields via user_profile v2 api.
+ * @memberOf refineryApp
+ */
+(function () {
+  'use strict';
 
-angular
-  .module('refineryApp')
-  .factory('userProfileV2Service', ['$resource', 'settings',
-    function ($resource, settings) {
-      var userProfile = $resource(
-        settings.appRoot + settings.refineryApiV2 + '/user_profile/:uuid/',
-        {
-          uuid: '@uuid',
-          format: 'json'
-        },
-        {
-          partial_update: {
-            method: 'PATCH'
-          }
+  angular
+    .module('refineryApp')
+    .factory('userProfileV2Service', userProfileV2Service);
+
+  userProfileV2Service.$inject = ['$resource', 'settings'];
+
+  function userProfileV2Service ($resource, settings) {
+    var userProfile = $resource(
+      settings.appRoot + settings.refineryApiV2 + '/user_profile/:uuid/',
+      {
+        uuid: '@uuid',
+        format: 'json'
+      },
+      {
+        partial_update: {
+          method: 'PATCH'
         }
-      );
-      return userProfile;
-    }
-  ]);
+      }
+    );
+    return userProfile;
+  }
+})();
