bf85da4cce2cd4f5188cdf20a2caa4fbfc702136
Jennifer Marx <jennyrutledge@gmail.com>
2018-06-29 14:21:39 -0400

    Add unit tests for api.

--- refinery/data_set_manager/test_views.py
+++ refinery/data_set_manager/test_views.py
@@ -1,12 +1,64 @@
 import json
 import re
+import uuid
 
 from django.contrib.auth.models import User
 
-from rest_framework.test import APIClient, APIRequestFactory, APITestCase
+from rest_framework.test import (APIClient, APIRequestFactory, APITestCase,
+                                 force_authenticate)
+from factory_boy.utils import create_dataset_with_necessary_models
 
 from core.views import NodeViewSet
+
+from .views import AddFilesToDataSetView
 from .models import Assay, Investigation, Node, Study
+
+
+class AddFilesToDataSetViewTests(APITestCase):
+    def setUp(self):
+        self.username = 'guest_user'
+        self.password = User.objects.make_random_password()
+        self.user = User.objects.create_user(self.username, 'user@fake.com',
+                                             self.password)
+
+        self.factory = APIRequestFactory()
+        self.client = APIClient()
+        self.url_root = '/api/v2/data_set_manager/add-files/'
+        self.view = AddFilesToDataSetView.as_view()
+        self.client.login(username=self.username, password=self.password)
+
+        # Create Datasets
+        self.data_set = create_dataset_with_necessary_models(user=self.user)
+
+        self.post_request = self.factory.post(
+            self.url_root,
+            data={'data_set_uuid': self.data_set.uuid},
+            format="json"
+        )
+
+    def test_post_returns_404_invalid_uuid(self):
+        post_request = self.factory.post(
+            self.url_root,
+            data={'data_set_uuid': uuid.uuid4()},
+            format="json"
+        )
+        post_response = self.view(post_request)
+        self.assertEqual(post_response.status_code, 404)
+
+    def test_post_returns_403_for_non_owners(self):
+        user_lm = User.objects.create_user('lab_member',
+                                           'member@fake.com',
+                                           self.password)
+        self.post_request.user = user_lm
+        force_authenticate(self.post_request, user=user_lm)
+        post_response = self.view(self.post_request)
+        self.assertEqual(post_response.status_code, 403)
+
+    def test_post_returns_success_202_with_no_files(self):
+        self.post_request.user = self.user
+        force_authenticate(self.post_request, user=self.user)
+        post_response = self.view(self.post_request)
+        self.assertEqual(post_response.status_code, 202)
 
 
 class NodeApiV2Tests(APITestCase):
