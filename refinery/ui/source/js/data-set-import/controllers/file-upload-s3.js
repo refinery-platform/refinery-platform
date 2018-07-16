63d1293040feb3f385b7be41a01868a301338ffc
Jennifer Marx <jkmarx@users.noreply.github.com>
2018-07-16 12:19:26 -0400

    Add node-uuid for importing added files. (#2866)

--- refinery/ui/source/js/data-set-import/controllers/file-upload-s3.js
+++ refinery/ui/source/js/data-set-import/controllers/file-upload-s3.js
@@ -92,7 +92,10 @@
         $scope.$apply(function () {
           file.success = true;
           if (vm.isNodeUpdate) {
-            addFileToDataSetService.update({ data_set_uuid: $window.dataSetUuid }).$promise
+            addFileToDataSetService.update({
+              data_set_uuid: $window.dataSetUuid,
+              node_uuid: vm.nodeUuid
+            }).$promise
               .then(function () {
                 vm.addFileStatus = 'success';
               }, function () {
