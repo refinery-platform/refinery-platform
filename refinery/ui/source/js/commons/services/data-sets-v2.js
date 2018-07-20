f943f7c5a92ffe82bc89fbe6c315f465c74d0ebe
Jennifer Marx <jennyrutledge@gmail.com>
2018-07-20 11:47:56 -0400

    Fix race condition bugs, only update for current params.

--- refinery/ui/source/js/commons/services/data-sets-v2.js
+++ refinery/ui/source/js/commons/services/data-sets-v2.js
@@ -2,13 +2,27 @@
 
 angular
   .module('refineryApp')
-  .factory('dataSetV2Service', ['$resource', 'settings',
-    function ($resource, settings) {
+  .factory('dataSetV2Service', ['$resource', '_', 'settings',
+    function ($resource, _, settings) {
       var dataSets = $resource(
         settings.appRoot + settings.refineryApiV2 + '/data_sets/:uuid/',
         {
-          uuid: '@uuid',
-          format: 'json'
+          uuid: '@uuid'
+        },
+        {
+          query: {
+            method: 'GET',
+            isArray: true,
+            interceptor: {
+              response: function (response) {
+                if (!_.has(response.config, 'params')) {
+                  response.config.params = {};
+                }
+                return response; // need header to avoid race conditions
+                // when filtering, otherwise just data is returned
+              }
+            }
+          }
         },
         {
           partial_update: {
