f6f22c56ecfd926188dac348f11476c4e6ad18e7
Jennifer Marx <jennyrutledge@gmail.com>
2018-08-13 11:07:24 -0400

    Remove console.

--- refinery/ui/source/js/data-set-import/services/file-upload-status-service.js
+++ refinery/ui/source/js/data-set-import/services/file-upload-status-service.js
@@ -11,8 +11,6 @@ function fileUploadStatusService () {
    * @param { string } status - none, queuing, running
    */
   vm.setFileUploadStatus = function (status) {
-    console.log('in the set');
-    console.log(status);
     var statusOptions = ['none', 'queuing', 'running'];
     if (statusOptions.indexOf(status) > -1) {
       vm.fileUploadStatus.status = status;
