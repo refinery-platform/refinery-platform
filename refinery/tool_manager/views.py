43e82b585209164edeb2e9ab54ba303f43db2217
Scott Ouellette <scottx611x@gmail.com>
2018-09-06 13:31:20 -0400

    Utilize `else` in try/except & move logger; A tool.launch() could fail and a tool wouldn't be created due to the atomic transaction. Move the logger afterwards

--- refinery/tool_manager/views.py
+++ refinery/tool_manager/views.py
@@ -158,13 +158,14 @@ class ToolsViewSet(ToolManagerViewSetBase):
             try:
                 with transaction.atomic():
                     tool = create_tool(tool_launch_configuration, request.user)
-                    logger.debug("Successfully created Tool: %s", tool.name)
                     tool.launch()
                     serializer = ToolSerializer(tool)
-                    return JsonResponse(serializer.data)
             except Exception as e:
                 logger.error(e)
                 return HttpResponseBadRequest(e)
+            else:
+                logger.debug("Successfully created Tool: %s", tool.name)
+                return JsonResponse(serializer.data)
 
     @detail_route(methods=['get'])
     def relaunch(self, request, *args, **kwargs):
