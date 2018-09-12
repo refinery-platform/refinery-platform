449cde1de4292614c367953e6d9bbfc82c7967f8
Jennifer Marx <jennyrutledge@gmail.com>
2018-09-12 15:15:12 -0400

    Remove str cast.

--- refinery/core/models.py
+++ refinery/core/models.py
@@ -515,9 +515,9 @@ class DataSet(SharableResource):
 
     def __unicode__(self):
         return (
-            unicode(self.name) + u' - ' +
-            unicode(self.get_owner_username()) + u' - ' +
-            unicode(self.summary)
+            self.name + ' - ' +
+            self.get_owner_username() + ' - ' +
+            self.summary
         )
 
     def save(self, *args, **kwargs):
@@ -2344,9 +2344,8 @@ class Event(models.Model):
         )
 
     def render_data_set_create(self):
-        return '{:%x %X}: {} created data set {}'.format(
-            self.date_time, self.user, self.data_set.name
-        )
+        return u'{:%x %X}: {} created data set'.format(
+            self.date_time, self.user) + self.data_set.name
 
     # Sub-types for data sets:
     PERMISSIONS_CHANGE = 'PERMISSIONS_CHANGE'
