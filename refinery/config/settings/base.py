79140363d3392375e33369dfc17f08d6b966915c
Scott Ouellette <scottx611x@gmail.com>
2018-09-19 13:12:56 -0400

    Move reCAPTCHA settings to more reasonable locations

--- refinery/config/settings/base.py
+++ refinery/config/settings/base.py
@@ -644,15 +644,3 @@ MIGRATION_MODULES = {
 }
 REFINERY_VISUALIZATION_REGISTRY = \
     "https://github.com/refinery-platform/visualization-tools/"
-
-# The default GOOGLE_RECAPTCHA values below are only valid for testing/dev
-# environments (They don't actually validate registering users)
-# One will need to follow the instructions here: []() to get proper API keys.
-GOOGLE_RECAPTCHA_SITE_KEY = get_setting(
-    "GOOGLE_RECAPTCHA_SITE_KEY",
-    default="6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI"
-)
-GOOGLE_RECAPTCHA_SECRET_KEY = get_setting(
-    "GOOGLE_RECAPTCHA_SECRET_KEY",
-    default="6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe"
-)
--- refinery/config/settings/prod.py
+++ refinery/config/settings/prod.py
@@ -5,3 +5,6 @@ from .base import *  # NOQA
 DEBUG = False
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+
+GOOGLE_RECAPTCHA_SITE_KEY = get_setting("GOOGLE_RECAPTCHA_SITE_KEY")
+GOOGLE_RECAPTCHA_SECRET_KEY = get_setting("GOOGLE_RECAPTCHA_SECRET_KEY")
