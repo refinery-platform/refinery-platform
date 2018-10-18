38ceefae0b5c57df59e4297348f25d42dccf81a7
Ilya Sytchev <isytchev@hsph.harvard.edu>
2018-10-17 23:36:23 -0400

    Add RDS config to Puppet

--- deployment/refinery-modules/refinery/manifests/postgresql.pp
+++ deployment/refinery-modules/refinery/manifests/postgresql.pp
@@ -1,15 +1,47 @@
 class refinery::postgresql {
+  $server_version = '9.3'
 
   if $::deployment_platform == 'aws' {
-    # puppetlabs-postgresql doesn't provide uninstallation functionality
-    # https://forge.puppet.com/puppetlabs/postgresql/changelog#2014-09-03---supported-release-400
-    package { 'postgresql-9.3':
-      ensure => 'absent',
+    $django_role = 'refinery'
+    $rds_settings = {
+      'PGUSER'     => 'root',
+      'PGPASSWORD' => $::rds_superuser_password,
+      'PGHOST'     => $::rds_endpoint_address,
+      'PGPORT'     => '5432',
+    }
+    class { '::postgresql::globals':
+      version                  => $server_version,
+      default_connect_settings => $rds_settings,
+    }
+    class { '::postgresql::server':
+      encoding             => 'UTF8',
+      locale               => 'en_US.UTF8',
+      manage_pg_hba_conf   => false,
+      manage_pg_ident_conf => false,
+      manage_recovery_conf => false,
+      service_manage       => false,
+    }
+    postgresql_psql { $django_role:
+      # cannot use postgresql::server::role due to a bug
+      # https://tickets.puppetlabs.com/browse/MODULES-5068
+      command     => "CREATE ROLE ${django_role} LOGIN PASSWORD 'password'",
+      db          => 'postgres',
+      environment => join_keys_to_values($rds_settings, '='),
+      unless      => "SELECT 1 FROM pg_roles WHERE rolname = '${django_role}'",
+    }
+    ::postgresql::server::database { 'refinery':
+      owner => $django_role,
+    }
+    ->
+    package { 'postgresql-common':
+      # local server is required for RDS configuration only due to a bug
+      # https://tickets.puppetlabs.com/browse/MODULES-5069
+      ensure => purged,
     }
   }
   else {
     class { '::postgresql::globals':
-      version  => '9.3',
+      version  => $server_version,
       encoding => 'UTF8',
       locale   => 'en_US.utf8',
     }
