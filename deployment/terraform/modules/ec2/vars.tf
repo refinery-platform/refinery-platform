9f3e3897786217a3bfe55d5c596c91a79374c00e
Ilya Sytchev <isytchev@hsph.harvard.edu>
2018-10-26 11:22:09 -0400

    Add comment about security_group_id variable for app server instance

--- deployment/terraform/modules/ec2/vars.tf
+++ deployment/terraform/modules/ec2/vars.tf
@@ -5,6 +5,7 @@ variable "media_bucket_name" {}
 variable "identity_pool_id" {}
 variable "instance_type" {}
 variable "key_pair_name" {}
+# cannot pass a list of IDs due to a bug: https://github.com/hashicorp/terraform/issues/13103
 variable "security_group_id" {}
 variable "subnet_id" {}
 variable "tags" {
