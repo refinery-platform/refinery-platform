a48c8fceb3df371524c671828887e2f8a49309b4
Scott Ouellette <scottx611x@gmail.com>
2018-10-29 15:17:54 -0400

    Replace `rds_engine_version` variable with hardcoded value

--- deployment/terraform/modules/rds/vars.tf
+++ deployment/terraform/modules/rds/vars.tf
@@ -7,7 +7,6 @@ variable "tags" {
 }
 variable "app_server_security_group_id" {}
 variable "availability_zone" {}
-variable "rds_engine_version" {}
 variable "snapshot_id" {}
 variable "master_user_password" {}
 variable "vpc_id" {}
