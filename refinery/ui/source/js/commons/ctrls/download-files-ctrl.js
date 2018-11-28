a8bde3e7cf0d28da9734713a4b57a66859950eb8
Scott Ouellette <scottx611x@gmail.com>
2018-11-28 16:47:44 -0500

    Tidy DownloadFilesCtrl and add test coverage

--- refinery/ui/source/js/commons/ctrls/download-files-ctrl.js
+++ refinery/ui/source/js/commons/ctrls/download-files-ctrl.js
@@ -1,3 +1,10 @@
+/**
+ * Download Files Ctrl
+ * @namespace DownloadFilesCtrl
+ * @desc Controller responsible for providing a valid cURL command based
+ * off of a User's currently filtered file selections in a FileBrowser.
+ * @memberOf refineryApp
+ */
 (function () {
   'use strict';
 
@@ -27,22 +34,26 @@
       authTokenService
   ) {
     var vm = this;
-    activate();
+    fetchAuthToken();
 
-    function activate () {
+    function fetchAuthToken () {
       authTokenService.query().$promise.then(function (authToken) {
         vm.authToken = authToken.token;
       });
     }
 
-    vm.downloadCsvUrl = function () {
-      return '\'' + $location.protocol() + '://' + $location.host() + ':' + $location.port() + vm.downloadCsvPath + '\'';
-    };
-
+    /**
+     * @name downloadCsvCurl
+     * @desc  VM method used to construct the cURL command that allows users
+     * to download their files outside of Refinery
+     * @memberOf DownloadFilesCtrl
+    **/
     vm.downloadCsvCurl = function (downloadCsvQuery) {
-      vm.downloadCsvPath = '/files_download?' + downloadCsvQuery;
-      return 'curl ' + vm.downloadCsvUrl() + ' -H \"Authorization:' +
-        '  Token ' + vm.authToken + '\" | cut -f 1 -d \',\' | tail -n +2 | xargs -n 1 curl -O -L';
+      return 'curl ' +
+        '\'' + $location.protocol() + '://' + $location.host() + ':' + $location.port() +
+        '/files_download?' + downloadCsvQuery + '\'' +
+        ' -H \"Authorization:  Token ' + vm.authToken +
+        '\" | cut -f 1 -d \',\' | tail -n +2 | xargs -n 1 curl -O -L';
     };
   }
 })();
