28218370f3764bb958162170b2a91072238c2f01
Scott Ouellette <scottx611x@gmail.com>
2018-11-28 11:29:51 -0500

    Refactor AuthTokenService to be inline with style guide

--- refinery/ui/source/js/commons/services/auth-token.js
+++ refinery/ui/source/js/commons/services/auth-token.js
@@ -1,18 +1,20 @@
-'use strict';
+(function () {
+  'use strict';
+  angular
+    .module('refineryApp')
+    .factory('authTokenService', authTokenService);
 
-angular
-  .module('refineryApp')
-  .factory('authTokenService', ['$resource', 'settings',
-    function ($resource, settings) {
-      return $resource(
-        settings.appRoot + settings.refineryApiV2 + '/obtain-auth-token/',
-        {},
-        {
-          query: {
-            method: 'GET',
-            isArray: false
-          }
-        }
-      );
-    }
-  ]);
+  authTokenService.$inject = ['$resource', 'settings'];
+
+  function authTokenService ($resource, settings) {
+    var authToken = $resource(
+      settings.appRoot + settings.refineryApiV2 + '/obtain-auth-token/',
+      {},
+      {
+        query: {}
+      }
+    );
+    return authToken;
+  }
+})();
+
