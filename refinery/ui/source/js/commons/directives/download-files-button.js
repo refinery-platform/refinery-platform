eb957f8c31aec5d2a96f09ec707c323c4708f9ac
Scott Ouellette <5629547+scottx611x@users.noreply.github.com>
2018-11-30 16:00:07 -0500

    Scottx611x/file browser csv download (#3106)
    * Add `ui.grid.exporter`
    
    * Implement `FileBrowserCtrl.downloadCSV()`
    
    * Add buttons to download a .csv of all/only currently visible row/column entries
    
    * Add ui-grid-exporter here as well
    
    * Suppress `Input Groups` column from csv export
    
    * Simplify downloadCSV logic
    
    * Show button to download csv of filtered dataset content only if filtering has occured
    
    * Update isFiltered logic
    
    * Move .csv download button into its own Angular component
    
    * Remove extraneous `totalRows` I introduced
    
    * Simplify `Download CSV` button HTML
    
    * Add APIv2 endpoint to allow for authenticated users to obtain a DRF API Token;
    The User Files feature that provides a `curl` command to download the current file selection only provides access to public data after 03df813b8aec42c6b6671b2243dbede4629bc4ca was introduced
    
    * Update the `user_files_csv` view to leverage DRF TokenAuthentication
    
    * Revert to a regular `directive` since a `component` wasn't behaving the same way
    
    * Update user_files_csv LiveServerTestCase to utilize token auth
    
    * Remove `dropdown-menu-right` class I introduced
    
    * Add service for `obtain-auth-token` endpoint
    
    * Add header utilizing a user's DRF API auth token to the curl command we provide them with
    
    * Add SessionAuthentication to user_file_csv
    
    * Update `downloadCsvQuery` to utilize `facet_filter/filter_attribute` handling introduced in: 013475309297269dfd930eb0a6fc00a8f892dc3c
    
    * Simplify user_files_csv tests
    
    * Factor out download files functionality from the UserfileBrowser for reuse in the FileBrowser
    
    * Fix import order
    
    * Store absolute urls to datafiles in Solr;
    The `curl` command spit out by each file browser cannot resolve relative urls
    
    * Remove separate controller, partial and directive for the download files modal
    
    * Add reusable components to commons for csv and file downloading
    
    * Update FileBrowser and UserFiles controllers to fulfill: downloadCsv & downloadCsvQuery
    
    * Pass controller information into the components
    
    * Remove text transformation (uppercasing) inherited by the download files modal when added to the FileBrowser
    
    * Update dependancy ordering
    
    * Use $onInit() to set vm.authToken
    
    * Use uib-popover directive since there is no html in the string
    
    * Update .csv download popover text
    
    * Separate download-files-modal into its own component
    
    * Refactor AuthTokenService to be inline with style guide
    
    * Wrap authToken fetching in activate()
    
    * Use mocker service to generate fake uuid
    
    * Update ordering
    
    * Add padding to download files button
    
    * Tidy DownloadFilesCtrl and add test coverage
    
    * Update import ordering
    
    * Add test coverage for `/files_download` when it is called from the FileBrowser;
    The FileBrowser will send over an assay_uuid along with its solr query since its dealing with a single DataSet at a time
    
    * Begin adding unit tests for File Browser CSV export functionality
    
    * Add test coverage for `isFiltered` and `setCsvFilename` and `downloadCsv`
    
    * Add test coverage for downloadCsvQuery
    
    * Add docstrings
    
    * Add test coverage for UserFileBrowserFilesCtrl
    
    * Add test coverage for common components
    
    * Add comments for each portion of Download Files curl command and exclude files that aren't available from being downloaded
    
    * Update DownloadFilesCtrl unit test
    
    * Update docstring [skip ci]
    
    * Fix UserFileBrowserFilesCtrl.downloadCsv and update tests
    
    * Wrap with IIFE
    
    * Add jsdoc
    
    * Group unit tests by method being tested

---
+++ refinery/ui/source/js/commons/directives/download-files-button.js
@@ -0,0 +1,16 @@
+(function () {
+  'use strict';
+  angular
+    .module('refineryApp')
+    .component('rpDownloadFilesButton', {
+      templateUrl: ['$window', function ($window) {
+        return $window.getStaticUrl(
+            'partials/commons/partials/download-files-button.html'
+        );
+      }],
+      bindings: {
+        modalInstance: '<',
+        resolve: '<',
+      },
+    });
+})();
--- refinery/user_files_manager/tests.py
+++ refinery/user_files_manager/tests.py
@@ -2,15 +2,18 @@ import json
 import logging
 from urlparse import urljoin
 
+import uuid
+from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from django.http import QueryDict
-from django.test import RequestFactory, TestCase, override_settings
+from django.test import TestCase, override_settings
 
 from guardian.utils import get_anonymous_user
 import mock
 import requests
+from rest_framework.authtoken.models import Token
 from rest_framework.test import (APIRequestFactory, APITestCase,
                                  force_authenticate)
 
@@ -43,44 +46,23 @@ class UserFilesAPITests(APITestCase):
             'nodes_count'
         ])
 
+    def _test_user_files_csv(self, assay_uuid=False, use_token_auth=False):
+        solr_mock_reference = 'search_solr' if assay_uuid else '_get_solr'
+        get_data = {"assay_uuid": uuid.uuid4()} if assay_uuid else {}
 
-class UserFilesUITests(StaticLiveServerTestCase):
-    def setUp(self):
-        # recommended solution to an auth_permission error, though doc says
-        # we probably won't need to call it since django will call it
-        # automatically when needed
-        ContentType.objects.clear_cache()
-
-    def test_ui(self):
-        response = requests.get(
-            urljoin(
-                self.live_server_url,
-                'files/'
-            )
-        )
-        self.assertIn("All Files", response.content)
-
-    @mock.patch('django.conf.settings.USER_FILES_COLUMNS', 'name,fake')
-    def test_csv(self):
-        response = requests.get(
-            urljoin(
-                self.live_server_url,
-                'files_download'
-            )
-        )
-        self.assertEqual(
-            response.content,
-            'url,name,fake\r\n'
+        request = self.factory.get(
+            "/files_download?fq=&limit=100000000&sort=", get_data
         )
 
+        user = User.objects.create_user(
+            'testuser', 'test@example.com', 'password'
+        )
+        if use_token_auth:
+            Token.objects.create(user=user)
+            force_authenticate(request, user=user, token=user.auth_token)
+        else:
+            force_authenticate(request, user=user)
 
-class UserFilesViewTests(TestCase):
-
-    @mock.patch('django.conf.settings.USER_FILES_COLUMNS', 'filename,fake')
-    def test_user_files_csv(self):
-        request = RequestFactory().get('/fake-url')
-        request.user = User.objects.create_user(
-            'testuser', 'test@example.com', 'password')
         mock_doc = {
             NodeIndex.DOWNLOAD_URL:
                 'fake-url',
@@ -92,12 +74,12 @@ class UserFilesViewTests(TestCase):
             # Doesn't matter if the names are backwards.
         }
         with mock.patch(
-            'user_files_manager.views._get_solr',
-            return_value=json.dumps({
-                'response': {
-                    'docs': [mock_doc]
-                }
-            })
+                'user_files_manager.views.' + solr_mock_reference,
+                return_value=json.dumps({
+                    'response': {
+                        'docs': [mock_doc]
+                    }
+                })
         ):
             response = user_files_csv(request)
             self.assertEqual(
@@ -105,6 +87,63 @@ class UserFilesViewTests(TestCase):
                 'url,filename,fake\r\n'
                 'fake-url,fake-filename,\r\n'
             )
+
+    @override_settings(USER_FILES_COLUMNS='filename,fake')
+    def test_get_user_files_csv_with_token_auth(self):
+        self._test_user_files_csv(use_token_auth=True)
+
+    @override_settings(USER_FILES_COLUMNS='filename,fake')
+    def test_get_user_files_csv_with_session_auth(self):
+        self._test_user_files_csv()
+
+    @override_settings(USER_FILES_COLUMNS='filename,fake')
+    def test_get_user_files_csv_without_auth(self):
+        request = self.factory.get("/files_download?fq=&limit=100000000&sort=")
+        User.objects.create_user('testuser', 'test@example.com', 'password')
+        response = user_files_csv(request)
+        response.render()
+        self.assertEqual(response.status_code, 403)
+
+    @override_settings(USER_FILES_COLUMNS='filename,fake')
+    def test_get_user_files_csv_with_assay_uuid(self):
+        self._test_user_files_csv(assay_uuid=True)
+
+
+class UserFilesUITests(StaticLiveServerTestCase):
+    def setUp(self):
+        # recommended solution to an auth_permission error, though doc says
+        # we probably won't need to call it since django will call it
+        # automatically when needed
+        ContentType.objects.clear_cache()
+
+        self.factory = APIRequestFactory()
+        self.view = user_files_csv
+
+    def test_ui(self):
+        response = requests.get(
+            urljoin(
+                self.live_server_url,
+                'files/'
+            )
+        )
+        self.assertIn("All Files", response.content)
+
+    @override_settings(USER_FILES_COLUMNS='name,fake')
+    def test_csv(self):
+        password = make_password("password")
+        user = User.objects.create_user(
+            'testuser', 'test@example.com', password
+        )
+        Token.objects.create(user=user)
+        request = self.factory.get(
+            urljoin(self.live_server_url, 'files_download')
+        )
+        force_authenticate(request, user=user, token=user.auth_token)
+        response = self.view(request)
+        self.assertEqual(
+            response.content,
+            'url,name,fake\r\n'
+        )
 
 
 class UserFilesUtilsTests(TestCase):
