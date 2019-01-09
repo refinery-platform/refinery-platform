f8f8fcbcee7b8c84bf74a85f1191ebba79201b6e
Scott Ouellette <scottx611x@gmail.com>
2019-01-09 13:52:23 -0500

    Add test coverage for ISAToolsJSONCreator._create_sources

--- refinery/data_set_manager/test_utils.py
+++ refinery/data_set_manager/test_utils.py
@@ -323,6 +323,18 @@ class ISAToolsJSONCreatorTests(MetadataImportTestBase):
             ),
         )
 
+    def test__create_sources(self):
+        self.assertEqual(
+            ordered(
+                self.isa_tools_json_creator._create_sources(
+                    self.isa_tools_json_creator.studies.first()
+                )
+            ),
+            ordered(
+                self.expected_isa_json["studies"][0]["materials"]["sources"]
+            ),
+        )
+
         )
 
     def test_isa_tab_based_datasets_supported_only(self):
