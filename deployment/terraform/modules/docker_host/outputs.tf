1b28c59356f9c0b440dcf2c68c0b7fa87d311381
Ilya Sytchev <isytchev@hsph.harvard.edu>
2019-01-10 20:03:08 -0500

    Add back port to Docket hostname output

--- deployment/terraform/modules/docker_host/outputs.tf
+++ deployment/terraform/modules/docker_host/outputs.tf
@@ -1,5 +1,5 @@
 output "docker_hostname" {
   # workaround for resource not found error when count for that resource is 0
   # https://github.com/hashicorp/terraform/issues/16681
-  value = "${element(concat(aws_instance.docker_host.*.private_ip, list("")), 0)}"
+  value = "tcp://${element(concat(aws_instance.docker_host.*.private_ip, list("")), 0)}:${var.docker_tcp_port}"
 }
