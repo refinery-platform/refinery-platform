de7276aa4071235004fec18fcf9a5fb13a187c58
Ilya Sytchev <isytchev@hsph.harvard.edu>
2019-01-11 10:53:01 -0500

    Make git-info.sh code more robust
    https://github.com/koalaman/shellcheck/wiki/SC2006
    https://github.com/koalaman/shellcheck/wiki/SC2046

--- deployment/terraform/live/git-info.sh
+++ deployment/terraform/live/git-info.sh
@@ -3,4 +3,4 @@
 # used by Terraform to obtain the current Git commit for deployment on AWS
 
 # produce a JSON object containing the commit value
-jq -n --arg commit `git rev-parse HEAD` --arg email `git config --get user.email` '{"commit": $commit, "email": $email}'
+jq -n --arg commit "$(git rev-parse HEAD)" --arg email "$(git config --get user.email)" '{"commit": $commit, "email": $email}'
