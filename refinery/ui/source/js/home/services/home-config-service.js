aeb62933d76fe0f63185f3b813c946d9190957c6
Jennifer Marx <jkmarx@users.noreply.github.com>
2019-01-31 10:27:12 -0500

    Jkmarx/update homepage services (#3184)
    * Update home ui to use service.
    
    * Refactor to improve markdown to html.
    
    * Add unit tests.
    
    * Fix comment.
    
    * Fix bug related to missing properties.
    
    * Fix unit test.

---
+++ refinery/ui/source/js/home/services/home-config-service.js
@@ -0,0 +1,53 @@
+/**
+ * Home Config Service
+ * @namespace homeConfigService
+ * @desc Service which gets site profile configs
+ * @memberOf refineryApp.refineryHome
+ */
+(function () {
+  'use strict';
+  angular
+    .module('refineryHome')
+    .factory('homeConfigService', homeConfigService);
+
+  homeConfigService.$inject = [
+    'siteProfileService'
+  ];
+
+  function homeConfigService (siteProfileService) {
+    var homeConfig = {
+      aboutMarkdown: '',
+      introMarkdown: '',
+      siteVideos: [],
+      twitterUsername: '',
+    };
+
+    var service = {
+      getConfigs: getConfigs,
+      homeConfig: homeConfig
+    };
+
+    return service;
+
+    /*
+    *----------------------------
+    * Method Definitions
+    * ---------------------------
+    */
+    /**
+     * @name getConfigs
+     * @desc Grab and store site profile configs for the homepage's text
+     * @memberOf refineryHome.getConfigs
+    **/
+    function getConfigs () {
+      var configs = siteProfileService.query();
+      configs.$promise.then(function (response) {
+        homeConfig.aboutMarkdown = response.about_markdown;
+        homeConfig.introMarkdown = response.intro_markdown;
+        homeConfig.twitter_username = response.twitter_username;
+        angular.copy(response.site_videos, homeConfig.siteVideos);
+      });
+      return configs.$promise;
+    }
+  }
+})();
