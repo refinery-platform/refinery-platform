f6eccfde44995a9b74d71f1fbb0e031eca1dddea
Jennifer Marx <jkmarx@users.noreply.github.com>
2019-03-15 18:14:56 -0400

    Jkmarx/remove attribute investigation publication resource (#3260)
    * Remove publication resource.
    
    * Remove investigation resource.
    
    * Remove attribute resource.

--- refinery/config/urls.py
+++ refinery/config/urls.py
@@ -18,8 +18,6 @@ from core.models import AuthenticationFormUsernameOrEmail
 from core.urls import core_router
 from core.utils import verify_recaptcha
 from core.views import CustomRegistrationView
-from data_set_manager.api import (AttributeResource, InvestigationResource,
-                                  PublicationResource)
 from data_set_manager.urls import data_set_manager_router
 from file_store.urls import file_store_router
 from tool_manager.urls import django_docker_engine_url, tool_manager_router
@@ -37,9 +35,6 @@ v1_api.register(WorkflowResource())
 v1_api.register(GroupManagementResource())
 v1_api.register(UserAuthenticationResource())
 v1_api.register(InvitationResource())
-v1_api.register(InvestigationResource())
-v1_api.register(PublicationResource())
-v1_api.register(AttributeResource())
 v1_api.register(ExtendedGroupResource())
 v1_api.register(UserProfileResource())
 
