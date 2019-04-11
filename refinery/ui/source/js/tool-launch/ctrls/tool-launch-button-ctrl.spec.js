33f3b8f01f3e0e7992631fcb5eab99e6f61d78c1
Jennifer Marx <jennyrutledge@gmail.com>
2019-04-11 09:41:06 -0400

    Fix bug with incorrect logic.

--- refinery/ui/source/js/tool-launch/ctrls/tool-launch-button-ctrl.spec.js
+++ refinery/ui/source/js/tool-launch/ctrls/tool-launch-button-ctrl.spec.js
@@ -3,18 +3,21 @@
 
   describe('Controller: Tool Launch Button Ctrl', function () {
     var ctrl;
+    var refinerySettings;
     var scope;
 
     beforeEach(module('refineryApp'));
     beforeEach(module('refineryToolLaunch'));
     beforeEach(inject(function (
       $rootScope,
-      $controller
+      $controller,
+      settings
     ) {
       scope = $rootScope.$new();
       ctrl = $controller('ToolLaunchButtonCtrl', {
         $scope: scope
       });
+      refinerySettings = settings;
     }));
 
     it('Tool Info Display ctrl should exist', function () {
@@ -27,6 +30,16 @@
 
     it('Validation method should exist for views', function () {
       expect(angular.isFunction(ctrl.needMoreNodes)).toBe(true);
+    });
+
+    it('Sets user is anonymous as true when undefined', function () {
+      expect(ctrl.userIsAnonymous).toBe(true);
+    });
+    it('Sets user is anonymous as false when updating', function () {
+      ctrl.$onInit();
+      refinerySettings.djangoApp.userId = 5;
+      scope.$apply();
+      expect(ctrl.userIsAnonymous).toBe(false);
     });
   });
 })();
