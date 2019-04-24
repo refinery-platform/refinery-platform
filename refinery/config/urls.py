b77014204e097ba9ba7467c2d2fd1963bb7b3170
Ilya Sytchev <isytchev@hsph.harvard.edu>
2019-04-24 17:49:20 -0400

    First pass

--- refinery/config/urls.py
+++ refinery/config/urls.py
@@ -11,7 +11,7 @@ from registration.backends.default import (views as registration_views,
 
 from core.forms import RegistrationFormWithCustomFields
 from core.models import AuthenticationFormUsernameOrEmail
-from core.urls import core_router
+from core.urls import core_api_urls
 from core.utils import verify_recaptcha
 from core.views import CustomRegistrationView, user_profile, user_profile_edit
 from data_set_manager.urls import data_set_manager_router
@@ -61,14 +61,15 @@ urlpatterns = [
 # RouterCombiner.extend(<router instance>) to include DRF Routers defined in
 # other apps urls.py files
 router = utils.RouterCombiner()
-router.extend(core_router)
+# router.extend(core_router)
 router.extend(data_set_manager_router)
 router.extend(file_store_router)
 router.extend(tool_manager_router)
 router.extend(user_files_router)
 # Wire up our DRF APIs using automatic URL routing
 urlpatterns += [
-    url(r"^api/v2/", include(router.urls)),
+    url(r'^api/v2/', include(router.urls)),
+    url(r'^api/v2/', include(core_api_urls)),
 ]
 
 # for using DjDT with mod_wsgi
