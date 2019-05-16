9850dbfb3348df0fb5c94c81144ecd16da4a558a
Ilya Sytchev <isytchev@hsph.harvard.edu>
2019-05-16 16:37:55 -0400

    Simplify database migration

--- refinery/core/migrations/0033_userprofile_uuid.py
+++ refinery/core/migrations/0033_userprofile_uuid.py
@@ -20,19 +20,15 @@ class Migration(migrations.Migration):
             name='temp_uuid',
             field=models.UUIDField(null=True),
         ),
-        # copy data to the new field
-        migrations.RunSQL(
-            "UPDATE core_userprofile SET temp_uuid = CAST (uuid AS uuid)",
-            migrations.RunSQL.noop
-        ),
         # to allow backward migrations after removing this field
         migrations.AlterField(
             model_name='userprofile',
             name='uuid',
             field=django_extensions.db.fields.UUIDField(null=True),
         ),
+        # copy data to the new field
         migrations.RunSQL(
-            migrations.RunSQL.noop,
+            "UPDATE core_userprofile SET temp_uuid = CAST (uuid AS uuid)",
             "UPDATE core_userprofile SET uuid = temp_uuid"
         ),
         migrations.RemoveField(
