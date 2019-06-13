72aff5523a331dedb4ada614bd717fe6859b88e3
Jennifer Marx <jennyrutledge@gmail.com>
2019-06-13 15:17:51 -0400

    Update dependencies due to compatibility.

--- deployment/bootstrap.sh
+++ deployment/bootstrap.sh
@@ -12,11 +12,18 @@ set -x
 /usr/bin/apt-get -qq update
 /usr/bin/apt-get -y autoremove
 
-/usr/bin/apt-get -q -y install git htop nmon puppet ruby-dev tree
+/usr/bin/apt-get -q -y install git htop nmon ruby-dev tree
 
-/usr/bin/gem install librarian-puppet -v 2.2.3 --no-rdoc --no-ri
+/usr/bin/wget https://apt.puppetlabs.com/puppet5-release-xenial.deb
+/usr/bin/dpkg -i puppet5-release-xenial.deb
+/usr/bin/apt-get update
+/usr/bin/apt-get install puppet-agent
+
+export PATH=/opt/puppetlabs/bin:$PATH
+
+/usr/bin/gem install librarian-puppet -v 3.0.0 --no-rdoc --no-ri
 
 cd /vagrant/deployment/puppet
-librarian-puppet config path /usr/share/puppet/modules --local
+librarian-puppet config path /opt/puppetlabs/puppet/modules --local
 librarian-puppet config tmp /tmp --local
-librarian-puppet install
+librarian-puppet install --verbose
