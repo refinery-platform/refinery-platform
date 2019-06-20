fb0c53bb29073ed991e54675efac9f90f01d5ad6
Ilya Sytchev <isytchev@hsph.harvard.edu>
2019-06-20 18:11:32 -0400

    Update bootstrap shell script

--- deployment/bootstrap.sh
+++ deployment/bootstrap.sh
@@ -8,15 +8,14 @@ export DEBIAN_FRONTEND=noninteractive
 # print commands and their expanded arguments
 set -x
 
-/usr/bin/apt-get clean
-/usr/bin/wget -q https://apt.puppetlabs.com/puppet5-release-xenial.deb
-/usr/bin/dpkg -i puppet5-release-xenial.deb
+/usr/bin/wget -q -P /var/cache/apt/archives https://apt.puppetlabs.com/puppet5-release-xenial.deb
+/usr/bin/dpkg -i /var/cache/apt/archives/puppet5-release-xenial.deb
 /usr/bin/apt-get -qq update
-/usr/bin/apt-get -y autoremove
-
 /usr/bin/apt-get -q -y install git htop nmon puppet-agent ruby-dev tree
+/usr/bin/apt-get -y autoremove
+/usr/bin/apt-get clean
 
-export PATH=/opt/puppetlabs/bin:$PATH
+PATH=/opt/puppetlabs/bin:$PATH
 
 /usr/bin/gem install librarian-puppet -v 3.0.0 --no-rdoc --no-ri
 
