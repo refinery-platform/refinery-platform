f73474cbcb5b014de99d2267f059570702091485
Ilya Sytchev <isytchev@hsph.harvard.edu>
2019-06-21 13:50:07 -0400

    Migrate Assay.uuid field to native Django UUIDField

---
+++ refinery/data_set_manager/migrations/0008_assay_uuid.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import uuid
+
+from django.db import migrations, models
+
+import django_extensions
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('data_set_manager', '0007_remove_attribute_definition'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='assay',
+            name='temp_uuid',
+            field=models.UUIDField(null=True),
+        ),
+        # to allow backward migrations after removing this field
+        migrations.AlterField(
+            model_name='assay',
+            name='uuid',
+            field=django_extensions.db.fields.UUIDField(null=True),
+        ),
+        # copy data to the new field
+        migrations.RunSQL(
+            "UPDATE data_set_manager_assay SET temp_uuid = CAST (uuid AS uuid)",
+            "UPDATE data_set_manager_assay SET uuid = temp_uuid"
+        ),
+        migrations.RemoveField(
+            model_name='assay',
+            name='uuid',
+        ),
+        migrations.RenameField(
+            model_name='assay',
+            old_name='temp_uuid',
+            new_name='uuid',
+        ),
+        migrations.AlterField(
+            model_name='assay',
+            name='uuid',
+            field=models.UUIDField(default=uuid.uuid4, unique=True, editable=False),
+        ),
+    ]
