9cc683446c78f2204c253c24b87f3f0c795e69bd
Ilya Sytchev <isytchev@hsph.harvard.edu>
2019-07-12 14:20:01 -0400

    Update galaxy_import_task_group_id migrations

--- refinery/analysis_manager/migrations/0001_initial.py
+++ refinery/analysis_manager/migrations/0001_initial.py
@@ -17,7 +17,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('refinery_import_task_group_id', models.CharField(max_length=36, null=True, editable=False, blank=True)),
-                ('galaxy_import_task_group_id', django_extensions.db.fields.UUIDField(max_length=36, null=True, editable=False, blank=True)),
+                ('galaxy_import_task_group_id', models.CharField(max_length=36, null=True, editable=False, blank=True)),
                 ('galaxy_export_task_group_id', django_extensions.db.fields.UUIDField(max_length=36, null=True, editable=False, blank=True)),
                 ('galaxy_history_state', models.CharField(blank=True, max_length=10, choices=[(b'SUCCESS', b'OK'), (b'FAILURE', b'Error'), (b'PROGRESS', b'Running'), (b'PENDING', b'Unknown')])),
                 ('galaxy_history_progress', models.PositiveSmallIntegerField(null=True, blank=True)),
