c043529a855d306cb4a14f592f8596c30090b876
ilan-gold <ilanbassgold@gmail.com>
2019-09-24 12:27:13 -0400

    revert travis debugging

--- refinery/analysis_manager/models.py
+++ refinery/analysis_manager/models.py
@@ -1,5 +1,4 @@
 import logging
-import os
 
 from django.db import models
 from django.db.models.fields import CharField, PositiveSmallIntegerField
@@ -111,11 +110,6 @@ def get_task_group_state(task_group_id):
     """return a list containing states of all tasks given a task set ID"""
     task_group_state = []
     percent_done = 0
-    from celery._state import current_app, default_app
-    print(current_app.backend, current_app.conf.CELERY_RESULT_BACKEND,
-          current_app.backend_cls, current_app._get_backend(), default_app,
-          current_app.loader_cls, os.environ.get('CELERY_LOADER'),
-          os.environ.get('DJANGO_SETTINGS_MODULE'))
 
     taskset = TaskSetResult.restore(task_group_id)
     if not taskset:
