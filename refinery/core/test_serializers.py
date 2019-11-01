8de84297fdbd935650b821b73f2d8e3285163c7b
ilan-gold <ilanbassgold@gmail.com>
2019-11-01 16:36:16 -0400

    since Analysis.workflow_copy could be blank or null :( we need to check

--- refinery/core/test_serializers.py
+++ refinery/core/test_serializers.py
@@ -58,8 +58,6 @@ class AnalysisSerializerTests(TestCase):
             workflow=self.workflow,
             time_start=timezone.now()
         )
-        # needed for workflow_copy
-        self.analysis.refresh_from_db()
         self.analysis.set_owner(self.user)
         self.serializer = AnalysisSerializer(self.analysis)
 
