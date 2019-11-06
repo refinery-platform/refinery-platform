ffaf6a1932c5bef5fd3b1177f394dda24a14ad5e
ilan-gold <ilanbassgold@gmail.com>
2019-11-06 13:11:43 -0500

    update spacing vs tabs

--- refinery/ui/source/js/commons/services/workflows.spec.js
+++ refinery/ui/source/js/commons/services/workflows.spec.js
@@ -1,58 +1,58 @@
 (function () {
-    'use strict';
-    describe('Common.service.workflows: unit tests', function () {
-        var $httpBackend;
-        var $rootScope;
-        var fakeUuid;
-        var service;
-        var settings;
-        var mocker;
-        var fakeResponse = {
-            workflow: {
-                name: 'foo'
-            }
-        };
+  'use strict';
+  describe('Common.service.workflows: unit tests', function () {
+    var $httpBackend;
+    var $rootScope;
+    var fakeUuid;
+    var service;
+    var settings;
+    var mocker;
+    var fakeResponse = {
+      workflow: {
+        name: 'foo'
+      }
+    };
 
-        beforeEach(function () {
-            module('refineryApp');
+    beforeEach(function () {
+      module('refineryApp');
 
-            inject(function ($injector) {
-                settings = $injector.get('settings');
-                $httpBackend = $injector.get('$httpBackend');
-                $rootScope = $injector.get('$rootScope');
-                service = $injector.get('workflowService');
-                mocker = $injector.get('mockParamsFactory');
-                fakeUuid = mocker.generateUuid()
-            });
-        });
-
-        describe('Service', function () {
-            it('should be defined', function () {
-                expect(service).toBeDefined();
-            });
-
-            it('should be a method', function () {
-                expect(typeof service).toEqual('function');
-            });
-
-            it('query for workflow should return a resolving promise', function () {
-                $httpBackend
-                    .expectGET(
-                        settings.appRoot +
-                        settings.refineryApiV2 +
-                        '/workflows/' + fakeUuid + '/'
-                    ).respond(200, fakeResponse);
-
-                var results;
-                var promise = service.query({uuid: fakeUuid}).$promise
-                    .then(function (response) {
-                        results = response;
-                    });
-                expect(typeof promise.then).toEqual('function');
-                $httpBackend.flush();
-                $rootScope.$digest();
-                expect(results.workflow.name).toEqual(fakeResponse.workflow.name);
-            });
-        });
+      inject(function ($injector) {
+        settings = $injector.get('settings');
+        $httpBackend = $injector.get('$httpBackend');
+        $rootScope = $injector.get('$rootScope');
+        service = $injector.get('workflowService');
+        mocker = $injector.get('mockParamsFactory');
+        fakeUuid = mocker.generateUuid()
+      });
     });
-})();
+
+    describe('Service', function () {
+      it('should be defined', function () {
+        expect(service).toBeDefined();
+      });
+
+      it('should be a method', function () {
+        expect(typeof service).toEqual('function');
+      });
+
+      it('query for workflow should return a resolving promise', function () {
+        $httpBackend
+          .expectGET(
+            settings.appRoot +
+            settings.refineryApiV2 +
+            '/workflows/' + fakeUuid + '/'
+        ).respond(200, fakeResponse);
+
+        var results;
+        var promise = service.query({ uuid: fakeUuid }).$promise
+          .then(function (response) {
+            results = response;
+          });
+        expect(typeof promise.then).toEqual('function');
+        $httpBackend.flush();
+        $rootScope.$digest();
+        expect(results.workflow.name).toEqual(fakeResponse.workflow.name);
+      });
+    });
+  });
+}())
