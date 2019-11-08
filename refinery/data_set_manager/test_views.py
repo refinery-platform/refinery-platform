d276253a4a023541790c6d346473207916107aaf
ilan-gold <ilanbassgold@gmail.com>
2019-11-08 13:15:42 -0500

    update formatting

--- refinery/data_set_manager/test_views.py
+++ refinery/data_set_manager/test_views.py
@@ -1314,7 +1314,8 @@ class ProcessISATabViewTests(MetadataImportTestBase):
             self.post_isa_tab(isa_tab_file=good_isa)
         self.successful_import_assertions()
 
-    @override_settings(CELERY_TASK_ALWAYS_EAGER=True, REFINERY_S3_USER_DATA=False)
+    @override_settings(CELERY_TASK_ALWAYS_EAGER=True,
+                       REFINERY_S3_USER_DATA=False)
     def test_post_good_isa_tab_file_with_datafiles(self):
         for name in ["rfc94.txt", "rfc134.txt"]:
             open(os.path.join(self.test_user_directory, name), "a").close()
@@ -1362,7 +1363,8 @@ class ProcessISATabViewTests(MetadataImportTestBase):
             AnnotatedNode.objects.filter(attribute_value="EDITED")
         )
 
-    @override_settings(CELERY_TASK_ALWAYS_EAGER=True, REFINERY_S3_USER_DATA=False)
+    @override_settings(CELERY_TASK_ALWAYS_EAGER=True,
+                       REFINERY_S3_USER_DATA=False)
     def test_metadata_revision_works_existing_datafiles_persisted(self):
         local_data_file_names = ["rfc94.txt", "rfc134.txt"]
         for name in local_data_file_names:
@@ -1402,7 +1404,8 @@ class ProcessISATabViewTests(MetadataImportTestBase):
             )
         )
 
-    @override_settings(CELERY_TASK_ALWAYS_EAGER=True, REFINERY_S3_USER_DATA=False)
+    @override_settings(CELERY_TASK_ALWAYS_EAGER=True,
+                       REFINERY_S3_USER_DATA=False)
     def test_metadata_revision_works_datafiles_added_during_revision(self):
         local_data_file_names = ["rfc94.txt", "rfc134.txt"]
         for name in local_data_file_names:
