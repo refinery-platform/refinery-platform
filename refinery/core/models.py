d63099003d29cb438e5756bb08cf449abcc43941
ilan-gold <ilanbassgold@gmail.com>
2019-12-02 16:50:51 -0500

    need list to cast from dict_values object

--- refinery/core/models.py
+++ refinery/core/models.py
@@ -2121,9 +2121,9 @@ class SiteStatistics(models.Model):
         def get_aggregate_sum(field_name):
             if not aggregates:
                 return getattr(self, field_name)
-            return SiteStatistics.objects.filter(
+            return list(SiteStatistics.objects.filter(
                 run_date__lte=self.run_date
-            ).aggregate(Sum(field_name)).values()[0]
+            ).aggregate(Sum(field_name)).values())[0]
 
         return [
             self.pk, get_aggregate_sum("datasets_shared"),
